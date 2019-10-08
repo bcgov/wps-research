0aaf1239d77b40b2764941b2227963cae6bf03e5
Ashlin Richardson <ashlinrichardson@users.noreply.github.com>
2019-10-07 22:26:23 -0700

    Add files via upload

--- class_wheel.cpp
+++ class_wheel.cpp
@@ -1,21 +1,20 @@
+/* visualize a class map by going around the colour wheel */
 #include"misc.h"
 int main(int argc, char ** argv){
 
   if(argc < 2) err("class_wheel [input binary file name]");
 
-  size_t nrow, ncol, nband, np, i, j;
-  str fn(argv[1]);
+  str fn(argv[1]); // input file name
   str hfn(hdr_fn(fn)); // auto-detect header file name
+  size_t nrow, ncol, nband, np, i, j;
   hread(hfn, nrow, ncol, nband); // read header
 
-  if(nband != 1){
-    err("this program only defined for 1-band images");
-  }
-
-  float * dat = bread(fn, nrow, ncol, nband); // read data into float array
   np = nrow * ncol;
-
   float r, g, b, h, s, v;
+  if(nband != 1) err("this program defines results for 1-band images");
+
+  // read data into float array
+  float * dat = bread(fn, nrow, ncol, nband);
 
   // accumulate the data
   map<float, size_t> count;
@@ -34,43 +33,38 @@ int main(int argc, char ** argv){
   for(it = count.begin(); it != count.end(); it++){
     float d = it->first;
     if(d == 0.){
-      code_r[d] = 0.;
-      code_g[d] = 0.;
-      code_b[d] = 0.;
+      code_r[d] = code_g[d] = code_b[d] = 0.;
     }
     else{
       s = v = 1.;
       h = 360. * (float)ci / (float)(count.size() - 1);
-      ci += 1; // next color index
       hsv_to_rgb(&r, &g, &b, h, s, v);
+
       code_r[d] = r;
       code_g[d] = g;
-      code_b[d] = b; // cout << "r, g, b= " << r << "," << g << "," << b << endl;
+      code_b[d] = b;
+      ci ++; // next colour index
     }
   }
 
   str ofn(fn + str("_wheel.bin"));
   str ohfn(fn + str("_wheel.hdr"));
-  hwrite(ohfn, nrow, ncol, 3); // 3 bands: it's an RGB file
+  hwrite(ohfn, nrow, ncol, 3); // rgb file: 3 bands
 
+  // write colour encoded output
   FILE * outf = fopen(ofn.c_str(), "wb");
-
   for0(i, np){
     r = code_r[dat[i]];
     fwrite(&r, sizeof(float), 1, outf);
   }
-
   for0(i, np){
     g = code_g[dat[i]];
     fwrite(&g, sizeof(float), 1, outf);
   }
-
   for0(i, np){
     b = code_b[dat[i]];
     fwrite(&b, sizeof(float), 1, outf);
   }
-
   fclose(outf);
-
   return 0;
 }
