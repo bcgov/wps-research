52792e09a838a5f37de9d5bae55b37da15a4c185
ashlinrichardson <richardson.ashlin@gmail.com>
2019-10-26 21:11:05 -0700

    cleanup

--- cleanup.sh
+++
@@ -1,5 +0,0 @@
-rm -v label/*.lab
-rm -v out/*.out
-rm -v nclass.*
-rm dmat.i
-rm dmat.d
--- plot.R
+++
@@ -1,6 +0,0 @@
-# plot.R
-d<-read.csv("tmp.csv")
-plot(d$knn_use, log(d$n_seg), type="l")
-
-plot(log(d$n_seg), d$knn_use, type="l")
-
