2e18d13e893dacbca03cdc3b6df6c8a457003127
ashlinrichardson <richardson.ashlin@gmail.com>
2019-11-07 13:28:41 -0800

    confusion matrix for binary class map

--- cpp/binary_confusion.cpp
+++ cpp/binary_confusion.cpp
@@ -1,8 +1,8 @@
 /* calculate confusion matrix for a binary class map with respect to truth
 
-Assume that no relabeling is required 
+Assume that no relabeling is required
 
-map tp, tn, fn, fp! 
+map tp, tn, fn, fp!
 
 */
 #include"misc.h"
@@ -11,11 +11,8 @@ int main(int argc, char ** argv){
   if(argc < 3) err("binary_confusion [input binary class file name] [input ground reference file name]");
 
   // binary confusion matrix
-  float cm [4] ={
-	  0.,
-	  0.,
-	  0.,
-	  0.};
+  float d, c_tn, c_tp, c_fn, c_fp;
+  c_tn = c_tp = c_fn = c_fp = 0.;
 
   str fn(argv[1]); // input file name
   str hfn(hdr_fn(fn)); // auto-detect header file name
@@ -24,7 +21,7 @@ int main(int argc, char ** argv){
 
   size_t nrow2, ncol2, nband2;
   str cfn(argv[2]);
-  str chfn(argv[2]);
+  str chfn(hdr_fn(cfn));
   hread(chfn, nrow2, ncol2, nband2);
 
   // check the inputs are the same shape
@@ -39,31 +36,53 @@ int main(int argc, char ** argv){
   float * dat = bread(fn, nrow, ncol, nband);
   float * cdat= bread(cfn, nrow, ncol, nband);
 
-  // accumulate the data
-  map<float, size_t> count;
+  str tnfn(fn + str("_tn.bin"));
+  str tnhfn(fn+ str("_tn.hdr"));
+
+  str tpfn(fn + str("_tp.bin"));
+  str tphfn(fn+ str("_tp.hdr"));
+
+  str fnfn(fn + str("_fn.bin"));
+  str fnhfn(fn+ str("_fn.hdr"));
+
+  str fpfn(fn + str("_fp.bin"));
+  str fphfn(fn+ str("_fp.hdr"));
+
+  // true negatives
+  hwrite(tnhfn, nrow, ncol, 1);
+  FILE * f = fopen(tnfn.c_str(), "wb");
   for0(i, np){
-    if(count.count(dat[i]) < 1){
-      count[dat[i]] = 0;
-    }
-    count[dat[i]] += 1;
-  }
-
-  float ci = 1.;
-  map<float, float> lookup;
-  for(map<float, size_t>::iterator it = count.begin(); it != count.end(); it++){
-    lookup[it->first] = ci ++;
-  }
-/*
-  hwrite(ohfn, nrow, ncol, 1); // write header
-
-  float d;
-  FILE * f = fopen(ofn.c_str(), "wb");
-
-  for0(i, np){
-	d = lookup[dat[i]];
-	fwrite(&d, sizeof(float), 1, f);
+    d = ((dat[i] == cdat[i] && dat[i] == 0.)? 1. : 0);
+    fwrite(&d, sizeof(float), 1, f);
   }
   fclose(f);
-  */
+
+  // true positives
+  hwrite(tphfn, nrow, ncol, 1);
+  f = fopen(tpfn.c_str(), "wb");
+  for0(i, np){
+    d = ((dat[i] == cdat[i] && dat[i] == 1.) ? 1. : 0);
+    fwrite(&d, sizeof(float), 1, f);
+  }
+  fclose(f);
+
+  // false negatives
+  hwrite(fnhfn, nrow, ncol, 1);
+  f = fopen(fnfn.c_str(), "wb");
+  for0(i, np){
+    d = ((dat[i] != cdat[i] && dat[i] == 0.) ? 1. : 0);
+    fwrite(&d, sizeof(float), 1, f);
+  }
+  fclose(f);
+
+  //false positives
+  hwrite(fphfn, nrow, ncol, 1);
+  f = fopen(fpfn.c_str(), "wb");
+  for0(i, np){
+    d = ((dat[i] != cdat[i] && dat[i] == 1.) ? 1. : 0);
+    fwrite(&d, sizeof(float), 1, f);
+  }
+  fclose(f);
+
   return 0;
 }
