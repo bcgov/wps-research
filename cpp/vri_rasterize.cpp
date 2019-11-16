edb27d8113b32a438e735b8be0628e5bbfde9807
ashlinrichardson <richardson.ashlin@gmail.com>
2019-11-15 23:26:42 -0800

    rasterize vri

--- cpp/vri_rasterize.cpp
+++ cpp/vri_rasterize.cpp
@@ -1,9 +1,46 @@
-#include"misc.h"
+#include"misc.h" // should check for float <--> int conversion out of bounds
+
+/* return true if string is numeric / digital */
+bool is_numeric(const std::string& s){
+  std::string::const_iterator it = s.begin();
+  while (it != s.end() && std::isdigit(*it)) ++it;
+  return !s.empty() && it == s.end();
+}
+
+/* encode list of strings, as a numeric vector. LUT nonempty if non-numeric entries */
+bool encode(vector<str> & d, vector<float> & e, map<str, float> & lookup){
+  long int a;
+  bool all_numeric = true;
+  vector<str>::iterator it;
+  for(it = d.begin(); it != d.end(); it++){
+    if(!is_numeric(*it)) all_numeric = false;
+  }
+
+  // use LUT if any non-numerics
+  if(all_numeric){
+    for(it = d.begin(); it != d.end(); it++){
+      e.push_back( (float)atof(it->c_str()));
+    }
+  }
+  else{
+    float next_i = 1.;
+    map<str, float> lookup;
+    for(it = d.begin(); it != d.end(); it++){
+      if(lookup.count(*it) < 1){
+        lookup[*it] = next_i++;
+      }
+      e.push_back(lookup[*it]);
+    }
+  }
+  // don't forget to print out the LUT
+  return all_numeric;
+}
+
 int main(int argc, char** argv){
   //if(argc < 3) err("vri_polygonize [vri polygon raster file (ENVI type 4)] [vri poly attr. csv file]");
 
-  size_t nrow, ncol, nband, i;
-  str prf("vri/vri_poly.bin"); // polygon raster file
+  size_t nrow, ncol, nband, i, j;
+  str prf("vri/vri.bin"); // polygon raster file
   str prf_hf(hdr_fn(prf));
   hread(prf_hf, nrow, ncol, nband);
   float * pr = bread(prf, nrow, ncol, nband);
@@ -11,11 +48,12 @@ int main(int argc, char** argv){
 
   map<float, size_t> cf;
   size_t np = nrow * ncol;
-  
+
   for0(i, np){
     float d = pr[i];
     if(cf.count(d) < 1) cf[d] = 0;
     cf[d] += 1;
+    if((float)(size_t)d != d) err("encoding failed");
   }
 
   map<size_t, size_t> cs;
@@ -25,9 +63,9 @@ int main(int argc, char** argv){
 
   for(it = cf.begin(); it != cf.end(); it++){
     size_t first = (size_t) it->first;
-    cs[first] = it->second; 
-    if(first < min_i) min_i = first;
-    if(first > max_i) max_i = first;
+    cs[first] = it->second;
+    if(first > min_i) min_i = first;
+    if(first < max_i) max_i = first;
   }
 
   if(cf.size() != cs.size()) err("conversion from float failed");
@@ -41,14 +79,82 @@ int main(int argc, char** argv){
 
   getline(f, hdr);
   vector<str> fields(split(hdr));
+  map<str, size_t> f_i;
+  for0(i, fields.size()){
+    f_i[fields[i]] = i;
+  }
+
   cout << fields << endl;
+  size_t idx = f_i[str("OBJECTID_1")];
+  map<size_t, vector<str>> data;
+  for0(i, fields.size()){
+    data[i] = vector<str>();
+  }
+
+  size_t x;
+  size_t xc = 0;
+  size_t rows = 0;
   while(getline(f, s)){
-	 vector<str> w(split_special(s));
-         cout << fields.size() << "," << w.size() << endl;
-	 if(w.size() != fields.size()){
-	   cout << w << endl;
-	   err("");
-	 }
+    rows ++;
+    vector<str> w(split_special(s));
+    if(w.size() != fields.size()){
+      cout << w << endl;
+      err("");
+    }
+    for0(i, fields.size()){
+      data[i].push_back(w[i]);
+    }
+  }
+
+  // map the segment id back to the dbf row number
+  map<size_t, size_t> idx_i;
+  for0(i, rows){
+    idx_i[(size_t)atol(data[idx][i].c_str())] = i;
+  }
+
+  for0(i, fields.size()){
+    vector<float> e;
+    map<str, float> lut;
+
+    bool numeric = encode(data[i], e, lut);
+    cout << fields[i] << ",\t" << (numeric?"Y":"N") << ",\t" << lut.size() << endl;
+
+    str pre(prf + str("_") + fields[i]);
+    str ofn(pre + str(".bin"));
+    str ohfn(pre + str(".hdr"));
+
+    // printf("+w %s\n", ofn.c_str());
+    FILE * outf = fopen(ofn.c_str(), "wb");
+    if(!outf){
+      err("failed to open output file");
+    }
+    if(numeric){
+      for0(j, nrow * ncol){
+        size_t id = idx_i[pr[j]]; //dbf row id for pixel
+        float f = (float)atof(data[i][id].c_str());
+        fwrite(&f, 1, sizeof(float), outf);
+      }
+    }
+    else{
+      for0(j, nrow * ncol){
+        size_t id = idx_i[pr[j]];
+        float f = lut[data[i][id]];
+        if(lut.count(data[i][id]) < 1){
+          err("encoding failed");
+        }
+        fwrite(&f, 1, sizeof(float), outf);
+      }
+      str lfn(pre + str(".lut"));
+      FILE * outlf = fopen(lfn.c_str(), "wb");
+      map<str, float>::iterator lf;
+      for(lf = lut.begin(); lf != lut.end(); lf++){
+        fprintf(outlf, "%s,%f\n", lf->first.c_str(), lf->second);
+      }
+      fclose(outlf);
+      // dont forget to write lut
+    }
+    fclose(outf);
+    hwrite(ohfn, nrow, ncol, nband);
   }
 
   f.close();
