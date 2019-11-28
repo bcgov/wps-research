13dbe8f1dc96c6b4b48dae6cb8f7cbdbbb3901c6
ashlinrichardson <richardson.ashlin@gmail.com>
2019-11-27 18:03:29 -0800

    improve comments

--- cpp/hclust.cpp
+++ cpp/hclust.cpp
@@ -5,67 +5,61 @@ dent cpp/hclust.cpp; vim cpp/hclust.cpp; rm bin/hclust; python setup.py; ./bin/h
 
 #include"misc.h"
 
-size_t d_count;
 float * dat; // multispectral data
 float * lab; // initial labels
 
-// parallelism stuff
 pthread_attr_t attr; // specify threads joinable
 pthread_mutex_t next_j_mtx; // lock for thread synchronization
 
-size_t dst, src;
+size_t d_count; // count of distance calcs
+size_t dst, src; // index of seg to merge into, and from
 size_t min_idx_i, min_idx_j; // idx of segs to merge
 
-vector<vector<size_t>> pix;
+vector<vector<size_t>> pix; // list of pixels, as a function of label
 vector<priority_queue<f_i>> dmat; // distance matrix: self-sorting rows
 size_t next_j, nr, nc, nb, np, max_label; // next job, row count, col count, band count, pixel count, max_label
 
 inline void distance(size_t & j, size_t & i, size_t & np_j, size_t & np_i, size_t & k, size_t & m, size_t & n, size_t & pk, float & dij, float &d){
   d = 0.;
 
-  // iterate points in segment j
-
   for0(m, np_j){
     size_t pj_m = pix[j][m]; // index of m-th point in seg j
 
-    //iterate points in segment i
     for0(n, np_i){
       size_t pi_n = pix[i][n]; // index of n-th point in seg i
 
-      // distance between points pi_n and pj_m
-      dij = 0.;
+      dij = 0.; // distance between pixels at index pi_n, pj_m
 
-      // iterate multispec bands
       for0(k, nb){
-        pk = np * k; // band offset
+        pk = np * k; // multispec band offset
         float dx = dat[pk + pi_n] - dat[pk + pj_m];
         dij += dx * dx;
       }
-      d += sqrt(dij);
+      d += sqrt(dij); // add the distance for each pair of points
     }
   }
-  // cprint(to_string(np_j) + str(",") + to_string(np_i) + str(",") + to_string(((float)np_j * (float)np_i)));
 
+  // convert total to average
   size_t npji = np_j * np_i;
-
-  if(false){
-    if(npji > 0.) d /= (float)npji;
-  }
+  if(npji > 0.) d /= (float)npji;
 }
 
-// the activity for each thread
 void * dmat_j(void * arg){
+  // thread activity: distance matrix row
+
   float d, dx, dij;
   size_t i, j, k, m, n, pk;
   long w = (long)arg; // work index
   cprint(str("init work ") + std::to_string(w));
 
+  // keep picking up jobs
   while(1){
-    // pick up a job
+
     mtx_lock(&next_j_mtx);
     j = next_j ++;
-    d_count += 1;
+    d_count ++;
     mtx_unlock(&next_j_mtx);
+
     size_t np_j = pix[j].size();
 
     // don't process if no pixels
@@ -77,7 +71,7 @@ void * dmat_j(void * arg){
     }
 
     if(j % 100 == 0){
-      float pct = 100. * (float)j / (float) max_label;
+      float pct = 100. * (float)j / (float)max_label;
       cprint(str(" work ") + to_string(w) + str(" job ") + to_string(j) + str(" % ") + to_string(pct));
     }
 
@@ -86,27 +80,21 @@ void * dmat_j(void * arg){
 
     // iterate over segments other than this one (j)
     for0(i, j){
-      // max_label + 1)
+
       size_t np_i = pix[i].size();
       if(i != j && np_i > 0){
 
-        // dist between segment j and i
-        distance(j, i, np_j, np_i, k, m, n, pk, dij, d);
+        distance(j, i, np_j, np_i, k, m, n, pk, dij, d); // distance between seg i and seg j
 
-        if(d > 10000.){
-          cprint(str("\td: ") + to_string(d) + str(" i,j=") + to_string(i) + str(",") + to_string(j));
-        }
-
-        // add distance and index of segment compared to
-        pq->push(f_i(d, i));
+        pq->push(f_i(d, i)); // add distance and index of compared seg
       }
     }
   }
 }
 void deploy_works(void *(*work)(void *)){
   next_j = d_count = 0;
-  int numCPU = sysconf(_SC_NPROCESSORS_ONLN);
-  // cout << "Number of cores: " << numCPU << endl;
+
+  int numCPU = sysconf(_SC_NPROCESSORS_ONLN); // cout << "Number of cores: " << numCPU << endl;
 
   // mutex setup
   pthread_mutex_init(&print_mtx, NULL);
@@ -128,29 +116,33 @@ void deploy_works(void *(*work)(void *)){
   for0(j, numCPU) pthread_join(my_pthread[j], NULL);
 }
 
-// the activity for each thread
 void * dmat_j_update(void * arg){
-  /* clean dmat row (j):
+
+  /* thread activity: update dmat row (j):
+
   for each row(j):
   A) if it's src, clear it
   B) if it's dst, clear it and recalc dist with respect to "everything" (under j); other elmt's reside in other rows
   C) if it's neither src nor dst, delete: all references to src or dst: and, recalculate any deleted elements */
+
   bool debug = false;
   float d, dx, dij;
   size_t i, j, k, m, n, pk, np_j, np_i;
-  long w = (long)arg; // work index
+  long w = (long)arg; // worker index
   if(debug) cprint(str("init work ") + std::to_string(w));
 
+  // keep picking up jobs
   while(1){
-    // pick up a job
+
     mtx_lock(&next_j_mtx);
     j = next_j ++;
     d_count += 1;
     mtx_unlock(&next_j_mtx);
+
     np_j = pix[j].size();
 
-    // A) clear dmat[j] if seg-j empty, or if should be empty (j== src)
     if(np_j == 0 && j == src){
+      // A) clear dmat[j] if seg-j empty, or if should be empty (j== src)
       while(dmat[j].size() > 0) dmat[j].pop();
       continue;
     }
@@ -162,28 +154,23 @@ void * dmat_j_update(void * arg){
 
     if(debug && (j % 100 == 0)){
       float pct = 100. * (float)j / (float) max_label;
-      cprint(str(" work ") + to_string(w) + str(" job ") + to_string(j) + str(" % ") + to_string(pct));
+      cprint(str(" worker ") + to_string(w) + str(" job ") + to_string(j) + str(" % ") + to_string(pct));
     }
 
     // pointer to dmat row for this element
     priority_queue<f_i> * pq = &dmat[j];
 
-    // B) if it's dst, clear dmat[j] and recalc with respect to "everything" below j
     if(j == dst){
-      while(dmat[j].size() > 0) dmat[j].pop(); // clear row
+      // B) if it's dst, clear dmat[j] and recalc with respect to "everything" below j
+      while(dmat[j].size() > 0) dmat[j].pop();
 
-      // iterate over segments below j
       for0(i, j){
-        // max_label + 1)
+        // iterate over segs below j
         np_i = pix[i].size();
-        if(i != j && np_i > 0){
 
+        if(i != j && np_i > 0){
           // dist between segment j and i
           distance(j, i, np_j, np_i, k, m, n, pk, dij, d);
-
-          if(debug && d > 10000.){
-            cprint(str("\td: ") + to_string(d) + str(" i,j=") + to_string(i) + str(",") + to_string(j));
-          }
 
           // add distance and index of segment compared to
           pq->push(f_i(d, i));
@@ -199,14 +186,15 @@ void * dmat_j_update(void * arg){
         tmp.push(pq->top());
         pq->pop();
       }
+
       if(pq->size() != 0) err("err"); // sanity check
 
-      // process the items
       while(tmp.size() > 0){
+        // process items
         if(tmp.top().i == src){
-          // drop
+          // drop item
           if(debug) cout << j << "\tdrop " << tmp.top().i << " d=" << tmp.top().d << endl;
-          // this distance represented w.r.t. dst now..
+          // that distance represented w.r.t. dst now
         }
         else if(tmp.top().i == dst){
           // recalc
@@ -222,13 +210,12 @@ void * dmat_j_update(void * arg){
           pq->push(f_i(d, i));
         }
         else{
-          // keep
+          // keep existing element
           pq->push(tmp.top());
         }
         tmp.pop();
       }
     }
-
   }
 }
 
@@ -238,11 +225,12 @@ int main(int argc, char** argv){
 
   size_t i, j, k, min_label;
   size_t nr2, nc2, nb2, nclust;
-  dat = load_envi(argv[1], nr, nc, nb);
+  dat = load_envi(argv[1], nr, nc, nb); // load raw-binary data
+
   np = nr * nc; // pixel count
 
   if(argc < 4){
-    //reduced args
+    //reduced args: no seed labels
     nr2 = nr;
     nc2 = nc;
     nb2 = nb;
@@ -258,7 +246,7 @@ int main(int argc, char** argv){
     nclust = (size_t) atoi(argv[3]);
   }
 
-  cout << "building set list.." << endl;
+  // examine seeds
   for0(i, np){
     float f = lab[i];
     j = (size_t)f;
@@ -273,32 +261,36 @@ int main(int argc, char** argv){
   }
   cout << "min_label: " << min_label << " max_label: " << max_label << endl;
 
-  // pix[i]: list of indices of pixels, corresponding to label i
+  // pix[i]: list of indices of pixels for label i
   vector<vector<size_t>>::iterator pi;
   for0(i, max_label + 1){
     pix.push_back(vector<size_t>());
     dmat.push_back(priority_queue<f_i>());
   }
 
-  for0(i, np){
-    j = (size_t)lab[i];
-    pix[j].push_back(i);
-  }
+  // build pixel list
+  for0(i, np) pix[(size_t)lab[i]].push_back(i);
 
-  // list of labels presently under consideration
+  // list of labels presently considered
   i = 0;
   list<size_t> labels;
   for(pi = pix.begin(); pi != pix.end(); pi++){
-    if(pi->size() > 0) labels.push_back(i);
+    if(pi->size() > 0){
+      labels.push_back(i);
+    }
     i++;
   }
 
   labels.sort();
-  cout << "labels:" << labels << endl;
 
+  // calculate dmat
   deploy_works(dmat_j);
+
+  // expected number of distance matrix elements
   size_t expected = ((max_label * max_label - max_label) / 2);
   cout << "max_label " << max_label << " max_label^2 " << max_label * max_label << " max_label ^2 - max_label / 2 " << expected << endl;
+
+  // count number of distance matrix elements
   d_count = 0;
   vector<priority_queue<f_i>>::iterator dmi;
   float d_min = FLT_MAX;
@@ -316,7 +308,8 @@ int main(int argc, char** argv){
   cout << "dcount " << d_count << "d_min " << d_min << " d_max " << d_max << endl;
   if(d_count != expected) err("unexpected distance count");
 
-  size_t iter = 0;
+  // start merging
+  size_t iter = 1;
   cout << "labels.size() n_classes " << labels.size() << endl;
   while(labels.size() > nclust){
 
@@ -328,6 +321,8 @@ int main(int argc, char** argv){
     for(it = labels.begin(); it != labels.end(); it++){
       j = *it;
       priority_queue<f_i> * pq = &dmat[j];
+
+      // ignore labels without pixels
       if(pq->size() == 0) continue;
       if(i == 0){
         min_idx_j = j;
@@ -338,43 +333,41 @@ int main(int argc, char** argv){
           min_dist = pq->top().d;
           min_idx_j = j;
         }
-
       }
-      i ++;
+      i++;
     }
 
     // indices of segs to merge
     min_idx_i = dmat[min_idx_j].top().i;
     cout << "iter= " << iter++ << " merge i,j= " << min_idx_i << "," << min_idx_j << " d=" << min_dist << " n_seg " << labels.size() << endl;
+
     // merge the points of the two clusters, into (by convention) the cluster with the smaller seg-index
     dst = (min_idx_i < min_idx_j)? min_idx_i: min_idx_j;
     src = (min_idx_i < min_idx_j)? min_idx_j: min_idx_i;
-    // cout << "src" << pix[src] << endl << "dst" << pix[dst] << endl;
 
     // add src list elements onto dst list
     vector<size_t>::iterator pi;
-    for(pi = pix[src].begin(); pi != pix[src].end(); pi++){
-      pix[dst].push_back(*pi);
-    }
-    pix[src].clear(); // src list now defunct, clear it
-    labels.remove(src); // remove defunct label from list (do we even use this?) probably not
-    // cout << "src" << pix[src] << endl << "dst" << pix[dst] << endl;
+    for(pi = pix[src].begin(); pi != pix[src].end(); pi++) pix[dst].push_back(*pi);
 
-    // update distane matrix
+    // clear now-defunct list and remove label
+    pix[src].clear();
+    labels.remove(src);
+
+    // revise distance matrix
     deploy_works(dmat_j_update);
 
-    // output labels
-    for0(i, np){
-      lab[i] = 0.;
-    }
+    // update labels
+    for0(i, np) lab[i] = 0.;
     i = 0;
-    for(vector<vector<size_t>>::iterator pj = pix.begin(); pj != pix.end(); pj++){
+    vector<vector<size_t>>::iterator pj;
+    for(pj = pix.begin(); pj != pix.end(); pj++){
       for(vector<size_t>::iterator pi = (*pj).begin(); pi != (*pj).end(); pi++){
         lab[*pi] = i;
       }
-      i ++;
+      i++;
     }
 
+    // output label maps. left zero-pad output filenames
     int n_zero = 6;
     str iter_s(to_string(iter));
     std::string iter_s2 = std::string(n_zero - iter_s.length(), '0') + iter_s;
@@ -385,19 +378,19 @@ int main(int argc, char** argv){
     fwrite(lab, sizeof(float), np, of);
     fclose(of);
 
+    // color-code every n-th map
     if(iter % 50 == 0){
-
       str cmd(str("bin/class_recode ") + ofn);
       cout << cmd << endl;
       system(cmd.c_str());
 
-      str cm2(str("bin/class_wheel ") + ofn + str("_recode.bin"));
-      cout << cm2 << endl;
-      system(cm2.c_str());
+      cmd = (str("bin/class_wheel ") + ofn + str("_recode.bin"));
+      cout << cmd << endl;
+      system(cmd.c_str());
 
-      str cm3(str("python py/read_multi.py ") + ofn + str("_recode.bin_wheel.bin") + str(" 1"));
-      cout << cm3 << endl;
-      system(cm3.c_str());
+      cmd = (str("python py/read_multi.py ") + ofn + str("_recode.bin_wheel.bin") + str(" 1"));
+      cout << cmd << endl;
+      system(cmd.c_str());
     }
   }
   return 0;
