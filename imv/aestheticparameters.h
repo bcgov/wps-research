d8e625379208272046b3c20b0bd112acefd3a657
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-06-01 09:47:31 -0700

    update

---
+++ imv/aestheticparameters.h
@@ -0,0 +1,13 @@
+/* m3ta3: reimagination of a (late 2011, early 2012) personal, primordial visualization library that inspired further developments at UVic, CFS and elsewhere.. */
+
+#pragma once
+#ifndef AESTHETICPARAMETERS_H
+#define AESTHETCPARAMETERS_H
+
+#define DEBUGMODE 0 
+static float ARROWHEAD_WIDTH = 0.175;
+static float ARROWHEAD_LENGTH = 0.25;
+static int DEFAULT3DSIZEX = 450;
+static int DEFAULT3DSIZEY = 450;
+
+#endif 
