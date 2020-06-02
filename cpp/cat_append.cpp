7813716c1b48e2e3e14050b97f2d7e7e69f6c8cb
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-06-02 11:58:47 -0700

    update

---
+++ cpp/cat_append.cpp
@@ -0,0 +1,45 @@
+// cat in-place: append files onto another file:
+// use case: if wanted to use cat to combine files, but insuff. space!
+
+#include"misc.h"
+
+int main(int argc, char ** argv){
+  if(argc < 3){
+    err("cat_append [file to cat onto] [other file] .. [possible additional file] ..");
+  }
+
+  int i;
+  for0(i, argc){
+    if(i > 0){
+      if(!exists(argv[i])){
+        err(str("file does not exist: ") + str(argv[i]));
+      }
+      else{
+        cout << argv[i] << endl;
+      }
+    }
+  }
+
+  string name;
+  cout << "concatenate other files onto: " << argv[1] << endl;
+  cout << "press RETURN or ctrl-c to exit";
+  getline(std::cin, name);
+
+  FILE * f = fopen(argv[1], "ab"); // open file, append mode!
+  for0(i, argc){
+    if(i > 1){
+      FILE * g = fopen(argv[i], "rb");
+      char s;
+      while(1){
+        s = fgetc(g);
+        if(s == EOF) break;
+        fwrite(&s, sizeof(char), 1, f);
+      }
+      fclose(g);
+    }
+  }
+  fclose(f);
+
+  return 0;
+
+}
