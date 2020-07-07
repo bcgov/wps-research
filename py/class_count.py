b080c9ac26667e3fa1eeff36a6aa8ea58435651c
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-07-07 12:08:44 -0700

    update

--- py/class_count.py
+++ py/class_count.py
@@ -5,8 +5,7 @@ if len(args) < 2:
     err("class_count [input binary file]")
     #args.append('20190926kamloops_data/WATERSP.tif_project_4x.bin_sub.bin')
 
-fn = args[1]
-samples, lines, bands, data = read_binary(fn)
+samples, lines, bands, data = read_binary(args[1])
 
 count = {}
 for d in data:
@@ -14,16 +13,15 @@ for d in data:
         count[d] = 0
     count[d] += 1
 
-mean = 0
-count_n = 0
-class_labels = count.keys()
+mean, count_n = 0, 0
+class_labels = list(count.keys())  # update for python3
 min_lab = max_lab = class_labels[0]
 max_c = min_c = count[min_lab]
 max_c_lab = min_c_lab = min_lab
 
-print "\tlabel, count"
+print("\tlabel, count")
 for c in class_labels:
-    print '\t' + str(c) + ',' + str(count[c])
+    print('\t' + str(c) + ',' + str(count[c]))
     if c < min_lab:
         min_lab = c
     if c > max_lab:
@@ -31,16 +29,15 @@ for c in class_labels:
 
     count_n += count[c]
     mean += count[c] * c
-    
 
     if count[c] < min_c:
         min_c, min_c_lab = count[c], c
     if count[c] > max_c:
         max_c, max_c_lab = count[c], c
 
-print "number of class labels,", len(class_labels)
-print "min class label: ", min_lab
-print "max class label: ", max_lab
-print "avg class label: ", mean / count_n 
-print "most freq.label: ", max_c_lab, " x", max_c
-print "leastfreq.label: ", min_c_lab, " x", min_c
+print("number of class labels,", len(class_labels))
+print("min class label: ", min_lab)
+print("max class label: ", max_lab)
+print("avg class label: ", mean / count_n)
+print("most  freq.label: ", max_c_lab, " x", max_c)
+print("least freq.label: ", min_c_lab, " x", min_c)
