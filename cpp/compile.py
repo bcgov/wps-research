4bcbeb3a1a83b782c0ffa0c999ed22d7bbe1b8e6
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-07-17 17:16:34 -0700

    update

--- cpp/compile.py
+++ cpp/compile.py
@@ -3,16 +3,20 @@
 # remove -w flag from g++ to examine warnings
 import os
 import sys
+import multiprocessing as mp
+i, n_cpu = 0, mp.cpu_count()
 
-files = os.popen("ls -1 *.cpp").readlines()
+files = os.popen('ls -1 *.cpp').readlines()
 files = [f.strip() for f in files]
 
-of = open("compile.sh", "wb")
+of = open('compile.sh", "wb')
     #!/usr/bin/env bash
-of.write("#!/usr/bin/env bash".encode())
+of.write('#!/usr/bin/env bash'.encode())
 for f in files:
     fn = f[:-4]
     if fn != "misc":
         s = '\ntest ! -f ' + fn + '.exe && g++ -w -O3 ' + fn + '.cpp  misc.cpp -o ' + fn + '.exe -lpthread'
+        s += '' if ((i + 1) % n_cpu == 0) else ' &'
         of.write(s.encode())
+        i += 1
 of.close()
