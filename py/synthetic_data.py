f14cf154d4f43f7f0392aa64800e98485ecfe4fe
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-08-18 15:50:12 -0700

    update

--- py/synthetic_data.py
+++ py/synthetic_data.py
@@ -7,8 +7,8 @@ if exists('stack.bin'): # don't overwrite a stack
     err('file: stack.bin already exists')
 
 nwin = 100 # number of pixels per side of "small" square
-nsq = 5 # number of squares/side of big square
-nb = nsq # 4 # number of bands, fake multispec data
+nsq = 3 # number of squares/side of big square
+nb = 3 # 4 # number of bands, fake multispec data
 
 L = nwin * nsq # image length
 print("image dimensions: " + str(L) + ',' + str(L) + ',' + str(nb))
@@ -20,14 +20,16 @@ n = (npx * (nb + n_class))
 print("n_float", n)
 d = np.zeros(n, dtype=np.float32)
 
-sigma = 1. / n_class
+sigma = .5 / n_class
 ci = 0
 
 # simulate the multi/hyperspectral data bands
 for k in range(0, nb):
     for i in range(0, L):
         for j in range(0, L):
-            bi = math.floor(i / nwin) if (k % 2 == 0) else math.floor(j/nwin)
+            bi = math.floor(i / nwin)
+            if((k % nsq) == 1): bi = math.floor(j/nwin)
+            if((k % nsq) == 2): bi = math.floor((L - i - 1) / nwin)
             d[ci] = np.random.normal((bi -.5 / n_class), sigma)
             ci += 1
 
@@ -49,7 +51,7 @@ for i in range(1, nb):
     f.write((',\nband ' + str(i + 1)).encode())
 
 for i in range(0, n_class):
-    f.write((',\ngt' + chr(97 + i)).encode())
+    f.write((',\n' + chr(97 + i)).encode())
 f.write("}".encode())
 
 f.close()
