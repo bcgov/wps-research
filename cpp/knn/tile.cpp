a9a691035c6bfa91d96f3d7e85ff4cc021f9370f
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-20 20:46:27 -0700

    update

--- cpp/knn/tile.cpp
+++ cpp/knn/tile.cpp
@@ -1,6 +1,12 @@
 #include"../misc.h" // tiler for ML app
 size_t nrow, ncol, nband, np;
 
+void incr(map<float, unsigned int> &m, float key){
+  if(m.count(key) < 1) m[key] = 0;
+  m[key] += 1;
+}
+
+
 int main(int argc, char ** argv){
   if(argc < 4) err("tile [input envi-type4 floating point stack bsq with gr] [# of groundref classes at end] [patch size]\n");
 
@@ -29,9 +35,9 @@ int main(int argc, char ** argv){
   FILE * f_patch_j = wopen("patch_j.dat");
   FILE * f_patch_label = wopen("patch_label.dat");
 
-  map<float, unsigned int> count; // count labels on a patch
   size_t truthed = 0;
   size_t nontruthed = 0;
+  map<float, unsigned int> count; // count labels on a patch
 
   for(i = 0; i < nrow - ps; i += ps){
     // start row for patch
@@ -42,25 +48,21 @@ int main(int argc, char ** argv){
         m = i + di;
         for0(dj, ps){
           n = j + dj;
-          for0(k, nb){
-            patch[ci ++] = dat[(k * nrow * ncol) + (m * ncol) + n];
-          }
+          for0(k, nb) patch[ci ++] = dat[(k * nrow * ncol) + (m * ncol) + n];
         }
       }
       if(ci != ps * ps * nb) err("patch element count mismatch");
-      // don't forget to calculate class for patch, by majority
       count.clear(); // mass for each label
 
-      // count labels on a patch
       for0(di, ps){
-        m = (i + di) * ncol;
-        for0(dj, ps){
+        m = (i + di) * ncol; // count labels on patch
+        
+	for0(dj, ps){
           n = (j + dj) + m;
-
 	  int no_match = true;
-          for0(k, nref){
-	    unsigned int key = k + 1;
-
+          
+	  for0(k, nref){
+            unsigned int key = k + 1;
             float d = dat[((k + nb) * np) + n];
             if(d == 1.){
 	      no_match = false;
@@ -79,7 +81,6 @@ int main(int argc, char ** argv){
       size_t max_c = 0;
       map<float, unsigned int>::iterator it; // lazy match
       for(it = count.begin(); it != count.end(); it++){
-
         if(it->first > 0 && it->second > max_c){
           max_c = it->second; // no leading class: doesn't count
           max_k = it->first;
@@ -93,22 +94,21 @@ int main(int argc, char ** argv){
         cout << count << " max_c " << max_c << endl;
         if(n_match > 1) printf("\tWarning: patch had competing classes\n");
       }
-      else{
-        nontruthed ++;
-      }
-
-      fwrite(patch, sizeof(float), floats_per_patch, f_patch);
+      else nontruthed ++;
+      fwrite(&i, sizeof(size_t), 1, f_patch_i);
+      fwrite(&j, sizeof(size_t), 1, f_patch_j);
       fwrite(&max_k, sizeof(float), 1, f_patch_label);
+      fwrite(patch, sizeof(float), floats_per_patch, f_patch);
     }
   }
   printf("\n");
-  printf("nwin:          %zu\n", (size_t)ps);
-  printf("image pixels:  %zu\n", np);
-  printf("pix per patch: %zu\n",(size_t)(ps * ps));
-  printf("approx patches:%zu\n", np / (ps * ps));
-  printf("total patches: %zu\n", truthed + nontruthed);
-  printf("truthed:       %zu\t\t[%.2f / 100]\n", truthed, 100. * (float)(truthed) / ((float)(truthed + nontruthed)));
-  printf("nontruthed:    %zu\n", nontruthed);
+  printf("  nwin:          %zu\n", (size_t)ps);
+  printf("  image pixels:  %zu\n", np);
+  printf("  pix per patch: %zu\n",(size_t)(ps * ps));
+  printf("  approx patches:%zu\n", np / (ps * ps));
+  printf("  total patches: %zu\n", truthed + nontruthed);
+  printf("  truthed:       %zu\t\t[%.2f / 100]\n", truthed, 100. * (float)(truthed) / ((float)(truthed + nontruthed)));
+  printf("  nontruthed:    %zu\n", nontruthed);
 
   // patch label
   // patch i, patch j
