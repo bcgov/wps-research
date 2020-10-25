b0a79c781956360649c0291cd6fe2830c918d1ed
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-24 20:43:38 -0700

    util..h

--- imv/imv.cpp
+++ imv/imv.cpp
@@ -6,122 +6,96 @@ that inspired further developments at UVic, CFS and elsewhere.. */ // todo: disp
 #include<iostream>
 
 int main(int argc, char ** argv){
+  int n_groundref = 0;
   init_mtx();
   groundref.clear();
-  IMG_FN = string("stack.bin");
+  IMG_FN = string("stack.bin"); // default image filename to load
 
-  if(argc < 2) printf("imv.cpp: [infile]\n");
+  if(argc < 2) printf("imv.cpp: [infile] optional: [nwin] [n bands groundref (one hot encoded, appearing at end of file..]\n");
   else IMG_FN = string(argv[1]);
+  if(argc > 3) n_groundref = atoi(argv[3]); // number of bands at end, to consider as groundref
 
-  if(!exists(IMG_FN)) err("failed to open input file");
-
-  string mfn(IMG_FN + string(".ml"));
-  cout << "multilook file name: " << mfn << endl;
+  if(!exists(IMG_FN)) err("failed to open input file"); // check if input file exists
+  string mfn(IMG_FN + string(".ml")); // look for saved multilook file // cout << "multilook file name: " << mfn << endl;
 
   // analysis window size
-  if(argc > 2) NWIN = atoi(argv[2]);
-  else NWIN = 49; // obviously this is for purposes of testing the extraction. Too big!
-  
-  if((NWIN - 1) % 2 != 0) err("analysis window size must be odd"); 
+  if(argc > 2){
+    NWIN = atoi(argv[2]);
+  }
+  else{
+    NWIN = 49; // obviously this is for purposes of testing the extraction. Too big!
+  }
+  if((NWIN - 1) % 2 != 0) err("analysis window size must be odd"); // assert analysis window size: odd
+  WIN_I = WIN_J = 0; // window location? what is this?
 
-  WIN_I = WIN_J = 0;
+  zprManager * myManager = zprManager::Instance(argc, argv); // window manager class
 
-  // window manager
-  zprManager * myManager = zprManager::Instance(argc, argv);
-
-  // get screen scale
-  size_t width = glutGet(GLUT_SCREEN_WIDTH);
+  size_t width = glutGet(GLUT_SCREEN_WIDTH); // this section: get screen scale
   size_t height = glutGet(GLUT_SCREEN_HEIGHT);
   size_t min_wh = width > height ? height: width;
-  printf("min_wh %f\n", (float)min_wh);
-  size_t min = 3 * min_wh / 5; // can adjust scale here. has been 3/6
-  // 2 / 5 // was 3 * min_wh / 5? 
- 
-  printf("min %f\n", (float)min);
-  size_t nr, nc, nb;
+  // printf("min_wh %f\n", (float)min_wh);
+  size_t min = 3 * min_wh / 6; // can adjust scale here. has been 3/6, 3/5, 2/5
+  // printf("min %f\n", (float)min);
+  size_t nr, nc, nb, nr2, nc2, np2;
 
-  // get image scale
-  string hfn(getHeaderFileName(IMG_FN));
-  cout << "hfn: " << hfn << endl;
+  string hfn(getHeaderFileName(IMG_FN)); // this section: get image scale
+  // cout << "hfn: " << hfn << endl;
   parseHeaderFile(hfn, nr, nc, nb);
-  printf(" infile: %s nrow %ld ncol %ld nband %ld\n", IMG_FN.c_str(), nr, nc, nb);
-  printf(" getFileSize %ld expected %ld\n", getFileSize(IMG_FN.c_str()), nr * nc * nb * sizeof(float));
+  // printf(" infile: %s nrow %ld ncol %ld nband %ld\n", IMG_FN.c_str(), nr, nc, nb);
+  // printf(" getFileSize %ld expected %ld\n", getFileSize(IMG_FN.c_str()), nr * nc * nb * sizeof(float));
   size_t np = nr * nc;
 
   // account for case that image may be small
   size_t min_wh_img = nr > nc ? nc: nr;
   //printf("min_wh_img %f\n", (float)min_wh_img);
   if(min > min_wh_img) min = min_wh_img;
-  
   //printf("min %f\n", (float)min);
 
-  // read band names
-  vec_band_names = parse_band_names(hfn);
-  groundref = parse_groundref_names(hfn);
+  vec_band_names = parse_band_names(hfn); // read band names
+  groundref = parse_groundref_names(hfn, n_groundref);
   if(true){
     vector<int>::iterator it;
     for(it = groundref.begin(); it != groundref.end(); it++){
       cout << "groundref " << *it << endl;
     }
   }
-
-  // determine scaling factor
-  size_t imin = nr > nc ? nc: nr; // printf("imin %d\n", imin);
+  size_t imin = nr > nc ? nc: nr;  // this section determine scaling factor 
   float scalef = (float)min / (float)imin;
-  printf("scalef %f\n", scalef);
+  // printf("scalef %f\n", scalef);
 
+  SUB_START_I = SUB_START_J = 0;
   SUB_SCALE_F = scalef;
-  SUB_START_I = 0;
-  SUB_START_J = 0;
-  IMG_NR = nr;
-  IMG_NC = nc;
-  IMG_NB = nb;
-
-  size_t nr2, nc2;
+  IMG_NR = nr; IMG_NC = nc; IMG_NB = nb;
   nr2 = (int)ceil((float)nr * scalef);
   nc2 = (int)ceil((float)nc * scalef);
   printf("nr2 %d nc2 %d\n", nr2, nc2);
 
-  // instead of loading the whole image, first load primary pane
+  // first load primary pane, instead of loading the whole image
   // SA<float> dat0(nr * nc * nb); // whole frame goes here
-  SA<float> dat(nr2 * nc2 * nb); // this is the overview / multilook buffer
-  size_t np2 = nr2 * nc2;
-  SA<float> bb(np); // whole image, one-band buffer
-
+  np2 = nr2 * nc2;
+  SA<float> dat(np2 * nb); // overview/ multilook image buffer
+  SA<float> bb(np); // whole image,  one-band buffer
+  
   size_t mfn_fs = getFileSize(mfn);
-
   if(exists(mfn) && mfn_fs == np2 * nb * sizeof(float)){
     FILE * f = fopen(mfn.c_str(), "rb");
-    if(!f){
-      printf("Error: failed to open multilook file\n");
-      exit(1);
-    }
+    if(!f) err("failed to open multilook file\n");
     fread(&dat[0], 1, np2 * nb * sizeof(float), f);
     fclose(f);
   }
   else{
-    // scene subsampling, parallelized by band
-    mlk_scene_nb = nb; // infile nbands
+    mlk_scene_nb = nb; // infile nbands. the section: scene subsampling, parallelized by band
     mlk_scene_nr = nr; // infile nrows
     mlk_scene_nc = nc; // infile ncols
     mlk_scene_nr2 = nr2; // infile nrows
     mlk_scene_nc2 = nc2; // infile ncols
-    mlk_scene_np2 = np2;
+    mlk_scene_np2 = np2; // number of pixels
     mlk_scene_scalef = scalef; // scaling factor
     mlk_scene_dat = &dat[0]; // float data output
     mlk_scene_fn = &IMG_FN; // input filename
     mlk_scene_groundref = &groundref; // groundref indices
 
-    //void multilook_scene(size_t k); // scene subsampling, parallelized by band
-    parfor(0, nb, multilook_scene);
-    /*
-    if(nread != nr * nc * nb * sizeof(float)){
-      printf("Error (imv.cpp): unexpected # of elements read (%d) expected (%d)\n", nread, nr * nc * nb);
-      exit(1);
-    }
-    */
-    // fclose(f);
-
+    parfor(0, nb, multilook_scene); // scene subsampling, parallelized by band
     FILE * f = fopen(mfn.c_str(), "wb");
     fwrite(&dat[0], 1, np2 * nb * sizeof(float), f);
     fclose(f);
@@ -182,14 +156,14 @@ int main(int argc, char ** argv){
   // set up window for overview image
   zprInstance * myZpr = myManager->newZprInstance(nr2, nc2, nb);
   glImage * myImage = new glImage(myZpr, &a);
-  myZpr->setTitle(string("Scene   "));
+  myZpr->setTitle(string("Scene "));
 
   // set up window for fullres subset image
   zprInstance * myZpr2 = myManager->newZprInstance(mm, mm, nb);
   glImage * myImage2 = new glImage(myZpr2, &b);
   SUB_GLIMG = (void *)myImage2;
   myZpr2->setRightOf(myZpr);
-  myZpr2->setTitle(string("Subset  "));
+  myZpr2->setTitle(string("Subset "));
 
   // target window setup
 
@@ -222,30 +196,23 @@ int main(int argc, char ** argv){
   zprInstance * myZpr4 = myManager->newZprInstance(200, 200, nb);
   // glBasicSphere * s = new glBasicSphere(0, myZpr4, 0, 0, 0, 1, 1, 1, 1., 10, 10);
 
-  // unit square axes
-  vec3 v0(0,0,0);
-  vec3 vx(1,0,0);
-  vec3 vy(0,1,0);
-  vec3 vz(0,0,1);
+  vec3 v0(0,0,0); // origin. this section: unit square axes
+  vec3 vx(1,0,0); vec3 vy(0,1,0); vec3 vz(0,0,1);
   glLine xL(myZpr4, v0, vx, 1, 0, 0);
   glLine yL(myZpr4, v0, vy, 0, 1, 0);
   glLine zL(myZpr4, v0, vz, 0, 0, 1);
 
   glPoints scatter(myZpr4, myImage3);
-  myZpr4->setRightOf(myZpr3);
-  //myZpr4->setScreenPosition(nc2, nr2 + 65);
+  myZpr4->setRightOf(myZpr3); //myZpr4->setScreenPosition(nc2, nr2 + 65);
   myZpr4->setTitle(string("Analysis"));
 
-  // preview scatter
-  zprInstance * myZpr5 = myManager->newZprInstance(200, 200, nb);
-  glLine xL2(myZpr5, v0, vx, 1, 0, 0);
-  glLine yL2(myZpr5, v0, vy, 0, 1, 0);
+  zprInstance * myZpr5 = myManager->newZprInstance(200, 200, nb); // preview scatter plot
+  glLine xL2(myZpr5, v0, vx, 1, 0, 0); glLine yL2(myZpr5, v0, vy, 0, 1, 0);
   glLine zL2(myZpr5, v0, vz, 0, 0, 1);
-  glPoints scatter2(myZpr5, myImage2);
-  myZpr5->setRightOf(myZpr2) ; //ScreenPosition(nc2, nr2 + 65); // ightOf(myZpr3);
-  myZpr5->setTitle(string("Subset  "));
-
-  printf("glutMainLoop()\n");
+  
+  glPoints scatter2(myZpr5, myImage2); // scatter plot for: what?
+  myZpr5->setRightOf(myZpr2) ; //ScreenPosition(nc2, nr2 + 65); 
+  myZpr5->setTitle(string("Subset ")); // printf("glutMainLoop()\n");
   initLighting();
   glutMainLoop();
   return 0;
