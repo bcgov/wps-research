03c61418a56b4b7d4e1da4901d4e81fc8826bcc2
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-25 15:03:24 -0700

    update

--- cpp/out/vis.py
+++ cpp/out/vis.py
@@ -16,12 +16,15 @@ except Exception:
 f = str(int(f))
 fn = f + '.bin'
 if not os.path.exists(fn):
-    err("file does not exist:" + fn)
+    fn = f + '.out'
+    if not os.path.exists(fn):
+        err("file does not exist:" + fn)
 
 def run(c):
     print(c)
     a = os.system(c)
     if(a != 0): err("cmd failed" + c)
+run('cp ' + fn + " " + fn[:-4] + '.bin')
 
 run("grep -n n_class ../n_class.csv")
 run("grep -n " + f + " ../n_class.csv")
