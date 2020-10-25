1403bccdb72bf28feb466fc5158371a98ff9564c
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-24 18:06:25 -0700

    update

---
+++ py/snappy_convert_slc.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python2
+''' given a collection of unzipped RCM Quad-pol data: for each SLC folder:
+        1) convert to PolSARPro format ("i/q format"
+        2) convert i/q format to s2 "Scattering Matrix" format
+        3) apply scm method (a visualization method of Shane Cloude)
+
+references:
+    [1] https://senbox.atlassian.net/wiki/spaces/SNAP/pages/19300362/How+to+use+the+SNAP+API+from+Python '''
+
+import os # usual python stuff
+import sys
+import snappy # ESA SNAP stuff
+from snappy import ProductIO
+
+def md(d):  # make folder if not yet exist
+    if not os.path.exists(d): os.mkdir(d)
+
+if not os.path.exists("/home/a/GitHub/bcws-psu-research/cpp/convert_iq_to_s2.exe"):
+    print("Error: failed to find convert_iq_to_s2.exe"); sys.exit(1)
+
+files = [f.strip() for f in os.popen("ls -1 RCM_SLC_ZIP/*.zip").readlines()]
+folders = [f[:-4] + '/' for f in files]
+
+for i in range(0, len(folders)):
+    f = folders[i] + 'manifest.safe'; print(f)
+
+    out_folder = folders[i] + 'PolSARPro'; md(out_folder)
+    s2_folder = folders[i]  + 's2'; md(s2_folder)
+    scm_folder = folders[i] + 'scm'; md(scm_folder)
+
+    d = ProductIO.readProduct(f)  # read SLC data 
+    ProductIO.writeProduct(d, out_folder, 'PolSARPro')  # convert to PolSARPro format
+
+    a = os.system("~/GitHub/bcws-psu-research/cpp/convert_iq_to_s2.exe " + out_folder + " " + s2_folder)
+    a = os.system(' '.join(['scm',  # a visualization method of Shane Cloude
+                            s2_folder, # input s2 matrix format data
+                            scm_folder, # output for visualization result
+                            'box', # filter type used for visualization
+                            '5', # filter window size
+                            'yes', # correct for average faraday rotation on scene
+                            '1', # alpha angle used for visualization
+                            '4'])) # vertical multiook factor
