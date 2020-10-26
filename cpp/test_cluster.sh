7f21c93b3ef02287a94adb3c4cd918cd3091253c
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-25 22:04:16 -0700

    update

--- cpp/test_cluster.sh
+++ cpp/test_cluster.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 rm ./cluster.exe
 ./compile.sh
-./cluster.exe  data/two_class.bin > tmp.txt # 249x249x5.bin
+# ./cluster.exe  data/two_class.bin 
+./cluster.exe  data/737x249x5.bin # data/249x249x5.bin
 # ./cluster.exe data/737x249x5.bin
