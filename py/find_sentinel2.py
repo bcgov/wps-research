9b82ee8773f2801cafd6cfa4febc994c89c84dee
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-26 10:24:43 -0700

    update

--- py/find_sentinel2.py
+++ py/find_sentinel2.py
@@ -33,7 +33,7 @@ else:
     pass_ = open('./.pass', 'rb').read().decode()
 
 def c(add= ''):
-    cmd = ('wget --no-check-certificate --user=' + user_ + ' --password=' + pass_ +
+    cmd = ('wget --no-check-certificate --user=' + user_.strip() + ' --password=' + pass_.strip() +
            ' --output-document=out.html "https://scihub.copernicus.eu/dhus/search?q=(platformname:Sentinel-2 AND cloudcoverpercentage:[0 TO 11] AND footprint:\\"' + foot_print + '\\")' + add + '"')
     return cmd
 
