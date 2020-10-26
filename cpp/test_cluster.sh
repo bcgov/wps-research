2110101925431916deafc821c23896066090668e
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-26 16:19:14 -0700

    update

--- cpp/test_cluster.sh
+++ cpp/test_cluster.sh
@@ -2,5 +2,7 @@
 rm ./cluster.exe
 ./compile.sh
 # ./cluster.exe  data/two_class.bin 
-./cluster.exe  data/737x249x5.bin # data/249x249x5.bin
-# ./cluster.exe data/737x249x5.bin
+# ./cluster.exe  data/737x249x5.bin 
+# ./cluster.exe data/249x249x5.bin
+./cluster.exe data/checkers.bin
+
