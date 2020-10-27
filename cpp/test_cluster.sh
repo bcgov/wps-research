7f82376cf11ba323e4afba27cb420a6e0789d9e4
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-26 20:23:31 -0700

    update

--- cpp/test_cluster.sh
+++ cpp/test_cluster.sh
@@ -4,5 +4,7 @@ rm ./cluster.exe
 # ./cluster.exe  data/two_class.bin 
 # ./cluster.exe  data/737x249x5.bin 
 # ./cluster.exe data/249x249x5.bin
-./cluster.exe data/checkers.bin
+./cluster.exe data/237x201x3.bin
+# ./cluster.exe data/checkers.bina
 
+cd nearest; bin2png
