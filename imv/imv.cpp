5116882ffb6f134e6af77556dd5fe17cecf43c49
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-03 00:10:39 -0800

    update

--- imv/imv.cpp
+++ imv/imv.cpp
@@ -67,7 +67,7 @@ int main(int argc, char ** argv){
   int height_lim = nr > height; // screen height too small for image
   int width_lim = nc > width; // screen width too small for image
 
-  scalef = 0.98 * (float)(height > width ? width : height) / (float)(nr > nc? nr: nc); // some simplification of above code..
+  scalef = 0.95 * (float)(height > width ? width : height) / (float)(nr > nc? nr: nc); // some simplification of above code..
   if( nr < width && nr < height && nc < width && nc < height) scalef = 1.; // account for "small image" case. 1-1 if can fit on screen!
 
   SUB_START_I = SUB_START_J = 0;
