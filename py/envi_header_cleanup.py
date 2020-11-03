9b51faecb013585fba7037697b4f2eccdbc17f88
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-03 14:46:37 -0800

    update

--- py/envi_header_cleanup.py
+++ py/envi_header_cleanup.py
@@ -10,7 +10,8 @@ data = open(args[1]).read().strip()
 n_band_names, in_band_names, nb = 0, False, 0
 data = data.replace("description = {\n", "description = {")
 data = data.replace("band names = {\n", "band names = {")
-lines = data.split("\n")
+lines, non_bandname_lines = data.split("\n"), []
+bandname_lines = []
 
 for i in range(0, len(lines)):
     line = lines[i].strip()
@@ -35,14 +36,37 @@ for i in range(0, len(lines)):
             lines[i] = line.replace(',', '')
 
     if in_band_names:
+        bandname_lines.append(line) # track band names we have
+    else:
+        non_bandname_lines.append(line) # record non-band-name lines,
+        # in case we need to fill the band-names in
+
+    if in_band_names:
         if len(line.split("}")) > 1:
             in_band_names = False
 
-data = ('\n'.join(lines)).strip()
-print(data)
+if nb != n_band_names:
+    if n_band_names > nb:
+        bandname_lines = bandname_lines[:nb]
+        bandname_lines[-1] = bandname_lines[-1].strip() + "}"
+    if n_band_names > 0 and n_band_names < nb:
+        bandname_lines[-1] = bandname_lines[-1].strip().strip('}')
+        for i in range(1, nb + 1):
+            if i > n_band_names:
+                bandname_lines[-1] = bandname_lines[-1].strip().strip("}").strip(",") + ','
+                pre = "band names = {" if i == 1 else ""
+                bandname_lines.append(pre + "Band " + str(i) + ",")
+        bandname_lines[-1] = bandname_lines[-1].strip().strip(',') + "}"
 
+    if n_band_names == 0:
+        bandname_lines.append("band names = {Band 1,")
+        for i in range(1, nb):
+            bandname_lines.append("Band " + str(i + 1) + ",")
+        bandname_lines[-1] = bandname_lines[-1].strip().strip(",") + "}"
+
+lines = non_bandname_lines + bandname_lines
+data = ('\n'.join(lines)).strip()
+
+print(data)
 open(args[1] + '.bak', 'wb').write(open(args[1]).read().encode())
 open(args[1], 'wb').write(data.encode())
-
-print("number of bands", nb)
-print("number of band names", n_band_names)
