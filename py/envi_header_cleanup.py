00f2b33fc6894ee0f44dc999bb5a2b548807982b
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-03 14:27:48 -0800

    update

--- py/envi_header_cleanup.py
+++ py/envi_header_cleanup.py
@@ -7,15 +7,18 @@ if not exist(args[1]):
     err("file not found: " + args[1])
 
 data = open(args[1]).read().strip()
-
+n_band_names, in_band_names, nb = 0, False, 0
 data = data.replace("description = {\n", "description = {")
 data = data.replace("band names = {\n", "band names = {")
-
 lines = data.split("\n")
 
-in_band_names = False
 for i in range(0, len(lines)):
     line = lines[i].strip()
+
+    w = line.split("=")
+    if len(w) > 1:
+        if w[0].strip() == 'bands':
+            nb = int(w[1].strip())
 
     if len(line.split("band names =")) > 1:
         in_band_names = True
@@ -23,6 +26,7 @@ for i in range(0, len(lines)):
     # print(line + (" TRUE" if in_band_names else ""))
 
     if in_band_names:
+        n_band_names += 1
         if len(line.split("}")) < 2:
             w = line.split(',')
             line = ''.join(w[:-1]) + ',' + w[-1]
