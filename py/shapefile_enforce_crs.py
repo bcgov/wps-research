d8e64e6014f0a6d7c65b9d01a9ab0eb914f727d5
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-04 11:24:46 -0800

    update

--- py/shapefile_enforce_crs.py
+++ py/shapefile_enforce_crs.py
@@ -1,4 +1,25 @@
 # convert shapefile to specified CRS, where a CRS is indicated in EPSG format
+import os
+import sys
+args = sys.argv
 
+def err(m):
+    print("Error: " + m); sys.exit(1)
 
+print(args)
+fn = args[1]
+try:
+    if fn[-4:] != '.shp':
+        err("shapefile input req'd")
+except Exception:
+    err("please check input file")
+
+cmd = "gdalsrsinfo -v " + fn
+data = os.popen(cmd).read().strip()
+
+if len(data.split("Validate Succeeds")) < 2:
+    err("command:\n\t" + cmd + "\ndid not give expected result. Result:\n\n" + data) 
+
+lines = data.split('\n')
+print(data)
 
