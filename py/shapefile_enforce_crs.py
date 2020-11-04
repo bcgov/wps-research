a3b9325431e68a093031921ee80f8cdf1ade411b
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-04 11:35:47 -0800

    update

--- py/shapefile_enforce_crs.py
+++ py/shapefile_enforce_crs.py
@@ -7,6 +7,11 @@ def err(m):
     print("Error: " + m); sys.exit(1)
 
 print(args)
+
+if len(args) < 2:
+    err("python3 shapefile_enforce_crs.py [input shapefile] [optional argument: destination crs EPSG number] # default EPSG 32609")
+
+
 fn = args[1]
 try:
     if fn[-4:] != '.shp':
@@ -21,5 +26,12 @@ if len(data.split("Validate Succeeds")) < 2:
     err("command:\n\t" + cmd + "\ndid not give expected result. Result:\n\n" + data) 
 
 lines = data.split('\n')
-print(data)
+
+src_EPSG = None
+for line in lines:
+    line = line.strip()
+    w = line.split('ID["EPSG')
+    if len(w) > 1:
+        w = w[1].strip('"').strip(',').strip(']').strip(']').strip()
+        src_EPSG = int(w)
 
