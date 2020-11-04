3fbd44a27900c56891abe0146db7984e75f2e232
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-04 12:02:41 -0800

    update

--- py/shapefile_info.py
+++ py/shapefile_info.py
@@ -27,10 +27,17 @@ def records(layer):
         feature = layer.GetFeature(i)
         yield json.loads(feature.ExportToJson())
 
-print("feature count: " + str(feature_count))
+print("feature_count," + str(feature_count))
+print("feature","type", "x","y")
 features = records(layer)
 feature_names, feature_ids = [], []
-for f in features:  # print(f.keys())
+for f in features:  
+    geom = "" #  print(f.keys())
+    try:
+        geom = f['geometry']
+    except Exception:
+        pass
+
     feature_id = f['id']
     feature_ids.append(feature_id) # print(f['properties'].keys())
     feature_name = ''
@@ -39,6 +46,6 @@ for f in features:  # print(f.keys())
     except Exception:
         pass # feature name not available
     feature_names.append(feature_name)
-    print("\t", feature_name)
 
-
+    if geom['type'] == 'Point':
+        print(feature_id, geom['type'], geom['coordinates'][0], geom['coordinates'][1])
