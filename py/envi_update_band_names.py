ec4b764c9948c3e9f9657273788cd7443b8d4c57
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-04 14:53:02 -0800

    update

--- py/envi_update_band_names.py
+++ py/envi_update_band_names.py
@@ -31,20 +31,28 @@ def get_band_names_lines(data):
             band_name_lines.append(lines[i])
             if len(lines[i].split("}")) > 1:
                 in_band_names = False
+    return band_name_lines
 
 if not exists(args[1]) or not exists(args[2]):
     err("please check input files:\n\t" + args[1] + "\n\t" + args[2])
 
 run('python3 ' + pd + 'envi_header_cleanup.py ' + args[1])
-run('python3 ' + pd + 'envi_header_cleanup.py ' + args[2])
-
-
+run('python3 ' + pd + 'envi_header_cleanup.py ' + args[2]) # should really call directly, whatever
 
 i_dat, o_dat = open(args[1]).read(),  open(args[2]).read()
 
 
-get_band_names_lines(i_dat)
-get_band_names_lines(o_dat)
+print("get band names..")
+
+bn_1 = get_band_names_lines(i_dat)
+bn_2 = get_band_names_lines(o_dat)
+
+lines = o_dat.strip().split('\n')
+
+for i in range(0, len(lines)):
+    line = lines[i]
+    if line in 
+
 
 
 
