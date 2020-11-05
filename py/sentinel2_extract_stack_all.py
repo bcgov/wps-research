48a8a2d8350e9fa924636e66f07d4a11b8375033
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-04 16:21:17 -0800

    update

--- py/sentinel2_extract_stack_all.py
+++ py/sentinel2_extract_stack_all.py
@@ -27,21 +27,34 @@ for z in zips:
     z = z.strip()
     safe = z[:-4] + ".SAFE" # print(safe)
     bins = [x.strip() for x in os.popen("ls -1 " + safe + os.path.sep + "*m_EPSG*.bin").readlines()] # don't pull the TCI true colour image. Already covered in 10m
+    print(safe)
 
     if len(bins) != 3:
         err("unexpected number of bin files (expected 3): " + str(bins))
 
     m10, m20, m60 = bins
-    print("10m", m10)
-    print("20m", m20)
-    print("60m", m60)
+    print("  10m:", m10)
+    print("  20m:", m20)
+    print("  60m:", m60)
 
-    print(safe)
-    for b in bins:
-        print('  ' + b) # print('  ' + b.split(sep)[-1])
+    #for b in bins:
+    #    print('  ' + b) # print('  ' + b.split(sep)[-1])
     if not os.path.exists(safe):
         cmd = "python3 " + extract + " " + z
         print(cmd)
         a = os.system(cmd)
 
+    
+    # names for files resampled to 10m
+    m20r = m20[:-4] + '_10m.bin'
+    print("   " + m20r)
+    m60r = m60[:-4] + '_10m.bin'
+    print("   " + m60r)
 
+    cmd = ['python3 ' + pd + 'raster_project_onto.py',
+           m20,
+           m10,
+           m20r]
+
+    cmd = ' '.join(cmd)
+    print(cmd)
