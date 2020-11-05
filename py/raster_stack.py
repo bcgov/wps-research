0b0337342a4eea6f51c6e4b60fa94c231205db5b
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-04 20:20:27 -0800

    update

--- py/raster_stack.py
+++ py/raster_stack.py
@@ -1,2 +1,22 @@
 # stack raster files
+import os
+import sys
+args = sys.argv
+sep = os.path.sep
+exists = os.path.exists
+pd = sep.join(__file__.split(sep)[:-1]) + sep
+
+if len(args) < 4:
+    err("python3 raster_stack.py [input raster 1] [input raster 2] .." +
+        " [input raster n] [output raster name]")
+
+rasters = args[1:-1]
+outf = args[-1]
+
+cmd = ['cat'] + rasters + ['>', outf]
+cmd = ' '.join(cmd)
+if not exists(outf):
+    print(cmd)
+    a = os.system(cmd)
+
 
