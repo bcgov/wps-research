59269506e21db2eb61d38e5b6c78961cb87f210b
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-05 15:02:31 -0800

    update

--- imv/imv.cpp
+++ imv/imv.cpp
@@ -5,6 +5,30 @@ that inspired further developments at UVic, CFS and elsewhere.. */ // todo: disp
 #include"newzpr.h"
 #include<iostream>
 
+
+//#include <iostream>
+#include <stdexcept>
+//#include <stdio.h>
+//#include <string>
+
+std::string exec(const char* cmd) {
+    char buffer[128];
+   std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try{
+        while(fgets(buffer, sizeof buffer, pipe) != NULL) {
+            result += buffer;
+        }
+    }
+    catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
+}
+
 int main(int argc, char ** argv){
   system("grep -n grep stretch newzpr.cpp");
 
@@ -223,6 +247,9 @@ int main(int argc, char ** argv){
   initLighting();
   
   bands_per_frame = 22;
+  
+
+  
   glutMainLoop();
   return 0;
 }
