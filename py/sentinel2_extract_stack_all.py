c5fd5e84065d76a7812a3c44c17b2527898e720f
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-04 16:25:55 -0800

    update

--- py/sentinel2_extract_stack_all.py
+++ py/sentinel2_extract_stack_all.py
@@ -46,19 +46,25 @@ for z in zips:
 
     
     # names for files resampled to 10m
-    m20r = m20[:-4] + '_10m.bin'
-    print("   " + m20r)
-    m60r = m60[:-4] + '_10m.bin'
-    print("   " + m60r)
+    m20r, m60r = m20[:-4] + '_10m.bin', m60[:-4] + '_10m.bin'
 
+    # resample the 20m
     cmd = ['python3 ' + pd + 'raster_project_onto.py',
            m20,
            m10,
            m20r]
-
-    cmd = ' '.join(cmd)
-
+ 
     if not exists(m20r):
-        run(cmd)
+        run(' '.join(cmd))
+
+
+    # resample the 60m
+    cmd = ['python3 ' + pd + 'raster_project_onto.py',
+           m60,
+           m10,
+           m60r]
+
+    if not exists(m60r):
+        run(' '.join(cmd))
 
     sys.exit(1)
