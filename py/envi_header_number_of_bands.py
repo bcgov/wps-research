c90563a50ce06c7afc52eeb8ca9e3f3a36c8d02a
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-05 14:37:51 -0800

    update

---
+++ py/envi_header_number_of_bands.py
@@ -0,0 +1,17 @@
+# output the number of bands, from an envi header
+import sys
+args = sys.argv
+
+if len(args) < 2:
+    print("python3 envi_header_number_of_bands.py [envi .hdr file] # list number of bands in .hdr file")
+    sys.exit(1)
+
+for i in range(0, len(lines)):
+    line = lines[i].strip()
+
+    w = line.split("=")
+    if len(w) > 1:
+        if w[0].strip() == 'bands':
+            nb = int(w[1].strip())
+
+print(nb)
