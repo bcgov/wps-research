75784070868cc3e06ebb8a233f34efa1a5da6d82
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-05 23:42:24 -0800

    update

--- py/raster_extract_spectra_plot.py
+++ py/raster_extract_spectra_plot.py
@@ -16,6 +16,7 @@ x_max, y_max = None, None
 
 X = []
 Y = []
+C = []
 plt.figure()
 for line in lines[1:]:
     w = line.split(",")
@@ -25,12 +26,34 @@ for line in lines[1:]:
     ci += 1 
     r, g, b, = w[4], w[5], w[6]
     r, g, b = float(r), float(g), float(b)
-    [r, g, b] = [r/1650., g/1650., b/1650.]
-    print("  ", pix, lin, [r,g,b])
-    plt.scatter([pix], [lin], color=(r,g,b)) #, 'x')
     X.append(pix)
     Y.append(lin)
+    C.append( [r,g,b] )
 
+
+C_min = list(C[0])
+C_max = list(C[0])
+for i in range(0, len(X)):
+    for j in range(0, 3):
+        if C[i][j] < C_min[j]: C_min[j] = C[i][j]
+        if C[i][j] > C_max[j]: C_max[j] = C[i][j]
+
+print("C_min", C_min)
+print("C_max", C_max)
+
+for i in range(0, len(X)):
+    Ci = list(C[i])
+
+    for j in range(0, 3):
+        Ci[j] -= C_min[j]
+        Ci[j] /= (C_max[j] - C_min[j])
+
+    C[i] = Ci
+
+for i in range(0, len(X)):
+    r,g,b =  C[i]
+    print(C[i])
+    plt.scatter(X[i], Y[i], color=(r,g,b))
 #  plt.plot(X,Y, 'x')
 plt.show()
 # fig.savefig('plotcircles.png', dpi = 3000)
