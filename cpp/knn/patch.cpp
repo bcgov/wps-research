d59cb26889bfbbe3a2f68447bf344066023a0397
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-07 20:55:53 -0800

    update

--- cpp/knn/patch.cpp
+++ cpp/knn/patch.cpp
@@ -38,10 +38,10 @@ int main(int argc, char ** argv){
   unsigned int floats_per_patch = ps * ps * nb; // floats per patch (image data)
   float * patch = falloc(sizeof(float) * floats_per_patch);
 
-  FILE * f_patch = wopen("patch.dat"); // patch data
-  FILE * f_patch_i = wopen("patch_i.dat"); // start row for patch 
-  FILE * f_patch_j = wopen("patch_j.dat"); // start col for patch
-  FILE * f_patch_label = wopen("patch_label.dat"); // patch label
+  FILE * f_patch = wopen((bfn + str("_patch").c_str()); // patch data
+  FILE * f_patch_i = wopen((bfn + str("_patch_i").c_str()); // start row for patch 
+  FILE * f_patch_j = wopen((bfn + str("_patch_j").c_str()); // start col for patch
+  FILE * f_patch_label = wopen((bfn + str("_patch_label")).c_str()); // patch label
 
   size_t truthed = 0;
   size_t nontruthed = 0;
