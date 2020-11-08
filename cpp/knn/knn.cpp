315dd721bd5550f1e304b878d256435207092a5f
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-07 23:58:58 -0800

    update

--- cpp/knn/knn.cpp
+++ cpp/knn/knn.cpp
@@ -7,9 +7,13 @@
 int main(int argc, char ** argv){
   printf("knn [input envi-type4 floating point stack bsq with gr] [input without gr to project onto] [KMax] [K]\n");
 
-  /* 1. code patches in linear segs, crash if alloc fail */
+  size_t nr, nc, nb;
+  str bfn(argv[1]);
+  str bf2(argv[2]);
 
-  /* 2. deduplicate? */
+  size_t kmax = atoi(argv[3]);
+  size_t K = atoi(argv[4]);
+
 
   /* 3. for each class, dmat calc (persist for params) -- incl. reflection in dcalc? (this is a non-square dmat) */
 
