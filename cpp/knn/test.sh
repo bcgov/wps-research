110379b4b29dba7f090b34a5c14653c0563c921f
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-11 20:29:59 -0800

    update

--- cpp/knn/test.sh
+++ cpp/knn/test.sh
@@ -1,5 +1,6 @@
 rm patch.exe & # catch fail
 rm dedup.exe & 
+rm knn.exe &
 wait
 g++ patch.cpp ../misc.cpp -o patch.exe & 
 g++ dedup.cpp ../misc.cpp -o dedup.exe &
@@ -7,6 +8,11 @@ wait
 
 ./patch.exe stack.bin 12 3
 ./dedup.exe stack.bin
+
+g++ knn.cpp ../misc.cpp -o knn.exe &
+wait
+
+./knn.exe stack.bin stack.bin 1111 55
 
 # run patch and dedup on target data too?
 
