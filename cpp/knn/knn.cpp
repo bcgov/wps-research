a6907cb573c99443b88ce261265875c4c8ac00f3
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-11 20:30:05 -0800

    update

--- cpp/knn/knn.cpp
+++ cpp/knn/knn.cpp
@@ -5,21 +5,61 @@
 
 #include"../misc.h"
 int main(int argc, char ** argv){
-  
-	//note: the inputs are both patched. Input to project onto is patched, but without GR! 
-	printf("knn [input envi-type4 floating point stack bsq with gr (1)] [input image without gr to project onto (2)] [KMax] [K]\n");
+  if(argc < 5){
+    //note: the inputs are both patched. Input to project onto is patched, but without GR!
+    printf("knn [input envi-type4 floating point stack bsq with gr (1)] [input image (gr not req'd): to project onto (2)] [KMax] [K]\n");
+    exit(1);
+  }
+  // 1) is called "reference" (groundref + image combined)
+  // 2) is called "source"
+  // 3) output (transformed source) called product
 
- // 1) is called "reference" (groundref + image combined)
- // 2) is called "source"
- // 3) output (transformed source) called product
+  size_t nr, nc, nb, ps, fpp, bpp, ref_ps, ref_nb, K, K_max;
+  str ref_f(argv[1]);
+  str src_f(argv[2]);
 
-  size_t nr, nc, nb;
-  str bfn(argv[1]);
-  str bf2(argv[2]);
+  K = atoi(argv[4]);
+  K_max = atoi(argv[3]); // how many things we persist
 
-  size_t K = atoi(argv[4]);
-  size_t kmax = atoi(argv[3]); // how many things we persist
+  ref_ps = int_read(ref_f + str("_ps"));
+  ref_nb = int_read(ref_f + str("_nb"));
+  ps = int_read(src_f + str("_ps"));
+  nb = int_read(src_f + str("_nb"));
+  if(ref_ps != ps) err("please ensure ref and src patch sizes are equal");
+  if(ref_nb != nb) err("please ensure ref and src images have same number of image data bands, excl. ground-ref");
 
+  printf("number of bands: %zu\n", ref_nb);
+  printf(" patch length: %zu\n", ref_ps);
+  fpp = ps * ps * nb; // floats per patch
+  printf("floats per patch: %zu\n", fpp);
+  bpp = fpp * sizeof(float);
+
+  size_t ref_pfs = fsize(ref_f + str("_patch"));
+  size_t src_pfs = fsize(src_f + str("_patch"));
+  size_t ref_np = ref_pfs / bpp;
+  size_t src_np = src_pfs / bpp;
+  if(ref_pfs % bpp > 0 || src_pfs % bpp > 0){
+    err("_patch file not multiple of bytes-per-patch");
+  }
+
+  printf("ref patches: %zu\n", ref_np);
+  printf("src patches: %zu\n", src_np); // now list ref patch indices
+
+  float * patch_label = float_read(ref_f + str("_patch_label"));
+
+
+  size_t i;
+  map<float, size_t> c;
+  for0(i, ref_np){
+    float L = patch_label[i];
+    if(c.count(L) < 1) c[L] = 0;
+    c[L] += 1;
+  }
+
+  cout << c << endl;
+
+
+  // now, make sure to not stomp the output name!
 
   /* 3. for each class, dmat calc (persist for params) -- incl. reflection in dcalc? (this is a non-square dmat) */
 
@@ -32,8 +72,7 @@ int main(int argc, char ** argv){
 
   /* 5. a enforce mathematical conditions for a probability density */
 
-  /* calculate entropy maps */
-
+  /* calculate entropy maps (don't worry about accuracy) */
 
   return 0;
 }
