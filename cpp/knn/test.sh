b278ab1b7412852a152c7d74cdb7eed5b3e5dca2
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-11 22:08:05 -0800

    update

--- cpp/knn/test.sh
+++ cpp/knn/test.sh
@@ -2,14 +2,14 @@ rm patch.exe & # catch fail
 rm dedup.exe & 
 rm knn.exe &
 wait
-g++ patch.cpp ../misc.cpp -o patch.exe & 
-g++ dedup.cpp ../misc.cpp -o dedup.exe &
+g++ -pthread patch.cpp ../misc.cpp -o patch.exe & 
+g++ -pthread dedup.cpp ../misc.cpp -o dedup.exe &
 wait
 
 ./patch.exe stack.bin 12 3
 ./dedup.exe stack.bin
 
-g++ knn.cpp ../misc.cpp -o knn.exe &
+g++ -pthread knn.cpp ../misc.cpp -o knn.exe &
 wait
 
 ./knn.exe stack.bin stack.bin 1111 55
