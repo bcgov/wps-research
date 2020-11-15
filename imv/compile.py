ee020d51b3befd3894c111a0f6c57c6bbb98622a
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-14 17:05:40 -0800

    update

--- imv/compile.py
+++ imv/compile.py
@@ -4,8 +4,9 @@ def run(cmd):
     print(cmd)
     return os.system(cmd)
 
+run("clean")
 run("rm -f imv.exe")
-run("make")
+run("./make.sh")
 run("chmod 755 imv.exe")
 which = os.popen("which imv").read().strip()
 print("which" + str([which]))
