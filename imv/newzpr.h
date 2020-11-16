98492da1c2571a1fe00b5d15cf64983429d2d7d3
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-16 12:54:10 -0800

    update

--- imv/newzpr.h
+++ imv/newzpr.h
@@ -964,7 +964,7 @@ class glImage: public glPlottable{
         glVertex2f(x - w, y); glVertex2f(x + w, y);
         glEnd();
 
-        myParent->drawText(x, NWIN-y, tgt_label.c_str());
+        myParent->drawText(x, y, tgt_label.c_str());
       }
     }
   }
