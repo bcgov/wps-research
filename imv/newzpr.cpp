03e4c5cee87becc27a3b4ea0533776c4c8070463
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-23 19:01:42 -0800

    update

--- imv/newzpr.cpp
+++ imv/newzpr.cpp
@@ -560,29 +560,20 @@ void zprInstance::processString(){
     }
   }
 
-  if(strcmpz(console_string, "k\0") && console_string[2] == '\0'){
+  if(strcmpz(console_string, "k\0") && console_string[1] == ' '){
+
+    str kk(&console_string[2]); 
+    size_t kmeans_k = atoi(kk.c_str());
     // k prefix: run K means, increasing K, until points with same references, are discriminated
     // open the output in imv!!!!
-    size_t kmeans_k;
-    for(kmeans_k = 2; kmeans_k < 100; kmeans_k++){
+   // for(kmeans_k = 2; kmeans_k < 100; kmeans_k++){
       str use_name(exec("whoami"));
       use_name = strip(use_name);
       str cmd(str("/home/") + use_name + str("/GitHub/bcws-psu-research/cpp/kmeans.exe tmp_subset.bin ") + to_string(kmeans_k));
 
       cout << "[" << cmd << "]" << endl;
       system(cmd.c_str());
-
-
-      // get the row / col idx of the different tagged points
-      // check if our points with different tags, in different classes..
-      // .. and points with same tags, in same classes
-      // if both were satisfied, we have a viable match
-      // else, go to next iteration
-
-      // vector target locations
-      // extern vector<size_t> targets_i;
-      // extern vector<size_t> targets_j;
-      // extern vector<str> targets_label; // add other fields using a map/ json structure?
+/*
       if(targets_i.size() != targets_j.size() || targets_j.size() != targets_label.size()) err("vector target locations arrays mismatch");
 
       FILE * labf = fopen("tmp_subset.bin_kmeans.bin", "rb");
@@ -592,51 +583,65 @@ void zprInstance::processString(){
       fclose(labf);
       size_t i;
       size_t n = targets_i.size();
-      cout << "tgt_i tgt_j tgt_lab kmeans_label";
+      cout << "tgt_i tgt_j tgt_lab kmeans_label" << endl;
 
       bool good = true; // innocent until proven guilty
       map<str, float> labs; // accumulate cluster-output class-labels, per target identifier string (label)
+      map<str, set<float>> classes;
       labs.clear();
       for0(i, n){
         size_t my_i = targets_i[i] - SUB_START_I;
         size_t my_j = targets_j[i] - SUB_START_J;
         size_t my_k = (my_i * SUB_MM) + my_j;
-        printf("\n%zu my_k %zu\n", SUB_MM* SUB_MM * IMG_NB, my_k);
-        cout << "  " << targets_label[i] << " -> " << lab[my_k] << endl; // ( size_t) lab[(targets_i[i] * SUB_MM
+        cout << targets_label[i] << " " <<  my_i << "," << my_j << "," << lab[my_k] << endl;
+        //printf("\n%zu lab[my_k] %zu", SUB_MM* SUB_MM * IMG_NB, (size_t)lab[ my_k]);
+        //cout << " " << targets_label[i] << " -> " << lab[my_k] << endl; // ( size_t) lab[(targets_i[i] * SUB_MM
+
+        if(classes.count(targets_label[i]) < 1){
+          classes[targets_label[i]].clear();
+        }
 
         // need image coordintes, in subscene reference coordinate scheme, for each target that's within the subscene window
         str id(targets_label[i]); // string identifier for the annotation "target"
-        if(labs.count(id) < 1){
-          labs[id] = lab[my_k]; // record a class label w the string identifier, which is associated with this target
-        }
-        else{
-          // if different than existing, fail
-          if(labs[id] != lab[my_k]){
-            printf("labs[id] %zu lab[my_k] %zu\n", labs[id], lab[my_k]);
-            good = false; // if there is a different label, for a target with the same string identifier, bail..
-            break;
-          }
-        }
-        printf("after\n");
+
+        classes[id].insert(lab[my_k]);
+        
+        if(labs.count(targets_label[i]) < 1){
+          labs[targets_label[i]] = lab[my_k];
+	}
+	else{
+}
+
+	
+
         // ok so that was make sure same targets have same label. now, different targets have different label
       }
-  	
-     if(true){
-      // different targets have different label
-      set<float> label_set;
-      map<str, float>::iterator it;
-      for(it = labs.begin(); it != labs.end(); it++){
-        cout << it->first << " -> " << it->second << endl;
-        label_set.insert(it->second);
+
+      for(map<str, set<float>>::iterator it = classes.begin(); it != classes.end(); it++){
+        str id(it->first);
+        set<float> x(it->second);
+        for(map<str, set<float>>::iterator ti = it; ti != classes.end(); ti++){
+          if(ti == it) continue;
+          set<float> y(ti->second);
+
+          set<float> z;
+          set_intersection(x.begin(),x.end(),y.begin(),y.end(),
+          std::inserter(z,z.begin()));
+
+          if(z.size() > 0){
+            good = false; 
+          }
+          cout << it->first << "->" << it->second << " " << ti->first << "->" << ti->second << " " << z << endl;
+        }
       }
-      if(labs.size() != label_set.size()) good = false; // different targets must have same label
-}
+
       cout << endl << "RESULT " << good << endl;
       cout << endl;
       free(lab);
       if(good) break;
-    }
-    str cmd("imv tmp_subset.bin_means.bin");
+     // exit(1);
+  */
+    cmd = str("rm -f tmp_subset.bin_means.ml; imv tmp_subset.bin_means.bin");
     cout << cmd << endl;
     system(cmd.c_str());
   }
