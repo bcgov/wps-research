2689203db8a0af24d9b196ba9cc6075de5d2c95d
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-24 17:07:35 -0800

    update

--- cpp/kmeans.cpp
+++ cpp/kmeans.cpp
@@ -1,8 +1,5 @@
-//implementation of k-means algorithm, with envi type-4 image input
-// Next: formulate as patch instead of vector?
+//implementation of k-means algorithm, with envi type-4 image input. Use patch?
 #include"misc.h"
-int debug = false; // set to true for output messages // tolerance should be adjustable
-// nb should PARALLELIZE on for each point loop, within iteration loop
 int main(int argc, char ** argv){
   size_t nrow, ncol, nband, np, i, j, k, n;// variables
   if(argc < 3) err("kmeans [input binary file name] [k]");
@@ -13,7 +10,6 @@ int main(int argc, char ** argv){
 
   hread(hfn, nrow, ncol, nband); // read header
   np = nrow * ncol; // number of input pix
-
   float * dat = bread(fn, nrow, ncol, nband); // load floats to array
   float * means = falloc(np * nband); // output nearest mean for visualization
 
@@ -32,7 +28,6 @@ int main(int argc, char ** argv){
       if(d > max[k]) max[k] = d;
     }
   }
-  for0(i, nband) printf("band %zu min %f max %f\n", i, min[i], max[i]);
   for0(i, np){
     for0(k, nband){
       float d = dat[(np * k) + i];
@@ -49,9 +44,7 @@ int main(int argc, char ** argv){
   float * count = falloc(K); // count
 
   for0(n, iter_max){
-    // for each iteration
-
-    // calculate class means
+    // for each iteration, calculate class means
     for0(i, nmf) mean[i] = 0.;
     for0(k, K) count[k] = 0.; // denominator for average
     for0(i, np){
@@ -62,13 +55,6 @@ int main(int argc, char ** argv){
       count[(size_t)label[i]] += 1;
     }
     for0(i, K) if(count[i] > 0) for0(j, nband) mean[(i * nband) + j] /= count[i];
-
-    //print out means
-    if(debug) for0(j, K){
-      printf("mean %zu", j);
-      for0(k, nband) printf(" %f", mean[j * nband + k]);
-      printf("\n");
-    }
 
     // for each point, reassign to nearest cluster centre
     for0(i, np){
@@ -88,11 +74,6 @@ int main(int argc, char ** argv){
         }
       }
       update[i] = nearest_i;
-      if(debug){
-        printf(" d %f k %zu ", nearest_d, nearest_i);
-        for0(k, nband) printf(" %f", dat[(np * k) + i]);
-        printf("\n");
-      }
     }
 
     size_t n_change = 0;
@@ -127,8 +108,5 @@ int main(int argc, char ** argv){
   }
   bwrite(means, omn, nrow, ncol, nband);
   hwrite(omh, nrow, ncol, nband, 4);
-  str cmd(str("cp ") + hfn + str(" ") + omh);
-  cout << cmd << endl;
-   // system(cmd.c_str());
   return 0;
 }
