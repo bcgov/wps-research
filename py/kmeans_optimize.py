e35ac8b08975378a0b103ec96ba1dbb8d9f2dfd3
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-01-05 14:01:50 -0800

    update

--- py/kmeans_optimize.py
+++ py/kmeans_optimize.py
@@ -15,19 +15,28 @@ if not os.path.exists(infile):
 tf = infile + "_targets.csv"
 if not os.path.exists(tf):
     error("targets file not found: " + str(tf))
-
+lines = open(tf).read().strip().split("\n")
+lines = [line.strip().split(",") for line in lines]
+hdr = lines[0] # 'row', 'lin', 'xoff', 'yoff'
+i_row = hdr.index('row')
+i_lin = hdr.index('lin')
+i_xof = hdr.index('xoff')
+i_yof = hdr.index('yoff')
+i_lab = hdr.index('feature_id')
 sep = os.path.sep
 path = sep.join(__file__.split(sep)[:-1]) + sep  # path to this file
 
 K = 5
 whoami = os.popen("whoami").read().strip()
-cmd = path + "../cpp/kmeans_multi.exe stack.bin " + str(K)
-
-run(cmd)
+run(path + "../cpp/kmeans_multi.exe stack.bin " + str(K))
 
 class_file = infile + "_kmeans.bin"
-
 samples, lines, bands, data = read_binary(class_file)
+
+for i in range(1, len(lines)):
+    line = lines[i]
+    print("row", line[i_row], line[i_lin], line[i_xof], line[i_yof], line[i_lab])
+
 
 #cmd = "python3 " + path + "read_multi.py " + infile + "_kmeans.bin" 
 #run(cmd)
