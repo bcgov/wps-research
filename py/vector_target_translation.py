3a7084f3bb8faa9c265fa6859907c19658404e11
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-01-05 11:30:27 -0800

    update

---
+++ py/vector_target_translation.py
@@ -0,0 +1,87 @@
+''' filter vector data by:
+1. translation
+2. remove out of range targets!'''
+
+from misc import *
+
+if len(args) < 6:
+    print(args)
+    err("Usage: vector_target_translation [targets.csv] [xoff] [yoff] [image header file] [translated targets file]")
+
+targets = open(args[1]).read().strip().split("\n")
+xoff, yoff, outf = None, None, None
+
+try:
+    xoff = int(args[2])
+except Exception as e:
+    xoff = int(open(args[2]).read().strip())
+
+try:
+    yoff = int(args[3])
+except Exception as e:
+    yoff = int(open(args[3]).read().strip())
+
+print("xoff", xoff, "yoff", yoff)
+
+hdr = args[4]
+outf = open(args[5], "wb")
+
+dat = [t.strip().split(',') for t in targets] # tabluar representation for csv
+dat = [[x.strip() for x in d] for d in dat] # remove extra whitespace
+
+cc = {} # check csv well formed
+for d in dat:
+    c = len(d)
+    if c != cc: cc[c] = 0
+    cc[c] += 1
+
+print(cc)
+
+if len(cc) != 1:
+    err("csv not accepted")
+
+fields = dat[0]
+print(fields)
+xoi = fields.index("xoff")
+yoi = fields.index("yoff")
+# print("xoi", xoi)
+# print("yoi", yoi)
+
+rowi = fields.index("row")
+lini = fields.index("lin")
+
+xo, yo = [], []
+row, lin = [], []
+for i in range(0, len(dat)):
+    d = dat[i]
+    xo.append(d[xoi])
+    yo.append(d[yoi])
+    row.append(d[rowi])
+    lin.append(d[lini])
+# print(xo)
+# print(yo)
+# print("row", row)
+# print("lin", lin)
+
+ns, nr, nb = read_hdr(hdr)
+ns, nr, nb = int(ns), int(nr), int(nb)
+i_use = []
+for i in range(1, len(dat)):
+    row[i] = 0 if (row[i] == '') else int(row[i])
+    lin[i] = 0 if (lin[i] == '') else int(lin[i])
+    row[i] -= xoff
+    lin[i] -= yoff
+    dat[i][rowi] = row[i]  # correct the data to output
+    dat[i][lini] = lin[i]
+    if row[i] >= 0 and lin[i] >= 0 and row[i] <= nr and lin[i] <= ns:
+        i_use.append(i)
+
+print("selected features:")
+for i in i_use:
+    print(i, dat[i][fields.index("feature_id")])
+
+outf.write((','.join(dat[0])).encode())
+for i in i_use:
+    dat[i] = [str(dat[i][j]) for j in range(0, len(dat[i]))]
+    outf.write(('\n' + (','.join(dat[i]))).encode())
+outf.close()
