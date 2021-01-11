58dd51e10011808785a32fea8b23c6ddc486e9dd
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-01-11 14:40:52 -0800

    update

--- cpp/cut.cpp
+++ cpp/cut.cpp
@@ -27,18 +27,26 @@ int main(int argc, char ** argv){
   hread(hfn, nrow, ncol, nband); // read header
   np = nrow * ncol; // number of input pix
 
-  size_t start_i = (starty * ncol) + startx;
-  size_t end_i = (endy * ncol) + endx;
   size_t nrow2 = endy - starty + 1;
   size_t ncol2 = endx - startx + 1;
   size_t np2 = nrow2 * ncol2;
 
   float * bb = falloc(np); // buffer for one band
+  float * bb2 = falloc(np2); // buffer for one band of output
+
   for0(k, nband){
     printf("band %zu of %zu\n", k + 1, nband);
     size_t fr = fread(bb, sizeof(float), np, f_i); // read a band
     if(fr != np) err("failed to read expected number of floats");
-    size_t fw = fwrite(&bb[start_i], sizeof(float), np2, f_o);
+    
+    for0(i, nrow2){
+     size_t ix = i * ncol2;
+     float * bb_start = &bb[((starty + i) * ncol) + startx];
+      for0(j, ncol2) bb2[ix + j] = bb_start[j];
+    }
+
+
+    size_t fw = fwrite(bb2, sizeof(float), np2, f_o);
     if(fw != np2) err("failed to write expected number of floats"); 
   }
 
