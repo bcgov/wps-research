590968b13549ce5c5ff49eef8b29bd00dada7686
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-01-28 16:56:08 -0800

    update

--- py/kmeans_optimize.py
+++ py/kmeans_optimize.py
@@ -8,6 +8,8 @@
 
 '''
 from misc import *
+from sklean.neighbours import KNeighboursClassifier
+
 infile = "stack.bin" # default input file
 
 if len(args) > 1: infile = args[1]
@@ -234,6 +236,22 @@ while go: # could have turned this into a recursive function!
     # SPLICE THE RESULTS BACK INTO THE CLASS MAP
     # LOOK AT BRAD NEW DATA
 
+    '''
+    >>> X = [[0], [1], [2], [3]]
+>>> y = [0, 0, 1, 1]
+>>> from sklearn.neighbors import KNeighborsClassifier
+>>> neigh = KNeighborsClassifier(n_neighbors=3)
+>>> neigh.fit(X, y)
+KNeighborsClassifier(...)
+>>> print(neigh.predict([[1.1]]))
+[0]
+>>> print(neigh.predict_proba([[0.9]]))
+[[0.66666667 0.33333333]]
+    '''
+
+    neigh = KNeighboursClassifier(n_neighbors = 2)
+    neigh.fit(X, y)
+    
     
 	
 
