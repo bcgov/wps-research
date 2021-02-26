af88acf99c53ebbf72e1f25bcf30bec9c0523076
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-02-25 22:13:37 -0800

    update

--- py/kmeans_optimize.py
+++ py/kmeans_optimize.py
@@ -112,156 +112,142 @@ outputfile2.close()
 print("calculate seed layer..") # should be parallelized in C/C++
 run(cpp_path + "raster_nearest_centre.exe " + infile + " target_mean.dat")
 
-go = True
-iteration = 0
-next_label = K
-print("next_label", next_label)
-good_labels = np.full(nrow*ncol, float("NaN"),dtype=np.float32) #None # will store labels of points that are finally classified, here..
+good_labels = np.full(nrow*ncol, float("NaN"),dtype=np.float32) # will store labels of points that get classified here
+whoami = os.popen("whoami").read().strip() # user name
+class_file = infile + "_kmeans.bin"
+seed_file = infile + "_nearest_centre.bin" # input file for kmeans
+run(cpp_path + "kmeans_iter.exe " + infile + " " + seed_file + " 1. ")
+# next_label += 1 # next iteration would need a higher label if it's reached..
+class_file = seed_file
+ncol, nrow, bands, data = read_binary(class_file) # read class map result from 
 
-while go: # don't forget duality between iteration and recursion
-    whoami = os.popen("whoami").read().strip() # user name
-    class_file = infile + "_kmeans.bin"
-    seed_file = infile + "_nearest_centre.bin"
-    if iteration > 0:
-        seed_file = infile + "_reseed.bin" #  class_file
-    run(cpp_path + "kmeans_iter.exe " + infile + " " + seed_file + " 1. ") 
-    next_label += 1 # next iteration would need a higher label if it's reached..
-    ncol, nrow, bands, data = read_binary(class_file) # read class map result from kmeans
+# calculate the set of kmeans labels associated with each class
+kmeans_label = {}
+for i in range(1, len(lines)): # for each vector point of ours
+    line = lines[i]
+    x, y = int(line[i_row]), int(line[i_lin]) # rowcol coords for the point
+    ix = (y * ncol) + x # print("row", line[i_row], line[i_lin], line[i_xof], line[i_yof], line[i_lab], "class", data[ix])
+    if ix < nrow * ncol:
+        kmeans_label[ix] = data[ix]
 
-    # calculate the set of kmeans labels associated with each class
-    kmeans_label = {}
-    for i in range(1, len(lines)): # for each vector point of ours
-        line = lines[i]
-        x, y = int(line[i_row]), int(line[i_lin]) # rowcol coords for the point
-        ix = (y * ncol) + x # print("row", line[i_row], line[i_lin], line[i_xof], line[i_yof], line[i_lab], "class", data[ix])
-        if ix < nrow * ncol:
-            kmeans_label[ix] = data[ix]
+kmeans_label_by_class = {}
+for p in class_label:
+    L = class_label[p]
+    kmeans_label_by_class[L] = [] if (L not in kmeans_label_by_class) else (kmeans_label_by_class[L])
+    kmeans_label_by_class[L].append(kmeans_label[p])
 
-    kmeans_label_by_class = {}
-    for p in class_label:
-        L = class_label[p]
-        kmeans_label_by_class[L] = [] if (L not in kmeans_label_by_class) else (kmeans_label_by_class[L])
-        kmeans_label_by_class[L].append(kmeans_label[p])
+for L in kmeans_label_by_class: # what would a vectorization for an op like this look like?
+    kmeans_label_by_class[L] = set(kmeans_label_by_class[L])
+print("kmeans_label_by_class", kmeans_label_by_class)
 
-    for L in kmeans_label_by_class: # what would a vectorization for an op like this look like?
-        kmeans_label_by_class[L] = set(kmeans_label_by_class[L])
-    print("kmeans_label_by_class", kmeans_label_by_class)
+'''e.g.:
+kmeans_label_by_class {'fireweeddeciduous': {4.0},
+                       'blowdownlichen': {2.0},
+                       'fireweedgrass': {5.0},
+                       'exposed': {3.0},
+                       'pineburned': {0.0},
+                       'pineburnedfireweed': {1.0}}
+'''
+found = False
+lines = open(infile + "_kmeans.hdr").read()
+lines = [line.strip() for line in lines]
+for i in range(0, len(lines)):
+    line = lines[i]
+    w = line.split()
+    try:
+        if w[0] == "kmeans_label_by_class":
+            found = True
+    except:
+        pass
 
+if not found:
+    open(infile + "_kmeans.hdr", "a").write("kmeans_label_by_class " + str(kmeans_label_by_class))
 
-    '''kmeans_label_by_class {'fireweeddeciduous': {4.0}, 'blowdownlichen': {2.0},
-                              'fireweedgrass': {5.0}, 'exposed': {3.0}, 'pineburned': {0.0},
-                              'pineburnedfireweed': {1.0}}
-    '''
-    found = False
-    lines = open(infile + "_kmeans.hdr").read()
-    lines = [line.strip() for line in lines]
-    for i in range(0, len(lines)):
-        line = lines[i]
-        w = line.split()
-        try:
-            if w[0] == "kmeans_label_by_class":
-                found = True
-        except:
-            pass
+# check if we're done? we're never done!
+bad, empty = False, set()
+confused_labels = set()
+confusion_intersection = []
+for k in kmeans_label_by_class:
+    kk = kmeans_label_by_class[k]
+    for j in kmeans_label_by_class:
+        if k == j:
+            continue
+        kj = kmeans_label_by_class[j]
+        inter = kk.intersection(kj)
+        if inter != empty:
+            bad = True
+            confused_labels.add(k)
+            confused_labels.add(j)
+            confusion_intersection.append(inter)
 
-    if not found:
-        open(infile + "_kmeans.hdr", "a").write("kmeans_label_by_class " + str(kmeans_label_by_class))
+print("confused_labels", confused_labels) # which labels are confused?
+print("labels", list(kmeans_label_by_class.keys()))
+all_labels = set(list(kmeans_label_by_class.keys()))
+non_confused_labels = all_labels.difference(confused_labels)
+print("unconfused labels", non_confused_labels) # which labels aren't confused?
 
-    # check if we're done
-    bad, empty = False, set()
-    confused_labels = set()
-    confusion_intersection = []
-    for k in kmeans_label_by_class:
-        kk = kmeans_label_by_class[k]
-        for j in kmeans_label_by_class:
-            if k == j:
-                continue
-            kj = kmeans_label_by_class[j]
-            inter = kk.intersection(kj)
-            if inter != empty:
-                bad = True
-                confused_labels.add(k)
-                confused_labels.add(j)
-                confusion_intersection.append(inter)
-               
-    print("confused_labels", confused_labels)
-    print("labels", list(kmeans_label_by_class.keys()))
-    all_labels = set(list(kmeans_label_by_class.keys()))
-    non_confused_labels = all_labels.difference(confused_labels)
-    print("unconfused labels", non_confused_labels)
+'''
+  1. store "good" labels to keep (final)... write out good label map.... (next iteration will need to merge with that one!!!!!!)
+  2. for the confused classes, write a new seed file with original seeds PLUS ONE SEED one more
+  3. new iteration should shard off the good stuff (if there is any) and keep on dividing the stuff that isn't good yet..
+ (these were deprecated notes for an iterative version) '''
 
-    '''
-      1. store the "good" labels to keep (final)... write out good label map.... (next iteration will need to merge with that one!!!!!!)
-      2. for the confused classes, write a new seed file with original seeds PLUS ONE SEED one more
-      3. new iteration should shard off the good stuff (if there is any) and keep on dividing the stuff that isn't good yet..
-    '''
+seeds = np.full(nrow*ncol, float("NaN"),dtype=np.float32)  # new seeds will be saved here..i.e., points still need to classify!
+kmeans_labels_good, kmeans_labels_confused = set(), set()
+for L in non_confused_labels:
+    for x in kmeans_label_by_class[L]:
+        kmeans_labels_good.add(x)
 
-    seeds = np.full(nrow*ncol, float("NaN"),dtype=np.float32)  # new seeds will be saved here..
+for L in confused_labels:
+    for x in kmeans_label_by_class[L]:
+        kmeans_labels_confused.add(x)
+print("kmeans_labels_good", kmeans_labels_good)
+print("kmeans_labels_confused", kmeans_labels_confused)
+for i in range(nrow*ncol):
+    if data[i] in kmeans_labels_good:
+        good_labels[i] = data[i]
+    if data[i] in kmeans_labels_confused:
+        seeds[i] = data[i] # points we still need to classify, e.g. by knn
 
-    kmeans_labels_good, kmeans_labels_confused = set(), set()
-    for L in non_confused_labels:
-        for x in kmeans_label_by_class[L]:
-            kmeans_labels_good.add(x)
+print("kmeans_labels_good", kmeans_labels_good)
+print(kmeans_label_by_class)
+write_binary(good_labels, infile + "_good.bin") # relabel the data and output
+write_hdr(infile + "_good.hdr", ncol, nrow, 1)
+good_kmeans_label_by_class = {}
+for label in kmeans_label_by_class:
+    if set(kmeans_label_by_class[label]).intersection(set(kmeans_labels_confused)) != set():
+        pass # confused
+    else:
+        good_kmeans_label_by_class[label] = kmeans_label_by_class[label]
+open(infile + "_good.hdr", "a").write("\nkmeans_label_by_class " + str(kmeans_label_by_class))
 
-    for L in confused_labels:
-        for x in kmeans_label_by_class[L]:
-            kmeans_labels_confused.add(x)
-    print("kmeans_labels_good", kmeans_labels_good)
-    print("kmeans_labels_confused", kmeans_labels_confused)
-    for i in range(nrow*ncol):
-        if data[i] in kmeans_labels_good:
-            good_labels[i] = data[i]
-        if data[i] in kmeans_labels_confused:
-            seeds[i] = data[i]
+print("n_nan", n_nan)
+print("target_ix", target_ix)
+print("target_data")
+for ix in target_data:
+    print(ix, class_label[ix], target_data[ix])
 
-    # put the good stuff on ice, now randomly select centres and crank up the N until something pops off...
-    # each iteration, save the good stuff. and attack the rest again!
-    print("kmeans_labels_good", kmeans_labels_good)
-    print(kmeans_label_by_class)
-    write_binary(good_labels, infile + "_good.bin") # relabel the data and output
-    write_hdr(infile + "_good.hdr", ncol, nrow, 1)
-    good_kmeans_label_by_class = {}
-    for label in kmeans_label_by_class:
-        if set(kmeans_label_by_class[label]).intersection(set(kmeans_labels_confused)) != set():
-            pass # confused
-        else:
-            good_kmeans_label_by_class[label] = kmeans_label_by_class[label]
-    open(infile + "_good.hdr", "a").write("\nkmeans_label_by_class " + str(kmeans_label_by_class))
+print("kmeans_labels_confused", kmeans_labels_confused)
+print("confusion_intersection", confusion_intersection)
+print("kmeans_labels_good", kmeans_labels_good)
+print("kmeans_labels_confused", kmeans_labels_confused)
+print("kmeans_label_by_class", kmeans_label_by_class)
 
-    # write_binary(seeds, infile + "_reseed.bin") # relabel the data and output
-    # write_hdr(infile + "_reseed.hdr", ncol, nrow, 1)
-    print("n_nan", n_nan)
-    # RUN KNN ON DATA WITH CONFUSED LABELS ONLY!!!!
-    # SPLICE THE RESULTS BACK INTO THE CLASS MAP
-    # LOOK AT BRAD NEW DATA
+run(cpp_path + "../py/read_multi.py " + infile +"_kmeans.bin 1")
+run("eog " + infile + "_kmeans.bin.png")
 
-    '''
-    >>> X = [[0], [1], [2], [3]]
-    >>> y = [0, 0, 1, 1]
-    >>> from sklearn.neighbors import KNeighborsClassifier
-    >>> neigh = KNeighborsClassifier(n_neighbors=3)
-    >>> neigh.fit(X, y)
-    KNeighborsClassifier(...)
-    >>> print(neigh.predict([[1.1]]))
-    [0]
-    >>> print(neigh.predict_proba([[0.9]]))
-    [[0.66666667 0.33333333]]'''
+# RUN KNN ON DATA WITH CONFUSED LABELS ONLY!!!! Splice results back into class map (didn't do this yet)
+'''
+>>> X = [[0], [1], [2], [3]]
+>>> y = [0, 0, 1, 1]
+>>> from sklearn.neighbors import KNeighborsClassifier
+>>> neigh = KNeighborsClassifier(n_neighbors=3)
+>>> neigh.fit(X, y)
+KNeighborsClassifier(...)
+>>> print(neigh.predict([[1.1]]))
+[0]
+>>> print(neigh.predict_proba([[0.9]]))
+[[0.66666667 0.33333333]]'''
 
-    print("target_ix", target_ix)
-    print("target_data")
-    for ix in target_data:
-        print(ix, class_label[ix], target_data[ix])
-
-    print("kmeans_labels_confused", kmeans_labels_confused)
-    print("confusion_intersection", confusion_intersection)
-    print("kmeans_labels_good", kmeans_labels_good)
-    print("kmeans_labels_confused", kmeans_labels_confused)
-    print("kmeans_label_by_class", kmeans_label_by_class)
-
-    run(cpp_path + "../py/read_multi.py " + infile +"_kmeans.bin 1")
-    run("eog " + infile + "_kmeans.bin.png")
-    neigh = KNeighborsClassifier(n_neighbors = 2)
-    # neigh.fit(X, y)
-    sys.exit(1)
-
-
+neigh = KNeighborsClassifier(n_neighbors = 2)
+# neigh.fit(X, y)
