44a3a64d0bbc57c03637323a36e8e47fd35c9cdd
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-02-25 22:18:53 -0800

    update

--- py/class_count.py
+++ py/class_count.py
@@ -2,8 +2,7 @@
 from misc import *
 
 if len(args) < 2:
-    err("class_count [input binary file]")
-    #args.append('20190926kamloops_data/WATERSP.tif_project_4x.bin_sub.bin')
+    err("class_count [input binary file]") #args.append('20190926kamloops_data/WATERSP.tif_project_4x.bin_sub.bin')
 
 samples, lines, bands, data = read_binary(args[1])
 
