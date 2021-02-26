3dd21246141aaf7093f91419836019acd221bf0f
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-02-25 22:20:07 -0800

    update

--- py/class_negate.py
+++ py/class_negate.py
@@ -1,12 +1,11 @@
-# 20200610 invert a float-valued binary class map
-# require the values be 0 or 1
-
+# 20200610 invert a float-valued binary class map: require the values be 0 or 1
 from misc import *
 
 if len(args) < 2:
     err("class_negate [input binary file]")
 
 fn = args[1]
+
 samples, lines, bands, data = read_binary(fn)
 
 data = np.array([1. - d for d in data])
