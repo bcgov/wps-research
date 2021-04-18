2bb5880ed83fb0a2aa24e3f4515d279bd1dc53f5
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-04-18 09:19:58 -0700

    update

--- py/find_sentinel2_all.py
+++ py/find_sentinel2_all.py
@@ -9,6 +9,11 @@ lines = lines[1:]
 for line in lines:
     print(line)
 
+    w = line.split(',')
+    foot_print = 'Intersects(' + w[1] + ',' + w[0] + ')'
+    print(foot_print)
+    # foot_print = 'Intersects(51.0602686,-120.9083258)' # default location: Kamloops
+    # VICTORIA: (48.4283334, -123.3647222)
 
 
 
