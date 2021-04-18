179785352066846d757d55e8f39dd07b4e6957af
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-04-18 09:14:22 -0700

    update

--- py/find_sentinel2_all.py
+++ py/find_sentinel2_all.py
@@ -1,9 +1,9 @@
 import os
 import sys
-path = os.path.abspath(__file__) + os.path.sep
-print(path)
+path = os.path.abspath(__file__).split(os.path.sep)
+path = path[:-1] + ['centroids.csv']
 
-lines = open(path + "centroids.csv").readlines()
+lines = open(os.path.sep.join(path)).readlines()
 lines = [x.strip() for x in lines]
 lines = lines[1:]
 for line in lines:
