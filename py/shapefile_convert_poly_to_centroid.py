eb1591455d41c6f42fe855293c619b8f9ff7431d
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-04-18 08:54:18 -0700

    update

--- py/shapefile_convert_poly_to_centroid.py
+++ py/shapefile_convert_poly_to_centroid.py
@@ -1,3 +1,11 @@
+'''
+
+cd path/to/shapefiles
+ogr2ogr -sql "SELECT ST_Centroid(geometry), * FROM countries" -dialect sqlite countries_centroid.shp countries.shp
+
+'''
+
+'''
 # Not tested. from:
 # https://gis.stackexchange.com/questions/216788/convert-polygon-feature-centroid-to-points-using-python
 
@@ -13,4 +21,4 @@ drv = ogr.GetDriverByName('Esri shapefile')
 ds2 = drv.CreateDataSource('countries_centroid.shp')
 ds2.CopyLayer(ly, '')
 ly = ds = ds2 = None  # save, close
-
+''
