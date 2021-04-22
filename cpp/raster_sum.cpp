389c7be2ab6055faa1417b707ebeb46f9c395955
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-04-21 22:43:38 -0700

    update

--- cpp/raster_sum.cpp
+++ cpp/raster_sum.cpp
@@ -1,30 +1,25 @@
-/* band math: add hyperspectral cubes together */
+/* raster_sum.cpp: band math, add hyperspectral cubes together */
 #include"misc.h"
 
 int main(int argc, char ** argv){
-  
   if(argc < 4) err("raster_sum.exe [raster cube 1] [raster cube 2] [output cube]\n");
-  
+
   str fn(argv[1]); // input image file name
-  if(!exists(fn)) err(str("failed to open input file: ") + fn);
+  str fn2(argv[2]); // input image 2 file name
+  if(!(exists(fn) && exists(fn2))) err("failed to open input file");
+
   str hfn(hdr_fn(fn)); // input header file name
-
-  str fn2(argv[2]);
-  if(!exists(fn2)) err(str("failed to open input file: ") + fn2);
-  str hfn2(hdr_fn(fn2)); // input header file name
-
+  str hfn2(hdr_fn(fn2)); // input 2 header file name
   str ofn(argv[3]); // output file name
-  str ohn(hdr_fn(ofn, true)); // output header file name
+  str ohn(hdr_fn(ofn, true)); // out header file name
 
+  size_t nrow, ncol, nband, np, nrow2, ncol2, nband2;
+  hread(hfn, nrow, ncol, nband); // read header 1
+  hread(hfn2, nrow2, ncol2, nband2); // read header 2
+  if(nrow != nrow2 || ncol != ncol2 || nband != nband2)
+    err("input image dimensions should match");
 
-  size_t nrow, ncol, nband, np;
-  hread(hfn, nrow, ncol, nband); // read header
   np = nrow * ncol; // number of input pix
-
-  size_t nrow2, ncol2, nband2;
-  hread(hfn2, nrow2, ncol2, nband2);
-  if(nrow != nrow2 || ncol != ncol2 || nband != nband2) err("input image dimensions should match");
-  
   size_t i, j, k, ix, ij, ik;
   float * out = falloc(nrow * ncol * nband);
   float * dat1 = bread(fn, nrow, ncol, nband);
@@ -45,4 +40,3 @@ int main(int argc, char ** argv){
   hwrite(ohn, nrow, ncol, nband);
   return 0;
 }
-
