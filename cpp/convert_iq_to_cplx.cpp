266461d8b4e597cc31f828bbf974db6cc23fc000
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-05-27 23:24:01 -0700

    update

---
+++ cpp/convert_iq_to_cplx.cpp
@@ -0,0 +1,36 @@
+// 20210514 convert iq format data to PolSARPro complex format (one complex band envi type 6)
+#include"misc.h"
+
+int main(int argc, char ** argv){
+  if(argc < 4) err("Convert iq format data to PolSARPro binary format (envi type 6)\n convert_iq_to_cplx [i file] [q file] [output file]");
+  str a(argv[0]);
+  str b(argv[1]);
+
+  size_t nr, nc, nb, np;
+  float * d_i = bread(a, nr, nc, nb); // read file
+  float * d_q = bread(b, nr, nc, nb); // read file
+  np = nr * nc;
+
+  float * dd = falloc(np * 2); // allocate floats
+
+  size_t j, j2;
+  for0(j, np){
+    j2 = 2 * j;
+    dd[j2] = d_i[j];
+    dd[j2 + 1] = d_q[j];
+  }
+
+  FILE * f = fopen(argv[2], "wb");
+  if(!f) err("failed to open output file");
+
+  size_t nw = fwrite(dd, sizeof(float), np * 2, f);
+  if(nw != np * 2) err("unexpected write size");
+
+  free(d_i);
+  free(d_q);
+  free(dd);
+
+  str c(str(argv[2]) + str(".hdr"));
+  hwrite(c, nr, nc, nb, 6);
+  return 0;
+}
