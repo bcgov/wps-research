81dfc748ee59c85dc1c122979c4c0ab11599d934
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-06-15 13:57:03 -0700

    update

--- data_tru/read.py
+++ data_tru/read.py
@@ -4,18 +4,27 @@ import pickle
 import matplotlib.pyplot as plt
 
 sep = os.path.sep
+
+# load supervised result
+supervised = {}
 bins = [x.strip() for x in os.popen("ls -1 brad/*.bin").readlines()]
 
 for b in bins:
     print("+r " + b)
     pfn = b.split(sep)[-1][:-9] + '.png'
     X = pickle.load(open(b, 'rb'))
+    supervised[pfn[:-4]] = X
     if not os.path.exists(pfn):
         plt.imshow(X)
         plt.title(pfn[:-4])
         plt.tight_layout()
         print("+w " + pfn)
         plt.savefig(pfn)
+
+
+print(supervised.keys())
+print(len(supervised.keys()))
+# load unsupervised result
 
 '''
 c = {}
