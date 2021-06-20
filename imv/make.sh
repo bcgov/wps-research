d6ee2a35e8dc6f638372496a19c220181d8df0a7
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-06-19 23:34:54 -0700

    update

--- imv/make.sh
+++ imv/make.sh
@@ -4,4 +4,4 @@ g++ 	-c -w  -O4 -fno-sched-spec 	my_math.cpp  &
 g++ 	-c -w  -O4 -fno-sched-spec 	image.cpp  &
 g++ 	-c -w  -O4 -fno-sched-spec 	util.cpp  &
 wait
-g++ 	imv.o newzpr.o my_math.o	image.o	util.o	-o	imv.exe  -lm -lpthread  -lGL -lGLU -lglut 
+g++ -O4 imv.o newzpr.o my_math.o	image.o	util.o	-o	imv.exe  -lm -lpthread  -lGL -lGLU -lglut 
