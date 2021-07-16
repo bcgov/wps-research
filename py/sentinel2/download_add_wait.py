6f01cd4d5f062c7452cfd065144d7106bff928c7
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-07-15 21:42:06 -0700

    update

--- py/sentinel2/download_add_wait.py
+++ py/sentinel2/download_add_wait.py
@@ -6,8 +6,9 @@ add a wait interval between downloads / polls
 was running this on .sentinel2_downloads.sh, followed by work_queue.py with two threads as two simultaneous downloads are allowed from copernicus'''
 import os
 import sys
+args = sys.argv
 
-lines = open("download.sh").readlines()
+lines = open("download.sh" if len(args) < 1 else args[1]).readlines()
 lines = [x.strip() for x in lines]
 
 for line in lines:
