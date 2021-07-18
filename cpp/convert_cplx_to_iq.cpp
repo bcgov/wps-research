974907ed6db0e8d47345a486bc9425407f76fdc4
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-07-17 22:34:03 -0700

    update

--- cpp/convert_cplx_to_iq.cpp
+++ cpp/convert_cplx_to_iq.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int main(int argc, char ** argv){
-  if(argc < 4) error("convert_cplx_to_iq.cpp: convert a complex channel (ENVI type 6) into real(i) and imag(q) components, implemented 20210717. \n\tuse: convert_cplx_to_iq [nrow] [ncol] [ch] \n\tNote: config.txt file must be present in input directory\n");
+  if(argc < 4) err("convert_cplx_to_iq.cpp: convert a complex channel (ENVI type 6) into real(i) and imag(q) components, implemented 20210717. \n\tuse: convert_cplx_to_iq [nrow] [ncol] [ch] \n\tNote: config.txt file must be present in input directory\n");
   
   size_t sf = sizeof(float);
   int nrow, ncol, row, col, i, j, k, ind;
@@ -20,19 +20,21 @@ int main(int argc, char ** argv){
   str ofn2(str(ifn) + str("_q.bin"));
   float a, b, c, d;
 
-  FILE * if1 = ropen(infn);
+  FILE * if1 = ropen(ifn);
   FILE * of1 = wopen(ofn1);
   FILE * of2 = wopen(ofn2);
   
   for0(row, nrow){
     if(nrow % 100 ==0) printf("\rProcessing row %d of %d ", row + 1, nrow);
     for0(col, ncol){
-      fread( &a, sf, 1, infile); fread( &b, sf, 1, infile);
-      fwrite(&a, sf, 1,    of1); fwrite(&b, sf, 1,    of2);
+      fread( &a, sf, 1, if1);
+      fread( &b, sf, 1, if1);
+      fwrite(&a, sf, 1, of1);
+      fwrite(&b, sf, 1, of2);
     }
   }
   printf("\r");
-  write_envi_hdr(str(ifn) + str("_i.hdr"), nrow, ncol);
-  write_envi_hdr(str(ifn) + str("_q.hdr"), nrow, ncol);
+  hwrite(str(ifn) + str("_i.hdr"), nrow, ncol, 1);
+  hwrite(str(ifn) + str("_q.hdr"), nrow, ncol, 1);
   return 0;
 }
