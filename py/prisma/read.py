413d2b0eb15567c2b13a47b244ee31bee1b28f6e
ashlinrichardson <richardson.ashlin@gmail.com>
2021-07-24 19:33:15 -0700

    update

--- py/prisma/read.py
+++ py/prisma/read.py
@@ -4,17 +4,42 @@ import h5py
 import sys
 import os
 
-filename = sys.argv[1]
+def err(m):
+    print("Error: " + str(m)); sys.exit(1)
 
-def iterate(x, s=""):
-    print(s,x)
+def write_hdr(hfn, samples, lines, bands):
+    lines = ['ENVI',
+             'samples = ' + str(samples),
+             'lines = ' + str(lines),
+             'bands = ' + str(bands),
+             'header offset = 0',
+             'file type = ENVI Standard',
+             'data type = 4',
+             'interleave = bsq',
+             'byte order = 0']
+    open(hfn, 'wb').write('\n'.join(lines).encode())
+
+filename = sys.argv[1]
+if filename[-3:] != 'he5':
+    err("unexpected filename")
+fn_base = filename[:-4]
+print(fn_base)
+
+datasets = []
+data_sets = {}
+
+def iterate(x, s="", parent=None):
+    #print(s,x)
     keys = None
     try:
         keys = x.keys()
         for k in keys:
-            iterate(x[k], s + "**")
+            iterate(x[k], s + "**", parent=x)
     except:
-        pass
+        if len(str(x).split(" dataset ")) > 1:
+            datasets.append([x, parent])
+            dsn = str(x).strip().split(':')[0].split('"')[1].strip('"')
+            data_sets[dsn] = [x, parent]
 
     #if keys is None:
     #    print(s, x)
@@ -22,7 +47,55 @@ def iterate(x, s=""):
 with h5py.File(filename, "r") as f:
     # List all groups
     iterate(f)
-    
+    print("available data sets:\n", list(data_sets.keys()))
+    # swaths = f['HDFEOS']['SWATHS']
+    want = ['SWIR_Cube', 'VNIR_Cube', 'Latitude', 'Longitude', 'Cw_Swir_Matrix', 'Cw_Vnir_Matrix', 'Fwhm_Swir_Matrix', 'Fwhm_Vnir_Matrix']
+
+    for w in want:
+        if w not in data_sets:
+            err("key not found: " + str(w))
+
+    for dsn in want:  # data set name
+        w = data_sets[dsn]
+        dsp = str(w[1]).strip().split('"')[1] + '/' + dsn
+        dsp = dsp.strip().strip('/').split('/')
+        print("dsp", dsp) # SWIR_Cube ['HDFEOS', 'SWATHS', 'PRS_L2D_HCO', 'Data Fields', 'SWIR_Cube']
+        x = f
+        for i in dsp: # f['HDFEOS']['SWATHS']['PRS_L1_HCO']['Data Fields']['SWIR_Cube'][()]
+            x = x[i]
+        x = x[()]
+        data = np.array(x)
+        N = len(data.shape) # how many dimensions? 3 is cube. 2 is 1-band..
+        fn = fn_base + '_' + (dsn.replace(' ', '_')) + '.bin'
+        hn = fn[:-4] + '.hdr'
+        print('+w', fn)
+        # print('\t', dsn, dsp, data.shape)
+        o_f = open(fn, 'wb')
+        
+        dt = '>f4' # default data type to write! always float32, byte order 0
+        if N == 3:
+            nband = data.shape[1]
+            nrow, ncol = data[:,0,:].shape
+            for i in range(nband):
+                print("\twriting band", str(i + 1))
+                data[:,i,:].astype(np.float32).tofile(o_f, '', dt)
+        elif N == 2:
+            nrow, ncol = data.shape
+            data.astype(np.float32).tofile(o_f, '', dt)
+        else:
+            err('unexpected dimensions')
+        if N == 3:
+            print("\tclosing file..")
+        o_f.close()
+
+        print('+w',hn)
+
+
+    '''
+
+
+    sys.exit(1)
+
     y = np.array(f['HDFEOS']['SWATHS']['PRS_L1_HCO']['Data Fields']['SWIR_Cube'][()])
     nband = y.shape[1]
     nrow, ncol = y[:,0,:].shape
@@ -34,7 +107,7 @@ with h5py.File(filename, "r") as f:
         print(i)
         y[:,i,:].astype(np.float32).tofile(o_f, '', dt)
     o_f.close()
-
+    '''
 
     #print(f['Info']['Header']['FrameNumber']) # keys())
 '''
