1917cdc7b31fe6ee650f37fe18fb5c7c07f74181
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-10-19 10:29:26 -0700

    update

--- py/find_sentinel2_all.py
+++ py/find_sentinel2_all.py
@@ -1,28 +1,16 @@
+# centroid.csv has the list of TILE ID with centroid coords incl.
 import os
 import sys
-path = os.path.abspath(__file__).split(os.path.sep)
-path = path[:-1]
-print(path)
+path = os.path.abspath(__file__).split(os.path.sep)[:-1]
 path = os.path.sep.join(path) + os.path.sep
+print(path)
 
-lines = open(path + 'centroid.csv').readlines()
-lines = [x.strip() for x in lines]
-lines = lines[1:]
-
+lines = [x.strip() for x in open(path + 'centroid.csv').readlines()][1:]
 for i in range(0, len(lines)):
-    line = lines[i]
-    w = line.split(',')
+    w = lines[i].split(',')
     foot_print = 'Intersects(' + w[1] + ',' + w[0] + ')'
     print(foot_print)
 
     fpifn = '.foot_print_' + str(i)
-
     open(fpifn, 'wb').write(foot_print.encode())
-
     a = os.system("python3 " + path + "find_sentinel2.py " + fpifn) 
-
-    # foot_print = 'Intersects(51.0602686,-120.9083258)' # default location: Kamloops
-    # VICTORIA: (48.4283334, -123.3647222)
-
-
-
