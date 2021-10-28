d5be67a12b199de6fb670ee9c08e9dba6ccc0049
ashlinrichardson <richardson.ashlin@gmail.com>
2021-10-27 20:35:10 -0700

    update

--- rcm/install_jpy.sh
+++ rcm/install_jpy.sh
@@ -1,10 +1,11 @@
-# tested on Ubuntu 20
+# tested on Ubuntu 20 with ESA SNAP 8.0 which demands java 8
 tar xvf jpy-0.9.0.tar.gz
-sudo apt install openjdk-17-jre openjdk-17-jdk python3-pip maven
+sudo apt install openjdk-8-jre openjdk-8-jdk python3-pip maven
 
 # might need to insert these in .bashrc
-export JDK_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
+export JDK_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export JAVA_HOME=$JDK_HOME
 
 cd jpy-0.9.0
 python3 setup.py --maven bdist_wheel
+
