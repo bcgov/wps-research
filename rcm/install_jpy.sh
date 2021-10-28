fa9b1c1e9543b86ada613c8aae65ef7c2d92588f
ashlinrichardson <richardson.ashlin@gmail.com>
2021-10-27 20:26:43 -0700

    update

--- rcm/install_jpy.sh
+++ rcm/install_jpy.sh
@@ -1,9 +1,9 @@
 # tested on Ubuntu 20
 tar xvf jpy-0.9.0.tar.gz
-sudo apt install openjdk-17-jre openjdk-17-jdk python3-pip
+sudo apt install openjdk-17-jre openjdk-17-jdk python3-pip maven
 
 # might need to insert these in .bashrc
-export JDK_HOME=/usr/lib/jvm/java-17-openjdk-amd64/bin/
+export JDK_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
 export JAVA_HOME=$JDK_HOME
 
 cd jpy-0.9.0
