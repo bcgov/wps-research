01e4e4e5e65220ea4e1c2a1f86dcd5f91ca7b916
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-11-16 12:41:07 -0800

    update

--- py/find_sentinel2_all.py
+++ py/find_sentinel2_all.py
@@ -1,10 +1,13 @@
 '''centroid.csv has the list of TILE ID with centroid coords incl.
 
 Write the centroid coordinates to "footprint" files and run find_sentinel2.py
-on each footprint (actually a centroid but whatever)
-'''
+on each footprint (actually a centroid but whatever)'''
 import os
 import sys
+args = sys.argv
+print("args", [args])
+labels = args[1:]
+print("labels", [labels])
 path = os.path.abspath(__file__).split(os.path.sep)[:-1]
 path = os.path.sep.join(path) + os.path.sep
 print(path)
@@ -12,9 +15,19 @@ print(path)
 lines = [x.strip() for x in open(path + 'centroid.csv').readlines()][1:]
 for i in range(0, len(lines)):
     w = lines[i].split(',')
+    label = w[2].strip('"')
+    
+    if len(labels) > 0:  # selection mode
+        if label not in labels:
+            continue
+    
+    print(w)
+    
     foot_print = 'Intersects(' + w[1] + ',' + w[0] + ')'
     print(foot_print)
 
     fpifn = '.foot_print_' + str(i)
     open(fpifn, 'wb').write(foot_print.encode())
-    a = os.system("python3 " + path + "find_sentinel2.py " + fpifn) 
+    cmd = "python3 " + path + "find_sentinel2.py " + fpifn
+    print(cmd)
+    a = os.system(cmd)
