00f5c448957ebee660e2049b9b63d68d269bd14c
ashlinrichardson <richardson.ashlin@gmail.com>
2021-11-21 22:20:20 -0800

    update

--- py/raster_reorder_increasing_nm.py
+++ py/raster_reorder_increasing_nm.py
@@ -1,3 +1,49 @@
 '''in-place/overwrite reordering of raster bands based on wavelength..
 ..only reorder the fields that are in nm (but put them first)'''
+from misc import *
+args = sys.argv
+sep = os.path.sep
+pd = sep.join(__file__.split(sep)[:-1]) + sep
 
+# instructions to run
+if len(args) < 2:
+    err('usage:\n\traster_reorder_increasing_nm.py [input file]')
+
+# check file and header exist
+fn, hdr = args[1], hdr_fn(args[1])
+assert_exists(fn)
+
+ncol, nrow, nband = [int(x) for x in read_hdr(hdr)]  # read image dims
+bn = band_names(hdr)  # read the band names
+
+# find the band-name indices that have a space-separated field, ending with nm
+bni = []
+to_sort = []
+for i in range(len(bn)):  # band name indices
+    spectral_value = None
+    w = bn[i].strip().split()
+    for j in range(len(w)):
+        if w[j][-2:] == 'nm':
+            spectral_value = float(w[j][:-2])
+            print(spectral_value)
+            to_sort.append([spectral_value, i])
+to_sort.sort(reverse=False)
+for s in to_sort:
+    print(s)
+
+lookup = {}
+for si in range(len(to_sort)):
+    s = to_sort[si]
+    lookup[s[1]] = si
+
+for s in lookup:
+    print(s, '->', lookup[s])
+
+s = ''
+for i in range(len(bn)):
+    s += (str(lookup[i]) + ' ')
+s = s.strip()
+print(s)
+
+
+sys.exit(1)
