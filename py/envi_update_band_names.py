932902cd5de3742c1648f25504072329c420cb61
ashlinrichardson <richardson.ashlin@gmail.com>
2021-11-21 21:43:05 -0800

    update

--- py/envi_update_band_names.py
+++ py/envi_update_band_names.py
@@ -18,7 +18,7 @@ if len(args) < 3:
     err("envi_update_band_names.py [.hdr file with band names to use] " +
         "[.hdr file with band names to overwrite]")
 
-def get_band_names_lines(data):
+def get_band_names_line_idx(data):
     band_name_lines, in_band_names = [], False
     lines = [x.strip() for x in data.strip().split("\n")]
     for i in range(0, len(lines)):
@@ -38,7 +38,7 @@ run('python3 ' + pd + 'envi_header_cleanup.py ' + args[1])
 run('python3 ' + pd + 'envi_header_cleanup.py ' + args[2]) # should really call directly, whatever
 
 i_dat, o_dat = open(args[1]).read(),  open(args[2]).read()
-bn_1, bn_2 = get_band_names_lines(i_dat), get_band_names_lines(o_dat)
+bn_1, bn_2 = get_band_names_line_idx(i_dat), get_band_names_line_idx(o_dat)
 lines = o_dat.strip().split('\n')
 
 for i in range(0, len(lines)):
