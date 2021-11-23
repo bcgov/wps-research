7da4cbf8f1e7e2b52ec7e74c080f04ed03728d6c
ashlinrichardson <richardson.ashlin@gmail.com>
2021-11-23 12:41:42 -0800

    update

---
+++ cpp/raster_zero_to_nan.cpp
@@ -0,0 +1,39 @@
+/* replace all-zero pixels in raster, with NAN*/
+#include"misc.h"
+int main(int argc, char ** argv){
+  if(argc < 2) err("raster_zero_to_nan.exe [input binary file]\n");
+ 
+  str fn(argv[1]); // input file name
+  str hfn(hdr_fn(fn)); // auto-detect header file name
+  size_t nrow, ncol, nband, np, i, j, k;
+  hread(hfn, nrow, ncol, nband); // read header
+  np = nrow * ncol; // number of input pix
+
+  float * dat = bread(fn, nrow, ncol, nband); // load floats to array
+
+  size_t ix;
+  float d;
+  for0(i, nrow){
+    for0(j, ncol){
+      ix = i * ncol + j;
+      bool all_zero = true
+      for0(k, nband){
+        if(dat[np * k + ix] != 0.){
+	  all_zero = false;
+	}
+      }
+      if(all_zero){
+        dat[np * k + ix] = NAN;
+      }
+    }
+  }
+
+  cout << "+w " << fn << endl;
+  FILE * f = fopen(fn.c_str(), "wb");
+  if(!f) err("failed to open output file");
+  fwrite(dat, sizeof(float) * nrow * ncol * nband, 1, f); // write data
+
+  fclose(f);
+  free(dat);
+  return 0;
+}
