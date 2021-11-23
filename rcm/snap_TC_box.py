7cfe069d8e2b9e087f6bae86fdcb75c3c4e29218
ashlinrichardson <richardson.ashlin@gmail.com>
2021-11-23 01:12:20 -0800

    update

---
+++ rcm/snap_TC_box.py
@@ -0,0 +1,60 @@
+import os
+import sys
+sep = os.path.sep
+exist = os.path.exists
+
+def err(m):
+    print("Error: " + m); sys.exit(1)
+
+def run(c):
+    print(c)
+    a = os.system(c)
+    if a != 0:
+        err('command failed: ' + c)
+
+folders = [x.strip() for x in os.popen("ls -1 -d *SLC").readlines()]
+snap = '/home/' + os.popen('whoami').read().strip() + '/snap/bin/gpt' # assume we installed snap
+ci = 1
+
+for p in folders:
+    print('*** processing ' + str(ci) + ' of ' + str(len(folders)))
+    if os.path.abspath(p) == os.path.abspath('.'): continue
+    
+    in_1 = p + sep + 'manifest.safe'  # input
+    in_2 = p + sep + 'tc.dim'  # terrain corrected output
+    c1 = ' '.join([snap,
+                  'Terrain-Correction',
+                  '-PoutputComplex=true',
+                  '-PnodataValueAtSea=false', # '-PsaveLayoverShadowMask=true',
+                  '-PimgResamplingMethod="NEAREST_NEIGHBOUR"', # why? because we will add an index for reverse geocoding...
+                  in_1, '-t ' + in_2])
+    
+    in_3 = p + sep + 'b7.dim'  # box filtered output # how to get parameters: ./gpt Polarimetric-Speckle-Filter -h
+    c2 = ' '.join([snap,
+                  'Polarimetric-Speckle-Filter',
+                  '-Pfilter="Box Car Filter"',
+                  '-PfilterSize=7',
+                  in_2, '-t', in_3])  # -t is for output file
+    
+    if not exist(in_2): run(c1)
+    if not exist(in_3): run(c2)
+    r_f, r_h = p + '_rgb.bin', p + '_rgb.hdr'
+    hf = in_3[:-3] + 'data' + sep + 'T11.hdr'
+    if not exist(r_h): run('cp ' + hf + ' ' + r_h)
+
+    dat = open(r_h).read().strip()
+    dat = dat.replace("bands = 1", "bands = 3")
+    dat = dat.replace("byte order = 1", "byte order = 0")
+    dat = dat.replace("band names = { T11 }", "band names = {red, \ngreen,\nblue}")
+    open(r_h, 'wb').write(dat.encode())  # write revised header
+
+    if not exist(r_f):
+        c, t = 'cat', in_3[:-3] + 'data' + sep + 'T'
+        for i in ['22.bin', '33.bin', '11.bin']:
+            ti = t + i
+            if not exist(ti):
+                run('sbo ' + (ti[:-3] + 'img') + ' ' + ti + ' 4')
+            c += (' ' + t + i)
+        c += ' > ' + r_f 
+        run(c)
+    ci += 1
---
+++ rcm/snap_mf3cc.py
@@ -0,0 +1,91 @@
+'''assuming we are inside a .dim folder from snap for compact-pol data..
+
+apply Dey et al [1] mf3cc compact-pol decom'''
+import os
+import sys
+args = sys.argv
+sep = os.path.sep
+
+def err(m):
+    print("Error: " + m); sys.exit(1)
+
+def run(c):
+    print(c); a = os.system(c)
+    if a != 0: err("command failed:\n\t" + c)
+
+print("snap_m3fcc # optional param: window size: default 9 # needs to be run from inside a .dim folder for compact-pol data")
+nwin = 9
+if len(args) > 1: nwin = int(args[1])
+
+''' input files:'''
+ins = ['i_RCH2.img', 'i_RCH.img', 'q_RCH2.img', 'q_RCH.img']
+for i in ins:
+    if not os.path.exists(i): err("req'd input file: " + i)
+if not os.path.exists("i_RCH.hdr"): err("req'd input file: i_RCH.hdr")
+
+# swap byte order from european to american convention
+run("snap2psp ./")
+
+'''output files from snap2psp'''
+bins = ['i_RCH2.bin',  'i_RCH.bin', 'q_RCH2.bin', 'q_RCH.bin']
+
+# get image dims, will need this to generate C2 matrix cf [2]
+nrow, ncol = None, None
+lines = [x.strip() for x in open('i_RCH.hdr').readlines()]
+for line in lines:
+    w = [x.strip() for x in line.split("=")]
+    if w[0] == 'samples': ncol = int(w[1])
+    if w[0] == 'lines': nrow = int(w[1])
+print("nrow", nrow, "ncol", ncol)
+
+run('convert_iq_to_cplx i_RCH.bin q_RCH.bin ch.bin') # convert to envi type6 data
+run('convert_iq_to_cplx i_RCH2.bin q_RCH2.bin cv.bin')
+
+run(' '.join(['cp_2_t2', str(nrow), str(ncol), 'ch.bin', 'cv.bin'])) # convert to C2 mtx cf [1]
+
+run('mv T11.bin C11.bin; mv T22.bin C22.bin') # use expected filenames
+run('mv T12_real.bin C12_real.bin; mv T12_imag.bin C12_imag.bin')
+
+run('eh2cfg ch.bin.hdr') # make sure there is config.txt
+run('mf3cc ./ ' + str(nwin)) # run the decom
+
+run('cat Pd_CP.bin Pv_CP.bin Ps_CP.bin Theta_CP.bin > stack.bin') # stack things
+
+lines = ['ENVI', # write a header for the stack
+         'samples = ' + str(ncol),
+         'lines = ' + str(nrow),
+         'bands = 4',
+         'header offset = 0',
+         'file type = ENVI Standard',
+         'data type = 4',
+         'interleave = bsq',
+         'byte order = 0',
+         'band names = {Pd_CP.bin,',
+         'Pv_CP.bin,',
+         'Ps_CP.bin,',
+         'Theta_CP.bin}']
+open('stack.hdr', 'wb').write(('\n'.join(lines)).encode())
+
+# clean up some intermediary files
+d = ['C11.bin',
+     'C12_imag.bin',
+     'C12_real.bin',
+     'C22.bin',
+     'ch.bin',
+     'cv.bin',
+     'i_RCH2.bin',
+     'i_RCH.bin',
+     'Pd_CP.bin',
+     'Ps_CP.bin',
+     'Pv_CP.bin',
+     'q_RCH2.bin',
+     'q_RCH.bin',
+     'Theta_CP.bin']
+
+for i in d:
+    run('rm ' + i)
+
+'''
+[1] Dey et al S. Dey, A. Bhattacharya, D. Ratha, D. Mandal and A. C. Frery, "Target Characterization and Scattering Power Decomposition for Full and Compact Polarimetric SAR Data," in IEEE Transactions on Geoscience and Remote Sensing, doi: https://doi.org/10.1109/TGRS.2020.3010840.
+
+[2] Cloude et al "Compact Decomposition Theory" IEEE GRSL 2011'''
