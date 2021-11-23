851fdc64df8c80084ea23ae0019444fc8b0dd1ee
ashlinrichardson <richardson.ashlin@gmail.com>
2021-11-23 10:18:24 -0800

    update

--- py/raster_reorder_increasing_nm.py
+++ py/raster_reorder_increasing_nm.py
@@ -18,51 +18,44 @@ ncol, nrow, nband = [int(x) for x in read_hdr(hdr)]  # read image dims
 bn = band_names(hdr)  # read the band names
 
 # find the band-name indices that have a space-separated field, ending with nm
-bni = []
-to_sort = []
-sv = []
+bni, to_sort, sv = [], [], []
 for i in range(len(bn)):  # band name indices
     spectral_value = None
     w = bn[i].strip().strip(',').strip('}').split()
-    print(w)
+    # print(w)
     for j in range(len(w)):
         if w[j][-2:] == 'nm':
             spectral_value = float(w[j][:-2])
-            print(spectral_value)
+            # print(spectral_value)
             to_sort.append([spectral_value, i])
     sv.append(spectral_value)
 
 to_sort.sort(reverse=False)
-print("to_sort", to_sort)
+# print("to_sort", to_sort)
 
 lookup = {i:to_sort[i][1] for i in range(len(to_sort))}
 '''
 for si in range(len(to_sort)):
     s = to_sort[si]
-    lookup[s[1]] = si 
-'''
-print("new thing in a position", lookup)
-
-for si in range(nband):
-    print(si, '->', lookup[si])
+    lookup[s[1]] = si '''
+# print("new thing in a position", lookup)
+# for si in range(nband):
+#    print(si, '->', lookup[si])
 
 s = ''
 for i in range(len(bn)):
     s += (str(lookup[i] + 1) + ' ')  # haha we used 1-idxing
 s = s.strip()
-print(s)
+# print(s)
 
 # now reorder the input file, to a temp file..
-ofn = fn + '_reorder.bin'
-ofh = fn + '_reorder.hdr'
-
+ofn, ofh = fn + '_reorder.bin', fn + '_reorder.hdr'
 run(' '.join(['python3',
               pd + 'raster_shuffle_bands.py',
               fn,
               ofn,
               s]))
-
 # now overwrite the input file, and the input header file, with the created files!
-#shutil.move(ofn_tmp, fn)
-#shutil.move(ofhn_tmp, hdr)
+shutil.move(ofn, fn)
+shutil.move(ofh, hdr)
 print('done')
