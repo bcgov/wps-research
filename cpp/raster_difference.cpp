91978466b7a88a93c545c8e51ed5fcedc55dd5f2
ashlinrichardson <richardson.ashlin@gmail.com>
2021-11-24 15:01:14 -0800

    update

--- cpp/raster_difference.cpp
+++ cpp/raster_difference.cpp
@@ -1,5 +1,5 @@
 /* raster_difference.cpp: given a parameter N (bands per date)
- assuming within-date bands are in order, difference a stack band by band :
+assuming within-date bands are in order, difference a stack band by band :
 
 Supposing M bands of N bands per date, there are M/N dates..
 
@@ -8,45 +8,51 @@ Supposing M bands of N bands per date, there are M/N dates..
 
 int main(int argc, char ** argv){
   if(argc < 3){
-    err("raster_difference.exe [multidate raster cube] [number of bands per date]\n");
+    err("raster_difference.exe [multidate cube] [# of bands per date]\n");
   }
 
-  size_t M = atoi(argv[2]);  // bands per date
-  printf("bands per date: %d\n", M);
+  size_t M = atoi(argv[2]); // bands per date
+  printf("bands per date: %zu\n", M);
 
   str fn(argv[1]); // input image file name
   if(!(exists(fn))) err("failed to open input file");
   str hfn(hdr_fn(fn)); // input header file name
-  
+
   str ofn(str(argv[1]) + str("_difference.bin")); // output file name
   str ohn(hdr_fn(ofn, true)); // out header file name
-  size_t nrow, ncol, nband, np;
+
+  printf("+w %s\n", ofn.c_str());
+  printf("+w %s\n", ohn.c_str());
+  
+  size_t nrow, ncol, nband, np, n_dates;
   hread(hfn, nrow, ncol, nband); // read header 1
+  n_dates = nband / M;
 
   np = nrow * ncol; // number of input pix
-  size_t i, j, k, ix, ij, ik;
+  size_t n, i, j, K, k1, k2, ix, ij, ik;
   float * out = falloc(nrow * ncol * nband);
-  float * dat1 = bread(fn, nrow, ncol, nband);
- 
-  printf("number of dates: %d\n", nband/M);
+  float * dat = bread(fn, nrow, ncol, nband);
+  printf("number of dates: %zu\n", nband/M);
 
-  size_t N, K;
-  for0(N, ((nband / M) - 1)){
-     cout << N << endl; // subtract date N from date N+1
-     for0(k, M){
-	     for0(i, nrow){
-	     for0(j, ncol){
+  for0(n, (n_dates - 1)){
+    printf("di=%zu\n", n);
+    // subtract date N+1 minus date N
+    for0(K, M){
+      // start idx of bands to subtract
+      k1 = (n * M + K) * np;
+      k2 = (M * np) + k1;
 
-			     // add on N * M to get this date's k...
-			     // bands per date
-
-			     // add on M to get the next date!
-		     }
-	     }
-     }
+      // create one product band
+      for0(i, nrow){
+        ix = i * ncol;
+        for0(j, ncol){
+          ij = ix + j;
+          out[k1 + ij] = dat[k2 + ij] - dat[k1 + ij];
+        }
+      }
+    }
   }
+  bwrite(out, ofn, nrow, ncol, nband - M);
+  hwrite(ohn, nrow, ncol, nband - M);
   return 0;
 }
-
-//  bwrite(out, ofn, nrow, ncol, nband);
-//  hwrite(ohn, nrow, ncol, nband);
