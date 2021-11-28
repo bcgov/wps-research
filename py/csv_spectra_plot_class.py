a7e00b11ce1a2f952d5e883439df9eb2a2f71d83
ashlinrichardson <richardson.ashlin@gmail.com>
2021-11-28 00:40:09 -0800

    update

--- py/csv_spectra_plot_class.py
+++ py/csv_spectra_plot_class.py
@@ -6,8 +6,19 @@ input: field label of class..
 import os
 import sys
 import csv
+import matplotlib
+import matplotlib.pyplot as plt
 args = sys.argv
 
+'''this should go in the misc.py'''
+mcolors = matplotlib.colors
+colors = list(mcolors.BASE_COLORS.keys()) + list(mcolors.TABLEAU_COLORS.keys())
+colors = colors[0:7] + colors[8:]
+markers = [".", ",", "o", "v", "^", "<", ">", "1", "2", "3", "4", "8", "s",
+           "p", "P", "*", "h", "H", "+", "x", "X", "D", "d", "|", "_", 0, 1,
+           2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+'''this should go in the misc.py'''
 def read_csv(f):
     data, i = [], 0
     reader = csv.reader(open(f),
@@ -28,7 +39,7 @@ def read_csv(f):
     fields = [x.strip().replace(' ', '_') for x in fields] # spaces are always bad!
     return fields, data
 
-
+'''read the csv and locate the spectra'''
 fields, data = read_csv(args[1])
 nf = len(fields)  # number of fields
 f_i = {fields[i]:i for i in range(nf)}
@@ -39,5 +50,39 @@ fi = f_i[args[2]]  # col index of selected field for legending
 
 spec_fi = []
 for i in range(nf):
-    
+    if fields[i][-2:] == 'nm':
+        spec_fi += [i]
+print('spectra col-ix', spec_fi)
+print('number of cols', len(spec_fi))
+
+'''before we plot, code the categorical possibilities from 0 to however many there are'''
+N = len(data[0]) # number of data points
+print("number of data points", N)
+lookup, next_i = {}, 0
+for i in range(N):
+    value = data[fi][i]
+    if value not in lookup:
+        lookup[value] = next_i
+        next_i += 1
+values = lookup.keys()
+reverse_lookup = {lookup[x]: x for x in values}
+print("lookup", lookup)   # take categorical value and encode it as int 0,1,..
+print("revers", reverse_lookup)  # recover original value from int code!
+plt.figure(figsize=(8*1.5,6*1.5))
+plt.title("Spectra aggregated by categorical field: " + args[2])
+plt.ylabel("Digital number")
+plt.xlabel("Date, resolution(m) and Frequency (nm)")
+
+x = range(N)
+for i in range(N):
+    value = data[fi][i] # categorical value
+    spectrum = [data[i][j] for j in spec_fi]
+    plt.plot(x, spectrum,
+             marker=markers[lookup[value]],
+             color=colors[lookup[value]],
+             label=value)
+    # don't forget to put the spectra field labels on the bottom as ticks!
+plt.legend() # loc='lower left') # upper right')
+plt.tight_layout()
+plt.show()
 
