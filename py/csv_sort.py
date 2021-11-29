537af5ccceba3b197c7a4fc241a21a1eb078e575
ashlinrichardson <richardson.ashlin@gmail.com>
2021-11-28 21:30:24 -0800

    update

--- py/csv_sort.py
+++ py/csv_sort.py
@@ -4,7 +4,7 @@ from misc import *
 if len(args) < 3:
     err('python3 csv_sort.py [input csv file name] [name of field to sort on (increasing)]')
 
-ofn, fi = args[1] + '_sort.csv', fi
+ofn, fi = args[1] + '_sort.csv', -1
 fields, data = read_csv(args[1])
 lookup = {fields[i]: i for i in range(len(fields))}
 
@@ -13,12 +13,12 @@ except: err('field not found:', args[2])
 
 N = len(data[0])
 sortd = [ [data[fi][i], i] for i in range(N)] # intentional naming
-sortd.sort(reverse=True)
+sortd.sort(reverse=False)  # increasing order
 
 print("+w", ofn)
 M = range(len(fields))
 lines = [','.join(fields)]
 for i in range(N):
-    lines += [','.join([data[j][sortd[i][1]] for j in M])]
+    lines += [','.join([ ('"' + data[j][sortd[i][1]] + '"') for j in M])]
 open(ofn, 'wb').write(('\n'.join(lines)).encode())
 
