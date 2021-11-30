7d4b6c9298ee100fa6bedd436a20fc35a16f0aa3
ashlinrichardson <richardson.ashlin@gmail.com>
2021-11-29 23:38:54 -0800

    update

--- py/csv_spectra_plot_class.py
+++ py/csv_spectra_plot_class.py
@@ -57,42 +57,58 @@ reverse_lookup = {lookup[x]: x for x in values}
 print("lookup", lookup)   # take categorical value and encode it as int 0,1,..
 print("revers", reverse_lookup)  # recover original value from int code!
 
-'''now do the actual plotting'''
-plt.figure(figsize=(8*2.5,6*2.5))
-plt.title("Spectra aggregated by categorical field: " + args[2])
-plt.ylabel("Digital number")
-plt.xlabel("Date, resolution(m) and Frequency (nm)")
-plt.gca().axes.get_yaxis().set_visible(False)
+def deriv(x, spectrum):
+    spec_new = []
+    for i in range(1, len(x)):
+        spec_new.append(spectrum[i] - spectrum[i - 1])
+    return x[1:], spec_new
 
-max_y, min_y = 0, 0
-ci = 0
-for i in range(N):
-    value = data[fi][i]
-    spec = [float(data[j][i]) for j in spec_fi]
-    for j in range(len(spec)):
-        y = spec[j]
-        max_y = y if y > max_y else max_y
-        min_y = y if y < min_y else min_y
-        ci += 1
+def integ(x, spectrum):
+    spec_new = [spectrum[0]]
+    for i in range(1, len(x)):
+        spec_new.append(spec_new[-1] + spectrum[i])
+    return x, spec_new
 
-print("ymin", min_y, "ymax", max_y)
+for case in ['regular', 'derivative', 'integral']:
+    '''now do the actual plotting'''
+    plt.figure(figsize=(8*2.5,6*2.5))
+    plt.title((case if case != 'regular' else '') + "Spectra aggregated by categorical field: " + args[2])
+    plt.ylabel("Digital number")
+    plt.xlabel("Date, resolution(m) and Frequency (nm)")
+    plt.gca().axes.get_yaxis().set_visible(False)
 
-used_value=set()
-x = range(len(spec_fi))
-for i in range(N):
-    value = data[fi][i] # categorical value
-    spectrum = [float(data[j][i]) for j in spec_fi]
-    print(value, spectrum)
-    plt.plot(x,
-             spectrum, # marker=markers[lookup[value]],
-             color=colors()[lookup[value]],
-             label=(value if value not in used_value else None))
-    used_value.add(value)
-    # don't forget to put the spectra field labels on the bottom as ticks!
-#plt.legend() # loc='lower left') # upper right')
-plt.xticks(x, [fields[i] for i in spec_fi], rotation='vertical')
-plt.legend()
-plt.tight_layout()
-fn = args[1] + "_spectra_plot_" + field_label + ".png"
-print("+w", fn)
-plt.savefig(fn)
+    max_y, min_y = 0, 0
+    ci = 0
+    for i in range(N):
+        value = data[fi][i]
+        spec = [float(data[j][i]) for j in spec_fi]
+        for j in range(len(spec)):
+            y = spec[j]
+            max_y = y if y > max_y else max_y
+            min_y = y if y < min_y else min_y
+            ci += 1
+    print("ymin", min_y, "ymax", max_y)
+
+    used_value=set()
+    for i in range(N):
+        x = range(len(spec_fi))
+        value = data[fi][i] # categorical value
+        spectrum = [float(data[j][i]) for j in spec_fi]
+        print(value, spectrum)
+        if case == 'derivative':
+            x, spectrum = deriv(x, spectrum)
+        if case == 'integral':
+            x, spectrum = integ(x, spectrum)
+        plt.plot(x,
+                 spectrum, # marker=markers[lookup[value]],
+                 color=colors()[lookup[value]],
+                 label=(value if value not in used_value else None))
+        used_value.add(value)
+        # don't forget to put the spectra field labels on the bottom as ticks!
+    #plt.legend() # loc='lower left') # upper right')
+    plt.xticks(x, [fields[i] for i in spec_fi[0:len(x)]], rotation='vertical')
+    plt.legend()
+    plt.tight_layout()
+    fn = args[1] + "_spectra_plot_" + (case if case != 'regular' else '') + '_' + field_label + ".png"
+    print("+w", fn)
+    plt.savefig(fn)
