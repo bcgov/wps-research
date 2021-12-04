6d6b197237a520cd9fb45d0ab86e654e5711ecb9
ashlinrichardson <richardson.ashlin@gmail.com>
2021-12-04 12:14:12 -0800

    cleanup

--- py/raster_plot.py
+++ py/raster_plot.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 '''20211202 simple version that was stripped down from 2020 ftl mvp version
-usage:
-    python read_multi.py sentinel2.bin
+usage: e.g:
+    python rasterplot.py sentinel2.bin 4 3 2
     
-tested on Python 2.7.15+ (default, Nov 27 2018, 23:36:35) [GCC 7.3.0] on linux2 (Ubuntu 18.04.2 LTS)
-with numpy.version.version '1.16.2' and matplotlib.__version__ '2.2.4'
+tested on Python 3.8.10 (default, Sep 28 2021, 16:10:42) 
+with numpy.version.version '1.20.2' and matplotlib.__version__ '3.4.1'
 
-installation of numpy and matplotlib (Ubuntu):
+e.g. installation of numpy and matplotlib (Ubuntu):
     sudo apt install python-matplotlib python-numpy '''
-
 from misc import *
 import matplotlib
 args = sys.argv
@@ -19,12 +18,10 @@ if len(args) < 2:
         ' [optional: red band idx]' + 
         ' [optional: green band idx]' +
         ' [optional: blue band idx] #band idx from 1')
-# check file and header exist
-fn, hdr = sys.argv[1], hdr_fn(sys.argv[1])
-assert_exists(fn)
+fn, hdr = sys.argv[1], hdr_fn(sys.argv[1])  # check header exists
+assert_exists(fn)  # check file exists
 
-# read header and print parameters
-samples, lines, bands = read_hdr(hdr)
+samples, lines, bands = read_hdr(hdr)  # read header and print parameters
 for f in ['samples', 'lines', 'bands']:
     exec('print("' + f + ' =" + str(' +  f + '))')
     exec(f + ' = int(' + f + ')')
@@ -33,46 +30,38 @@ npx = lines * samples # number of pixels.. binary IEEE 32-bit float data
 data = read_float(sys.argv[1]).reshape((bands, npx))
 print("bytes read: " + str(data.size))
 
+bn = None
+try: bn = band_names(hdr)  # try to read band names from hdr
+except: pass
 
-# select bands for visualization: default value [3, 2, 1]. Try changing to anything from 0 to 12-1==11! 
-# band_select = [3, 2, 1] if bands > 3 else [0, 1, 2]
+# select bands for visualization # band_select = [3, 2, 1] if bands > 3 else [0, 1, 2]
 band_select, ofn = [0, 1, 2], None
-try:  # see if we can set the (r,g,b) encoding (band selection) from the command args
+try:  # see if we can set the (r,g,b) encoding (band selection) from command args
     for i in range(0, 3):
         bs = int(args[i + 2]) - 1
-        if bs < 0 or bs >= bands:
+        if bs < 0 or bs >= bands: 
             err('band index out of range')
-
         band_select[i] = bs
-except Exception:
-    pass
-try: ofn = '_'.join([fn] + args[2: 2 + 3] + ["rgb.png"])
-except: ofn = '_'.join([fn] + [str(b + 1) for b in band_select] + ["rgb.png"])
+except: pass
 
-n_points = 0
-if bands == 1:
-    band_select = [0, 0, 0,] # could be class map. or just one band map
+middle = None  # reproducibility: put band idx used, in output fn
+try: middle = args[2: 2 + 3]
+except: middle = [str(b + 1) for b in band_select]
+ofn = '_'.join([fn] + middle + ["rgb.png"])
 
-rgb = np.zeros((lines, samples, 3))
-for i in range(0, 3):
-    rgb[:, :, i] = data[band_select[i], :].reshape((lines, samples))
+n_points, rgb = 0, np.zeros((lines, samples, 3))
+band_select = [0, 0, 0,] if bands == 1 else band_select # could be class map. or just one band map
+bn = [bn[i] for i in band_select] if bn else bn  # cut out the band names used, if applicable
+
+def scale_rgb(i):  # for i in range(3)
+    rgb_i = data[band_select[i], :].reshape((lines, samples))
     
     if True: # if not override_scaling
-        print("scaling")  # scale band in range 0 to 1
-        rgb_min, rgb_max = np.nanmin(rgb[:, :, i]), np.nanmax(rgb[:, :, i])
-        print("rgb_min: " + str(rgb_min) + " rgb_max: " + str(rgb_max))
-        rgb[:, :, i] -= rgb_min
-        rng = rgb_max - rgb_min
-        if rng != 0.:
-            rgb[:, :, i] /= rng #(rgb_max - rgb_min)
-
-        # so called "x-% linear stretch"
-        values = rgb[:, :, i]
+        values = rgb_i  # now do the so called x-% linear stretch (separate bands version)
         values = values.reshape(np.prod(values.shape)).tolist()
         values.sort()
 
-        # sanity check
-        if values[-1] < values[0]:
+        if values[-1] < values[0]:   # sanity check
             err("failed to sort")
 
         for j in range(0, npx - 1):
@@ -81,28 +70,29 @@ for i in range(0, 3):
 
         n_pct = 2. # percent for stretch value
         frac = n_pct / 100.
-        rgb_min = values[int(math.floor(float(npx)*frac))]
-        rgb_max = values[int(math.floor(float(npx)*(1. - frac)))]
-        rgb[:, :, i] -= rgb_min
+        rgb_min, rgb_max = values[int(math.floor(float(npx)*frac))],\
+                           values[int(math.floor(float(npx)*(1. - frac)))]
         rng = rgb_max - rgb_min
-        if rng > 0.: rgb[:, :, i] /= (rgb_max - rgb_min)
+        rgb_i = (rgb_max - rgb_min) / (rng if rng != 0. else 1.)
+        
+        # clip
+        rgb_i[rgb_i < 0.] = 0.
+        rgb_i[rgb_i > 1.] = 1.
+    return rgb_i
 
-        # need to add this update in misc.py as well, and move this code out
-        d = rgb[:, :, i]
-    
-        for j in range(rgb.shape[0]):
-            for k in range(rgb.shape[1]):
-                d = rgb[j,k,i]
-                if d < 0.: rgb[j,k,i] = 0.
-                if d > 1.: rgb[j,k,i] = 1.
-print("done scaling..")
+rgb_i = parfor(scale_rgb, range(3), 3)
+for i in range(3):
+    rgb[:, :, i] = rgb_i[i]
 
-# plot the image: no class labels
-if True:
-    fig = plt.figure(figsize=(8.,8. * float(lines) / float(samples)))
-    ax = fig.add_subplot(1,1,1)
-    ff = os.path.sep.join((fn.split(os.path.sep))[:-1]) + os.path.sep
+if True:  # plot image: no class labels 
+    base_in = 12.
+    fig = plt.figure(figsize=(base_in, base_in * float(lines) / float(samples)))
+    ax = fig.add_subplot(1, 1, 1)
+    ff = os.path.sep.join((os.path.abspath(fn).split(os.path.sep))[:-1]) + os.path.sep
     title_s = fn.split("/")[-1] if not exists(ff + 'title_string.txt') else open(ff + 'title_string.txt').read().strip() 
+    x_label = ''
+    if bn:
+        x_label += '(R,G,B) = (' + (','.join(bn)) + ')'
     plt.title(title_s, fontsize=11)
     plt.style.use('dark_background')
 
@@ -111,8 +101,10 @@ if True:
     rgb = rgb / (d_max - d_min)
 
     plt.imshow(rgb) #, vmin = 0., vmax = 1.) #plt.tight_layout()
+    print(ff)
     if exists(ff + 'copyright_string.txt'):
-        plt.xlabel(open(ff+ 'copyright_string.txt').read().strip())
+        x_label += (' ©' + open(ff+ 'copyright_string.txt').read().strip())
+    plt.xlabel(x_label)
     print("+w", ofn)
     plt.tight_layout()
     plt.savefig(ofn)
