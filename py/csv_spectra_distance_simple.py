39f82e0ea57fd98fd100a57ed542fa011883de1c
ashlinrichardson <richardson.ashlin@gmail.com>
2021-12-04 17:01:22 -0800

    update

--- py/csv_spectra_distance_simple.py
+++ py/csv_spectra_distance_simple.py
@@ -1,4 +1,6 @@
-'''calculate distance from spectra in file resulting from raster_extract_spectra_simple.py,
+'''test this on smaller data
+
+python3 ~/GitHub/bcws-psu-research/py/csv_spectra_distance_simple.py ./sub.bin_first_spectrum.csv sub.binalculate distance from spectra in file resulting from raster_extract_spectra_simple.py,
 to a raster
 
 python3 csv_spectra_distance_simple.py [input csv file] [raster file]'''
@@ -7,6 +9,7 @@ import sys
 import csv
 import numpy
 import math
+from misc import parfor
 from misc import read_csv
 from misc import exist
 from misc import err
@@ -15,14 +18,21 @@ from misc import read_binary
 from misc import write_hdr
 from misc import write_binary
 args = sys.argv
+from multiprocessing import Lock
+lock = Lock()
+n_processed = 0
 
 if len(args) < 3:
-    err('python3 csv_spectra_distance_simple.py [csv spectra file (one spectrum)] [raster file]')
+    err('python3 csv_spectra_distance_simple.py [csv spectra file (one spectrum)] ' +
+        ' [field to select from] [field value to select]' +     
+        ' [raster file]')
 
-csv_fn, dfn = args[1], args[2]
+csv_fn, dfn = args[1], args[4]
+select_field = args[2]
+select_value = args[3]
 
 '''read the csv and locate the spectra'''
-fields, data = read_csv(csv_fn)
+fields, csv_data = read_csv(csv_fn)
 nf = len(fields)  # number of fields
 f_i = {fields[i]:i for i in range(nf)}
 
@@ -33,40 +43,68 @@ for i in range(nf):
 print('spectra col-ix', spec_fi)
 print('number of cols', len(spec_fi))
 
-'''before we plot, code the categorical possibilities from 0 to however many there are'''
-N = len(data[0]) # number of data points
-if N != 1:
-    err("expected one spectral data point only")
+select_i = f_i[select_field] # index of col indicated to match on..
 
-if True:
-    i = 0
-    spec = [float(data[j][i]) for j in spec_fi]
-    print("spectrum", spec)
+''' average the spectra where field select_field matches the value select_value'''
+N = len(csv_data[0]) # number of data points
+n_select, spec_avg = 0., [0. for i in range(len(spec_fi))] # averaged spectra goes here
 
-    ncol, nrow, nband, data = read_binary(dfn)
-    np = nrow * ncol
-    if len(spec) != nband:
-        err("unexpected number of spectral bands")
+for i in range(N):
+    if csv_data[select_i][i] == select_value:
+        n_select += 1
+        spec =  [float(csv_data[j][i]) for j in spec_fi]  # extract a spectrum
+        print(spec)
+        spec_avg = [spec_avg[j] + spec[j] for j in range(len(spec_fi))] # add it onto the to-be average
 
-    out = numpy.zeros(nrow * ncol)
+# divide by N
+spec_avg = [spec_avg[i] / n_select for i in range(len(spec_avg))]
 
-    for i in range(nrow):
-        print(i)
-        for j in range(ncol):
-            d = 0.
-            ix = i * ncol + j
-            for k in range(nband):
-                x = spec[k] - data[ix + (k * np)]
-                d += math.sqrt(x * x)
-            out[ix] = d  
-    numbers = []
-    w = csv_fn.split(os.path.sep)[-1][:-4].split('_')
-    for i in w:
-        try:
-            numbers.append(int(i))
-        except Exception:
-            pass
-    ofn = '_'.join([dfn] + [str(x) for x in numbers]) + '_distance.bin'
-    ohn = ofn[:-4] + '.hdr'
-    write_binary(out, dfn + '_distance.bin')
-    write_hdr(dfn + '_distance.hdr', ncol, nrow, 1)
+# check we selected something
+if n_select < 1:
+    err("no spectra selected")
+
+# image stuff
+ncol, nrow, nband, data = read_binary(dfn)
+if len(spec_fi) != nband:
+    err("unexpected number of spectral bands")
+np = nrow * ncol
+out = numpy.zeros(np)
+
+def dist_row(i):
+    global lock, out, spec, nrow, ncol, nband, data, n_processed   #for i in range(nrow):
+    
+    with lock:
+        n_processed += 1
+    print('%', 100. * n_processed / nrow, i)
+
+    result = []
+    ix = i * ncol
+    knp = [k * np for k in range(nband)]
+    for j in range(ncol):
+        d = 0.
+        ij = ix + j
+        for k in range(nband):
+            x = spec[k] - data[ij + knp[k]]
+            d += x * x # math.sqrt(x * x)
+        result.append(d)
+    return 0
+
+print('assembling')
+x = parfor(dist_row, range(nrow))
+for i in range(nrow):
+    out[i*ncol: (i+1)*ncol] = x[i]
+
+numbers = []
+w = csv_fn.split(os.path.sep)[-1][:-4].split('_')
+for i in w:
+    try:
+        numbers.append(int(i))
+    except Exception:
+        pass
+ofn = ('_'.join([dfn] +
+       [str(x) for x in numbers]) + '_' +
+       select_field.replace(' ','_') + '_eq_' +
+       select_value.replace(' ', '_') + '_distance.bin')
+ohn = ofn[:-4] + '.hdr'
+write_binary(out, ofn)
+write_hdr(ohn, ncol, nrow, 1)
