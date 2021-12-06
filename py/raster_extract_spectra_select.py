7034e8d3895e8b26890c8337fc8ab4342f3b607c
ashlinrichardson <richardson.ashlin@gmail.com>
2021-12-06 00:06:04 -0800

    update

--- py/raster_extract_spectra_select.py
+++ py/raster_extract_spectra_select.py
@@ -13,7 +13,7 @@ from misc import *
 
 if len(args) < 5:
     err('python3 raster_extract_spectra_select.py ' +
-        '[csv spectra file (no spectra but x, y (wgs-84) and select-field)] ' +
+        '[csv file (no spectra but x, y (wgs-84) and select-field)] ' +
         '[field to select from (select-field)] ' +
         '[value to select (select-value)]' +     
         '[raster file to select spectra from]')
@@ -23,9 +23,15 @@ csv_fn, select_field, select_value, img_fn = args[1:5]
 '''read the csv'''
 fields, csv_data = read_csv(csv_fn)
 nf = len(fields)  # number of fields
-f_i = {fields[i]:i for i in range(nf)}
 
-for i in ['x', 'y', select_field]:  # assert it contains lat/lon..
+# sanitize for evil commas !
+fields = [fields[i].strip().replace(',', ';') for i in range(len(fields))]
+for i in range(len(csv_data)):
+    for j in range(len(csv_data[i])):
+        csv_data[i][j] = csv_data[i][j].strip().replace(',', ';')
+
+f_i = {fields[i]:i for i in range(nf)} # create a lookup... use it to:
+for i in ['x', 'y', select_field]:  # assert the header contains lat/lon..
     if i not in fields:
         err('expected field in ' + csv_fn + ': ' + i)
 [x_i, y_i, select_i] = [f_i[i]
@@ -58,19 +64,19 @@ print("stdv", stdv)
 csv_ofn = '_'.join([csv_fn, 'spectra', select_field, select_value + '.csv'])
 print('+w', csv_ofn)
 
-lines = [fields + ['row', 'col'] + bn] # first line: header
+lines = [fields + ['row', 'col', 'csv_file', 'select_field', 'select_value', 'image'] + bn] # first line: header
 # print(lines)
 for i, row, col, spec in new:
     orig = [csv_data[j][i] for j in range(nf)]
-    rowcol =  [str(row), str(col)] 
+    rowcol =  [str(row), str(col), csv_fn, select_field, select_value, img_fn] 
     spec = [str(x) for x in spec]
     lines += [orig + rowcol + spec]
 lines = [','.join(line) for line in lines]
 open(csv_ofn, 'wb').write(('\n'.join(lines)).encode())
 
 csv_afn = '_'.join([csv_fn, 'spectra_mean', select_field, select_value + '.csv'])
-lines = [bn]
-lines += [[str(j) for j in mean]]
+lines = [['csv_file', 'select_field', 'select_value', 'image'] + bn]
+lines += [[csv_fn, select_field, select_value, img_fn] + [str(j) for j in mean]]
 print(lines)
 lines = [','.join(line) for line in lines]
 open(csv_afn, 'wb').write(('\n'.join(lines)).encode())
@@ -78,8 +84,8 @@ print('+w', csv_afn)
 
 
 csv_sfn = '_'.join([csv_fn, 'spectra_stdv', select_field, select_value + '.csv'])
-lines = [bn]
-lines += [[str(j) for j in stdv]]
+lines = [['csv_file', 'select_field', 'select_value', 'image'] + bn]
+lines += [[csv_fn, select_field, select_value, img_fn] + [str(j) for j in stdv]]
 lines = [','.join(line) for line in lines]
 open(csv_sfn, 'wb').write(('\n'.join(lines)).encode())
 print('+w', csv_sfn)
