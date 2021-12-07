351bef970d3eaf9de6c7704292744d1b47f92cd7
ashlinrichardson <richardson.ashlin@gmail.com>
2021-12-06 21:07:36 -0800

    add label-format output

--- cpp/raster_dominant.cpp
+++ cpp/raster_dominant.cpp
@@ -8,8 +8,11 @@ int main(int argc, char ** argv){
   if(!(exists(fn))) err("failed to open input file");
   str hfn(hdr_fn(fn)); // input header file name
 
-  str ofn(str(argv[1]) + str("_dominant.bin")); // output file name
+  str ofn(str(argv[1]) + str("_dominant.bin")); // output file name: 1-hot encoding
   str ohn(hdr_fn(ofn, true)); // out header file name
+
+  str(oln(str(argv[1]) + str("dominant_label.bin")); // output file, as label
+  str(olh(str(argv[1]) + str("dominant_label.hdr"));
 
   printf("+w %s\n", ofn.c_str());
   printf("+w %s\n", ohn.c_str());
@@ -19,8 +22,9 @@ int main(int argc, char ** argv){
   np = nrow * ncol; // number of input pix
 
   size_t n, i, j, k, ix, ij, ik;
-  float * out = falloc(nrow * ncol * nband);
+  float * out = falloc(nrow * ncol * nband); // save as 1-hot
   float * dat = bread(fn, nrow, ncol, nband);
+  float * lab = falloc(nrow * ncol); // save as label
 
   float dom_v; // dominant value this pixel
   size_t dom_k = 0; // dominant index
@@ -41,17 +45,25 @@ int main(int argc, char ** argv){
 	  dom_k = k;
 	}
       }
+      lab[ij] = (float)dom_k; // save dominant as label
 
       for0(k, nband){
         ik = (np * k) + ij;
 	out[ik] = 0.;
-	if(dom_k == k){
-	   out[ik] = 1. ; //dat[ik];
-	}
+	if(dom_k == k)
+	   out[ik] = 1. ;
       }
     }
   }
   bwrite(out, ofn, nrow, ncol, nband);
   hwrite(ohn, nrow, ncol, nband);
+  
+  bwrite(lab, oln, nrow, ncol, 1);
+  hwrite(olh, nrow, ncol, 1);
+
+  // also 
+  
+  free(out); 
+  free(dat);
   return 0;
 }
