026909649ccbbfed5b459e35ee661a8b77f43b70
ashlinrichardson <richardson.ashlin@gmail.com>
2022-02-12 16:01:28 -0800

    update

--- cpp/sentinel2_active.cpp
+++ cpp/sentinel2_active.cpp
@@ -1,10 +1,33 @@
 #include"misc.h"
 int main(int argc, char ** argv){
-	size_t nrow, ncol, nband;
-	vector<string> s;
-	hread(str("swir.hdr"), nrow, ncol, nband, s);
-	cout << s << endl;
-	return 0;
+  if(argc < 2) err("sentinel2_active.exe [input file name]");
+  str fn(argv[1]);
+  str hfn(hdr_fn(fn));
+
+  size_t nrow, ncol, nband;
+  int bi[3];
+  int j;
+  bi[0] = bi[1] = bi[2] = -1;
+  vector<string> s;
+  vector<string> t;
+  t.push_back(str("945n"));
+  t.push_back(str("1610n"));
+  t.push_back(str("2190n"));
+  hread(hfn, nrow, ncol, nband, s);
+
+  int i, n;
+  n = s.size();
+  for0(i, n){
+    for0(j, 3){
+      if(contains(s[i], t[j])){
+        bi[j] = i;
+      }
+    }
+  }
+
+  for0(j, 3){
+    cout << "\t" << bi[j] << endl;
+  }
+
+  return 0;
 }
-
-
