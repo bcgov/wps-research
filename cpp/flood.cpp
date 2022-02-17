20d7bdafa079d794b16ba3537ed20403b7970846
ashlinrichardson <richardson.ashlin@gmail.com>
2022-02-17 00:45:16 -0800

    update

--- cpp/flood.cpp
+++ cpp/flood.cpp
@@ -8,17 +8,16 @@ float * out_f;
 
 int flood(long int i, long int j, int depth){
   printf("flood(%ld, %ld, %d) label=%ld d=%f depth=%d\n", i, j, depth, i_next, dat[i * ncol + j],depth);
-	int ret = 0;
-	// printf("i %ld j %ld (%f)\n", i, j, dat[i * ncol + j]);
+  int ret = 0;
   if(i < 0 || j < 0 ||  i >= nrow || j >=ncol ){
-	printf("oub\n");
-	  return 0;
+    printf("oub\n");
+    return 0;
   }
   long int ij = i * ncol + j;
   float d = dat[ij];
 
   if(d != 1. || visited[ij]){
-	  print("ret\n");
+	  printf("ret\n");
 	  return 0; // labelled or not under mask
   }
   visited[ij] = true;
@@ -26,19 +25,20 @@ int flood(long int i, long int j, int depth){
   nf ++; // marked something
 //  printf("\ti %ld j %ld label=%zu depth=%d\n", i, j, i_next, depth);
 
-  for(long int di = 1; di >= -1 ; di --){
+  for(long int di = 1; di >= -1; di -= 1){
     long int ii = i + di;
-    for(long int dj = 1; dj >= -1; dj --){
+    for(long int dj = 1; dj >= -1; dj -=1){
       long int jj = j + dj;
+      printf("di %ld dj %ld (%ld, %ld) \n", di, dj, i, j);
       if((!(di == 0 && dj == 0)) &&
-         (i + di > 0) &&
-	 (j + dj > 0) &&
+         (i + di >= 0) &&
+	 (j + dj >= 0) &&
 	 (i + di < nrow) &&
 	 (j + dj < ncol)){
 
-        if(!visited[ii*ncol + jj] &&
-	    dat[ii*ncol + jj] == 1.){
-		//printf("  call(%ld, %ld) di %ld dj %ld\n", ii,jj, di, dj);
+        long int ik = ii * ncol + jj;
+        if(!visited[ik] && dat[ik] == 1.){
+	  printf("  call(%ld, %ld) di %ld dj %ld\n", ii,jj, di, dj);
           ret += flood(ii, jj, depth+1);
 	}
       }
@@ -50,7 +50,7 @@ int flood(long int i, long int j, int depth){
 int main(int argc, char ** argv){
   if(argc < 2) err("flood.exe [input file name] # raster flood fill on mask");
   size_t np, k, n;
- long int ij;
+  long int ij;
   long int i;
   long int j;
 
