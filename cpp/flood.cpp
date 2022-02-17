6f24537a0074d25072822ed6128f4121d22b721d
ashlinrichardson <richardson.ashlin@gmail.com>
2022-02-16 23:23:11 -0800

    update

--- cpp/flood.cpp
+++ cpp/flood.cpp
@@ -3,25 +3,32 @@
 comps of image areas equalling 1. 20220216 */
 
 float * dat;
+int * visited;
 size_t *out, i_next, nrow, ncol, nband, nf;
+float * out_f;
 
 void flood(long int i, long int j, size_t label){
+  printf("i %ld j %ld\n", i, j);
   if(i < 0 || j < 0 || i >=nrow || j >=ncol) return;
-  size_t ij = i * ncol + j;
+  long int ij = i * ncol + j;
   float d = dat[ij];
 
-  if(d != 1. || out[ij] > 0) return; // labelled or not under mask
+  if(d != 1. || visited[ij]) return; // labelled or not under mask
+  visited[ij] = true;
   out[ij] = label; // label this point
   nf ++; // marked something
-  printf("\ti %zu j %zu label=%zu\n", i, j, label);
+  printf("\ti %ld j %ld label=%zu\n", i, j, label);
 
-  int di, dj;
+  long int di, dj;
   long int ii, jj;
   for(di = -1; di <= 1; di++){
     ii = i + di;
     for(dj = -1; dj <= 1; dj++){
       jj = j + dj;
-      if(ii != i && jj != j) flood(ii, jj, label);
+      if(di != 0 && dj != 0){
+	    if(!visited[ii*ncol + jj] && dat[ii*ncol + jj] !=0.)
+	      flood(ii, jj, label);
+      }
     }
   }
 }
@@ -35,24 +42,43 @@ int main(int argc, char ** argv){
   str hfn(hdr_fn(fn)); /* headers */
   str hf2(hdr_fn(ofn, true));
 
-  hread(hfn, nrow, ncol, nband);
+  str ofn4(fn + "_flood4.bin");
+  str hfn4(fn + "_flood4.hdr");
+
+  size_t d_type = hread(hfn, nrow, ncol, nband);
   np = nrow * ncol;
   if(nband != 1) err("expected 1-band image");
+  if(d_type != 4) err("expected type-4 image");
 
   out = (size_t *)(void *)alloc(np * sizeof(size_t));
   dat = bread(fn, nrow, ncol, nband);
+  visited = (int *)(void *)alloc(np * sizeof(int));
+  out_f = falloc(np);
 
   i_next = 1;
-  for0(i, np) out[i] = -1.;
-  for0(i, nrow) for0(j, ncol){
-    nf = 0;
-    flood(i, j, i_next);
-    if(nf > 0) i_next ++;
+  for0(i, np){
+	  out[i] = 0;
+	  visited[i] = false;
   }
+  for0(i, nrow) for0(j, ncol){
+    if(!visited[i * ncol + j]){
+      nf = 0;
+      flood((long int)i, (long int)j, i_next);
+      if(nf > 0){
+  	    cout << i << "," << j << "=" << i_next << endl;
+  	    i_next ++;
+      }
+     }
+  }
+  for0(i, np) out_f[i] = (float)out[i];
 
   FILE * f = wopen(ofn);
   fwrite(out, sizeof(size_t), np, f);
   hwrite(hf2, nrow, ncol, 1, 16); /* type 16 = size_t */
+
+  bwrite(out_f, ofn4, nrow, ncol, 1);
+  hwrite(hfn4, nrow, ncol, 1, 4);
+  free(visited);
   free(dat);
   free(out);
   return 0;
