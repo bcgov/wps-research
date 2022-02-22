a4e8413596c325446aed489d18486733bed2348e
ashlinrichardson <richardson.ashlin@gmail.com>
2022-02-21 20:27:54 -0800

    update

--- py/envi_header_cat.py
+++ py/envi_header_cat.py
@@ -53,8 +53,8 @@ i_dat, o_dat = open(args[1]).read(),  open(args[2]).read()
 bn_1, bn_2 = get_band_names_lines(i_dat), get_band_names_lines(o_dat)
 lines1 , lines2 = i_dat.strip().split('\n'), o_dat.strip().split('\n')
 
-# print(i_dat)
-# print(o_dat)
+print(i_dat)
+print(o_dat)
 band_count = len(bn_1) + len(bn_2) # add band counts
 print("band_count", band_count)
 
@@ -76,7 +76,7 @@ for i in range(len(lines2)):
 lines2 = lines2 + bn_1
 
 for i in range(len(lines2)):
-    if len(lines2[i].split('bands =')) > 1:
+    if len(lines2[i].split('bands ')) > 1:
         lines2[i] = lines2[i].split('=')[0] + '= ' + str(band_count)
 
     if len(lines2[i].split("description =")) > 1:
