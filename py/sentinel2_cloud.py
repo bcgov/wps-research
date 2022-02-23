eae21a13c79e1b80254239f9d3c642e80eb7a0d3
ashlinrichardson <richardson.ashlin@gmail.com>
2022-02-22 23:36:34 -0800

    update

---
+++ py/sentinel2_cloud.py
@@ -0,0 +1,81 @@
+from misc import * 
+sep = os.path.sep
+exists = os.path.exists
+pd = sep.join(__file__.split(sep)[:-1]) + sep
+project = pd + 'raster_project_onto.py' # project onto command
+
+d = os.path.abspath(args[1] if len(args) > 1 else os.getcwd())
+print(d)
+
+if d[-5:] != '.SAFE':
+    print('expected folder ending in .SAFE')
+    err('python3 sentinel2_cloud.py [Sentinel2 L2 folder: ___.SAFE] ')
+
+CLD = os.popen('find ' + d + os.path.sep + ' -name "*CLDPRB_20m.jp2"').readlines()
+CLD = [x.strip() for x in CLD]
+
+if len(CLD) > 1:
+    print(CLD)
+    err('too many files, check folder')
+CLD = CLD[0]
+
+d += sep
+
+out_20m = d + 'cloud_20m.bin'
+if not exists(out_20):
+    run(' '.join(['gdal_translate',
+                  '-of ENVI',
+                  '-ot Float32', 
+                  CLD,
+                  out_20m]))
+
+out_file = d + 'cloud.bin'
+
+if not exists(out_file):
+    run(' '.join(['python3',
+                   project,
+                   out_20m,
+                   d + 'SENTINEL2_L2A_EPSG_32610_10m.bin',
+                   out_file]))
+
+''' From https://sentinels.copernicus.eu/web/sentinel/technical-guides/sentinel-2-msi/level-2a/algorithm
+Figure 3: Scene Classification Values Label 	Classification
+0 	NO_DATA
+1 	SATURATED_OR_DEFECTIVE
+2 	DARK_AREA_PIXELS
+3 	CLOUD_SHADOWS
+4 	VEGETATION
+5 	NOT_VEGETATED
+6 	WATER
+7 	UNCLASSIFIED
+8 	CLOUD_MEDIUM_PROBABILITY
+9 	CLOUD_HIGH_PROBABILITY
+10 	THIN_CIRRUS
+11 	SNOW
+'''
+
+cmd = 'find ' + d + ' -name "*SCL_20m.jp2"'
+SCL = os.popen(cmd).readlines()
+SCL = [x.strip() for x in SCL]
+
+if len(SCL) > 1:
+    print(SCL)
+    err('too many files, check folder')
+SCL = SCL[0]
+
+out_20m = d + 'class_20m.bin'
+if not exists(out_20m):
+    run(' '.join(['gdal_translate',
+                  '-of ENVI',
+                  '-ot Float32',
+                  SCL,
+                  out_20m]))
+
+out_file = d + 'class.bin'
+if not exists(out_file):
+    run(' '.join(['python3',
+                   project,
+                   out_20m,
+                   d + 'SENTINEL2_L2A_EPSG_32610_10m.bin',
+                   out_file]))
+
