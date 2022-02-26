49bf2189c34162dc74235bf92dbc9d2f28c573cd
ashlinrichardson <richardson.ashlin@gmail.com>
2022-02-26 14:13:30 -0800

    update

---
+++ cpp/float_max.cpp
@@ -0,0 +1,15 @@
+/* Find the largest successive float that's intra-convertible with integer */
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char ** argv){
+  float x = 0.;
+  size_t y = 0;
+
+  while(((float)y == x) &&
+        ((size_t)x == y)){
+    x ++;
+    y ++;
+  }
+  printf("%zu\n", y - 1);
+  return 0;
+}
