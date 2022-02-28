91aa32e4b9475c911eab8030bfa4e082fc7da86a
ashlinrichardson <richardson.ashlin@gmail.com>
2022-02-27 19:25:55 -0800

    update

--- cpp/binary_hull.cpp
+++ cpp/binary_hull.cpp
@@ -44,13 +44,11 @@ int main(int argc, char ** argv){
   vector<str> lines(split(r, '\n'));
   //cout << "[" << r << "]" << endl;
   str X(lines[lines.size() -1]); // polygon
-  trim(X, 'P'); trim(X, 'O');
-  trim(X, 'L'); trim(X, 'Y');
-  trim(X, 'G'); trim(X, 'O');
-  trim(X, 'N'); trim(X, ' ');
-  trim(X, ' '); trim(X, '(');
-  trim(X, '('); trim(X, ')');
-  trim(X, ')'); cout << X<<endl;
+  trim(X, 'P'); trim(X, 'O'); trim(X, 'L'); trim(X, 'Y');
+  trim(X, 'G'); trim(X, 'O'); trim(X, 'N');
+  trim(X, ' '); trim(X, ' ');
+  trim(X, '('); trim(X, '('); trim(X, ')'); trim(X, ')');
+  cout << X<<endl;
   exit(1);
 
   /*
