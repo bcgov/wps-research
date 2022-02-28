2d38c9a507e41bdbb4e7e48dc9af0642bb176157
ashlinrichardson <richardson.ashlin@gmail.com>
2022-02-27 16:44:09 -0800

    update

--- cpp/misc.cpp
+++ cpp/misc.cpp
@@ -1,4 +1,21 @@
 #include"misc.h"
+/* run system command and collect results */
+std::string exec(const char* cmd) {
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+            result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
+}
 
 bool not_space(int data){
     return !std::isspace(data);
