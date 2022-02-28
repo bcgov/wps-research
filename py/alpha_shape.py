67e0a5096e8c0260baf842f6da4455db20100c02
ashlinrichardson <richardson.ashlin@gmail.com>
2022-02-27 16:33:58 -0800

    update

--- py/alpha_shape.py
+++ py/alpha_shape.py
@@ -11,21 +11,31 @@ for line in fileinput.input():
         X = copy.deepcopy(line)
     ci += 1
 
-print(" ****** X=", X)
-print(type(X))
+# print(" ****** X=", X)
+# print(type(X))
 X = X.strip()
-print(X)
+# print(X)
 X = X.split()
-print(X)
+# print(X)
 N = int(X[1])
-print(N)
+# print(N)
 X = X[2:]
-print(X)
+# print(X)
 
 if len(X) != 2*N:
     err("bad data count")
+X = [float(x) for x in X]
+ci = 0
+points = []
+for i in range(N):
+    points.append((X[ci], X[ci+1]))
+    ci += 2
 
-
-
-
-
+alpha = 0.95 * alphashape.optimizealpha(points) # optimal alpha
+alpha_shape = alphashape.alphashape(points, alpha); # create alpha shape
+print(alpha_shape)
+if False:
+    fig, ax = plt.subplots() # plot init
+    ax.scatter(*zip(*points)) # plot inputs
+    ax.add_patch(PolygonPatch(alpha_shape, alpha=.2)) # plot alpha shape
+    plt.savefig("alpha_shape.png")
