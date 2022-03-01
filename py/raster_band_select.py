3ba7853d7533a98c3181a947134ab040c17efd68
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-01 10:56:05 -0800

    update

--- py/raster_band_select.py
+++ py/raster_band_select.py
@@ -1,5 +1,52 @@
 '''raster_band_select.py:
 band-subselection by matching on a string (such as by date)
 '''
+from misc import args, sep, exists, pd, run, hdr_fn, band_names, err, read_hdr
 
+if len(args) < 3:
+    err('raster_band_select.py [input binary file] ' +
+        '[string to match band names e.g. 20210721 for jul 21 2021')
 
+fn, s = args[1], args[2]
+cd = pd + '..' + sep + 'cpp' + sep
+
+hfn = hdr_fn(fn)
+samples, lines, bands = read_hdr(hfn)
+nrow, ncol = lines, samples
+
+bi = []
+bn = band_names(hfn)
+for i in range(len(bn)):
+    if len(bn[i].split(s)) > 1:
+        bi.append(i)
+        print('  ', str(i + 1), '  ', bn[i])
+
+if not exists(fn + '_001.hdr'):
+    run(cd + 'unstack.exe ' + fn)
+
+for i in bi:
+    f = fn + '_' + str(i).zfill(3) + '.bin'
+    print(f)
+fni = [fn + '_' + str(i).zfill(3) + '.bin' for i in bi]
+bands = str(len(bi))  # number of bands to write
+ofn = fn + '_' + s + '.bin'
+ohn = fn + '_' + s + '.hdr'
+
+if not exists(ohn):
+    run('cp ' + hfn + ' ' + ohn)
+
+c = ['python3',
+     pd + 'envi_header_modify.py',
+     ohn,
+     nrow,
+     ncol,
+     bands]
+c += [('"' + bn[bi[i]] + '"') for i in range(len(bi))]
+c = ' '.join(c)
+run(c)
+
+c = ['cat']
+c += fni
+c += ['>', ofn]
+c = ' '.join(c)
+run(c)
