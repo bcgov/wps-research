4c0150ba3197a27c6854161fde06e8a85d790237
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-02 13:04:46 -0800

    update

--- py/alpha_shape_multi.py
+++ py/alpha_shape_multi.py
@@ -8,22 +8,24 @@ import pickle
 import fileinput
 import alphashape
 import numpy as np
+from misc import colors
 import matplotlib.pyplot as plt
 from descartes import PolygonPatch
 from misc import args, err
 
+c = colors()
 files = [x.strip() for x in os.popen('ls -1 *.pkl').readlines()]
 print(files)
 
+print('+w alpha_shape.png')
+fig, ax = plt.subplots() # plot init
+
+ci = 0
 for fn in files:
     n = fn.split('.')[0]
-
-pickle.dump([X_bak, points, alpha, alpha_shape, patch_alpha], open(pfn, 'wb'))
-sys.exit(1)
-
-if True:
-    print('+w alpha_shape.png')
-    fig, ax = plt.subplots() # plot init
-    ax.scatter(*zip(*points)) # plot inputs
-    ax.add_patch(PolygonPatch(alpha_shape, alpha=patch_alpha)) # plot alpha shape
-    plt.savefig("alpha_shape.png")
+    print(n, c[ci])
+    [X_bak, points, alpha, alpha_shape, patch_alpha] =  pickle.load(open(fn, 'rb'))
+    ax.scatter(*zip(*points), color=c[ci]) # plot inputs
+    ax.add_patch(PolygonPatch(alpha_shape, alpha=patch_alpha, facecolor=c[ci], edgecolor=c[ci]) ) # plot alpha shape
+    ci += 1
+plt.savefig("alpha_shape.png")
