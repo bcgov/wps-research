01ddce0e88ddf1b3a47b1a925a6933aeff68e351
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-02 21:58:18 -0800

    update

--- cpp/raster_mult.cpp
+++ cpp/raster_mult.cpp
@@ -1,5 +1,5 @@
 /* raster_mult.cpp: band math, multiply hyperspectral cubes together
- adapted from raster_sum.cpp 20220302 */
+adapted from raster_sum.cpp 20220302 */
 #include"misc.h"
 
 int main(int argc, char ** argv){
@@ -13,19 +13,30 @@ int main(int argc, char ** argv){
   str ofn(argv[3]); // output file name
   str ohn(hdr_fn(ofn, true)); // out header file name
 
-  size_t nrow, ncol, nband, np, nf, i, nrow2, ncol2, nband2;
+  size_t nrow, ncol, nband, np, nf, i, k, nrow2, ncol2, nband2;
   hread(hfn, nrow, ncol, nband); // read header 1
   hread(hfn2, nrow2, ncol2, nband2); // read header 2
-  if(nrow != nrow2 || ncol != ncol2 || nband != nband2)
-    err("input image dimensions should match");
+  if(nrow != nrow2 || ncol != ncol2 || (nband != nband2 && nband2 != 1))
+  err("input image dimensions should match");
 
   np = nrow * ncol; // # input pix
   nf = np * nband; // # input float
   float * out = falloc(nf); // out buffer
   float * dat1 = bread(fn, nrow, ncol, nband);
-  float * dat2 = bread(fn2, nrow, ncol, nband);
-  for0(i, nf) out[i] = dat1[i] * dat2[i];
+  float * dat2 = bread(fn2, nrow, ncol, nband2);
+  if(nband == nband2){
+    for0(i, nf) out[i] = dat1[i] * dat2[i];
+  }
+  else{
+    if(nband2 != 1) err("second image should have one band");
+    for0(k, nband){
+      for0(i, np){
+        out[(k * np) + i] = dat1[(k * np) + i] * dat2[i];
+      }
+    }
+  }
   bwrite(out, ofn, nrow, ncol, nband);
   hwrite(ohn, nrow, ncol, nband);
+  cout << "nband " << nband << " nband2 " << nband2 << endl;
   return 0;
 }
--- cpp/sentinel2_active.cpp
+++ cpp/sentinel2_active.cpp
@@ -6,24 +6,50 @@ int main(int argc, char ** argv){
 
   str fn(argv[1]); /* binary files */
   str ofn(fn + "_active.bin");
-  str hfn(hdr_fn(fn));  /* headers */
+  str hfn(hdr_fn(fn)); /* headers */
   str hf2(hdr_fn(ofn, true));
 
   vector<string> s, t; /* band names + ones to use */
-  t.push_back(str("945n"));
-  t.push_back(str("1610n"));
-  t.push_back(str("2190n"));
-  
+  t.push_back(str("945nm"));
+  t.push_back(str("1610nm"));
+  t.push_back(str("2190nm"));
+
   hread(hfn, nrow, ncol, nband, s);
   np = nrow * ncol;
   n = s.size();
-  
-  long int bi[3] = {-1, -1, -1};
-  for0(i, n) for0(j, 3) if(contains(s[i], t[j])) bi[j] = i * np;
-  
+
+  long int bi[3];
+  bi[0] = bi[1] = bi[2] = -1;
+
+  for0(i, n){
+    for0(j, 3){
+      if(contains(s[i], t[j])){
+        bi[j] = i * np;
+        printf("bi[%zu]=%zu \"%s\"\n", j, bi[j], s[i].c_str());
+      }
+    }
+  }
+
+  for0(i, 3) if(bi[i] < 0){
+    printf("Missing band: %s\n", t[i].c_str());
+    err("Missing band"); 
+  }
+
+
+
+
   float * out = falloc(np);
   float * dat = bread(fn, nrow, ncol, nband);
-  for0(ij, np) out[ij] = dat[ij + bi[2]] - dat[ij + bi[1]] > 175.;
+  
+  float * b1, *b2, *b3;
+  b1 = &dat[bi[0]]; b2 = &dat[bi[1]]; b3 = &dat[bi[2]]; 
+//  for0(i, np) out[i] = (float)(dat[bi[2] + i] - dat[bi[1] + i]) > 175.;
+  for0(i, np) out[i] = (float)(b2[i] - b1[i]) > 175.;
+  cout << "second" << endl;
+  for0(i, np){ 
+      	  //  if(i % 100 == 0)  printf("i %zu np %zu\n", i, np);
+         out[i] *= (float)(b3[i] > b2[i]);
+  }
   bwrite(out, ofn, nrow, ncol, 1);
   hwrite(hf2, nrow, ncol, 1);
   free(dat); /* plot spectra? */
