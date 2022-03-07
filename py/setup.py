ac0098faeaa5a9a3ff905ab14e1cef9e0c9cf91d
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-03-07 14:06:37 -0800

    update

--- py/setup.py
+++ py/setup.py
@@ -1,17 +1,22 @@
-from py.misc import *
+from misc import *
 
-cpp = os.popen("find cpp/*.cpp").readlines()
+# setup for python stuff
+run('sudo apt install python3-pip python3-setuptools')
+run('sudo apt update && sudo apt upgrade')
+run('python3 -m pip install numpy scikit-learn matplotlib alphashape descartes')
+
+cpp = os.popen("find ../cpp/*.cpp").readlines()
 for c in cpp:
     c = c.strip()
     w = c.split("/")
     leaf = w[-1]
     stem = leaf.split(".")[0]
-    bf = 'bin/' + stem # + '.exe'
-    cmd = "g++ -w -O4 -march=native -o " + bf + " " + c + " cpp/misc.cpp -lpthread"  # -g 
+    bf = '../bin/' + stem # + '.exe'
+    cmd = "g++ -w -O4 -march=native -o " + bf + " " + c + " ../cpp/misc.cpp -lpthread"  # -g 
     if stem != "misc" and not exists(bf):
         run(cmd)
 
-py = os.popen("find py/*.py").readlines()
+py = os.popen("find ./*.py").readlines()
 for p in py:
     w = p.strip().split("/")[-1]
     fn = w.split(".")[0]
@@ -30,7 +35,7 @@ for p in py:
              'system(cmd.c_str());',
              'return(0);',
              '}']
-    bf = 'bin/' + fn #+ '.exe'
+    bf = '../bin/' + fn #+ '.exe'
     if w != "__init__.py" and not exists(bf):
         open(wf, 'wb').write('\n'.join(lines).encode())
         cmd = 'g++ -w -O3 -o ' + bf + ' ' + wf
