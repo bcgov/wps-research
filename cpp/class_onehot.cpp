9663998b520fa7bddf4020745b4a8a39bdd40383
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-03-07 14:07:18 -0800

    update

--- cpp/class_onehot.cpp
+++ cpp/class_onehot.cpp
@@ -1,7 +1,10 @@
 /* convert a class map to one-hot encoding. Revised 20220227 */
 #include"misc.h"
 int main(int argc, char ** argv){
-  if(argc < 2) err("class_onehot [input binary (classification) file name]");
+  if(argc < 2) err("class_onehot [input binary (classification) file name] [optional argument: minimum number of pixels for class to write]");
+
+  size_t min_npix = 0;
+  if(argc > 2) min_npix = (size_t)atoi(argv[2]);
 
   size_t nrow, ncol, nband, np, i, j;
   str fn(argv[1]); // input file name
@@ -11,6 +14,7 @@ int main(int argc, char ** argv){
   np = nrow * ncol;
   if(nband != 1) err("this program defines results for 1-band images");
   size_t p1 = np / 100;
+  printf("bread..\n");
 
   // read data into float array
   float * dat = bread(fn, nrow, ncol, nband);
@@ -23,6 +27,7 @@ int main(int argc, char ** argv){
       printf("Counting %zu / 100\n", i/p1);
     }
   }
+  cout << count << endl;
 
   float d;
   float ci = 1.;
@@ -31,8 +36,13 @@ int main(int argc, char ** argv){
   for(it = count.begin(); it != count.end(); it++) lookup[it->first] = ci ++;
   cout << "lookup:" << lookup << endl;
 
+
+  /* this part should be parallelised */
+  size_t c_i = 1;
   for(it = count.begin(); it != count.end(); it++){
     float di = it->first;
+    if(min_npix > 0) if(count[di] < min_npix) continue;
+    printf("Write %e (size %zu) [%zu of %zu\n", di, count[di], c_i++, count.size()); 
     ci = lookup[di];
     str num(to_string(ci));
     num.erase(num.find_last_not_of('0') + 1, std::string::npos); // trailing 0
