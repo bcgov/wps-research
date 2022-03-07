4cc4c089d3e989bc72dda5377c7c188efdd39259
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-03-07 13:55:17 -0800

    update

---
+++ py/setup.sh
@@ -0,0 +1,4 @@
+# setup for python stuff
+sudo apt install python3-pip python3-setuptools
+sudo apt update && sudo apt upgrade
+python3 -m pip install numpy scikit-learn matplotlib alphashape descartes
