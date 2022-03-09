a5eedb34397533a2efa60ced19e1c11f66bc26dc
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-03-09 10:17:44 -0800

    bugfix

--- cpp/raster_quickstats.cpp
+++ cpp/raster_quickstats.cpp
@@ -12,77 +12,64 @@ int main(int argc, char ** argv){
   size_t nrow, ncol, nband, np;
   hread(hfn, nrow, ncol, nband); // read header 1
   np = nrow * ncol; // number of input pix
-  
+
   double d, diff;
   size_t i, j, k, ix, ij, ik;
   float * dat = bread(fn, nrow, ncol, nband);
 
+  double * n = dalloc(nband);
+  double * avg = dalloc(nband);
   double * fmax = dalloc(nband);
   double * fmin = dalloc(nband);
   double * total = dalloc(nband);
-  double * avg = dalloc(nband);
-  double * n = dalloc(nband); 
-  double * total_squared = dalloc(nband);
   double * stdev = dalloc(nband);
- 
   double * n_inf = dalloc(nband); // count infinity
   double * n_nan = dalloc(nband); // count nan
+  double * total_squared = dalloc(nband);
 
   for0(i, nband){
     fmax[i] = DBL_MIN;
     fmin[i] = DBL_MAX;
-    total[i] = n[i] = avg[i] = total_squared[i] = stdev[i] = 0.;
-    n_inf[i] = n_nan[i] = 0.;
+    total[i] = n[i] = avg[i] = total_squared[i] = stdev[i] = n_inf[i] = n_nan[i] = 0.;
   }
 
-  for0(i, nrow){
-    ix = i * ncol;
-    
-    for0(j, ncol){
-      ij = ix + j;
-      for0(k, nband){
-        ik = ij + k * np;
-        d = dat[ik];
-
-        if(!(isinf(d) || isnan(d))){
-          if(d < fmin[k]) fmin[k] = d;
-          if(d > fmax[k]) fmax[k] = d;
-          total[k] += d;
-          n[k] ++;
-        }
-	else{
-	  if(isinf(d)) n_inf++;
-	  if(isnan(d)) n_nan++;
-	}
+  for0(i, np){
+    for0(k, nband){
+      d = dat[k * np + i];
+      if(!(isinf(d) || isnan(d))){
+        if(d < fmin[k]) fmin[k] = d;
+        if(d > fmax[k]) fmax[k] = d;
+        total[k] += d;
+        n[k] ++;
+      }
+      else{
+        if(isinf(d)) n_inf[k]++;
+        if(isnan(d)) n_nan[k]++;
       }
     }
   }
-  
   for0(k, nband) avg[k] = total[k] / n[k];
 
-  for0(i, nrow){
-    ix = i * ncol;
-    
-    for0(j, ncol){
-      ij = ix + j;
-      
-      for0(k, nband){
-        ik = ij + k * np;
-        d = dat[ik];
-
-        if(!(isinf(d) || isnan(d))){
-          diff = d - avg[k];
-          total_squared[k] += diff * diff;
-        }
+  for0(i, np){
+    for0(k, nband){
+      d = dat[np * k + i];
+      if(!(isinf(d) || isnan(d))){
+        diff = d - avg[k];
+        total_squared[k] += diff * diff;
       }
     }
   }
 
-  for0(k, nband) stdev[k] = sqrt(total_squared[k] / n[k]);
-  
+  for0(k, nband){
+    printf("k total_squared[k] n[k] %zu %e %e\n", k, total_squared[k], n[k]);
+    stdev[k] = sqrt(total_squared[k] / n[k]);
+  }
+
   printf("band_i,Min,Max,Mean,Stdv,n_nan,n_inf");
-  for0(k, nband) printf("\n%d,%e,%e,%e,%e,%e,%e",
-		        k, fmin[k], fmax[k], avg[k], stdev[k], n_nan[k], n_inf[k]);
+  for0(k, nband){
+    printf("\n%zu,%e,%e,%e,%e,%e,%e", k + 1,
+ 	   fmin[k], fmax[k], avg[k], stdev[k], n_nan[k], n_inf[k]);
+  }
   printf("\n");
   return 0;
 }
