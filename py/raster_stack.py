c60a3b71f49cb80aadd59de00bdcd8e5b9ea9acd
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-08 21:24:10 -0800

    update

--- py/raster_stack.py
+++ py/raster_stack.py
@@ -1,57 +1,42 @@
 # stack raster files
 import os
 import sys
-args = sys.argv
-sep = os.path.sep
-exists = os.path.exists
-pd = sep.join(__file__.split(sep)[:-1]) + sep
-
-def err(m):
-    print("Error: " + m); sys.exit(1)
-
-def run(c):
-    print(c)
-    a = os.system(c)
-    if a != 0: err("failed to run: " + str(c))
+from misc import args, sep, exists, pd
 
 if len(args) < 4:
     err("python3 raster_stack.py [input raster 1] [input raster 2] .." +
         " [input raster n] [output raster name]")
 
-rasters = args[1:-1]
-outf = args[-1]
-ofhn = args[-1][:-4] + '.hdr'
-
-
-cmd = ['cat'] + rasters + ['>', outf]
+rasters = args[1:-1]  # raster files to cat together
+outf = args[-1]  # output filename
+ofhn = args[-1][:-4] + '.hdr'  # output header file name
+cmd = ['cat'] + rasters + ['>', outf]  # cat command
 cmd = ' '.join(cmd)
 print(cmd)
-print("")
+print('')
 if not exists(outf):
     a = os.system(cmd)
 
 for r in rasters:
     print(" ", r)
 
-
 cmd = ['python3', # envi_header_cat.py is almost like a reverse-polish notation. Have to put the "first thing" on the back..
        pd + 'envi_header_cat.py',
        rasters[1][:-4] + '.hdr',
        rasters[0][:-4] + '.hdr',
        ofhn]  # 'raster.hdr']
-
 cmd = ' '.join(cmd)
 run(cmd)
 
-print("")
+print('')
 
+# envi_header_cat.py almost like a RPN. Put "first thing" on the back
 if len(rasters) > 2:
     for i in range(2, len(rasters)):
-        cmd = ['python3', # envi_header_cat.py is almost like a reverse-polish notation. Have to put the "first thing" on the back..
+        cmd = ['python3',
                pd + 'envi_header_cat.py',
                rasters[i][:-4] + '.hdr',
                ofhn,  # 'raster.hdr',
                ofhn]  # 'raster.hdr']
-
         cmd = ' '.join(cmd)
         run(cmd)
