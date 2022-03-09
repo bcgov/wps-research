ef6ce0d2abd94803fb2491fb1bb960d87b5e28d0
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-08 20:53:00 -0800

    raster band subselection

--- cpp/unstack.cpp
+++ cpp/unstack.cpp
@@ -1,24 +1,44 @@
+/* Band unstacking. Should revise this with band names now
+20220308 revised to accept specific bands only */
 #include"misc.h"
 
 int main(int argc, char *argv[]){
-  if(argc < 2) err("unstack [input data file]");
-
+  if(argc < 2) err("unstack [input data file] [optional arg: band to extract (from 1)] ... [optional arg: band to extract (from 1)]");
+  size_t i, j, nr, nc, nb, np;
+  
+  set<int> selected;
+  for(i = 2; i < argc; i++) selected.insert(atoi(argv[i]));
+  
   FILE * f;
   str ifn(argv[1]);
   str hfn(hdr_fn(ifn));
-  size_t i, j, nr, nc, nb, np;
   hread(hfn, nr, nc, nb);
+  
   np = nr * nc;
+  for(set<int>::iterator it = selected.begin(); it != selected.end(); it ++){
+    if(*it < 1 || *it > nb){
+      err("selected band out of bounds");
+    }
+  }
 
   float * d = bread(ifn, nr, nc, nb); // read input data
-
+  
   for0(i, nb){
-    str obfn(ifn + str("_") + zero_pad(to_string(i + 1), 3) + str(".bin"));
-    str obhn(ifn + str("_") + zero_pad(to_string(i + 1), 3) + str(".hdr"));
-    f = wopen(obfn.c_str());
-    fwrite(&d[np * i], sizeof(float), np, f);
-    fclose(f);
-    hwrite(obhn, nr, nc, 1, 4); // always type 4! always one band
+    bool this_band = true;
+    if(selected.size() > 0){
+      if(selected.count((int)(i + 1)) < 1){
+        this_band = false;
+      }
+    }
+    if(this_band){
+      str pre(ifn + str("_") + zero_pad(to_string(i + 1), 3));
+      str ofn(pre + str(".bin"));
+      str ohn(pre + str(".hdr"));
+      f = wopen(ofn.c_str());
+      fwrite(&d[np * i], sizeof(float), np, f);
+      fclose(f);
+      hwrite(ohn, nr, nc, 1, 4); // always type 4, one band
+    }
   }
   return 0;
 }
