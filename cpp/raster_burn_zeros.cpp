a3b07081e73bccec34704a4b3d68ec43290d4938
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-09 20:03:15 -0800

    update

---
+++ cpp/raster_burn_zeros.cpp
@@ -0,0 +1,47 @@
+/* 20220309 raster_burn_zeroes.cpp: burn the zeros from one image onto
+another, in-place:
+use to remove data on image #2, where image #1 has nodata */
+#include"misc.h"
+
+int main(int argc, char ** argv){
+  if(argc < 3){
+    err("raster_sum.exe [first raster to burn zeros from] [onto this raster (in place)]\n");
+  }
+  str fn(argv[1]); // input image file name
+  str fn2(argv[2]); // input image 2 file name
+  if(!(exists(fn) && exists(fn2))) err("failed to open input file");
+
+  str hfn(hdr_fn(fn)); // input header file name
+  str hfn2(hdr_fn(fn2)); // input 2 header file name
+  str ofn(argv[3]); // output file name
+  str ohn(hdr_fn(ofn, true)); // out header file name
+
+  size_t nrow, ncol, nband, np, nrow2, ncol2, nband2;
+  hread(hfn, nrow, ncol, nband); // read header 1
+  hread(hfn2, nrow2, ncol2, nband2); // read header 2
+  if(nrow != nrow2 || ncol != ncol2 || nband != nband2)
+  err("input image dimensions should match");
+
+  np = nrow * ncol; // number of input pix
+  size_t i, j, k;
+  float * out = falloc(nrow * ncol * nband);
+  float * dat1 = bread(fn, nrow, ncol, nband);
+  float * dat2 = bread(fn2, nrow, ncol, nband);
+
+  bool is_zero;
+  for0(i, np){
+    is_zero = true;
+    for0(k, nband){
+      if(dat1[i + k * np] != 0.){
+        is_zero = false;
+      }
+    }
+    if(is_zero) for0(k, nband) dat2[i + k * np] = 0.;
+    else{
+      j = i + k * np;
+      for0(k, nband) dat2[j] = dat1[j];
+    }
+  }
+  bwrite(out, fn2, nrow, ncol, nband);
+  return 0;
+}
