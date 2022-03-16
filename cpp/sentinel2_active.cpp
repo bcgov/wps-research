e3ecbd0c5335db61d622b6237c2c0504a8be8fe7
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-15 19:57:50 -0700

    update

--- cpp/sentinel2_active.cpp
+++ cpp/sentinel2_active.cpp
@@ -1,8 +1,14 @@
 #include"misc.h"
 int main(int argc, char ** argv){
+  if(argc < 2){
+    err("sentinel2_active.exe [input file name]");
+  }
 
-  if(argc < 2) err("sentinel2_active.exe [input file name]");
   size_t nrow, ncol, nband, np, i, j, k, n, ij;
+  float * out, * dat, * b1, * b2, * b3;
+  long int bi[3];
+
+  for0(i, 3) bi[i] = -1;
 
   str fn(argv[1]); /* binary files */
   str ofn(fn + "_active.bin");
@@ -18,38 +24,35 @@ int main(int argc, char ** argv){
   np = nrow * ncol;
   n = s.size();
 
-  long int bi[3];
-  bi[0] = bi[1] = bi[2] = -1;
-
   for0(i, n){
     for0(j, 3){
       if(contains(s[i], t[j])){
-        bi[j] = i * np;
+        bi[j] = i * np; /* found a required band */
         printf("bi[%zu]=%zu \"%s\"\n", j, bi[j], s[i].c_str());
       }
     }
   }
-
-  for0(i, 3) if(bi[i] < 0){
-    printf("Missing band: %s\n", t[i].c_str());
-    err("Missing band"); 
+  for0(i, 3){
+    if(bi[i] < 0){
+      printf("Missing band: %s\n", t[i].c_str());
+      err("Missing band");
+    }
   }
 
+  dat = bread(fn, nrow, ncol, nband); /* read the data */
+  b1 = &dat[bi[0]]; /* select the bands */
+  b2 = &dat[bi[1]];
+  b3 = &dat[bi[2]];
+  out = falloc(np);
 
-
-
-  float * out = falloc(np);
-  float * dat = bread(fn, nrow, ncol, nband);
-  
-  float * b1, *b2, *b3;
-  b1 = &dat[bi[0]]; b2 = &dat[bi[1]]; b3 = &dat[bi[2]]; 
-//  for0(i, np) out[i] = (float)(dat[bi[2] + i] - dat[bi[1] + i]) > 175.;
-  for0(i, np) out[i] = (float)(b2[i] - b1[i]) > 175.;
-  cout << "second" << endl;
-  for0(i, np){ 
-      	  //  if(i % 100 == 0)  printf("i %zu np %zu\n", i, np);
-         out[i] *= (float)(b3[i] > b2[i]);
+  for0(i, np){
+    out[i] = (float)(b2[i] - b1[i]) > 175.;
   }
+
+  for0(i, np){
+    out[i] *= (float)(b3[i] > b2[i]); /* reduce fp? */
+  }
+
   bwrite(out, ofn, nrow, ncol, 1);
   hwrite(hf2, nrow, ncol, 1);
   free(dat); /* plot spectra? */
