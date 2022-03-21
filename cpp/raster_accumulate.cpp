78949cab8381dbdfaff07ca092bd6580b3136214
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-21 15:43:19 -0700

    update

--- cpp/raster_accumulate.cpp
+++ cpp/raster_accumulate.cpp
@@ -1,42 +1,39 @@
-/* raster_accumulate.cpp:
-20220320: cumulative sum (by band index) of bands */
+/* raster_accumulate.cpp: cumulative sum (by band) of single-band
+raster sequence 20220320 */
 #include"misc.h"
 
 int main(int argc, char ** argv){
-  if(argc < 2) err("raster_sum.exe [raster cube]\n");
+  size_t nrow, ncol, nband, np, i, j, k, ii, ik;
+  if(argc < 2){
+    err("raster_accumulate.exe [raster cube] [optional arg: max(result, 1.)] \n");
+  }
+  int max_1 = argc > 2;
 
-  str fn(argv[1]); // input image file name
+  str fn(argv[1]); // input image
+  str hfn(hdr_fn(fn)); // input header
   if(!exists(fn)) err("failed to open input file");
-  str hfn(hdr_fn(fn)); // input header file name
 
-  str ofn(fn + str("_accumulate.bin")); // output file name
-  str ohn(hdr_fn(ofn, true)); // out header file name
+  str ofn(fn + str("_accumulate.bin")); // output image
+  str ohn(hdr_fn(ofn, true)); // output header, create mode
 
-  size_t nrow, ncol, nband, np;
-  hread(hfn, nrow, ncol, nband); // read header 1
-
+  hread(hfn, nrow, ncol, nband); // read dimensions from header
   np = nrow * ncol; // number of input pix
-  size_t i, j, k, ix, ij, ik;
-  float * out = falloc(nrow * ncol * nband);
-  float * dat = bread(fn, nrow, ncol, nband);
+
+  float * dat = bread(fn, nrow, ncol, nband); // read input
+  float * out = falloc(nrow * ncol * nband); // output buffer
 
   for0(k, nband){
-    size_t ik = np * k;
-    size_t ii;
-    if(k == 0){
-      for0(i, np){
-        out[i] = dat[i];
-      }
-    }
+    ik = np * k;
+    if(k == 0) for0(i, np) out[i] = dat[i]; //first band same
     else{
       for0(i, np){
         ii = i + ik;
-        out[ii] = dat[ii] + out[ii - np];
-	if(out[ii] > 1.) out[ii] = 1.;
+        out[ii] = dat[ii] + out[ii - np]; // add this band + last result
+        if(max_1 && out[ii] > 1.) out[ii] = 1.; // max result = 1.
       }
     }
   }
-  bwrite(out, ofn, nrow, ncol, nband);
-  hwrite(ohn, nrow, ncol, nband);
+  hwrite(ohn, nrow, ncol, nband); // write out dimensions to header
+  bwrite(out, ofn, nrow, ncol, nband); // write out image
   return 0;
 }
