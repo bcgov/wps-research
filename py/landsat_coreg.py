d7eda6a4928dc6e97744d5533dd295d6423c3f77
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-03-25 15:49:54 -0700

    update

--- py/landsat_coreg.py
+++ py/landsat_coreg.py
@@ -14,4 +14,10 @@ X = ["./L8_vernon/LC08_L2SP_045025_20210803_20210811_02_T1/LC08_L2SP_045025_2021
      "./L7_vernon/LE07_L2SP_046025_20210717_20210812_02_T1/LE07_L2SP_046025_20210717_20210812_02_T1.bin_spectral_interp.bin_active.bin",
      "./L7_vernon/LE07_L2SP_046024_20210818_20210913_02_T1/LE07_L2SP_046024_20210818_20210913_02_T1.bin_spectral_interp.bin_active.bin"]
 
+for x in X:
+    run(['python3 ' + pd + 'raster_project_onto.py',
+         x,
+         'footprint3.bin',
+         x + '_coreg.bin'])
+
 
