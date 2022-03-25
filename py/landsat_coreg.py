cc7e29b805ca3ef3e0db8fc069938862c3d6acad
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-03-25 16:53:26 -0700

    update

--- py/landsat_coreg.py
+++ py/landsat_coreg.py
@@ -14,10 +14,32 @@ X = ["./L8_vernon/LC08_L2SP_045025_20210803_20210811_02_T1/LC08_L2SP_045025_2021
      "./L7_vernon/LE07_L2SP_046025_20210717_20210812_02_T1/LE07_L2SP_046025_20210717_20210812_02_T1.bin_spectral_interp.bin_active.bin",
      "./L7_vernon/LE07_L2SP_046024_20210818_20210913_02_T1/LE07_L2SP_046024_20210818_20210913_02_T1.bin_spectral_interp.bin_active.bin"]
 
+files = []
 for x in X:
-    run(['python3 ' + pd + 'raster_project_onto.py',
-         x,
-         'footprint3.bin',
-         x + '_coreg.bin'])
+    cf = x + '_coreg.bin'
+    #print(cf)
+    files.append(cf)
+    if not exists(cf):
+        run(['python3 ' + pd + 'raster_project_onto.py',
+              x,
+              'footprint3.bin',
+              cf])
 
+Y = []
+for f in files:
+    w = f.split(sep)[-1].split('_')
+    Y.append([w[3], f])
 
+Y.sort()
+
+cmd = 'cat'
+for f in Y:
+    cmd += (' ' + f[1])
+cmd += ' > landsat.bin'
+
+if not exists('landsat.bin'):
+    run(cmd)
+
+run('cp footprint3.hdr landsat.hdr')
+
+cmd = 'python3 ' + pd + 'envi_header_modify.py'
