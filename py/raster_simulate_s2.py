c6bf1978abff91d65876901660a881e9b125e081
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-24 20:57:31 -0700

    update

--- py/raster_simulate_s2.py
+++ py/raster_simulate_s2.py
@@ -1,19 +1,19 @@
-'''20220324: interpolate hyperspectral image to these frequencies:
-band names = {20210719 60m: B1 443nm,
-20210719 10m: B2 490nm,
-20210719 10m: B3 560nm,
-20210719 10m: B4 665nm,
-20210719 20m: B5 705nm,
-20210719 20m: B6 740nm,
-20210719 20m: B7 783nm,
-20210719 10m: B8 842nm,
-20210719 20m: B8A 865nm,
-20210719 60m: B9 945nm,
-20210719 20m: B11 1610nm,
-20210719 20m: B12 2190nm}'''
+'''20220324: interpolate hyperspectral image to the frequencies at right:
+    band names = {20210719 60m: B1 443nm,
+                  20210719 10m: B2 490nm,
+                  20210719 10m: B3 560nm,
+                  20210719 10m: B4 665nm,
+                  20210719 20m: B5 705nm,
+                  20210719 20m: B6 740nm,
+                  20210719 20m: B7 783nm,
+                  20210719 10m: B8 842nm,
+                  20210719 20m: B8A 865nm,
+                  20210719 60m: B9 945nm,
+                  20210719 20m: B11 1610nm,
+                  20210719 20m: B12 2190nm}'''
 from misc import err, run, cd, args
 if len(args) < 2:
-    err('python3 raster_simulate_s2.py [input hyperspectral file name (ENVI format)]')
+    err('python3 raster_simulate_s2.py [input hyperspectral raster (ENVI format)]')
 
 run([cd + 'raster_spectral_interp.exe',
      args[1],
