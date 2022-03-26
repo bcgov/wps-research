d82c1ca13115cd1a417d0b0e27e9922bf290897c
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-25 22:36:19 -0700

    update

--- cpp/unstack.cpp
+++ cpp/unstack.cpp
@@ -15,19 +15,34 @@ int main(int argc, char *argv[]){
   hread(hfn, nr, nc, nb);
   vector<str> band_names(parse_band_names(hfn));
 
+  /* don't include band names in output filenames
+      if band names look like folders*/
+  int use_bn = true;
+  for(vector<str>::iterator it = band_names.begin();
+      it != band_names.end();
+      it++){
+    str x(*it);
+    vector<str> y(split(x, ' '));
+    vector<str> z(split(x, '/'));
+    if(y.size() > 1 || z.size() > 1) use_bn = false;
+  }
+
   np = nr * nc;
-  for(set<int>::iterator it = selected.begin(); it != selected.end(); it ++){
+  for(set<int>::iterator it = selected.begin();
+      it != selected.end();
+      it ++){
     if(*it < 1 || *it > nb){
       err("selected band out of bounds");
     }
   }
 
   float * d = bread(ifn, nr, nc, nb); // read input data
-  
   for0(i, nb){
     if(selected.size() < 1 || selected.count((int)(i + 1)) > 0){
       str pre(ifn + str("_") + zero_pad(to_string(i + 1), 3));
-      pre += (str("_") + band_names[i]);
+      if(use_bn){
+        pre += (str("_") + band_names[i]);
+      }
       str ofn(pre + str(".bin"));
       str ohn(pre + str(".hdr"));
       f = wopen(ofn.c_str());
