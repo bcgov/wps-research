f056e24b50d6c04d367cf135cb4bb0c3141ae1e0
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-04-05 15:58:09 -0700

    update

--- py/gcp/install_gcp.py
+++ py/gcp/install_gcp.py
@@ -1,6 +1,4 @@
-'''
-https://cloud.google.com/sdk/docs/install
-'''
+'''https://cloud.google.com/sdk/docs/install'''
 import os
 import sys
 sys.path.append("..")
@@ -8,7 +6,8 @@ from misc import run, pd, sep, exists
 
 remote_p  = 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/'
 remote_f = 'google-cloud-sdk-379.0.0-linux-x86_64.tar.gz'
-remote = remote_p + remote_f
+remote = remote_p + remote_f # remote path
+
 dp = os.path.abspath(pd + 'gcp' + sep) + sep  # local directory path
 tfn = dp + remote_f  # local target file name
 
@@ -17,20 +16,20 @@ cmd = ' '.join(['wget',
                 'http://dl.google.com/dl/cloudsdk/channels/rapid/downloads/' + remote_f])
 print('+r', tfn)
 if not exists(tfn):
-    run(cmd)
+    run(cmd)  # download the remote file
 
-sdk_folder = dp + 'google-cloud-sdk' + sep
+sdk_folder = dp + 'google-cloud-sdk' + sep  # local extraction location
 if not exists(sdk_folder):
     run('tar xvf ' + tfn)
 
-gsutil = os.popen('which gsutil').read().strip() # what comes up under gsutil
+gsutil = os.popen('which gsutil').read().strip() # what comes up under gsutil?
 gsutil_t = sdk_folder + 'bin' + sep + 'gsutil' # where our gsutil command should be!
 if gsutil != gsutil_t:
-    run(sdk_folder + 'install.sh')
+    run(sdk_folder + 'install.sh')  # add install info to ~/.bashrc file
     cmd = sep.join(['source ',  'home', os.popen('whoami').read().strip(), '.bashrc'])
-    run(cmd)
+    run(cmd) # load the ~.bashrc file to be able to use the command
 
-if gsutil != gsutil_t:
+if gsutil != gsutil_t:  # check command without prefix in expected location
     err('expected: ' + gsutil + ' == ' + gsutil_t)
 else:
     print('gsutil installed')
@@ -38,7 +37,6 @@ else:
 ''' do we need this step:?
      ./google-cloud-sdk/bin/gcloud init
 '''
-
 
 # install crcmod
 try:
