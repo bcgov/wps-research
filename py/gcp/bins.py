732a4040f3bcb6e59a68517b9147547f9cf6ba19
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-04-06 10:39:13 -0700

    update

--- py/gcp/bins.py
+++ py/gcp/bins.py
@@ -1,8 +1,21 @@
 '''open .bin files listed in bins.txt, with imv'''
 import os
 import sys
-from ../misc import run
-lines = open("bins.txt").readlines()
+sep = os.path.sep
+sys.path.append(sep.join(os.path.abspath(__file__).split(sep)[:-2]) + sep)
+from misc import run, args
 
-for line in lines:
-    run("imv " + line.strip())
+print('python3 sentinel2_imv.py [optional arg: just list sorted dates]')
+
+cmd = 'find ./ -name "*_10m.bin"'
+X = [x.strip().split('_') for x in os.popen(cmd).readlines()]
+X = [[x[2], x] for x in X]
+X.sort()
+
+for x in X:
+    fn = '_'.join(x[1])
+    cmd = 'imv ' + fn
+    if len(args) < 2:
+        run(cmd)
+    else:
+        print(fn)
