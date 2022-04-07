7217ef770ea303357d8573a3ff3016663844d65c
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-04-07 16:24:47 -0700

    update

--- py/sentinel2_swir_dominant_all.py
+++ py/sentinel2_swir_dominant_all.py
@@ -1,21 +1,39 @@
 ''' dominant classifier on SWIR subselection, for all S2 dates in folder 20220407'''
 import os
 import sys
-from misc import run, exists
+import multiprocessing as mp
+from misc import run, exists, parfor
+
+try:
+    run('find ./ -name "*norm*" | xargs rm')
+except:
+    pass
+try:
+    run('find ./ -name "*dominant*" | xargs rm')
+except:
+    pass
+
 lines = [x.strip() for x in os.popen('find ./ -name "S*10m.bin_swir.bin"').readlines()]
 
 x = []
+cmd1 = []
+cmd2 = []
 for line in lines:
     df = line
     cmd = '~/GitHub/bcws-psu-research/cpp/raster_normalize.exe ' + df
     df += '_norm.bin'
     if not exists(df):
-        run(cmd)
+        cmd1.append(cmd) #run(cmd)
     cmd = '~/GitHub/bcws-psu-research/cpp/raster_dominant.exe ' + df
     df = line + '_dominant.bin'
     if not exists(df):
-        run(cmd)
+        cmd2.append(cmd) # run(cmd)
     x.append(df)
+
+print("normalizing..")
+parfor(run, cmd1, int(mp.cpu_count()/2))
+print("classifying..")
+parfor(run, cmd2, int(mp.cpu_count()/2))
 
 X = []
 for i in x:
