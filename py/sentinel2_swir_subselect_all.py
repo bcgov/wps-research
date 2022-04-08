95c724e3479a40ecbfafec81d174eca04fe75682
ashlinrichardson <richardson.ashlin@gmail.com>
2022-04-07 21:50:08 -0700

    update

--- py/sentinel2_swir_subselect_all.py
+++ py/sentinel2_swir_subselect_all.py
@@ -1,9 +1,12 @@
 ''' SWIR subselection for all bands 20220407'''
 import os
 import sys
+import multiprocessing as mp
+from misc import parfor, run
 lines = [x.strip() for x in os.popen('find ./ -name "S*10m.bin"').readlines()]
 
+cmds = []
 for line in lines:
     cmd = '~/GitHub/bcws-psu-research/cpp/sentinel2_swir_subselect.exe ' + line
-    print(cmd)
-    a = os.system(cmd)
+    cmds.append(cmd)
+parfor(run, cmds, mp.cpu_count() / 2)
