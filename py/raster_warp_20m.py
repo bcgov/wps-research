b8b3b88ffd813dfc8a75123e934ad9c7c5d9e372
ashlinrichardson <richardson.ashlin@gmail.com>
2022-04-11 17:32:43 -0700

    update

--- py/raster_warp_20m.py
+++ py/raster_warp_20m.py
@@ -2,7 +2,7 @@
 using parallelism (all cores) 20220411'''
 import os
 import sys
-from misc import err, run, args
+from misc import err, run, args, exists
 
 message = "python3 raster_warp_20m.py [input raster file name]"
 
@@ -10,6 +10,12 @@ if len(args) < 2:
     err(message)
 infile = args[1]
 ofn = infile + '_20m.bin'
+
+if not exists(infile):
+    err("please check input file")
+
+if exists(ofn):
+    err("output file already exists: " + ofn)
 
 run(' '.join(['gdalwarp -of ENVI -tr 20 20',
               '-multi',
