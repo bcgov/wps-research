e2f0ccf9a979e892e0e2404b7cf1549cbd63f034
ashlinrichardson <richardson.ashlin@gmail.com>
2022-04-11 17:41:35 -0700

    update

--- py/raster_warp_20m.py
+++ py/raster_warp_20m.py
@@ -2,7 +2,7 @@
 using parallelism (all cores) 20220411'''
 import os
 import sys
-from misc import err, run, args, exists
+from misc import err, run, args, exists, pd
 
 message = "python3 raster_warp_20m.py [input raster file name]"
 
@@ -17,10 +17,19 @@ if not exists(infile):
 print('+w ' + ofn)
 
 if exists(ofn):
-    err("output file already exists: " + ofn)
+    print("output file already exists: " + ofn)
+else:
+    run(' '.join(['gdalwarp -of ENVI -tr 20 20',
+                  '-multi',
+                  '-wo NUM_THREADS=val/ALL_CPUS',
+                  infile,
+                  ofn]))
 
-run(' '.join(['gdalwarp -of ENVI -tr 20 20',
-              '-multi',
-              '-wo NUM_THREADS=val/ALL_CPUS',
-              infile,
-              ofn]))
+run(['python3',
+     pd + 'envi_header_cleanup.py',
+     infile[:-4] + '.hdr'])
+
+run(['python3',
+     pd + 'envi_header_copy_mapinfo.py',
+     infile[:-4] + '.hdr',
+     ofn[:-4] + '.hdr'])
