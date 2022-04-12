b5e80c950f6ad6e928a6b2a1da1b5c6cb902e486
ashlinrichardson <richardson.ashlin@gmail.com>
2022-04-11 17:28:27 -0700

    resize raster to 20m

---
+++ py/raster_warp_20m.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from misc import err, run, args
+
+message = "python3 raster_warp_20m.py [input raster file name]"
+
+if len(args) < 2:
+    err(message)
+infile = args[1]
+ofn = infile + '_20m.bin'
+
+run(' '.join(['gdalwarp -of ENVI -tr 20 20',
+              infile,
+              ofn])
