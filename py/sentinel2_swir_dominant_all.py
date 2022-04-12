7432ac797c23a5a5c9562af57d5916e190fa98e7
ashlinrichardson <richardson.ashlin@gmail.com>
2022-04-11 21:39:21 -0700

    update

--- py/sentinel2_swir_dominant_all.py
+++ py/sentinel2_swir_dominant_all.py
@@ -3,6 +3,8 @@ import os
 import sys
 import multiprocessing as mp
 from misc import run, exists, parfor, args
+N_THREAD = mp.cpu_count()
+print('sentinel2_swir_dominant_all.py [optional arg: dry run]')
 '''
 if False:
     try:
@@ -15,7 +17,7 @@ if False:
         pass
 '''
 lines = [x.strip() for x in os.popen('find ./ -name "S*10m.bin_swir.bin"').readlines()]
-'''
+
 x = []
 cmd1 = []
 cmd2 = []
@@ -35,11 +37,11 @@ print(cmd1)
 print(cmd2)
 if len(cmd1) > 0 and len(args) < 2:
     print("normalizing..")
-    parfor(run, cmd1, int(mp.cpu_count()/2))
+    parfor(run, cmd1, int(N_THREAD / 2))
 if len(cmd2) > 0 and len(args) < 2:
     print("classifying..")
-    parfor(run, cmd2, int(mp.cpu_count()/2))
-'''
+    parfor(run, cmd2, int(N_THREAD / 2))
+
 x = lines
 X = []
 for i in x:
@@ -52,4 +54,3 @@ print()
 for i in X:
     x = i[1]
     run('imv ' + x + '_norm.bin_dominant.bin')
-
