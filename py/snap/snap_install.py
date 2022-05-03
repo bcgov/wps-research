213a7ee51e027ccef89d581a4696cdebb7580c55
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-05-03 11:45:45 -0700

    update

--- py/snap/snap_install.py
+++ py/snap/snap_install.py
@@ -13,7 +13,7 @@ import stat
 sep = os.path.sep
 my_path = sep.join(os.path.abspath(__file__).split(sep)[:-1]) + sep
 sys.path.append(my_path + "..")  # relative import
-from misc import run, exists, pd, sep
+from misc import run, exists, pd, sep, err
 
 path = mirror if use_mirror else main
 fn = path.split('/')[-1]
@@ -34,3 +34,59 @@ else:
 x = str(oct(stat.S_IMODE(os.lstat(target).st_mode)))[-3:]
 if x != '744':
     run(['sudo', 'chmod', '744', target])
+
+if not exists('/usr/local/bin/esa-snap'):
+    def re_name():
+        run('sudo mv /usr/local/bin/snap /usr/local/bin/esa-snap')
+
+    if exists('/usr/local/bin/snap'):
+        re_name()
+    else:
+        print('installing ESA snap..' +
+              'Please select all defaults incl. python support')
+        run('sudo ' + target)
+        re_name()
+
+try:
+    import snappy
+except Exception:
+    print('configuring snappy..')
+    def pyc():
+        x = os.popen('which python3 2>&1').read().strip()
+        if x == '' or x.split(':')[-1].strip() == 'command not found':
+            return None
+        else:
+            return x
+
+    if pyc() == None:
+        print('python3 not found, attempting install..')
+        run('sudo apt install python3')
+
+    py_cmd = pyc()
+    if py_cmd == None:
+        err('python3 not found, pls. check python path')
+
+    sc = '/opt/snap/bin/snappy-conf'
+    if not exists(sc):
+        err(sc + ' not found, please check snap install path')
+    
+    result = os.popen(' '.join([sc, py_cmd, '2>&1'])).read().strip()
+    if len(result.split('Please check the log file')) > 1:
+        logfile = None
+        lines = [x.strip() for x in result.split('\n')]
+        for line in lines:
+            w = line.split('Please check the log file')
+            if len(w) > 1:
+                logfile = w[-1].strip().strip('.').strip("'")
+            
+        print(logfile)
+        lines = [x.strip() for x in open(logfile).read().strip().split('\n')]
+        
+        for line in lines:
+            w = line.split("ERROR: The module 'jpy' is required to run snappy")
+            if len(w) > 1:
+                print('attempting to install jpy..')
+                print(line)
+    
+
+
