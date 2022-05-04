a5646326fbaa2a8bc11257f4685936e5adef521b
ashlinrichardson <richardson.ashlin@gmail.com>
2022-05-03 21:02:52 -0700

    update

--- py/raster_pixels_loc.py
+++ py/raster_pixels_loc.py
@@ -29,50 +29,45 @@ crs = osr.SpatialReference()
 print(src.GetProjectionRef())
 crs.ImportFromWkt(src.GetProjectionRef())
 
-# create lat/long crs with WGS84 datum
+# create lat/long crs: WGS84 datum
 crsGeo = osr.SpatialReference()
-crsGeo.ImportFromEPSG(4326) # 4326 is the EPSG id of lat/long crs
+crsGeo.ImportFromEPSG(4326)  # 4326 is EPSG-id of lat/lon crs
 t = osr.CoordinateTransformation(crs, crsGeo)
 
 dat = np.zeros(npx * 2)  # output raster for lon, lat
-for Y_line in range(nrow): # row 0-idx
+for Y_line in range(nrow):  # row 0-idx
     y_i = Y_line * ncol
     y_i2 = y_i + npx
     yl = Y_line # + .5
-    for X_pixel in range(ncol): # col 0-idx
+    for X_pixel in range(ncol):  # col 0-idx
         xp = X_pixel # + .5
-       
         X_geo = gx[0] + xp * gx[1] + yl * gx[2]
         Y_geo = gx[3] + xp * gx[4] + yl * gx[5]
-        
-        # shift to center of the pixel
-        X_geo += px_w / 2.
+        X_geo += px_w / 2.  # shift to pixel centre
         Y_geo += px_h / 2.
         (lat, lon, z) = t.TransformPoint(X_geo, Y_geo)
-
         dat[y_i + X_pixel] = lon
         dat[y_i2 + X_pixel] = lat 
 fn = img + '_lonlat.bin'
-write_binary(dat, fn)
-
-hi = hdr_fn(img)
 hn = img + '_lonlat.hdr'
+write_binary(dat, fn)  # write out raster
+hi = hdr_fn(img)
 
-print('+w', hn)
+print('+w', hn)  # copy existing hdr..
 shutil.copyfile(hi, hn)
 
-# update image shape & band names
-run(['python3', # python interp
-     pd + 'envi_header_modify.py', # python script path
-     hn, # header file name
-     str(nrow), # number of rows
-     str(ncol), # number of cols
-     '2',  # two bands
-     'lon',  # band name 1
-     'lat']) # band name 2
-
-# update map information
+# .. now update dimensions & band names
 run(['python3',  # python interp
-     pd + 'envi_header_copy_mapinfo.py', # python script path
-     hi, # input header file to copy from
+     pd + 'envi_header_modify.py',  # python script path
+     hn,  # header file name
+     str(nrow),  # number of rows
+     str(ncol),  # number of cols
+     '2',   # two bands
+     'lon',   # band name 1
+     'lat'])  # band name 2
+
+# update map info for raster
+run(['python3',  # python interp
+     pd + 'envi_header_copy_mapinfo.py',  # python script path
+     hi,  # input header file to copy from
      hn])  # copy mapinfo to this header file
