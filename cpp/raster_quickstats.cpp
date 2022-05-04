cdda3d846314f9a314b892b696918d8da451eff5
ashlinrichardson <richardson.ashlin@gmail.com>
2022-05-03 21:13:00 -0700

    update

--- cpp/raster_quickstats.cpp
+++ cpp/raster_quickstats.cpp
@@ -1,9 +1,13 @@
 /* qs.cpp: quick stats for image cube */
 #include"misc.h"
 int main(int argc, char ** argv){
-  if(argc < 2) err("qs.exe [raster cube 1]");
+  if(argc < 2)
+    err("qs.exe [raster cube 1]"); // usage
+  
   str fn(argv[1]); // input image file name
-  if(!exists(fn)) err("failed to open input file");
+  if(!exists(fn))
+    err("failed to open input file");
+
   str hfn(hdr_fn(fn)); // input header file name
   size_t nrow, ncol, nband, np;
   hread(hfn, nrow, ncol, nband); // read header 1
@@ -20,30 +24,36 @@ int main(int argc, char ** argv){
   double * n_inf = dalloc(nband); // count infinity
   double * n_nan = dalloc(nband); // count nan
   double * total_squared = dalloc(nband);
-  float * dat = bread(fn, nrow, ncol, nband);
+  float * dat = bread(fn, nrow, ncol, nband); // output
 
   for0(i, nband){
     fmax[i] = -DBL_MAX;
-    fmin[i] = DBL_MAX;
-    total[i] = n[i] = avg[i] = total_squared[i] = stdev[i] = n_inf[i] = n_nan[i] = 0.;
+    fmin[i] =  DBL_MAX;
+    total[i] = n[i] = avg[i] = stdev[i] = 0.;
+    total_squared[i] = n_inf[i] = n_nan[i] = 0.;
   }
 
   for0(i, np){
     for0(k, nband){
       d = dat[k * np + i];
       if(!(isinf(d) || isnan(d))){
-        if(d < fmin[k]) fmin[k] = d;
-        if(d > fmax[k]) fmax[k] = d;
+        if(d < fmin[k])
+	  fmin[k] = d;
+        if(d > fmax[k])
+	  fmax[k] = d;
         total[k] += d;
         n[k] ++;
       }
       else{
-        if(isinf(d)) n_inf[k]++;
-        if(isnan(d)) n_nan[k]++;
+        if(isinf(d))
+	  n_inf[k]++;
+        if(isnan(d))
+	  n_nan[k]++;
       }
     }
   }
-  for0(k, nband) avg[k] = total[k] / n[k];
+  for0(k, nband)
+    avg[k] = total[k] / n[k];
 
   for0(i, np){
     for0(k, nband){
@@ -54,12 +64,11 @@ int main(int argc, char ** argv){
       }
     }
   }
-  for0(k, nband){
+  for0(k, nband)
     stdev[k] = sqrt(total_squared[k] / n[k]);
-  }
+ 
   printf("band_i,Min,Max,Mean,Stdv,n_nan,n_inf");
-
-  for0(k, nband){
+  for0(k, nband)
     printf("\n%zu,%e,%e,%e,%e,%e,%e",
 	    k + 1,  // band ix
 	    fmin[k], // min value this band
@@ -68,7 +77,6 @@ int main(int argc, char ** argv){
 	    stdev[k], // standard deviation this band
 	    n_nan[k], // nan count this band
 	    n_inf[k]); // inf count this band
-  }
   printf("\n");
   return 0;
 }
