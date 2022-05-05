3ab4ab7ad4b3b0cc2c4e42843896f982f70f8307
ashlinrichardson <richardson.ashlin@gmail.com>
2022-05-04 21:58:04 -0700

    update

--- cpp/raster_ni.cpp
+++ cpp/raster_ni.cpp
@@ -2,31 +2,31 @@
 #include"misc.h"
 
 int main(int argc, char ** argv){
-  if(argc < 2) err("raster_ni.exe [raster cube, 2 bands\n");
+  if(argc < 2)
+    err("raster_ni.exe [raster cube, 2 bands\n");
 
-  str fn(argv[1]); // input image file name
-  if(!(exists(fn))) err("failed to open input file");
+  str fn(argv[1]); // input image file
+  if(!(exists(fn))) err("failed to open input");
 
-  str hfn(hdr_fn(fn)); // input header file name
-  str ofn(fn + str("_ni.bin")); // output file name
-  str ohn(hdr_fn(ofn, true)); // out header file name
+  str hfn(hdr_fn(fn)); // input header file 
+  str ofn(fn + str("_ni.bin")); // output file
+  str ohn(hdr_fn(ofn, true)); // out header file
 
   size_t nrow, ncol, nband, np, i;
-  hread(hfn, nrow, ncol, nband); // read header 1
-  if(nband != 2) err("two band input required for normalized index");
+  hread(hfn, nrow, ncol, nband); // read header
+  if(nband != 2) err("normalied index: 2-bands req'd");
 
-  np = nrow * ncol; // number of input pix
-  float * out = falloc(np);
+  np = nrow * ncol;
+  float * out = falloc(np); // alloc pixls
   float * dat = bread(fn, nrow, ncol, nband);
+
+  float t, b, d;
   float * b1 = dat;
   float * b2 = &dat[np];
-
   for0(i, np){
-	  float top = b1[i] - b2[i];
-	  float bot = b1[i] + b2[i];
-	  float d = top / bot;
-	  if(isnan(d) || isinf(d)) d = NAN;
-	  out[i] = d;
+    t = b1[i] - b2[i];
+    b = b1[i] + b2[i]; d = t / b;
+    out[i] = (isnan(d) || isinf(d))? NAN: d;
   }
   bwrite(out, ofn, nrow, ncol, 1);
   hwrite(ohn, nrow, ncol, 1);
