42fa8233b72abae75105ce3e12c1cf73b9151a73
ashlinrichardson <richardson.ashlin@gmail.com>
2022-05-09 16:48:06 -0700

    update

---
+++ install_sen2cor.sh
@@ -0,0 +1,5 @@
+mkdir ~/sen2cor/
+cd ~/sen2cor/
+wget http://step.esa.int/thirdparties/sen2cor/2.5.5/Sen2Cor-02.05.05-Linux64.run
+chmod 755 Sen2Cor-02.05.05-Linux64.run
+./Sen2Cor-02.05.05-Linux64.run
