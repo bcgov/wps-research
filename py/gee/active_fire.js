ac4cbcbbe20a4a118383c66c111ac58e2b8ae276
ashlinrichardson <richardson.ashlin@gmail.com>
2022-05-11 10:52:50 -0700

    update

--- py/gee/active_fire.js
+++ py/gee/active_fire.js
@@ -1,24 +1,24 @@
 /*20220510 active_fire.js*/
 
 /* PARAMS ==============================================================*/
-var t1 = ee.Date('2021-8-02T00:00', 'Etc/GMT-8');
+var t1 = ee.Date('2021-8-02T00:00', 'Etc/GMT-8')
 var N_DAYS = 14  // date range: [t1, t1 + N_DAYS]
 var CLOUD_THRES = 22.2 // cloud cover max %
 /* END PARAMS ==========================================================*/
 
 // function from online example:
 function maskS2clouds(image) {
-  var qa = image.select('QA60'); // Bits 10, 11: clouds, cirrus, resp.
-  var cloudBitMask = 1 << 10; // set both 0 == clear conditions
-  var cirrusBitMask = 1 << 11;
-  var mask = qa.bitwiseAnd(cloudBitMask).eq(0).and(qa.bitwiseAnd(cirrusBitMask).eq(0));
-  return image.updateMask(mask).divide(10000);
+  var qa = image.select('QA60') // Bits 10, 11: clouds, cirrus, resp.
+  var cloudBitMask = 1 << 10 // set both 0 == clear conditions
+  var cirrusBitMask = 1 << 11
+  var mask = qa.bitwiseAnd(cloudBitMask).eq(0).and(qa.bitwiseAnd(cirrusBitMask).eq(0))
+  return image.updateMask(mask).divide(10000)
 }
 
 /* get DEM, LandCover, Sentinel-2 "L2A" (level two atmospherically-
 -corrected "bottom of atmosphere (BOA) reflectance) data */
-var nasa_dem = ee.Image('NASA/NASADEM_HGT/001').select('elevation'); 
-var land_cover = ee.ImageCollection("ESA/WorldCover/v100").first(); 
+var nasa_dem = ee.Image('NASA/NASADEM_HGT/001').select('elevation') 
+var land_cover = ee.ImageCollection("ESA/WorldCover/v100").first()
 var data = ee.ImageCollection('COPERNICUS/S2_SR').filterDate(t1,
 	t1.advance(N_DAYS, 'day'))
 var dates = data.map(function(image){
@@ -27,7 +27,8 @@ var dates = data.map(function(image){
 print(dates)
 
 /* apply cloud threshold and mask */
-data = data.filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', CLOUD_THRES)).map(maskS2clouds).mean();
+data = data.filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',
+				CLOUD_THRES)).map(maskS2clouds).mean()
 var visualization = {min: 0.0, max: 1.0,
 	             bands: ['B12', 'B11', 'B9']}; // rgb vis: fires are "red"
 
@@ -39,7 +40,7 @@ var r = data.expression(rule, { 'R': data.select('B12'),
       		   	 	'LC': land_cover.select('Map'),
     				'DEM': nasa_dem});
 Map.addLayer(r, {min: 0, max: 1})  // the binary classification
-Map.addLayer(data, visualization, 'RGB'); // SWIR vis
+Map.addLayer(data, visualization, 'RGB') // SWIR vis
 Map.setCenter(-119.97, 50.34)
 
 /* Values of ESA/WorldCover/v100:
