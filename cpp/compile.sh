f0df318333663ffd6a26264a4fa9f38140c5a7cc
ashlinrichardson <richardson.ashlin@gmail.com>
2022-05-15 16:12:55 -0700

    update

--- cpp/compile.sh
+++ cpp/compile.sh
@@ -2,51 +2,74 @@
 test ! -f band_splice.exe && g++ -w -O4 band_splice.cpp  misc.cpp -o band_splice.exe -lpthread &
 test ! -f binary_confusion.exe && g++ -w -O4 binary_confusion.cpp  misc.cpp -o binary_confusion.exe -lpthread &
 test ! -f binary_dilate.exe && g++ -w -O4 binary_dilate.cpp  misc.cpp -o binary_dilate.exe -lpthread &
+test ! -f binary_hull.exe && g++ -w -O4 binary_hull.cpp  misc.cpp -o binary_hull.exe -lpthread &
 test ! -f binary_invert.exe && g++ -w -O4 binary_invert.cpp  misc.cpp -o binary_invert.exe -lpthread &
+test ! -f binary_list.exe && g++ -w -O4 binary_list.cpp  misc.cpp -o binary_list.exe -lpthread &
 test ! -f cat_append.exe && g++ -w -O4 cat_append.cpp  misc.cpp -o cat_append.exe -lpthread &
 test ! -f class_count.exe && g++ -w -O4 class_count.cpp  misc.cpp -o class_count.exe -lpthread &
+test ! -f class_link.exe && g++ -w -O4 class_link.cpp  misc.cpp -o class_link.exe -lpthread &
 test ! -f class_match_onto.exe && g++ -w -O4 class_match_onto.cpp  misc.cpp -o class_match_onto.exe -lpthread &
 test ! -f class_mean.exe && g++ -w -O4 class_mean.cpp  misc.cpp -o class_mean.exe -lpthread &
 test ! -f class_mode_filter.exe && g++ -w -O4 class_mode_filter.cpp  misc.cpp -o class_mode_filter.exe -lpthread &
 test ! -f class_onehot.exe && g++ -w -O4 class_onehot.cpp  misc.cpp -o class_onehot.exe -lpthread &
 test ! -f class_recode.exe && g++ -w -O4 class_recode.cpp  misc.cpp -o class_recode.exe -lpthread &
 test ! -f class_replace_nan.exe && g++ -w -O4 class_replace_nan.cpp  misc.cpp -o class_replace_nan.exe -lpthread &
-test ! -f class_wheel.exe && g++ -w -O4 class_wheel.cpp  misc.cpp -o class_wheel.exe -lpthread &
+test ! -f class_wheel.exe && g++ -w -O4 class_wheel.cpp  misc.cpp -o class_wheel.exe -lpthread
 test ! -f cluster.exe && g++ -w -O4 cluster.cpp  misc.cpp -o cluster.exe -lpthread &
 test ! -f convert_cplx_to_iq.exe && g++ -w -O4 convert_cplx_to_iq.cpp  misc.cpp -o convert_cplx_to_iq.exe -lpthread &
-test ! -f convert_iq_to_cplx.exe && g++ -w -O4 convert_iq_to_cplx.cpp  misc.cpp -o convert_iq_to_cplx.exe -lpthread
+test ! -f convert_iq_to_cplx.exe && g++ -w -O4 convert_iq_to_cplx.cpp  misc.cpp -o convert_iq_to_cplx.exe -lpthread &
 test ! -f convert_iq_to_s2.exe && g++ -w -O4 convert_iq_to_s2.cpp  misc.cpp -o convert_iq_to_s2.exe -lpthread &
 test ! -f csv_spectra_class_raster_dist.exe && g++ -w -O4 csv_spectra_class_raster_dist.cpp  misc.cpp -o csv_spectra_class_raster_dist.exe -lpthread &
 test ! -f csv_split.exe && g++ -w -O4 csv_split.cpp  misc.cpp -o csv_split.exe -lpthread &
 test ! -f cut.exe && g++ -w -O4 cut.cpp  misc.cpp -o cut.exe -lpthread &
 test ! -f cv.exe && g++ -w -O4 cv.cpp  misc.cpp -o cv.exe -lpthread &
+test ! -f float_max.exe && g++ -w -O4 float_max.cpp  misc.cpp -o float_max.exe -lpthread &
+test ! -f flood.exe && g++ -w -O4 flood.cpp  misc.cpp -o flood.exe -lpthread &
 test ! -f hclust.exe && g++ -w -O4 hclust.cpp  misc.cpp -o hclust.exe -lpthread &
 test ! -f hsv2rgb.exe && g++ -w -O4 hsv2rgb.cpp  misc.cpp -o hsv2rgb.exe -lpthread &
 test ! -f htrim2.exe && g++ -w -O4 htrim2.cpp  misc.cpp -o htrim2.exe -lpthread &
 test ! -f kmeans.exe && g++ -w -O4 kmeans.cpp  misc.cpp -o kmeans.exe -lpthread &
 test ! -f kmeans_iter.exe && g++ -w -O4 kmeans_iter.cpp  misc.cpp -o kmeans_iter.exe -lpthread &
-test ! -f kmeans_multi.exe && g++ -w -O4 kmeans_multi.cpp  misc.cpp -o kmeans_multi.exe -lpthread &
+test ! -f kmeans_multi.exe && g++ -w -O4 kmeans_multi.cpp  misc.cpp -o kmeans_multi.exe -lpthread
 test ! -f multilook.exe && g++ -w -O4 multilook.cpp  misc.cpp -o multilook.exe -lpthread &
 test ! -f multilook_large.exe && g++ -w -O4 multilook_large.cpp  misc.cpp -o multilook_large.exe -lpthread &
 test ! -f multiply.exe && g++ -w -O4 multiply.cpp  misc.cpp -o multiply.exe -lpthread &
 test ! -f nodata.exe && g++ -w -O4 nodata.cpp  misc.cpp -o nodata.exe -lpthread &
-test ! -f raster_at.exe && g++ -w -O4 raster_at.cpp  misc.cpp -o raster_at.exe -lpthread
+test ! -f raster_accumulate.exe && g++ -w -O4 raster_accumulate.cpp  misc.cpp -o raster_accumulate.exe -lpthread &
+test ! -f raster_at.exe && g++ -w -O4 raster_at.cpp  misc.cpp -o raster_at.exe -lpthread &
+test ! -f raster_band_insert_interp_from.exe && g++ -w -O4 raster_band_insert_interp_from.cpp  misc.cpp -o raster_band_insert_interp_from.exe -lpthread &
+test ! -f raster_burn_zeros.exe && g++ -w -O4 raster_burn_zeros.cpp  misc.cpp -o raster_burn_zeros.exe -lpthread &
 test ! -f raster_difference.exe && g++ -w -O4 raster_difference.cpp  misc.cpp -o raster_difference.exe -lpthread &
 test ! -f raster_dominant.exe && g++ -w -O4 raster_dominant.cpp  misc.cpp -o raster_dominant.exe -lpthread &
 test ! -f raster_increment.exe && g++ -w -O4 raster_increment.cpp  misc.cpp -o raster_increment.exe -lpthread &
+test ! -f raster_inequality2.exe && g++ -w -O4 raster_inequality2.cpp  misc.cpp -o raster_inequality2.exe -lpthread &
 test ! -f raster_inequality.exe && g++ -w -O4 raster_inequality.cpp  misc.cpp -o raster_inequality.exe -lpthread &
+test ! -f raster_l2.exe && g++ -w -O4 raster_l2.cpp  misc.cpp -o raster_l2.exe -lpthread &
 test ! -f raster_mask.exe && g++ -w -O4 raster_mask.cpp  misc.cpp -o raster_mask.exe -lpthread &
-test ! -f raster_mode_filter.exe && g++ -w -O4 raster_mode_filter.cpp  misc.cpp -o raster_mode_filter.exe -lpthread &
+test ! -f raster_mode_filter.exe && g++ -w -O4 raster_mode_filter.cpp  misc.cpp -o raster_mode_filter.exe -lpthread
+test ! -f raster_mult.exe && g++ -w -O4 raster_mult.cpp  misc.cpp -o raster_mult.exe -lpthread &
 test ! -f raster_nan_to_zero.exe && g++ -w -O4 raster_nan_to_zero.cpp  misc.cpp -o raster_nan_to_zero.exe -lpthread &
 test ! -f raster_nearest_centre.exe && g++ -w -O4 raster_nearest_centre.cpp  misc.cpp -o raster_nearest_centre.exe -lpthread &
 test ! -f raster_negate.exe && g++ -w -O4 raster_negate.cpp  misc.cpp -o raster_negate.exe -lpthread &
+test ! -f raster_ni2.exe && g++ -w -O4 raster_ni2.cpp  misc.cpp -o raster_ni2.exe -lpthread &
+test ! -f raster_ni.exe && g++ -w -O4 raster_ni.cpp  misc.cpp -o raster_ni.exe -lpthread &
 test ! -f raster_normalize.exe && g++ -w -O4 raster_normalize.cpp  misc.cpp -o raster_normalize.exe -lpthread &
 test ! -f raster_quickstats.exe && g++ -w -O4 raster_quickstats.cpp  misc.cpp -o raster_quickstats.exe -lpthread &
 test ! -f raster_relative_difference.exe && g++ -w -O4 raster_relative_difference.cpp  misc.cpp -o raster_relative_difference.exe -lpthread &
+test ! -f raster_scale.exe && g++ -w -O4 raster_scale.cpp  misc.cpp -o raster_scale.exe -lpthread &
+test ! -f raster_scale_onto.exe && g++ -w -O4 raster_scale_onto.cpp  misc.cpp -o raster_scale_onto.exe -lpthread &
+test ! -f raster_spectral_interp.exe && g++ -w -O4 raster_spectral_interp.cpp  misc.cpp -o raster_spectral_interp.exe -lpthread &
+test ! -f raster_sub_compare.exe && g++ -w -O4 raster_sub_compare.cpp  misc.cpp -o raster_sub_compare.exe -lpthread &
 test ! -f raster_sub.exe && g++ -w -O4 raster_sub.cpp  misc.cpp -o raster_sub.exe -lpthread &
 test ! -f raster_sum.exe && g++ -w -O4 raster_sum.cpp  misc.cpp -o raster_sum.exe -lpthread &
+test ! -f raster_threshold.exe && g++ -w -O4 raster_threshold.cpp  misc.cpp -o raster_threshold.exe -lpthread
+test ! -f raster_ts_dedup.exe && g++ -w -O4 raster_ts_dedup.cpp  misc.cpp -o raster_ts_dedup.exe -lpthread &
 test ! -f raster_zero_to_nan.exe && g++ -w -O4 raster_zero_to_nan.cpp  misc.cpp -o raster_zero_to_nan.exe -lpthread &
-test ! -f snip.exe && g++ -w -O4 snip.cpp  misc.cpp -o snip.exe -lpthread
+test ! -f rgb2hsv.exe && g++ -w -O4 rgb2hsv.cpp  misc.cpp -o rgb2hsv.exe -lpthread &
+test ! -f sbo_inplace.exe && g++ -w -O4 sbo_inplace.cpp  misc.cpp -o sbo_inplace.exe -lpthread &
+test ! -f sentinel2_active.exe && g++ -w -O4 sentinel2_active.cpp  misc.cpp -o sentinel2_active.exe -lpthread &
+test ! -f sentinel2_swir_subselect.exe && g++ -w -O4 sentinel2_swir_subselect.cpp  misc.cpp -o sentinel2_swir_subselect.exe -lpthread &
+test ! -f snip.exe && g++ -w -O4 snip.cpp  misc.cpp -o snip.exe -lpthread &
 test ! -f squiggle.exe && g++ -w -O4 squiggle.cpp  misc.cpp -o squiggle.exe -lpthread &
 test ! -f unstack.exe && g++ -w -O4 unstack.cpp  misc.cpp -o unstack.exe -lpthread &
 test ! -f vri_rasterize.exe && g++ -w -O4 vri_rasterize.cpp  misc.cpp -o vri_rasterize.exe -lpthread &
