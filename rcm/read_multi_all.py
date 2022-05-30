2a434f6d74ce63b1bb623df31e3816629a38a955
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-05-30 10:08:54 -0700

    update

--- rcm/read_multi_all.py
+++ rcm/read_multi_all.py
@@ -6,7 +6,7 @@ import sys
 cmds = []
 files = os.popen('find ./ -name "rgb.bin"').readlines()
 
-py = '/home/' + os.popen('whoami').read().strip() + '/GitHub/bcws-psu-research/py/read_multi.py'
+py = '/home/' + os.popen('whoami').read().strip() + '/GitHub/wps-research/py/read_multi.py'
 if not os.path.exists(py):
     print("Error: can't find file:", py)
     sys.exit(1)
