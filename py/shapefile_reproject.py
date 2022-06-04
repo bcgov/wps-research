906719ce4387544f3f6bf65064909c365ccc0e29
ashlinrichardson <richardson.ashlin@gmail.com>
2022-06-04 11:57:32 -0700

    update

--- py/shapefile_reproject.py
+++ py/shapefile_reproject.py
@@ -1,77 +1,80 @@
+'''
+References:
+[1] https://pcjericks.github.io/py-gdalogr-cookbook/projection.html
+[2] https://gdal.org/programs/gdalsrsinfo.html
+'''
 import sys
 from osgeo import ogr
+from osgeo import osr
 from osgeo import gdal
 from osgeo import gdalconst
 from misc import err, args, exist
 
 if len(args) < 4:
-    err('python3 shapefile_reproject.py [input shapefile] [output CRS (EPSG)] [output shapefile]')
+    err('python3 shapefile_reproject.py [input shapefile] [shapefile or raster to get CRS from] [output shapefile]')
 # https://gis.stackexchange.com/questions/265589/change-shapefile-coordinate-system-using-python
 
-in_shp = args[1]
-if not exist(in_shp):
-    err('please check input file')
+in_path = args[1]
+out_shp = args[3]
+if not exist(in_path) or not exist(args[2]):
+    err('please check input files')
 
-err("implementation not finished")
-sys.exit(1)
-
-in_epsg = 5514
-out_epsg = 4326
-in_shp = 'path/to/input.shp'
-out_shp = '/path/to/reprojected.shp'
+if exist(out_shp):
+    err('output file already exists')
 
 driver = ogr.GetDriverByName('ESRI Shapefile')
+dataset = driver.Open(in_path)  # input layer
+layer = dataset.GetLayer()
+sr = layer.GetSpatialRef()
 
-# input SpatialReference
-inSpatialRef = osr.SpatialReference()
-inSpatialRef.ImportFromEPSG(in_epsg)
+sr2 = None  # spatial reference to be matched in reprojected dataset
+try:
+    d2 = gdal.Open(args[2])
+    sr2 = osr.SpatialReference(wkt=d2.GetProjection())
+except:
+    driver2 = ogr.GetDriverByName('ESRI Shapefile')
+    dataset2 = driver.Open(args[2])
+    layer2 = dataset.GetLayer()  # from layer
+    sr2 = layer.GetSpatialRef()
+    '''
+    # from Geometry
+    feature = layer.GetNextFeature()
+    geom = feature.GetGeometryRef()
+    spatialRef = geom.GetSpatialReference()'''
+print("CRS to mATCH:", sr2)
 
-# output SpatialReference
-outSpatialRef = osr.SpatialReference()
-outSpatialRef.ImportFromEPSG(out_epsg)
+# proj = osr.SpatialReference(wkt=d.GetProjection())
+# print(proj.GetAttrValue('AUTHORITY',1))
 
 # create the CoordinateTransformation
-coordTrans = osr.CoordinateTransformation(inSpatialRef, outSpatialRef)
-
-# get the input layer
-inDataSet = driver.Open(in_path)
-inLayer = inDataSet.GetLayer()
+coordTrans = osr.CoordinateTransformation(sr, sr2)
 
 # create the output layer
-if os.path.exists(out_shp):
-    driver.DeleteDataSource(out_shp)
 outDataSet = driver.CreateDataSource(out_shp)
 outLayer = outDataSet.CreateLayer("reproject", geom_type=ogr.wkbMultiPolygon)
 
 # add fields
-inLayerDefn = inLayer.GetLayerDefn()
-for i in range(0, inLayerDefn.GetFieldCount()):
-    fieldDefn = inLayerDefn.GetFieldDefn(i)
+layerDefn = layer.GetLayerDefn()
+for i in range(0, layerDefn.GetFieldCount()):
+    fieldDefn = layerDefn.GetFieldDefn(i)
     outLayer.CreateField(fieldDefn)
 
 # get the output layer's feature definition
 outLayerDefn = outLayer.GetLayerDefn()
 
 # loop through the input features
-inFeature = inLayer.GetNextFeature()
-while inFeature:
-    # get the input geometry
-    geom = inFeature.GetGeometryRef()
-    # reproject the geometry
-    geom.Transform(coordTrans)
-    # create a new feature
-    outFeature = ogr.Feature(outLayerDefn)
-    # set the geometry and attribute
+inFeature = layer.GetNextFeature()
+while inFeature:   # get the input geometry
+    geom = inFeature.GetGeometryRef()     # reproject the geometry
+    geom.Transform(coordTrans)     # create a new feature
+    outFeature = ogr.Feature(outLayerDefn)      # set the geometry and attribute
     outFeature.SetGeometry(geom)
     for i in range(0, outLayerDefn.GetFieldCount()):
-        outFeature.SetField(outLayerDefn.GetFieldDefn(i).GetNameRef(), inFeature.GetField(i))
-    # add the feature to the shapefile
-    outLayer.CreateFeature(outFeature)
-    # dereference the features and get the next input feature
+        outFeature.SetField(outLayerDefn.GetFieldDefn(i).GetNameRef(), inFeature.GetField(i))   # add the feature to the shapefile
+    outLayer.CreateFeature(outFeature) # dereference the features and get the next input feature
     outFeature = None
-    inFeature = inLayer.GetNextFeature()
+    inFeature = layer.GetNextFeature()
 
-# Save and close the shapefiles
-inDataSet = None
+dataset = None  # save and close shapefiles
 outDataSet = None
 
