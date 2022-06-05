fb8777e8f6dfaf96e2d2200d58214b2a01733d9d
ashlinrichardson <richardson.ashlin@gmail.com>
2022-06-05 11:21:32 -0700

    update

--- cpp/raster_burn_zeros.cpp
+++ cpp/raster_burn_zeros.cpp
@@ -1,12 +1,12 @@
-/* 20220309 raster_burn_zeroes.cpp: burn the zeros from one image onto
-another, in-place:
-use to remove data on image #2, where image #1 has nodata */
+/* 20220309 raster_burn_zeroes.cpp: burn the zeros from one image onto another,
+in-place: use to remove data on image #2, where image #1 has nodata */
 #include"misc.h"
 #include<math.h>
 
 int main(int argc, char ** argv){
   if(argc < 3){
-    err("raster_sum.exe [first raster to burn zeros from] [onto this raster (in place)]\n");
+    printf("raster_burn.cpp: burn non-data (NAN) onto image #2, where image #1 has nodata (NAN, or (all-zero and nband > 1))\n");
+    err("raster_burn_zeros.exe [first raster to burn zeros from] [onto this raster (in place)]\n");
   }
   str fn(argv[1]); // input image file name
   str fn2(argv[2]); // input image 2 file name
@@ -22,46 +22,37 @@ int main(int argc, char ** argv){
     err("input image dimensions should match");
  
   np = nrow * ncol; // number of input pix
+  float d;
   size_t i, j, k;
   float * dat1 = bread(fn, nrow, ncol, nband);
   float * dat2 = bread(fn2, nrow, ncol, nband2);
   bool is_zero, is_nan;
+
   for0(i, np){
     is_zero = true;
     is_nan = false;
+
     for0(k, nband){
-      float d = dat1[i + k * np];
-      if( d != 0){ 
-        is_zero = false;
-      }
-      if(isnan(d) || isinf(d)){
-        is_nan = true;
-      }
-    }
-    if(is_zero){
-      for0(k, nband2){
-        dat2[i + k * np] = 0.;
-      }
-    }
-    if(is_nan || is_zero){
-      for0(k, nband2){
-        dat2[i + k * np] = NAN;
-      }
+      d = dat1[i + k * np];  // check if raster #1 is zero or NAN
+      if(d != 0) is_zero = false;
+      if(isnan(d) || isinf(d)) is_nan = true;
     }
 
-    is_zero = true;
-    for0(k, nband){
-      float d = dat2[i + k * np];
-      if( d != 0){
-        is_zero = false;
-      }
+    if(is_nan || (is_zero && nband1 > 1)){
+      for0(k, nband2) dat2[i + k * np] = NAN; // set raster #2 to NAN
     }
-    if(is_zero){
-      for0(k, nband2){
-        dat2[i + k * np] = NAN;
+    else{
+      is_zero = true; // check if raster #2 is already zero
+      for0(k, nband){
+        d = dat2[i + k * np];
+        if(d != 0) is_zero = false;
+      }
+      if(is_zero && (nband2 > 1)){
+        for0(k, nband2) dat2[i + k * np] = NAN; // replace with NAN
       }
     }
   }
+
   printf("+w %s\n", fn2.c_str());
   bwrite(dat2, fn2, nrow, ncol, nband2);
   return 0;
