6d2f7aa808ebb5d9ff58e1191951534ce858cda7
ashlinrichardson <richardson.ashlin@gmail.com>
2022-06-07 20:07:11 -0700

    update

--- cpp/bsq2bip.cpp
+++ cpp/bsq2bip.cpp
@@ -1,24 +1,28 @@
-/* 20220516 convert bsq to bip */
+/* 20201031 convert bsq data to bip data */
 #include"misc.h"
 int main(int argc, char ** argv){
-  if(argc < 2) err("bsq2bip [input raster]");
-  str fn(argv[1]);
-  str hfn(hdr_fn(fn));
-  size_t np, nr, nc, nb, i, k, ci;
-  hread(hfn, nr, nc, nb);
-  np = nr * nc;
+  if(argc < 2) err("bsq2bip [input binary file name]\n");
 
-  float * dat = bread(fn, nr, nc, nb); // read the input data
-  float * out = falloc(np * nb);
-  str ofn(fn + str("_bip.bin"));
-  str ohn(fn + str("_bip.hdr"));
+  str ifn(argv[1]); // input filename
+  str hfn(hdr_fn(ifn)); // input header
+  size_t nr, nc, nb, np; // image dims
+  hread(hfn, nr, nc, nb); // read image dims
+  float * d = bread(ifn, nr, nc, nb); // read input image data
+  np = nr * nc; // number of pix
 
-  ci = 0;
-  for0(i, np)
-    for0(k, nb)
-      out[ci++] = dat[k * np + i];
-  
-  bwrite(out, ofn, nr, nc, nb);
-  hwrite(ohn, nr, nc, nb);
+  str ofn(ifn + str("_bip.bin")); // output image
+  str ohn(ifn + str("_bip.hdr")); // output header
+  hwrite(ohn, nr, nc, nb, 4, BIP); // write BIP format output header
+
+  size_t i, j, k, ki, ii; 
+  float * d2 = falloc(np * nb);
+  for0(k, nb){
+    ki = k * np;
+    for0(i, nr){
+      ii = i * nc;	
+      for0(j, nc) d2[((ii + j) * nb) + k] = d[ki + ii + j];
+    }
+  }
+  bwrite(d2, ofn, nr, nc, nb); // write bip data
   return 0;
 }
