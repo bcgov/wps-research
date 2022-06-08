376c07e07b39b338e3879bc14c5fcc3bdde23328
ashlinrichardson <richardson.ashlin@gmail.com>
2022-06-07 21:04:07 -0700

    update

--- cpp/kgc.cpp
+++ cpp/kgc.cpp
@@ -4,7 +4,7 @@ int main(int argc, char **argv){
   str fn(argv[1]);
   run(str("bsq2bip.exe ") + fn);
   run(str("precond.exe ") + fn + str("_bip.bin"));
-  run(str("dedup.exe ") + fn + str("_bip.bin_precond.bin"));
+  run(str("bip_dedup.exe ") + fn + str("_bip.bin_precond.bin"));
   run(str("kgc2020.exe ") + fn + str("_bip.bin_precond.bin ") + fn + str("_bip.bin_precond.bin_dedup"));
   return 0;
 }
