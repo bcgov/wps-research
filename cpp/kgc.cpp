d5920fcf9877769a4e691db059a4bcb95b23980d
ashlinrichardson <richardson.ashlin@gmail.com>
2022-06-07 21:03:25 -0700

    update

---
+++ cpp/kgc.cpp
@@ -0,0 +1,10 @@
+#include"misc.h"
+int main(int argc, char **argv){
+  if(argc < 2) err("kgc.exe [input binary file bsq]");
+  str fn(argv[1]);
+  run(str("bsq2bip.exe ") + fn);
+  run(str("precond.exe ") + fn + str("_bip.bin"));
+  run(str("dedup.exe ") + fn + str("_bip.bin_precond.bin"));
+  run(str("kgc2020.exe ") + fn + str("_bip.bin_precond.bin ") + fn + str("_bip.bin_precond.bin_dedup"));
+  return 0;
+}
