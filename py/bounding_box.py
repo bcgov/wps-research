84a42fd6db035933ed4f3bbd7517f67d8e04c7f7
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-07-28 11:42:20 -0700

    update

---
+++ py/bounding_box.py
@@ -0,0 +1,53 @@
+''' Copied from 
+https://itecnote.com/tecnote/r-how-to-calculate-the-bounding-box-for-a-given-lat-lng-location/ '''
+
+# degrees to radians
+def deg2rad(degrees):
+    return math.pi*degrees/180.0
+# radians to degrees
+def rad2deg(radians):
+    return 180.0*radians/math.pi
+
+# Semi-axes of WGS-84 geoidal reference
+WGS84_a = 6378137.0  # Major semiaxis [m]
+WGS84_b = 6356752.3  # Minor semiaxis [m]
+
+# Earth radius at a given latitude, according to the WGS-84 ellipsoid [m]
+def WGS84EarthRadius(lat):
+    # http://en.wikipedia.org/wiki/Earth_radius
+    An = WGS84_a*WGS84_a * math.cos(lat)
+    Bn = WGS84_b*WGS84_b * math.sin(lat)
+    Ad = WGS84_a * math.cos(lat)
+    Bd = WGS84_b * math.sin(lat)
+    return math.sqrt( (An*An + Bn*Bn)/(Ad*Ad + Bd*Bd) )
+
+# Bounding box surrounding the point at given coordinates,
+# assuming local approximation of Earth surface as a sphere
+# of radius given by WGS84
+def bounding_box(latitudeInDegrees, longitudeInDegrees, halfSideInKm):
+    lat = deg2rad(latitudeInDegrees)
+    lon = deg2rad(longitudeInDegrees)
+    halfSide = 1000. * halfSideInKm
+
+    # Radius of Earth at given latitude
+    radius = WGS84EarthRadius(lat)
+    # Radius of the parallel at given latitude
+    pradius = radius*math.cos(lat)
+
+    latMin = lat - halfSide/radius
+    latMax = lat + halfSide/radius
+    lonMin = lon - halfSide/pradius
+    lonMax = lon + halfSide/pradius
+
+    return (rad2deg(latMin), rad2deg(lonMin), rad2deg(latMax), rad2deg(lonMax))
+
+def dps2deg(degrees, primes, seconds):
+    return degrees + primes/60.0 + seconds/3600.0
+
+def deg2dps(degrees):
+    intdeg = math.floor(degrees)
+    primes = (degrees - intdeg)*60.0
+    intpri = math.floor(primes)
+    seconds = (primes - intpri)*60.0
+    intsec = round(seconds)
+    return (int(intdeg), int(intpri), int(intsec))
