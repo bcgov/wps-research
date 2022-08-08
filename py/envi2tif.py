2e11577fbd35a75980901d5cfed6a482c71ae47c
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-08-08 16:18:38 -0700

    create good tif file

--- py/envi2tif.py
+++ py/envi2tif.py
@@ -14,12 +14,18 @@ fn = args[1]
 
 run('htrim2.exe ' + fn + ' 1. 1.')
 
+# multiply by 255.
+run('raster_smult.exe ' +
+    fn + '_ht.bin ' + 
+    '255.')
+
 # copy map info
-run('python3 ' + pd + 'envi_header_copy_mapinfo.py '  + fn[:-3] + 'hdr ' + fn + '_ht.hdr')
-run('python3 ' + pd + 'envi_update_band_names.py '    + fn[:-3] + 'hdr ' + fn + '_ht.hdr')
+run('python3 ' + pd + 'envi_header_copy_mapinfo.py '  + fn[:-3] + 'hdr ' + fn + '_ht.bin_smult.hdr')
+run('python3 ' + pd + 'envi_update_band_names.py '    + fn[:-3] + 'hdr ' + fn + '_ht.bin_smult.hdr')
+
 
 # convert to TIF (byte format)
 run('gdal_translate -of GTiff -ot Byte ' +
-    (fn + '_ht.bin') +
+    (fn + '_ht.bin_smult.bin') +
     ' ' +  
-    (fn + '_ht.tif'))
+    (fn + '_ht.bin_smult.tif'))
