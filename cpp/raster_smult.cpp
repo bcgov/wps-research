fc2f368ab4dd17fea7fa8fd2f17fa93bbbf4ab33
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-08-08 10:51:00 -0700

    update

--- cpp/raster_smult.cpp
+++ cpp/raster_smult.cpp
@@ -5,10 +5,12 @@ adapted from raster_mult.cpp 20220807 */
 int main(int argc, char ** argv){
   if(argc < 3) err("raster_sum.exe [raster cube] [scalar value to multiply] [optional linear offset]\n");
   str fn(argv[1]); // input image file name
+  str hfn(hdr_fn(fn));
+
   float scalar = atof(argv[2]);
   float offset = 0.;
   if(argc > 3) offset = atof(argv[3]);
-  if(!(exists(fn)) err("failed to open input file");
+  if(!(exists(fn))) err("failed to open input file");
 
   str ofn(fn + str("_smult.bin"));  // output file name
   str ohn(hdr_fn(ofn, true)); // out header file name
