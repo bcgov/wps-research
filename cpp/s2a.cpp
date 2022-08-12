10663c5ab6e6006c31b3b89d99707b0fb939934a
ashlinrichardson <richardson.ashlin@gmail.com>
2022-08-11 23:26:14 -0700

    update

--- cpp/s2a.cpp
+++ cpp/s2a.cpp
@@ -5,11 +5,19 @@ int main(int argc, char ** argv){
   str f("sub.bin");
   str swir(f + str("_swir.bin"));
   str s_d(swir + str("_dominant.bin"));
-  
+  str s_d1(s_d + str("_001.bin"));
+  str a(f + str("_active.bin"));
+
   run(str("sentinel2_active.exe " + f));
   run(str("sentinel2_swir_subselect.exe " + f));
   run(str("raster_dominant.exe " + swir));
   run(str("unstack.exe ") + s_d + str(" 1"));
+
+  run(str("raster_sum.exe ") + a + str(" ") + s_d1 + str(" result.bin"));
+  run(str("python3 ~/GitHub/wps-research/py/envi_header_copy_mapinfo.py sub.hdr result.hdr"));
+
+  run(str("python3 ~/GitHub/wps-research/py/binary_polygonize.py result.bin"));
+  run(str("python3 ~/GitHub/wps-research/py/envi2tif.py ") + swir);
 
   return 0;
 }
