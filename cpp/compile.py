bb1102a7fdaae6195e6c844e91b318a8cbe25a83
ashlinrichardson <richardson.ashlin@gmail.com>
2022-08-13 09:18:09 -0700

    switch to multicore operator

--- cpp/compile.py
+++ cpp/compile.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-# compile all .cpp files in folder to .exe files
-# remove -w flag from g++ to examine warnings
+'''
+compile all .cpp files in folder to .exe files
+remove -w flag from g++ to examine warnings
+20220813 add work queue
+20220813 add python program wrapping
+'''
 import os
 import sys
 import multiprocessing as mp
@@ -21,12 +25,12 @@ for f in files:
             print(s)
         else:
             s = '\ntest ! -f ' + fn + '.exe && g++ -w -O4 ' + fn + '.cpp  misc.cpp -o ' + fn + '.exe -lpthread; ' + symlink
-        s += '' if ((i + 1) % n_cpu == 0) else ' &'
+        # s += '' if ((i + 1) % n_cpu == 0) else ' &'  # don't need this since we'll use multicore
         of.write(s.encode())
         i += 1
 of.write("\nwait".encode())
 of.close()
 
 a = os.system("chmod 755 compile.sh")
-a = os.system("./compile.sh")
+a = os.system("multicore ./compile.sh")
 
