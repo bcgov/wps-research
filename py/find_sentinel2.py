b312b10d33e0f6300f05f8e6f74fcdc0ccdadd21
ashlinrichardson <richardson.ashlin@gmail.com>
2022-08-12 19:39:00 -0700

    update

--- py/find_sentinel2.py
+++ py/find_sentinel2.py
@@ -7,6 +7,7 @@ Query sentinel-2 products over a given point (alternately, a place name). Later:
 import os
 import sys
 import math
+from misc import exists
 foot_print = 'Intersects(51.48252764574755,-123.95386296901019)' # bc 	2022-C50155 	52.003216999999999 	-123.13905 	2022-05-05 17:23 	2022-05-30 15:01 	UC 	H 
 # careful, check if the lat/long are reversed in order (when comparing point location vs. polygon location?)
 
