8ee51a14f8074432b5fb1bf4ede5f9b302c25541
ashlinrichardson <richardson.ashlin@gmail.com>
2022-08-13 09:23:20 -0700

    update

--- cpp/compile.py
+++ cpp/compile.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
-'''
-compile all .cpp files in folder to .exe files
-remove -w flag from g++ to examine warnings
+'''compile all .cpp files in folder to .exe files. N.B. remove -w flag from g++ to examine warnings
 20220813 add work queue
 20220813 add python program wrapping
+    Compile all python files in python folder using a C wrapper (access from common folder)
 '''
 import os
 import sys
-import multiprocessing as mp
-i, n_cpu = 0, mp.cpu_count()
+from misc import run, me
 
-files = os.popen('ls -1 *.cpp').readlines()
+files = os.popen('ls -1 ' + get_cd() + '*.cpp').readlines()
 files = [f.strip() for f in files]
 
 of = open('compile.sh', 'wb')
@@ -25,12 +23,43 @@ for f in files:
             print(s)
         else:
             s = '\ntest ! -f ' + fn + '.exe && g++ -w -O4 ' + fn + '.cpp  misc.cpp -o ' + fn + '.exe -lpthread; ' + symlink
-        # s += '' if ((i + 1) % n_cpu == 0) else ' &'  # don't need this since we'll use multicore
         of.write(s.encode())
-        i += 1
 of.write("\nwait".encode())
+
+'''
+if True:  # process files in python folder
+    files = os.popen('ls -1 /home/' + me() + 'GitHub' + 
+    elif ext == 'py':
+        wrap_file = 'wrap-py_' + fn + '.cpp'
+        # for py files, write a cpp wrapper so we can call from bin folder
+        cf = open(wrap_file, 'wb') #'wrap_py.cpp', 'wb')
+        lines = ['#include<stdlib.h>',
+                 '#include<iostream>',
+                 '#include<string>',
+                 'using namespace std;',
+                 'int main(int argc, char ** argv){',
+                 #'  string cmd("/cygdrive/c/Program\\\\ Files/Python35/python.exe ");',
+                 '  string cmd("/cygdrive/c/Python27/python.exe ");',
+                 '  cmd += string("R:/' + os.popen("whoami").read().strip() + '/bin/py/' + fn + '.py");',
+                 '  for(int i=1; i<argc; i++){',
+                 '    cmd += string(" ") + string(argv[i]);',
+                 '  }',
+                 'std::cout << cmd << endl;',
+                 'system(cmd.c_str());',
+                 'return(0);',
+                 '}']
+        cf.write('\n'.join(lines).encode())
+        cf.close()
+        cmd = 'g++ -w -O3 -o ' + fn + '.exe ' + ' ' + wrap_file #wrap_py.cpp '
+        print('\t' + cmd)
+        cmds += " " + cmd # a = os.system(cmd)
+        if(True): # 20191113 might set to true later?
+            cmd =  ("rm -f " + wrap_file) #wrap_py.cpp")
+            cmds += "; " + cmd;
+'''
+
 of.close()
 
-a = os.system("chmod 755 compile.sh")
-a = os.system("multicore ./compile.sh")
+run("chmod 755 compile.sh")
+run("multicore ./compile.sh")
 
