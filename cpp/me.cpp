c4fa53eefa16625a8972ae4199850ed1afec9766
ashlinrichardson <richardson.ashlin@gmail.com>
2022-08-15 21:13:15 -0700

    update

--- cpp/me.cpp
+++ cpp/me.cpp
@@ -4,8 +4,8 @@ int main(int argc, char ** argv){
   if(argc < 2) err("me [directory name] # make directory and enter it");
 
   str dn(argv[1]);
-  system(str("mkdir ") + dn);
-  system(str("cd ") + dn);
+  system((str("mkdir ") + dn).c_str());
+  system((str("cd ") + dn).c_str());
   return 0;
 }
 
