e2b58ccde85abab7d6e05890c6752db8ce9ce5f8
ashlinrichardson <richardson.ashlin@gmail.com>
2022-08-18 23:34:14 -0700

    update

--- cpp/htd.cpp
+++ cpp/htd.cpp
@@ -9,7 +9,7 @@ int main(int argc, char ** argv){
   str ht(swir + str("_ht.bin"));
   str dom(ht + str("_dominant.bin"));
 
-  run(str("htrim2 ") + ht + str(" ") + (str(".5 .5") if argc < 2 else (str(argv[1]) + str(" ") + str(argv[1]))));
+  run(str("htrim2 ") + ht + str(" ") + (argc < 2 ? str(".5 .5") : (str(argv[1]) + str(" ") + str(argv[1]))));
 
   run(str("raster_dominant.exe " + ht));
   run(str("unstack.exe ") + dom + str(" 1"));
