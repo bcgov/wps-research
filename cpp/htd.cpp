0b8c76c227bafb2cacac9605c9239b01ad70dd5c
ashlinrichardson <richardson.ashlin@gmail.com>
2022-08-18 23:03:07 -0700

    update

---
+++ cpp/htd.cpp
@@ -0,0 +1,24 @@
+/* 20220818 htrim .5 then dominant, extract band 1 */
+#include "misc.h"
+
+int main(int argc, char ** argv){
+  str f("sub.bin");
+  str swir(f + str("_swir.bin"));
+
+  str ht(swir + str("_ht.bin"));
+  str dom(ht + str("_dominant.bin"));
+
+  run(str("htrim2 ") + ht + str(" .5 .5"));
+
+  run(str("raster_dominant.exe " + ht));
+  run(str("unstack.exe ") + dom + str(" 1"));
+
+  run(str("raster_sum.exe ") + a + str(" ") + s_d1 + str(" result.bin"));
+
+  run(str("python3 ~/GitHub/wps-research/py/envi_header_copy_mapinfo.py sub.hdr sub.bin_swir.bin_ht.bin_dominant.bin_001.hdr"));
+  run(str("clean"));
+  run(str("imv sub.bin_swir.bin_ht.bin_dominant.bin_001.bin"));
+  return 0;
+}
+
+
