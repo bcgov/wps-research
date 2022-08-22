8314dc5b347b0b05313145245bba47e5bba1c4a0
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-08-22 15:21:51 -0700

    update

--- cpp/fh.cpp
+++ cpp/fh.cpp
@@ -3,6 +3,15 @@
 int main(int argc, char ** argv){
   if(argc < 2) err("fh [envi header file name .hdr] # fix header file");
 
-  run(str("envi_header_cleanup.py ") + str(argv[1]));
+  str fn(argv[1]);
+  if(exists(fn)){
+  }
+  else{
+    if(exists(fn + str(".hdr")){
+      fn += str(".hdr");
+    }
+  }
+
+  run(str("envi_header_cleanup.py ") + fn);
   return 0;
 }
