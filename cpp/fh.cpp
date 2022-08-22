066293a8d36d86436878e9e8f6a4414eb29d0f6a
Ashlin Richardson <ashlin.richardson@gmail.com>
2022-08-22 15:31:23 -0700

    update

--- cpp/fh.cpp
+++ cpp/fh.cpp
@@ -1,17 +1,35 @@
 /* fix envi header */
 #include"misc.h"
 int main(int argc, char ** argv){
-  if(argc < 2) err("fh [envi header file name .hdr] # fix header file");
-
-  str fn(argv[1]);
-  if(exists(fn)){
+  vector<str> good;
+  
+  if(argc < 2){
+    vector<str> lines(split(exec("ls -1 *.hdr"), '\n'));
+    for(vector<str>::iterator it = lines.begin(); it != lines.end(); it++){
+      if(exists(*it)){
+        good.push_back(*it);
+      }
+    }
+    cout << lines << endl;
+    if(good.size() < 1){
+      err("fh [envi header file name .hdr] # fix header file");
+    }
   }
   else{
-    if(exists(fn + str(".hdr"))){
-      fn += str(".hdr");
+    str fn(argv[1]);
+    if(exists(fn)){
+      good.push_back(fn);
+    }
+    else{
+      if(exists(fn + str(".hdr"))){
+        fn += str(".hdr");
+	good.push_back(fn);
+      }
     }
   }
 
-  run(str("envi_header_cleanup.py ") + fn);
+  for(vector<str>::iterator it = good.begin(); it != good.end(); it++){
+    run(str("envi_header_cleanup.py ") + *it);
+  }
   return 0;
 }
