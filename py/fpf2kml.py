c4ec1240d5ed65daa3bd5581b30e5afff1e1f9db
ashlinrichardson <richardson.ashlin@gmail.com>
2022-08-24 21:54:40 -0700

    convert esa copernicus footprint string in file to KML

---
+++ py/fpf2kml.py
@@ -0,0 +1,27 @@
+'''20220824 convert "fpf" format (copernicus footprint string file) to kml
+'''
+import os
+import sys
+from osgeo import ogr
+args = sys.argv
+fn = args[1] if len(args) > 1 else 'fpf'
+ofn = fn + '.kml'
+wkt = open(fn).read().strip()[11:-1]
+
+# wkt = 'POLYGON((-124.79178589364845 50.836204617080405,-124.79178589364845 50.914095382919584,-124.66834810635157 50.914095382919584,-124.66834810635157 50.836204617080405,-124.79178589364845 50.836204617080405))'
+poly = ogr.CreateGeometryFromWkt(wkt)
+kml = poly.ExportToKML()
+
+data = '''<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+  <Placemark>
+    <name>polygon</name>
+    <description>polygon</description>'''
+
+data2 = '''</Placemark>
+</kml>'''
+
+data = data + kml + data2 # print(data)
+
+print('+w', ofn)
+open(ofn, 'wb').write(data.encode())
