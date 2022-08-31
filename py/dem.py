4731d8b0cd04df96ba330e4cc985976196a0a81b
ashlinrichardson <richardson.ashlin@gmail.com>
2022-08-31 11:50:28 -0700

    update

--- py/dem.py
+++ py/dem.py
@@ -1,10 +1,15 @@
-'''
-run https://github.com/bopen/elevation
+'''20220831
+running https://github.com/bopen/elevation automatically
 
 Deps:
     pip3 install elevation
-    pip3 install rasterio
-    pip3 install fiona
+    pip3 install rasterio    # for raster reference/footprint file (here we assume raster)
+    pip3 install fiona       # for shapefile reference/footprint file
+
+Steps:
+    1. raster_pixels_loc.py # determine lat/lon for each pixel in image
+    2. raster_quickstats    # determine min,max for both lat and lon
+    3. # {increase, decrease} the {lat/lon}  {max, min} respectively, to make sure we get results for the data
 
 '''
 from misc import run, err, args
