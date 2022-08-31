d3f2dbd5c3ad94f55a8046edefe719c2d75a36d0
ashlinrichardson <richardson.ashlin@gmail.com>
2022-08-30 18:51:22 -0700

    update

--- cpp/raster_total.cpp
+++ cpp/raster_total.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char ** argv){
   if(argc < 2) err("raster_total [raster cube]\n");
-  size_t nrow, ncol, nband, np, nrow2, ncol2, nband2;
-  size_t i, j, k, ix, ij, ik, m;
+  size_t nrow, ncol, nband, np;
   float * out, * dat;
+  size_t i, j, k;
 
   str fn(argv[1]);
   str ofn(fn + str("_total.bin"));
@@ -16,13 +16,13 @@ int main(int argc, char ** argv){
   hread(hfn, nrow, ncol, nband); // read header
 
   np = nrow * ncol; // number of input pix
-  out = falloc(np);
-  for0(i, np) out[i] = 0.;
+  out = falloc(np); // one band output
+  for0(i, np) out[i] = 0.;  // set output to zero
     
   dat = bread(fn, nrow, ncol, nband);
   for0(i, np){
     for0(k, nband){
-	    out[i] += dat[(nband * np) + i];
+	    out[i] += dat[(k * np) + i];
     }
   }
   printf("+w %s\n", ofn.c_str());
