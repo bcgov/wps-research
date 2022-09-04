444ad74646c97a9437d306e6d256e4626e9dbaa7
ashlinrichardson <richardson.ashlin@gmail.com>
2022-09-03 19:48:49 -0700

    update

--- py/dem.py
+++ py/dem.py
@@ -17,4 +17,6 @@ from misc import run, err, args
 fn = args[1]
 out_fn = fn + '_dem.tif'
 
+
+# eio clip -o dem.tif --bounds -1.267663421630859E+02 5.595667114257812E+01 -1.246634750366211E+02 5.620212707519531E+01
 run('eio --product SRTM3 clip -o ' + out_fn + ' --reference ' + fn)
