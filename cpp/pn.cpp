a4e3ba35eb3a115cd9a10e05f9a4bbfc729f1ea1
ashlinrichardson <richardson.ashlin@gmail.com>
2022-09-05 22:38:45 -0700

    update

--- cpp/pn.cpp
+++ cpp/pn.cpp
@@ -1,37 +1,38 @@
 /* 20220905 pn.cpp: simple "positive / negative" binary classifier */
 #include"misc.h"
-
 float * dat;
 size_t nrow, ncol, nband, np;
 
 float d(size_t i, size_t j){
+   cout << "d " << i << " " << j << endl;
   float dd = 0.;
   float di;
   size_t k, nk;
   for0(k, nband){
-    nk = (np * nband);
-    di = dat[i + nk] - dat[j + nk];
+    nk = (np * k);
+    size_t ix = i + nk;
+    size_t iy = j + nk;
+    cout << "ix " << ix << " iy " << iy << endl;
+    di = dat[ix] - dat[iy];
     dd += di * di;
   }
   return (float)sqrt((double)dd);
 }
 
 int main(int argc, char ** argv){
-
   str fn(argv[1]);
   if(argc < 2 || !exists(fn)) err("please check input file");
 
   str cfn(fn + str("_targets.csv"));
-
   vector<str> hdr;
+  
   vector< vector<str>> lines(read_csv(cfn, hdr));
-
+  
   if(hdr[0] != str("feature_id") || hdr[1] != str("row") || hdr[2] != str("lin")){
     err("check csv header");
   }
 
   // "row (= col) = j, line (= row) = i" format
-
   vector<long int> pi;
   vector<long int> pj;
   vector<long int> ni;
@@ -52,7 +53,6 @@ int main(int argc, char ** argv){
   if(pi.size() == 0 || ni.size() == 0){
     err("both positive and negative sets must have at least one element");
   }
-
   cout << pi << endl;
   cout << pj << endl;
   cout << ni << endl;
@@ -62,6 +62,57 @@ int main(int argc, char ** argv){
   hread(hfn, nrow, ncol, nband); // read header
   np = nrow * ncol;
   dat = bread(fn, nrow, ncol, nband);
+  printf("dat[0] %f\n", dat[0]);
 
+  size_t Np = pi.size(); // size of positive class
+  size_t Nn = ni.size(); // size of negative class
+
+  long int nni, npi;
+  nni = npi = -1;
+  float nnd, npd, dd;
+  nnd = npd = FLT_MAX;
+
+  size_t i, j, k;
+  for0(i, nrow){
+    for0(j, ncol){
+      nni = npi = -1;
+      nnd = npd = FLT_MAX;
+
+      for0(k, Np){
+	cout << "k " << k << " pi[k] " << pi[k] << " pj[k] " << pj[k] << " nrow " << nrow << " ncol " << ncol << endl;
+	size_t ix = i * ncol + j;
+	size_t iy = pi[k] * ncol + pj[k];
+	dd = d(ix, iy);
+	if(k == 0){
+          npd = dd;
+	  npi = 0;
+	}
+	else{
+	  if(dd < npd){
+	    npd = dd;
+	    npi = k;
+	  }
+	}
+      }
+
+      for0(k, Nn){
+	cout << "k " << k << endl;
+        dd = d(i * ncol + j, ni[k] * ncol + nj[k]);
+        if(k == 0){     
+          nnd = dd;
+          nni = 0;
+        }
+        else{
+          if(dd < nnd){
+            nnd = dd;
+            nni = k;
+          }
+        }
+      }  
+
+      printf("nnd %f nni %zu npd %f npi %zu\n", nnd, (size_t)nni, npd, (size_t)npi);
+      exit(1);
+    }
+  }
   return 0;
 }
