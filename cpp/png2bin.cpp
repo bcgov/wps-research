ca70fa9b8e6949df9b2d420951f45719d32a5e30
ashlinrichardson <richardson.ashlin@gmail.com>
2022-09-07 19:16:51 -0700

    update

---
+++ cpp/png2bin.cpp
@@ -0,0 +1,13 @@
+/* 20220906: convert PNG file to bin file */
+#include"misc.h"
+int main(int argc, char ** argv){
+  if(argc < 2){
+    err("png2bin [input .png filename]");
+  }
+  str fn(argv[1]);
+  str ofn(fn + str(".png"));
+   
+  run(str("gdal_translate -of ENVI -ot Float32 -b 1 ") + fn + str(" ") + ofn);
+
+  return 0;
+}
