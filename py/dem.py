b7ab12b82a0245a23e1bedd3638b8ec3b0ef9479
ashlinrichardson <richardson.ashlin@gmail.com>
2022-09-14 13:48:08 -0700

    update

--- py/dem.py
+++ py/dem.py
@@ -9,14 +9,13 @@ Deps:
 Steps:
     1. raster_pixels_loc.py # determine lat/lon for each pixel in image
     2. raster_quickstats    # determine min,max for both lat and lon
-    3. # {increase, decrease} the {lat/lon}  {max, min} respectively, to make sure we get results for the data
+    3. # {increase, decrease} the {lat/lon}  {max, min} respectively, to make sure we get results for all the data (broaden the area slightly so we don't miss any at the edge)
 
 '''
 from misc import run, err, args
 
 fn = args[1]  # input raster footprint name
 out_fn = fn + '_dem.tif'  # output dem file name!
-
 
 # eio clip -o dem.tif --bounds -1.267663421630859E+02 5.595667114257812E+01 -1.246634750366211E+02 5.620212707519531E+01
 run('eio --product SRTM3 clip -o ' + out_fn + ' --reference ' + fn)
