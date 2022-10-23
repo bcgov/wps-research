4960a3dc66ebf4bffcca42c15462f547eadf175e
ashlinrichardson <richardson.ashlin@gmail.com>
2022-10-22 20:52:52 -0700

    update

---
+++ py/bin.py
@@ -0,0 +1,16 @@
+'''open .bin files in subfolders, with imv'''
+import os
+import sys
+sep = os.path.sep
+from misc import run, args
+
+print('python3 bin.py')
+
+cmd = 'find ./ -name "*.bin"'
+X = [x.strip() for x in os.popen(cmd).readlines()]
+X.sort()
+
+for x in X:
+    fn = x
+    cmd = 'imv ' + fn
+    print(cmd)
