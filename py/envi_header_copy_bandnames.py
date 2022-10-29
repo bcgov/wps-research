f5fab0e8bd54219d77d46568770657d4c32a5742
ashlinrichardson <richardson.ashlin@gmail.com>
2022-10-28 20:28:18 -0700

    update

---
+++ py/envi_header_copy_bandnames.py
@@ -0,0 +1,33 @@
+'''copy map info from one ENVI header file to another'''
+import os
+import sys
+from misc import *
+args = sys.argv
+sep = os.path.sep
+exists = os.path.exists
+pd = sep.join(__file__.split(sep)[:-1]) + sep
+
+if len(args) < 3:
+    err('envi_header_copy_bandnames.py [source .hdr file] [dest .hdr file to modify]')
+if not exists(args[1]) or not exists(args[2]): 
+    err('please check input files:\n\t' + args[1] + '\n\t' + args[2])
+
+# need to run this first to make sure the band name fields are where we expect!
+run('python3 ' + pd + 'envi_header_cleanup.py ' + args[1])
+run('python3 ' + pd + 'envi_header_cleanup.py ' + args[2])
+
+
+samples, lines, bands = read_hdr(args[2])
+band_names = [x.strip() for x in os.popen("envi_header_band_names.py " + args[1]).readlines()][1:]
+
+
+c = ' '.join(['envi_header_modify.py',
+              args[2],
+              str(lines),
+              str(samples),
+              str(bands)] + band_names)
+print(c)
+# err('envi_header_modify.py [.hdr file to modify] [nrow] [ncol] [nband] [band 1 name]... [band n name]')
+
+
+
