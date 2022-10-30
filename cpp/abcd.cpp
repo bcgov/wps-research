8ac04c816ebc27d909f267c027bf4fa30b41f4ed
ashlinrichardson <richardson.ashlin@gmail.com>
2022-10-30 00:26:56 -0700

    update

--- cpp/abcd.cpp
+++ cpp/abcd.cpp
@@ -76,7 +76,9 @@ int main(int argc, char** argv){
 			 		    to_string(skip_off));
   bwrite(x, pre + str(".bin"), nr[2], nc[2], nb[1]);  // write out
   hwrite(pre + str(".hdr"), nr[2], nc[2], nb[1]);
-  system((str("python3 ~/GitHub/wps-research/py/raster_plot.py ") + pre +
-	  str(".bin 1 2 3 1")).c_str());
+  int r = system((str("python3 ~/GitHub/wps-research/py/raster_plot.py ") + pre +
+	          str(".bin 1 2 3 1")).c_str());
+
+  run((str("envi_header_copy_bandnames.py ") + str(hdr_fn(argv[1 + 1])) + str(" ") + pre + str(".hdr")).c_str());
   return 0;
 }
