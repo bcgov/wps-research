96430e136be477650d03c4d6721443583c62f390
ashlinrichardson <richardson.ashlin@gmail.com>
2022-12-16 17:50:38 -0800

    update

---
+++ py/csv_to_map.py
@@ -0,0 +1,27 @@
+'''20221216: convert two cols from CSV, to python map'''
+import os
+import sys
+import csv
+args = sys.argv
+
+if len(args) < 4:
+    print("csv_to_map.py [csv file] [col name 1] [col name 2]")
+    sys.exit(1)
+reader = csv.reader(args[1], delimiter=',', quotechar='"')
+
+ri = 0
+arrow = {}
+fields = []
+domain_i, range_i = -1, -1
+for row in reader:
+    r = [x.strip() for x in row]
+    if ri == 0:
+        fields = r
+        lookup = {fields[i]: i for i in range(len(fields))}
+        domain_i = lookup[args[2]]
+        range_i = lookup[args[3]]
+    else:
+        arrow[r[domain_i]] = r[range_i]
+    ri += 1
+
+print(arrow)
