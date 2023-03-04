fbc1d4bcb5285951cab3e2edf5014b93668d86d5
ashlinrichardson <richardson.ashlin@gmail.com>
2023-03-03 17:42:03 -0800

    update

--- py/aws/list.py
+++ py/aws/list.py
@@ -1,6 +1,26 @@
-
+import os
+import sys
+import datetime
+year = datetime.date.today().year
 def incr(c): return chr(ord(c) + 1)
 def decr(c): return chr(ord(c) - 1)
 def cenc(s): return "".join([incr(si) for si in list(s)])
 def cdec(s): return "".join([decr(si) for si in list(s)])
+a = cdec("t4;00tfoujofm.qspevdut.db.njssps0")
+ls = cdec("bxt!t4!mt!..op.tjho.sfrvftu!")
+s2 = cdec("Tfoujofm.30")
+
+c1 = ls + a + s2 + 'S2MSI1C/' + str(year) + '/02/09/'
+c2 = ls + a + s2 + 'S2MSI2A/' + str(year) + '/02/09/'
+
+def call(c):
+    t = [x.strip() for x in os.popen(c).read().strip().split('\n')]
+    print('\n'.join(t))
+
+
+print(c1)
+call(c1)
+
+print(c2)
+call(c2)
 
