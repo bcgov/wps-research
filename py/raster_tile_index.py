22dd6c2492d670ca2d1fbabf806d39f1e59702c9
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-03-17 00:25:11 -0700

    update

---
+++ py/raster_tile_index.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from misc import timestamp
+ts = timestamp()
+
+rasters = [x.strip() for x in os.popen('find ./ -name "*.bin"').strip().split('\n')]
+
+open(ts + '_files.txt', 'wb').write(('\n'.join(rasters)).encode())
+
+shp = ts + '_tileindex.shp'
+
+cmd = 'gdaltindex -t_srs EPSG:4326 ' + shp + ' --optfile ' + ts + '_files.txt'
+print(cmd)
+
