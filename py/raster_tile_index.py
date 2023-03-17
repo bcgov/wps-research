4768d4c56a886825a4460b4f05e9f4ef85a75d37
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-03-17 00:28:00 -0700

    update

--- py/raster_tile_index.py
+++ py/raster_tile_index.py
@@ -3,12 +3,15 @@ import sys
 from misc import timestamp
 ts = timestamp()
 
+# list available rasters
 rasters = [x.strip() for x in os.popen('find ./ -name "*.bin"').read().strip().split('\n')]
 
+# write list of rasters
 open(ts + '_files.txt', 'wb').write(('\n'.join(rasters)).encode())
 
+# specify output file
 shp = ts + '_tileindex.shp'
 
+# print out command to generate tile index file
 cmd = 'gdaltindex -t_srs EPSG:4326 ' + shp + ' --optfile ' + ts + '_files.txt'
 print(cmd)
-
