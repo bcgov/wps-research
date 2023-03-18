6b432340e40781eee07c1a9458b9fce87024421a
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-03-17 19:34:52 -0700

    update

--- R/install_R.sh
+++ R/install_R.sh
@@ -1,11 +1,11 @@
 # Install R v. 4 on ubuntu
 # from:
 #   https://rtask.thinkr.fr/installation-of-r-4-0-on-ubuntu-20-04-lts-and-tips-for-spatial-packages/
-sudo apt install update-manager-core
-sudo apt upgrade
-sudo apt dist-upgrade
+# sudo apt install update-manager-core
+sudo apt update && sudo apt upgrade
+# sudo apt dist-upgrade
 sudo apt autoremove
-sudo do-release-upgrade
+# sudo do-release-upgrade
 sudo apt-get purge r-base* r-recommended r-cran-*
 sudo apt autoremove
 sudo apt update
@@ -15,6 +15,8 @@ sudo apt update
 gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | sudo apt-key add -
 sudo apt install r-base r-base-core r-recommended r-base-dev
+
+sudo apt install libudunits2-dev
 
 # may need to update R packages with: run "sudo R" then enter:
 #  update.packages(ask = FALSE, checkBuilt = TRUE)
