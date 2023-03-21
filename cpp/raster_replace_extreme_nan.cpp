1a18aecfac0733822951be1ab480f51b005d2230
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-03-20 23:35:11 -0700

    update

---
+++ cpp/raster_replace_extreme_nan.cpp
@@ -0,0 +1,33 @@
+/* 20230320 replace pixels with extreme magnitude, with NAN */
+#include"misc.h"
+
+inline float fabs(float f){
+  return (f < 0) ? -f : f;
+}
+
+int main(int argc, char ** argv){
+  if(argc < 2) err("raster_replace_extreme_nan.exe [input binary file]\n");
+ 
+  str fn(argv[1]); // input file name
+  str hfn(hdr_fn(fn)); // auto-detect header file name
+  size_t nrow, ncol, nband, np, i, j, k, nf;
+  cout << "+r " << hfn << endl;
+  hread(hfn, nrow, ncol, nband); // read header
+  np = nrow * ncol; // number of input pix
+  nf = np * nband;
+  cout << "+r " << fn << endl;
+  
+  float * dat = bread(fn, nrow, ncol, nband); // load floats to array
+  size_t ix;
+  float d = 0.;
+  for0(i, nf) d = max(d, fabs(dat[i]));
+  for0(i, nf) dat[i] = (dat[i] == d) ? NaN: dat[i]; 
+
+  cout << "+w " << fn << endl;
+  FILE * f = fopen(fn.c_str(), "wb");
+  if(!f) err("failed to open output file");
+  fwrite(dat, sizeof(float) * nrow * ncol * nband, 1, f); // write data
+  fclose(f);
+  free(dat);
+  return 0;
+}
