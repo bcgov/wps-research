01950257366f252e7bbb96dcfffbed7f77494059
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-04-24 22:35:32 -0700

    update

--- cpp/unique2.cpp
+++ cpp/unique2.cpp
@@ -1,33 +1,27 @@
 /* 20230424 unique2.cpp: deduplicate records by line */
 #include "misc.h"
 int main(int argc, char ** argv){
-  if(argc < 2)
-    err("unique2.cpp [input text file name]\n");
+  if(argc < 2) err("unique2.cpp [input text file name]\n");
+  
   str fn(argv[1]);
   str ofn(fn + str("_unique.txt"));
   
-  ifstream dfile(dfn);
-  if(!dfile.is_open())
-    err(string("failed to open input data file:") + dfn);
+  ifstream dfile(fn);
+  if(!dfile.is_open()) err(string("failed to open file:") + fn);
 
-  unordered_set<str> d;
   str line;
+  unordered_set<str> d;
 
-  while(getline(dfile, line)){
-    d.insert(line);
-  }
+  while(getline(dfile, line) d.insert(line);
   dfile.close();
 
   ofstream outfile(ofn);
-  if(!outfile.is_open())
-    err(string("failed to write-open file:") + ofn);
+  if(!outfile.is_open()) err(string("failed to open file:") + ofn);
 
   unordered_set<str>::iterator it;
-  for(it = d.begin(); it != d.end(); it++)
-    outfile << *it << endl;
-
+  for(it = d.begin(); it != d.end(); it++) outfile << *it << endl;
   outfile.close();
-
+  
   return 0;
 }
 
