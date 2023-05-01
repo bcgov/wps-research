06ec4b9384960da1867b7fdc4839c150cd291b81
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-04-30 20:16:07 -0700

    update

---
+++ py/install_gdal.sh
@@ -0,0 +1,8 @@
+# 20230430 install latest GDAL by building from source
+git clone https://github.com/OSGeo/gdal.git
+cd gdal
+mkdir -p build
+cd build
+cmake ..
+cmake --build .
+sudo cmake --build . --target install
