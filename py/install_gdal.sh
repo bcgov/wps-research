1b0125e3c6927f316c89b9a00243886b16b6aef9
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-04-30 22:05:22 -0700

    update

--- py/install_gdal.sh
+++ py/install_gdal.sh
@@ -13,4 +13,5 @@ sudo cmake --build . --target install
 
 # https://github.com/OSGeo/gdal/releases/download/v3.6.4/gdal-3.6.4.tar.gz
 pip3 uninstall gdal
-pip3 install GDAL==$(gdal-config --version) --global-option=build_ext --global-option="-I/usr/include/gdal"
+pip3 install --no-cache-dir --force-reinstall 'GDAL[numpy]==3.6.4'
+# pip3 install GDAL==$(gdal-config --version) --global-option=build_ext --global-option="-I/usr/include/gdal"
