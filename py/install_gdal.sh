45ba47f4fa11806ec13de4e9ba8e635f6678afa3
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-04-30 22:01:38 -0700

    update

--- py/install_gdal.sh
+++ py/install_gdal.sh
@@ -1,9 +1,16 @@
 # 20230430 install latest GDAL by building from source
-git clone https://github.com/OSGeo/gdal.git
-cd gdal
+wget https://github.com/OSGeo/gdal/releases/download/v3.6.4/gdal-3.6.4.tar.gz
+tar xvf gdal-3.6.4.tar.gz
+# git clone https://github.com/OSGeo/gdal.git
+# cd gdal
+cd gdal-3.6.4
 mkdir -p build
 cd build
-# cmake ..
-cmake ..  -BUILD_PYTHON_BINDINGS:ON
+cmake ..
+# cmake ..  -DBUILD_PYTHON_BINDINGS:ON
 cmake --build .
 sudo cmake --build . --target install
+
+# https://github.com/OSGeo/gdal/releases/download/v3.6.4/gdal-3.6.4.tar.gz
+pip3 uninstall gdal
+pip3 install GDAL==$(gdal-config --version) --global-option=build_ext --global-option="-I/usr/include/gdal"
