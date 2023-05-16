5d78fb1b7cdb23b106dc7dae3f7fd3c024f5bdc3
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-15 22:01:29 -0700

    update

---
+++ py/sentinel2_unzip.py
@@ -0,0 +1,21 @@
+'''20230515 unzip sentinel2 files that are not already unzipped
+'''
+import os
+import sys
+from misc import parfor
+
+cmds = []
+files = [x.strip() for x in os.popen('ls -1 *.zip').readlines()]
+for f in files:
+	d = f[:-4] + '.SAFE'
+	if os.path.exists(d):
+		pass
+	else:
+		cmds += 'unzip ' + f
+
+def run(c):
+	return os.system(c)
+
+parfor(run, cmds, 2)
+
+
