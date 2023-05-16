99b4c39c5158357bce7263756cb346f2118d491b
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-16 12:39:50 -0700

    update

--- cpp/s2u2.cpp
+++ cpp/s2u2.cpp
@@ -1,7 +1,7 @@
 /* 20230515 */
 #include"misc.h"
 int main(int argc, char ** argv){
-  system("unzp");
+  system("safe_unzip.py");
   system("sentinel2_stack_all_20m.py 1");
   system("clean");
   return 0;
