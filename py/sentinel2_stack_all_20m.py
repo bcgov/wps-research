4ff9ef5e5d47ba012c221425370ed5055105613d
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-18 20:49:22 -0700

    update

--- py/sentinel2_stack_all_20m.py
+++ py/sentinel2_stack_all_20m.py
@@ -1,4 +1,5 @@
 '''
+20230515 add swir-only option. NOte: need to modify the band extraction to select the relevant bands only.
 20221026 Adapted from sentinel2_stack_all.py.
 (*) This version creates a 20m product. 
 (*) This version will (in progress) replace atmospheric effects with NAN
@@ -42,10 +43,13 @@ https://docs.python.org/3/library/xml.etree.elementtree.html
 '''
 import os
 import sys
+import shutil
 import multiprocessing as mp
-from misc import args, sep, exists, parfor, err, run, pd
+from misc import args, sep, exists, parfor, err, run, pd, band_names, read_hdr
 
-swir_only = len(args) > 3
+
+print(args)
+swir_only = len(args) > 2
 
 N_THREAD = mp.cpu_count()
 ehc = pd + 'envi_header_cleanup.py' # envi header cleanup command.. makes file open in "imv" 
@@ -113,8 +117,12 @@ for safe in safes:   # S2A_MSIL2A_20170804T190921_N0205_R056_T10UFB_20170804T191
                 cmd += '; ' + (' '.join(['python3',
                                          ehc,
                                          hfn]))
-                cmds.append(cmd)
-            print(cmd)
+                if swir_only and (len(cmd.split(':10m:')) > 1 or len(cmd.split(':TCI:')) > 1):
+                    pass 
+                else:
+                    cmds.append(cmd)
+                    print(cmd)
+
     parfor(run, cmds, 4)  # 4 hw mem channels a good guess?
     bins = [x.strip() for x in os.popen("ls -1 " +
                                         safe +
@@ -126,7 +134,7 @@ for safe in safes:   # S2A_MSIL2A_20170804T190921_N0205_R056_T10UFB_20170804T191
     for b in bins:
         print('* ' + b)
 
-    if len(bins) < 3:
+    if len(bins) < 3 and not swir_only:
         print("unexpected number of bin files (expected 3)")
         continue
         err('unexpected number of bin files (expected 3): ' +
@@ -136,15 +144,25 @@ for safe in safes:   # S2A_MSIL2A_20170804T190921_N0205_R056_T10UFB_20170804T191
     # m10, m20, m60 = bins
     m20, m60 = m10.replace('_10m_', '_20m_'),\
                m10.replace('_10m_', '_60m_')
-    print('10m:', m10); print('20m:', m20); print('60m:', m60)
+    if swir_only:
+        m20 = bins[0]
+        m60 = bins[1]
+        del m10
+    else:
+        print('10m:', m10)
+    print('20m:', m20); print('60m:', m60)
     
-    for m_i in [m10, m20, m60]:
-        if not exists(m_i):
-            err('file not found:' + m_i)
+    if not swir_only:
+        for m_i in [m10, m20, m60]:
+            if not exists(m_i):
+                err('file not found:' + m_i)
 
     # names for files resampled to 10m
-    m10r, m60r = (m10[:-4] + '_20m.bin',
-                  m60[:-4] + '_20m.bin')
+    try:
+        m10r = m10[:-4] + '_20m.bin' 
+    except:
+        pass
+    m60r = m60[:-4] + '_20m.bin'
 
     def resample(pars): # resample src onto ref, w output file dst
         src, ref, dst = pars
@@ -158,18 +176,25 @@ for safe in safes:   # S2A_MSIL2A_20170804T190921_N0205_R056_T10UFB_20170804T191
             run(' '.join(cmd))
         return 0
     
-    a = parfor(resample,
-               [[m10, m20, m10r], # resample the 20m
-                [m60, m20, m60r]], 2) # resample the 60m
+    if not swir_only:
+        a = parfor(resample,
+                   [[m10, m20, m10r], # resample the 20m
+                    [m60, m20, m60r]], 2) # resample the 60m
+    else:
+        resample([m60, m20, m60r])
 
-    sfn = (safe + sep + m10.split(sep)[-1].replace("_10m", "")[:-4]
-            + '_20m.bin')  # name of stacked file
+    try:
+        sfn = (safe + sep + m10.split(sep)[-1].replace("_10m", "")[:-4]
+                + '_20m.bin')  # name of stacked file
+    except:
+        sfn = (safe + sep + m20.split(sep)[-1].replace("_20m", "")[:-4]
+                + '_20m.bin')
 
     sfn = TILE_ID + '_' + DATE + '.bin'
 
     print("sfn", sfn)
     cmd = ['cat', # cat bands together, remember to cat the header files after
-            m10r,
+            m10r if not swir_only else '',
             m20,
             m60r,
             '>',
@@ -185,11 +210,24 @@ for safe in safes:   # S2A_MSIL2A_20170804T190921_N0205_R056_T10UFB_20170804T191
     cmd = ['python3', # envi_header_cat.py like an rpn, first thing goes on back
            pd + 'envi_header_cat.py',
            m20[:-4] + '.hdr',
-           m10r[:-4] + '.hdr',
+           m10r[:-4] + '.hdr' if not swir_only else "",
            shn,
            dp20,
            dp10]
-    run(' '.join(cmd))
+    if not swir_only:
+        run(' '.join(cmd))
+    else:
+        print(m20[:-4] + '.hdr', "-->", shn)
+        shutil.copyfile(m20[:-4] + '.hdr', shn)
+        run('fh ' + shn)
+        band_names_20 = [(dp20 + x) for x in band_names(shn)]
+        print(band_names_20)
+        band_names_20 = ['"' + x.replace('"', '').replace('(', '[').replace(')',']') + '"' for x in band_names_20]
+        samples, lines, bands = read_hdr(shn)
+        #     err('envi_header_modify.py [.hdr file to modify] [nrow] [ncol] [nband] [band 1 name]... [band n name]')
+        cmd = 'python3 ~/GitHub/wps-research/py/envi_header_modify.py ' + shn + ' ' + str(lines) + ' ' + str(samples) + ' ' + str(bands) + ' ' + ' '.join(band_names_20)
+        print([cmd])
+        run(cmd)
 
     cmd = ['python3',
             pd + 'envi_header_cat.py',
@@ -206,6 +244,12 @@ for safe in safes:   # S2A_MSIL2A_20170804T190921_N0205_R056_T10UFB_20170804T191
     open(shn, 'wb').write(mod)
     print('+w', shn)
  
+    if swir_only:
+        run('sentinel2_swir_subselect ' + sfn)
+        shutil.copyfile(sfn + '_swir.bin', sfn)
+        shutil.copyfile(sfn + '_swir.hdr', shn)
+        run('fh ' + shn)
+
     cmd = ['python3',
            pd + 'raster_reorder_increasing_nm.py',
            sfn]
@@ -227,7 +271,7 @@ for safe in safes:   # S2A_MSIL2A_20170804T190921_N0205_R056_T10UFB_20170804T191
     # set no-data areas to NAN:
     run("raster_zero_to_nan " + sfn)
     
-if len(args) > 2:
+if len(args) > 1 and len(args) < 3:
     # cat the bin files together, combining headers
     cmd = ['python3', pd + 'raster_stack.py']
     cmd = cmd + raster_files + ['raster.bin']
