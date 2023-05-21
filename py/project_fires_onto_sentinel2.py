bc9e619bc6a300c96fc753bcac6a3f2dedad210f
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-20 23:46:41 -0700

    update

--- py/project_fires_onto_sentinel2.py
+++ py/project_fires_onto_sentinel2.py
@@ -39,6 +39,7 @@ def print_feature(feature):
 		print(f"{field_name}: {field_value}")
 
 # intersect one shapefile with another
+my_tiles = {}
 def shapefile_intersect(s1_path, s2_path):
     # Open the first shapefile
 	s1_driver = ogr.GetDriverByName('ESRI Shapefile')
@@ -75,12 +76,16 @@ def shapefile_intersect(s1_path, s2_path):
 			# print_feature(feature2)
 			if geometry1.Intersects(geometry2):
 				print("Features intersect!")
-				print_feature(feature1)
+				#print_feature(feature1)
+				my_fire = feature1.GetField("FIRE_NUM")
+				my_tile = feature2.GetField("Name")
+				if my_fire not in my_tiles:
+					my_tiles[my_fire] = set()
+				my_tiles[my_fire].add(my_tile)
 				print("-------------------")
-				print_feature(feature2)
+				#print_feature(feature2)
 				#print(geometry1)
 				#print(geometry2)
-				sys.exit(1)
 	# Clean up and close the shapefile data sources
 	s1_dataSource = None
 	s2_dataSource = None
@@ -89,3 +94,6 @@ s1 = 'reproject/prot_current_fire_polys_reproject.shp'
 s2 = 'sentinel2_bc_tiles_shp/Sentinel_BC_Tiles_reprojected.shp' #tiles_4326 # 'sentinel2_bc_tiles_shp/Sentinel_BC_Tiles_EPSG_4326.shp'
 
 shapefile_intersect(s1, s2)
+
+for fire in my_tiles:
+	print(fire, my_tiles[fire])
