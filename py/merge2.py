c78edd4981bde61dd73d54952a51fce8b6e18259
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-23 22:43:46 -0700

    update

--- py/merge2.py
+++ py/merge2.py
@@ -1,26 +1,42 @@
-'''20230516 run gdal_merge.py on all .bin files in present folder'''
+'''20230516 run gdal_merge.py on all .bin files in present folder https://github.com/OSGeo/gdal/issues/3098
+'''
 import os
-from misc import run
+import sys
+from misc import run, parfor, exists
 lines = [x.strip() for x in os.popen('ls -1 *.bin').readlines()]
 
-open('.file_list', 'wb').write(('\n'.join(lines)).encode())
+if not exists('resample'):
+	os.mkdir('resample')
+
+cmds = []
+for L in lines:
+	if not exists('resample/' + L):
+		cmds += [' '.join(['gdalwarp',
+						   '-wm 1024',
+						   '-wo NUM_THREADS=4',
+						   '-multi',
+						   '-r bilinear',
+						   '-srcnodata nan',
+						   '-dstnodata nan',
+						   '-of ENVI',
+						   '-ot Float32',
+						   '-t_srs EPSG:4326',
+						   L,
+						   'resample/' + L])]
+parfor(run, cmds, 2)
+
 run(' '.join(['gdalbuildvrt',
-        	  '-input_file_list .file_list',
-			  'tmp.vrt']))
-run(' '.join(['gdal_translate',
-			  'tmp.vrt', 
-			  '-of ENVI -ot Float32',
-			  "-a_nodata '-nan'",
-			  'merge.bin']))
+			  '-srcnodata nan',
+		      '-vrtnodata nan',
+			  '-resolution highest',
+			  '-overwrite',
+			  'merge.vrt',
+			  'resample/*.bin']))
+
+if not exists('merge.bin'):
+	run('gdalwarp -wm 4096 -wo NUM_THREADS=8 -multi -overwrite -r bilinear -of ENVI -ot Float32 -srcnodata nan -dstnodata nan merge.vrt merge.bin')  # -t_srs EPSG:4326
+open('.file_list', 'wb').write(('\n'.join(lines)).encode())
+
 run('fh merge.hdr')
 run('envi_header_copy_bandnames.py ' + lines[0][:-4] + '.hdr merge.hdr')
 
-
-'''
-gdalwarp  -of ENVI -ot Float32 -t_srs EPSG:4326 T10.bin T10_r.bin
-gdalwarp  -of ENVI -ot Float32 -t_srs EPSG:4326 T11.bin T11_r.bin 
-raster_zero_to_nan T11_r.bin 
-raster_zero_to_nan T10_r.bin 
-gdalbuildvrt merged.vrt T10_r.bin T11_r.bin 
-gdal_translate merged.vrt -of ENVI -ot Float32 -a_nodata '-nan' merge.bin
-'''
