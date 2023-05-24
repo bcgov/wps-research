6ef3aa6bb712e6024ed270678a85cf8b725bc055
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-24 16:09:04 -0700

    update

--- imv/util.cpp
+++ imv/util.cpp
@@ -634,7 +634,7 @@ void multilook_scene(size_t k){
 
 	size_t x;
 	for0(x, mlk_scene_nr){
-    if(x % 100 == 0){
+    if(x % 1000 == 0){
 			printf("x=%zu/ %zu  SEEK %zu\n", x + 1, mlk_scene_nr, (np*k) + mlk_scene_nc * x);
 		}
     fseek(f, (np * k) + mlk_scene_nc * x, SEEK_SET);
