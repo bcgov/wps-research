ce6a2bf16a40342408a0f781473a8bd05d58287b
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-24 09:06:22 -0700

    update

--- py/merge2.py
+++ py/merge2.py
@@ -1,5 +1,9 @@
 '''20230516 run gdal_merge.py on all .bin files in present folder https://github.com/OSGeo/gdal/issues/3098
+
+EPSG 
 '''
+EPSG = 3005  # BC Albers
+# EPSG = 3347 # Canada LCC
 import os
 import sys
 from misc import run, parfor, exists
@@ -20,7 +24,7 @@ for L in lines:
 						   '-dstnodata nan',
 						   '-of ENVI',
 						   '-ot Float32',
-						   '-t_srs EPSG:4326',
+						   '-t_srs EPSG:' + str(EPSG),
 						   L,
 						   'resample/' + L])]
 parfor(run, cmds, 2)
@@ -34,8 +38,18 @@ run(' '.join(['gdalbuildvrt',
 			  'resample/*.bin']))
 
 if not exists('merge.bin'):
-	run('gdalwarp -wm 4096 -wo NUM_THREADS=8 -multi -overwrite -r bilinear -of ENVI -ot Float32 -srcnodata nan -dstnodata nan merge.vrt merge.bin')  # -t_srs EPSG:4326
-open('.file_list', 'wb').write(('\n'.join(lines)).encode())
+	run(' '.join(['gdalwarp',
+				  '-wm 4096',
+				  '-wo NUM_THREADS=8',
+				  '-multi',
+				  '-overwrite',
+				  '-r bilinear',
+				  '-of ENVI',
+			      '-ot Float32',
+			      '-srcnodata nan',
+				  '-dstnodata nan',
+				  'merge.vrt',
+				  'merge.bin']))  
 
 run('fh merge.hdr')
 run('envi_header_copy_bandnames.py ' + lines[0][:-4] + '.hdr merge.hdr')
