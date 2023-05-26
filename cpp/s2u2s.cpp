054856c9ef25033dd113db6a2b4a676824da753d
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-26 00:25:13 -0700

    update

--- cpp/s2u2s.cpp
+++ cpp/s2u2s.cpp
@@ -1,7 +1,7 @@
 /* 20230516  Extract all sentinel2 .SAFE folders to SWIR (re)sampled at 20m. */
 #include"misc.h"
 int main(int argc, char ** argv){
-  //system("safe_unzip.py");
+  system("safe_unzip_gid.py");
   system("sentinel2_stack_all_20m.py 1 1");
   system("clean");
   return 0;
