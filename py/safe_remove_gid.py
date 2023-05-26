28e6ffe62a04ccb7b8c61d98354976783e9f21ce
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-26 08:37:55 -0700

    update

--- py/safe_remove_gid.py
+++ py/safe_remove_gid.py
@@ -1,4 +1,5 @@
-'''20230526 delete zip files from other Jurisdictions'''
+'''20230526 delete zip files from other Jurisdictions
+and delete .SAFE folders as well! For datasets outside of the listed GID'''
 import os
 import sys
 import datetime
@@ -20,3 +21,10 @@ for gid in zip_gid:
         for f in zip_gid[gid]:
             run('rm ' + f)
 
+
+safes = [x.strip() for x in os.popen('ls -1 | grep .SAFE').readlines()]
+for safe in safes:
+    w = safe.split('_')
+    gid = w[5]
+    if gid not in select:
+        run('rm -rf ' + safe)
