63c0a2175fc163b66bb7c92e50a719c54db5582f
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-28 17:27:52 -0700

    update

--- rcm/snap_process_compact2.py
+++ rcm/snap_process_compact2.py
@@ -107,10 +107,7 @@ for p in folders:
     if not exist(p_5) or RE_CALC:
         run([snap,
             'Terrain-Correction',
-            #'-PoutputComplex=true',
             '-PnodataValueAtSea=false',
-            # '-PsaveLayoverShadowMask=true',
-            # '-PimgResamplingMethod="NEAREST_NEIGHBOUR"', # why? because we will add an index for reverse geocoding...
             '-Ssource=' + p_4, 
             '-t ' + p_5])
 
@@ -129,102 +126,3 @@ for p in folders:
             '-PfilterSize=' + str(FILTER_SIZE),
             '-Ssource=' + p_5,
             '-t ' + p_6])  # output folder
-
-
-
-'''
-    # run all the CP-pol decoms
-    decoms = ["'M-Chi Decomposition'",
-              "'M-Delta Decomposition'",
-              "'H-Alpha Decomposition'",
-              "'2 Layer RVOG Model Based Decomposition'",
-              "'Model-free 3-component decomposition'"]
-
-    decom_folders = []
-    for decom in decoms:
-        decom_name = decom.strip("'").replace(' ', '_')
-        p_5 = p + sep + decom_name + '.dim'
-        decom_folders.append(p_5)
-        
-        if not exist(p_5) or RE_CALC:
-            run([snap,
-                 'CP-Decomposition',
-                 '-Pdecomposition=' + decom,
-                 '-PwindowSizeXStr=' + str(FILTER_SIZE),
-                 '-PwindowSizeYStr=' + str(FILTER_SIZE),
-                 '-Ssource=' + p_4,
-                 '-t ' + p_5])
-
-        stack_f = p_5[:-3] + 'data' + sep + 'stack.bin'
-        print(stack_f)
-        if not exist(stack_f) or RE_CALC:
-            cmd = ['python3',
-                    pd + 'snap2psp_inplace.py',
-                    p_5[:-3] + 'data' + sep,
-                    '1']
-            run(cmd)
-'''
-'''
-    # 20211125 analysis
-    c1 = ' '.join([snap,
-                  'Terrain-Correction',
-                  '-PoutputComplex=true',
-                  '-PnodataValueAtSea=false', # '-PsaveLayoverShadowMask=true',
-                  '-PimgResamplingMethod="NEAREST_NEIGHBOUR"', # why? because we will add an index for reverse geocoding...
-                  in_1, '-t ' + in_2])
-
-    in_3 = p + sep + 'b7.dim'  # box filtered output # how to get parameters: ./gpt Polarimetric-Speckle-Filter -h
-    c2 = ' '.join([snap,
-                  'Polarimetric-Speckle-Filter',
-                  '-Pfilter="Box Car Filter"',
-                  '-PfilterSize=7',
-                  in_2, '-t', in_3])  # -t is for output file
-
-    use_C = False  # default to T matrix.
-    if not exist(in_2):
-        run(c1)
-    if not exist(in_3):
-        run(c2)
-    r_f, r_h = p + '_rgb.bin', p + '_rgb.hdr'
-    hf = in_3[:-3] + 'data' + sep + 'T11.hdr'
-    if exist(hf):
-        if not exist(r_h):
-            run('cp ' + hf + ' ' + r_h)
-
-    else:
-        hf = in_3[:-3] + 'data' + sep + 'C11.hdr'
-        if exist(hf):
-            use_C = True
-            if not exist(r_h):
-                run('cp ' + hf + ' ' + r_h)
-        else:
-            err('not found', hf)
-
-    dat = open(r_h).read().strip()
-    dat = dat.replace("bands = 1", "bands = " + ("3" if not use_C else "4"))
-    dat = dat.replace("byte order = 1", "byte order = 0")
-    if not use_C:
-        dat = dat.replace("band names = { T11 }",
-                          "band names = {T22,\nT33,\nT11}")
-    else:
-        dat = dat.replace("band names = { C11 }",
-                          "band names = {C11,\nC22,\nC12_real,\nC12_imag}")
-
-    open(r_h, 'wb').write(dat.encode())  # write revised header
-    print(r_h, ':')
-    a = os.system('cat ' + r_h)
-
-    if not exist(r_f):
-        file_pre = 'T' if not use_C else 'C'  # file prefix: T or C mtx
-        c, t = 'cat', in_3[:-3] + 'data' + sep + file_pre # ('T' if not use_C else 'C')
-        file_i = ['22.bin', '33.bin', '11.bin'] if not use_C else ['11.bin', '22.bin', '12_real.bin', '12_imag.bin']
-        for i in file_i:
-            # (['22.bin', '33.bin', '11.bin'] if not use_C else ['11.bin', '22.bin', '12_real.bin', '12_imag.bin']) :
-            ti = t + i
-            if not exist(ti):
-                run('./sbo ' + (ti[:-3] + 'img') + ' ' + ti + ' 4')
-            c += (' ' + t + i)
-        c += ' > ' + r_f
-        run(c)
-    ci += 1
-'''
