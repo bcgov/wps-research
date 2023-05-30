8e306d491f7f1b28a99120389fdb37d9e309f18c
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-29 19:49:21 -0700

    update

--- cpp/raster_bb.cpp
+++ cpp/raster_bb.cpp
@@ -68,13 +68,13 @@ int main(int argc, char ** argv){
   for0(k, nband){
     double wavelength_nm = (double) lambda[k];  // our wavelength is in nanometres
     double wavelength_um = 0.001 * wavelength_nm; // the form expected wavelength in micrometre(um)
-    double CWL = wavelength_um * 0.000001;
+    double Lambda = wavelength_um * 0.000001;
     for0(i, np){
       d = dat[i + k * np]; // get the value of pixl "i" for band "k"
       if(d == 0. || isnan(d) || isinf(d)) T = NAN; // check if value is zero or NAN (no-data)
       else{
-        RADIANCE = ((double)d)  / 10000.;
-        T = h * c / (K * CWL) / log((2. * h * c * c) / (RADIANCE * 1000000. * pow(CWL, 5.)) + 1.);
+        RADIANCE = ((double)d) / 10000.;
+        T = ((h * c) / (K * Lambda))  / log((2. * h * pow(c, 2.) * pow(Lambda, -5.) / (RADIANCE * 1000000.)) + 1.);
       }
       if(i %1000 == 0) printf("d %f T %e\n", d, T);
       dat[i + k * np] = (float)T;
