cf1c4da8c7acb28838ed3ad26ca29e6e2a35ccfd
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-01 23:38:16 -0700

    only open data we havent viewed already

--- py/imv.py
+++ py/imv.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 import os
 import sys
+exist = os.path.exists
+
+ignore = '.imv_ignore'
+if not exist(ignore):
+    open(ignore, 'wb').write("tmp_subset.bin".encode())
+
 
 lines = os.popen("ls -1 *.bin").readlines()
 lines = [x.strip() for x in lines]
 
 for line in lines:
-    if line != "tmp_subset.bin":
+    ignore_files = set([x.strip() for x in open(ignore).read().strip().split('\n')])
+    if line != "tmp_subset.bin" and line not in ignore_files:
         a = os.system("imv " + line)
-
+        ignore_files.add(line)
+        open(ignore,'wb').write(('\n'.join(list(ignore_files))).encode())
+        sys.exit(0)
 # need to plot filename title on imv
