c56d0ba2f88424093593434e6a52533c3906ba83
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-04 17:07:53 -0700

    update

--- py/raster_tile_index.py
+++ py/raster_tile_index.py
@@ -6,7 +6,7 @@ from misc import timestamp
 ts = timestamp()
 
 # list available rasters
-rasters = [x.strip() for x in os.popen('ls -1 "*.bin"').read().strip().split('\n')]
+rasters = [x.strip() for x in os.popen('ls -1 *.bin').read().strip().split('\n')]
 
 # write list of rasters
 open(ts + '_files.txt', 'wb').write(('\n'.join(rasters)).encode())
