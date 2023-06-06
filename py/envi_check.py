4d35f8f0d6e1e5840a431bde90e46ba12c35fdd8
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-05 22:23:09 -0700

    update

--- py/envi_check.py
+++ py/envi_check.py
@@ -1,12 +1,33 @@
 '''20230605 check .bin files to see if the file sizes match the header'''
-from misc import err
+from misc import err, hdr_fn, read_hdr, args
+import os
 
 lines = [x.strip() for x in os.popen("ls -1 *.bin").readlines()]
 
-for line in lines:
-    print(line)
+fails = []
+for f in lines:
+    [samples, lines, bands] = [int(x)
+                               for x in read_hdr(hdr_fn(f))]
+    f_size = os.stat(f).st_size
+    expected = samples * lines * bands * 4 
 
+    if f_size == expected:
+        print(f, '[OK]')
+    else:
+        print(f, f_size, '/', expected)
+        fails += [f]
 
+if len(fails) > 0:
+    print("Checksum failed for files:")
+    print(' '.join(fails))
 
+    if len(args) > 1:
+        for f in fails:
+            hf = hdr_fn(f)
+            for x in [hf, f]:
+                print('rm', x)
+                os.remove(x)
+else:
+    print("All .bin files OK")
 
 
