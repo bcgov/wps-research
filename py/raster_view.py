5bc9830bfde1d36f0d8bb53425750eeb30a805c7
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-14 19:10:09 -0700

    update

--- py/raster_view.py
+++ py/raster_view.py
@@ -8,28 +8,37 @@ from OpenGL.GLUT import *
 image_data = None
 image_width = None
 image_height = None
+num_bands = None
 
 
 def load_image(image_path):
-    global image_data, image_width, image_height
+    global image_data, image_width, image_height, num_bands
     dataset = gdal.Open(image_path, gdal.GA_ReadOnly)
     image_width = dataset.RasterXSize
     image_height = dataset.RasterYSize
-    band = dataset.GetRasterBand(1)
-    image_data = band.ReadAsArray(0, 0, image_width, image_height).astype(np.float32)
+    num_bands = dataset.RasterCount
+    image_data = np.zeros((image_height, image_width, num_bands), dtype=np.float32)
+    
+    for i in range(num_bands):
+        band = dataset.GetRasterBand(i + 1)
+        image_data[:, :, i] = band.ReadAsArray(0, 0, image_width, image_height).astype(np.float32)
+    
     dataset = None
+
 
 def draw_scene():
     glClear(GL_COLOR_BUFFER_BIT)
     normalized_image_data = (image_data - np.min(image_data)) / (np.max(image_data) - np.min(image_data))
     flipped_image_data = np.flipud(normalized_image_data)  # flip the image vertically
     glRasterPos2i(-1, -1)  # draw the image
-    glDrawPixels(image_width, image_height, GL_RED, GL_FLOAT, flipped_image_data)
+    glDrawPixels(image_width, image_height, GL_RGB, GL_FLOAT, flipped_image_data)
     glutSwapBuffers()
+
 
 def keyboard_callback(key, x, y):
     if key == b'\x1b':  # Escape key
         glutLeaveMainLoop()
+
 
 def main():
     if len(sys.argv) < 2:
@@ -48,5 +57,7 @@ def main():
     glutKeyboardFunc(keyboard_callback)
     glutMainLoop()
 
+
 if __name__ == "__main__":
     main()
+
