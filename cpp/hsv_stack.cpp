cbb76ef1e8ba81f291cbe602a30699267eafcf0b
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-16 00:03:51 -0700

    update

---
+++ cpp/hsv_stack.cpp
@@ -0,0 +1,13 @@
+/* 20220811 */
+#include "misc.h"
+
+int main(int argc, char ** argv){
+  str f("sub.bin");a
+  run(str("envi2tif.py sub.bin &"));
+  run(str("htrim2 ") + f + str(" .25"));
+  run(str("hsv2rgb sub.bin_ht.bin"));
+  run("raster_stack.py sub.bin_ht.bin sub.bin_ht.bin_hsv.bin stack.bin");
+  run("envi_header_copy_mapinfo.py sub.hdr stack.hdr");   
+  run("kgc stack.bin 33333 700 0");
+  return 0;
+}
