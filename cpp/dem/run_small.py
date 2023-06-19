65fec18779f503524b091edd6456e004473ef6c8
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-19 10:30:35 -0700

    update

--- cpp/dem/run_small.py
+++ cpp/dem/run_small.py
@@ -6,7 +6,7 @@ def run(c):
     return a
 
 run("make")
-run("raster_multilook dat/stack.bin 4")
+run("multilook dat/stack.bin 4")
 run("raster_scale dat/stack.bin_mlk.bin")
 run("envi_header_copy_bandnames.py dat/stack.hdr dat/stack.bin_mlk.hdr")
 run("./dem dat/stack.bin_mlk.bin_scale.bin")
