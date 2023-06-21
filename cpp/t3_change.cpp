b6ae637d6ae5824cd9bdbcc2d2d3a6b1b38bc31b
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-21 00:25:50 -0700

    update

--- cpp/t3_change.cpp
+++ cpp/t3_change.cpp
@@ -4,21 +4,15 @@
 int main(int argc, char ** argv){
   str sep("/");
   vector<str> channels;
-  channels.push_back(str("T11.bin"));
-  channels.push_back(str("T12_imag.bin"));
-  channels.push_back(str("T12_real.bin"));
-  channels.push_back(str("T13_imag.bin"));
-  channels.push_back(str("T13_real.bin"));
-  channels.push_back(str("T22.bin"));
-  channels.push_back(str("T23_imag.bin"));
-  channels.push_back(str("T23_real.bin"));
+  channels.push_back(str("T11.bin")); channels.push_back(str("T12_imag.bin"));
+  channels.push_back(str("T12_real.bin")); channels.push_back(str("T13_imag.bin"));
+  channels.push_back(str("T13_real.bin")); channels.push_back(str("T22.bin"));
+  channels.push_back(str("T23_imag.bin")); channels.push_back(str("T23_real.bin"));
   channels.push_back(str("T33.bin"));
 
   if(argc < 3) err("t3_change.py [t3 path: pre] [t3 path 2: post]");
-
   size_t nr, nc, nb, np, i, j;
-  vector<float*> a;
-  vector<float*> b;
+  vector<float*> a, b;
   float * out;
 
   for0(i, 9){
@@ -26,22 +20,19 @@ int main(int argc, char ** argv){
     str hfn(hdr_fn(str(argv[1]) + sep + channels[i]));
     hread(hfn, nr, nc, nb);
     a.push_back(bread(fn, nr, nc, nb));
+
+    str fn2(str(argv[2]) + sep + channels[i]);
+    str hfn2(hdr_fn(str(argv[2]) + sep + channels[i]));
+    hread(hfn2, nr, nc, nb);
+    b.push_back(bread(fn2, nr, nc, nb));
   }
 
-  for0(i, 9){
-    str fn(str(argv[2]) + sep + channels[i]);
-    str hfn(hdr_fn(str(argv[2]) + sep + channels[i]));
-    hread(hfn, nr, nc, nb);
-    b.push_back(bread(fn, nr, nc, nb));
-  }
-
-  np = nr * nc;
-
-  out = falloc(np * 3);
+  np = nr * nc; // pixels per band
+  out = falloc(np * 3); // 3-channel out buffer
 
   for0(i, np){
-    if(i % 11111 == 0) printf("%f %zu/%zu\n", 100. * (float)(i+1) / (float)np, i+1, np);
-    
+    if(i % 1000000 == 0) printf("%f %zu/%zu\n", 100. * (float)(i+1) / (float)np, i+1, np);
+
     herm3<cf> A;
     herm3<cf> B;
     A.initT3(a[0][i], a[1][i], a[2][i], a[3][i], a[4][i], a[5][i], a[6][i], a[7][i], a[8][i]);
@@ -49,14 +40,9 @@ int main(int argc, char ** argv){
     herm3<cf> C(B - A);
 
     herm3<cf> D;
-    A.inv();
-    //* B);
+    A.inv(); //* B);
 
-    vec3<cf> E1;
-    vec3<cf> E2;
-    vec3<cf> E3;
-    vec3<cf> L;
-
+    vec3<cf> E1, E2, E3, L;
     TYPE R = eig(C , L, E1, E2, E3);
 
     out[i] = abs(L.a);
@@ -75,8 +61,8 @@ int main(int argc, char ** argv){
 
   }
 
-  str ofn(str(argv[1]) + str("_") + str(argv[2]) + str("lambda.bin"));
-  str ohn(str(argv[1]) + str("_") + str(argv[2]) + str("lambda.hdr"));
+  str ofn(str("lambda_") + str(argv[1]) + str("_") + str(argv[2]) + str(".bin"));
+  str ohn(str("lambda_") + str(argv[1]) + str("_") + str(argv[2]) + str(".hdr"));
   bwrite(out, ofn, nr, nc, 3);
   hwrite(ohn, nr, nc, 3);
 
