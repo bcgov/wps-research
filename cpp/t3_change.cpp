4eacae254c25e751df625498f2f5a19e8d76a2cb
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-20 23:06:14 -0700

    update

--- cpp/t3_change.cpp
+++ cpp/t3_change.cpp
@@ -1,3 +1,4 @@
+#include"matrix3.h"
 #include"misc.h"
 
 int main(int argc, char ** argv){
@@ -16,8 +17,7 @@ int main(int argc, char ** argv){
     err("t3_change.py [t3 path 1] [t3 path 2]");
   }
 
-  int i;
-  size_t nr, nc, nb, np;
+  size_t nr, nc, nb, np, i, j;
   float * a[9];
   float * b[9];
   
@@ -39,6 +39,14 @@ int main(int argc, char ** argv){
 
   np = nr * nc;
   for0(i, np){
+    herm3<cf> A;
+    herm3<cf> B;
+     /*void initT3(float T11R, float T12I, float T12R, float T13I, float T13R,
+              float T22R, float T23I, float T23R, float T33R) */
+
+    A.initT3(a[0][i], a[1][i], a[2][i], a[3][i], a[4][i], a[5][i], a[6][i], a[7][i], a[8][i]);
+    B.initT3(b[0][i], b[1][i], b[2][i], b[3][i], b[4][i], b[5][i], b[6][i], b[7][i], b[8][i]);
+
   }
 
 
