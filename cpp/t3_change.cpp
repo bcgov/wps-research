abdb237c2a966dd7c5da65da09f2eb9130af1295
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-21 00:10:43 -0700

    update

--- cpp/t3_change.cpp
+++ cpp/t3_change.cpp
@@ -3,18 +3,25 @@
 
 int main(int argc, char ** argv){
   str sep("/");
-  str channels[9] ={
-    str("T11.bin"), str("T12_imag.bin"), str("T12_real.bin"), str("T13_imag.bin"), str("T13_real.bin"), str("T22.bin"), str("T23_imag.bin"), str("T23_real.bin"), str("T33.bin")
-  };
-  if(argc < 3){
-    err("t3_change.py [t3 path 1 (pre)] [t3 path 2 (post)]");
-  }
+  vector<str> channels;
+  channels.push_back(str("T11.bin"));
+  channels.push_back(str("T12_imag.bin"));
+  channels.push_back(str("T12_real.bin"));
+  channels.push_back(str("T13_imag.bin"));
+  channels.push_back(str("T13_real.bin"));
+  channels.push_back(str("T22.bin"));
+  channels.push_back(str("T23_imag.bin"));
+  channels.push_back(str("T23_real.bin"));
+  channels.push_back(str("T33.bin"));
 
-size_t nr, nc, nb, np, i, j;
-float * a[9];
-float * b[9];
+  if(argc < 3) err("t3_change.py [t3 path: pre] [t3 path 2: post]");
+
+  size_t nr, nc, nb, np, i, j;
+float * a[9]
+;
+float * b[9]
+;
 float * out;
-
 
 for0(i, 9){
 a[i] = NULL;
@@ -38,12 +45,18 @@ out = falloc(np * 3);
 
 for0(i, np){
 
-if(i % 5000 == 0) printf("%zu\n", i);
+if(i % 5000 == 0){
+  printf("%zu\n", i);
+}
 herm3<cf> A;
 herm3<cf> B;
 A.initT3(a[0][i], a[1][i], a[2][i], a[3][i], a[4][i], a[5][i], a[6][i], a[7][i], a[8][i]);
 B.initT3(b[0][i], b[1][i], b[2][i], b[3][i], b[4][i], b[5][i], b[6][i], b[7][i], b[8][i]);
 herm3<cf> C(B - A);
+
+herm3<cf> D;
+A.inv();
+//* B);
 
 vec3<cf> E1;
 vec3<cf> E2;
@@ -67,8 +80,8 @@ if(abs(L.a) != 0. && abs(L.b) != 0. && abs(L.c) != 0.){
 }
 
 }
-  bwrite(out, str("lambda.bin"),  nr, nc, 3);
-  hwrite(str("lambda.hdr"), nr, nc, 3);
+bwrite(out, str("lambda.bin"), nr, nc, 3);
+hwrite(str("lambda.hdr"), nr, nc, 3);
 
 return 0;
 }
