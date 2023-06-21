1c909571738d08d1996c24dc872ed92da3e8f189
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-20 21:23:32 -0700

    update

--- py/snap_stack_extract_T3.py
+++ py/snap_stack_extract_T3.py
@@ -8,18 +8,31 @@ hfn = hdr_fn(fn)
 bn = band_names(hfn)
 
 phone_book = {}
-
 lookup = {bn[i]: i for i in range(len(bn))}
 
 for b in bn:
     w = b.split('_')
-
     ti = w[0].split('T')[1]
-
+    
     if ti[0] == '4' or ti[1] == '4':
         continue  # use T3 elements only
+    
     if not w[-1] in phone_book:
         phone_book[w[-1]] = []
     phone_book[w[-1]] += [b]
 
-print(phone_book)
+for p in phone_book:
+    print(p)
+    for b in phone_book[p]:
+        #print("\t", b)
+        w = b.split("_")
+        #print("  ",w[:-2])
+
+        ofn = w[-1] + '/' + ('_'.join(w[:-2])) + '.bin'
+        ohn = w[-1] + '/' + ('_'.join(w[:-2])) + '.hdr'
+
+        cmd = ' '.join(['unstack2',
+                        fn,
+                        str(lookup[b]),
+                        ofn])
+        print(cmd)
