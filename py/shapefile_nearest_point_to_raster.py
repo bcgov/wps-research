3bf08422182b2c27de674169b00154b2500be0c0
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-07-13 00:07:15 -0700

    update

--- py/shapefile_nearest_point_to_raster.py
+++ py/shapefile_nearest_point_to_raster.py
@@ -13,8 +13,8 @@ shapefile_ds = ogr.Open(shapefile_path)
 raster_ds = gdal.Open(raster_path)
 
 raster_extent = raster_ds.GetGeoTransform()
-raster_centroid_x = (raster_extent[0] + raster_extent[1]) / 2
-raster_centroid_y = (raster_extent[3] + raster_extent[5]) / 2
+raster_centroid_x = (raster_extent[0] + raster_extent[1])# / 2
+raster_centroid_y = (raster_extent[3] + raster_extent[5])# / 2
 raster_centroid = ogr.Geometry(ogr.wkbPoint)
 raster_centroid.AddPoint(raster_centroid_x, raster_centroid_y)
 
@@ -25,6 +25,7 @@ nearest_distance = float('inf')
 for feature in layer:
     point_geometry = feature.GetGeometryRef()
     distance = point_geometry.Distance(raster_centroid)
+    print(feature.GetFieldAsString(0), point_geometry, raster_centroid)
 
     if distance < nearest_distance:
         nearest_distance = distance
@@ -51,3 +52,22 @@ if nearest_point is not None:
         print(f"{field_name}:{field_value}")
 else:
     print("No points found in the shapefile.")
+
+
+'''
+If the centroid of the raster footprint appears to be off and needs to be multiplied by 2, it suggests that there might be an issue with the transformation to the desired coordinate system (EPSG 4326) or the interpretation of the resulting coordinates.
+
+Here are a few potential reasons why this could occur:
+
+    Coordinate System Mismatch: It's possible that the shapefile or the raster is not actually being transformed to EPSG 4326 (WGS 84) during the coordinate transformation process. Double-check that the correct EPSG code (4326) is being used for the transformation and that both the shapefile and raster are transformed using the same code.
+
+    Unit Conversion: Ensure that the units of the shapefile and raster are correctly accounted for during the transformation. For example, if the shapefile is in meters and the raster is in degrees, the resulting coordinates will not align properly. Make sure that the units of both datasets are compatible and consistent.
+
+    Incorrect Extent or Resolution: Verify that the extent and resolution of the raster are correctly interpreted and applied during the transformation. If the extent or resolution is not properly considered, it can lead to inconsistencies in the resulting coordinates.
+
+    Data Interpretation: Check how the transformed coordinates are being interpreted or used in subsequent calculations. It's possible that there might be an error in the calculation or interpretation of the centroid coordinates, which is unrelated to the transformation itself.
+
+To diagnose the issue, you can print and inspect the transformed coordinates of some specific points in both the shapefile and the raster, both before and after the transformation. This can help identify any discrepancies or inconsistencies in the transformation process.
+
+Additionally, verifying the correctness of the transformed coordinates by comparing them with known reference points or using visualization techniques can aid in identifying the problem.
+'''
