cf8c7ae6a46a633bc89d2415e137663ffdbf1ce5
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-07-27 16:00:43 -0700

    update

---
+++ py/sentinel2_mrap.py
@@ -0,0 +1,123 @@
+'''20230727 extract most recent available pixel (MRAP) from sentinel2 series.. assume tile based
+
+Difference from sentinel2_extract_swir.py: 
+can't process in parallel (By tile, at least)
+'''
+from envi import envi_header_cleanup
+import multiprocessing as mp
+from osgeo import gdal
+from misc import args
+import numpy as np
+import sys
+import os
+
+my_bands = {}
+my_proj = None
+my_geo = None
+my_xsize = None
+my_ysize = None
+nbands = None
+
+def extract(file_name):
+    global my_proj
+    global my_geo
+    global my_bands
+    global my_xsize
+    global my_ysize
+    global nbands
+    print("+r", file_name)
+    d = gdal.Open(file_name)
+    
+    for i in range(1, d.RasterCount + 1):
+        band = d.GetRasterBand(i)
+        array_data = band.ReadAsArray().astype(np.float32)
+        
+        if i not in my_bands:
+            my_bands[i] = array_data
+
+        else:
+            nans = np.isnan(array_data)
+            update = my_bands[i]
+            update[~nans] = array_data[~nans]
+            my_bands[i] = update
+   
+    if my_proj is None:
+        my_proj = d.GetProjection()
+    if my_geo is None:
+        my_geo = d.GetGeoTransform()
+    if my_xsize is None:
+        my_xsize, my_ysize = d.RasterXSize, d.RasterYSize    
+        nbands = d.RasterCount 
+
+if __name__ == "__main__":
+    file_name = None
+    if len(args) != 2:
+        err("python3 sentinel2_mrap.py [sentinel-2 gid]")
+    else:
+        gid = args[1]
+
+        lines = [x.strip() for x in os.popen("ls -1r S*.bin")]
+        lines2 = [x.split('_') for x in lines]
+        lines3 = [[x[2], x] for x in lines2]
+        lines3.sort()
+
+        lines = ['_'.join(x[1]) for x in lines3]
+        for line in lines:
+            print(line)
+        # extract(file_name)
+
+
+        for line in lines:
+            extract(line)
+        # now save the output
+
+
+    driver = gdal.GetDriverByName('ENVI')
+
+    print("mrap_" + args[1] + ".bin",
+                             my_xsize,
+                             my_ysize,
+                             nbands,
+                             gdal.GDT_Float32)
+
+    stack_ds = driver.Create("mrap_" + args[1] + ".bin",
+                             my_xsize,
+                             my_ysize,
+                             nbands,
+                             gdal.GDT_Float32)
+
+    stack_ds.SetProjection(my_proj)
+    stack_ds.SetGeoTransform(my_geo)
+
+    for i in range(1, nbands + 1):
+        rb = stack_ds.GetRasterBand(i)
+        rb.WriteArray(my_bands[i])
+
+    stack_ds = None
+'''
+Table 3: SCL bit values
+
+Classification
+0: No data
+1: Saturated or defective
+2: Dark area pixels
+3: Cloud shadows
+4: Vegetation
+5: Bare soils
+6: Water
+7: Unclassified
+8: Cloud medium probability
+9: Cloud high probability
+10: Thin cirrus
+11: Snow or ice
+
+
+We don't want:
+0: No data
+1: Saturated or defective
+2: Dark area pixels
+3: Cloud shadows
+8: Cloud medium probability
+9: Cloud high probability
+10: Thin cirrus
+'''
