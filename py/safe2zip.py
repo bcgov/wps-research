0d5545069a973385bea73a9020135f6cd3615ebf
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-07-27 14:30:02 -0700

    update

--- py/safe2zip.py
+++ py/safe2zip.py
@@ -1,4 +1,6 @@
-from misc import err, exists
+'''20230726: create zip files (e.g. for use by sentinel2_extract_swir.py) from .SAFE folders, e.g. downloaded from gcp'''
+
+from misc import err, run, exists, parfor
 import sys
 import os
 
@@ -6,7 +8,14 @@ lines = [x.strip() for x in os.popen('ls -1').readlines()]
 
 for line in lines:
     if line[-5:] == '.SAFE':
-        print(line)
+        b = line[:-5] 
+        zfn = b + '.zip'
+        
+        cmd = 'zip -r ' + zfn + ' ' + line
+        if not exists(zfn):
+            run(cmd)        
+
+
 
 
 
