d6fce0086e71e378d65651783401994cccddf7a4
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-08-27 13:42:08 -0700

    update

--- py/raster_pixel_loc.py
+++ py/raster_pixel_loc.py
@@ -36,7 +36,7 @@ srs_info = {x[0]: x[1] if len(x) > 1 else '' for x in w}
 print(srs_info)
 
 if 'zone' not in srs_info:
-    err("Assumed to be in UTM coordinates")
+    print("was Assumed to be in UTM coordinates")
 
 pts_in = []
 data = open(args[2]).read().strip()
@@ -63,6 +63,41 @@ pts_in = [i[1] for i in Y]
 # prepare to write KML
 print("srs_info", srs_info)
 kml = simplekml.Kml()
+
+# def^ne source & target coordinate systems
+# source_crs = pyproj.CRS(srs_info['proj'])  # Use the CRS information from your data
+# Open the raster dataset to get its CRS
+raster_dataset = gdal.Open(args[1]) # 'your_raster_file.tif')  # Replace with the path to your raster file
+if raster_dataset is None:
+    raise Exception("Failed to open the raster dataset.")
+print(raster_dataset)
+print(gdal.Info(raster_dataset, format='json'))
+wkt = raster_dataset.GetProjection()
+src = osr.SpatialReference()
+src.ImportFromWkt(wkt)
+
+x = gdal.Info(raster_dataset, format='json')
+print("X")
+for y in x:
+    print(y, x[y])
+print("X")
+
+# source image
+src = gdal.Open(args[1]) # , gdalconst.GA_ReadOnly)
+src_proj, src_geotrans = src.GetProjection(), src.GetGeoTransform()
+src_sr = osr.SpatialReference(wkt=src.GetProjection())  # spatial reference
+print("src_proj", src_proj)
+print("src_geot", src_geotrans)
+print("src_sr ", src_sr)
+src = None
+
+
+
+
+source_crs = pyproj.CRS(gdal.Info(raster_dataset, format='json')['coordinateSystem']['wkt'])
+target_crs = pyproj.CRS('EPSG:4326')  # WGS 84, which is commonly used for lat/lon
+transformer = pyproj.Transformer.from_crs(source_crs, target_crs, always_xy=True)
+
 for k in range(len(pts_in)):
     P = pts_in[k]
     n_coords, pts = len(P), []
@@ -71,8 +106,20 @@ for k in range(len(pts_in)):
         Y_line = P[i][0] # float(args[i2 + 2 ]) # row 0-idx
         X_geo = GT[0] + X_pixel * GT[1] + Y_line * GT[2]
         Y_geo = GT[3] + X_pixel * GT[4] + Y_line * GT[5]
-        latlon = utm.to_latlon(X_geo, Y_geo, int(srs_info['zone']), 'N')
-        print(X_pixel, Y_line, X_geo, Y_geo, latlon[0], latlon[1])
+        
+
+        # latlon = utm.to_latlon(X_geo, Y_geo, int(srs_info['zone']), 'N')
+        lon, lat = transformer.transform(X_geo, Y_geo)
+        latlon = [lon,lat]
+         
+        '''
+latlon (58.54544607900768, -120.24008482430301)
+lon -120.24008482768689 lat 58.54544604697952
+3031 365 660620.0 6492740.0 58.54544607900768 -120.24008482430301'''
+
+        # print("latlon", latlon)
+        # print("lon", lon, "lat", lat)   
+        # print(X_pixel, Y_line, X_geo, Y_geo, latlon[0], latlon[1])
         pts.append(latlon)
     pts2 = [(p[1], p[0]) for p in pts]
     pol = kml.newpolygon(name=("fire" + ("" if k == 0 else ('_' + str(k)))))
