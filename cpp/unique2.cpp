5cfb9cd6ec230c94ae3e967ae8116807f2b621fe
ashlinrichardson <ashlin.richardson@gmail.com>
2024-02-26 23:22:36 -0800

    update

--- cpp/unique2.cpp
+++ cpp/unique2.cpp
@@ -1,17 +1,38 @@
-/* 20230424 unique2.cpp: deduplicate records by line
- 
-Input file needs to fit in RAM */
-#include "misc.h"
+/* 20230424 unique2.cpp: deduplicate records by line. 20240226: no dependencies
+Input file needs to fit in RAM
+
+compilation:
+  g++ unique2.cpp -O3 -o unique2
+
+running: e.g.:
+  unique2 EarthNetworks_BCWS_LX_2023.csv 
+
+  wc -l EarthNetworks_BCWS_LX_2023.csv
+  wc -l EarthNetworks_BCWS_LX_2023.csv_unique.csv
+
+*/
+#include<unordered_set>
+#include<stdlib.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+#define str string
+using namespace std;
+
+int err(const char * m){
+  cout << "Error: " << m << endl; 
+  exit(1);
+}
 
 int main(int argc, char ** argv){
   if(argc < 2) err("unique2.cpp [input text file name]\n");
-  
+
   str fn(argv[1]);
   str ofn(fn + str("_unique.txt"));
   
   ifstream dfile(fn);
-  if(!dfile.is_open()) err(string("failed to open file:") + fn);
-
+  if(!dfile.is_open()) err((str("failed to open file:") + fn).c_str());
+  
   str line;
   unordered_set<str> d;
 
@@ -19,12 +40,12 @@ int main(int argc, char ** argv){
   dfile.close();
 
   ofstream outfile(ofn);
-  if(!outfile.is_open()) err(string("failed to open file:") + ofn);
+  if(!outfile.is_open()) err((str("failed to open file:") + ofn).c_str());
 
   unordered_set<str>::iterator it;
   for(it = d.begin(); it != d.end(); it++) outfile << *it << endl;
   outfile.close();
-  
+
   return 0;
 }
 
