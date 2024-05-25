31de1c8586301096d6bc8f4d3242bb4b03185f9a
ashlinrichardson <ashlin.richardson@gmail.com>
2024-05-24 23:19:24 -0700

    update

--- data/ubc/view.py
+++ data/ubc/view.py
@@ -40,9 +40,9 @@ def plot(dataset):
     rgb = np.zeros((height, width, 3))
 
     # Read the data from the raster bands (assuming RGB bands are 1, 2, and 3)
-    rgb[:, :, 2] = scale(dataset.GetRasterBand(1).ReadAsArray().reshape((height, width)))
+    rgb[:, :, 0] = scale(dataset.GetRasterBand(1).ReadAsArray().reshape((height, width)))
     rgb[:, :, 1] = scale(dataset.GetRasterBand(2).ReadAsArray().reshape((height, width)))
-    rgb[:, :, 0] = scale(dataset.GetRasterBand(3).ReadAsArray().reshape((height, width)))
+    rgb[:, :, 2] = scale(dataset.GetRasterBand(3).ReadAsArray().reshape((height, width)))
     ''' A data cube indexed by row, column and band index (band index is in 1,2,3 rather: 0,1,2 from 0) 
 
     0,1,2 are not actually red, green blue. They are B12, B11, B9 from Sentinel-2:
