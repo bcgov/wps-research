1d73aae968ce9794f98c4d546fe64220fbbd6b78
ashlinrichardson <ashlin.richardson@gmail.com>
2024-05-24 23:28:16 -0700

    update

--- py/raster_histogram.py
+++ py/raster_histogram.py
@@ -11,6 +11,7 @@ import matplotlib
 args = sys.argv
 n_bins = 5000
 
+'''select input file and select the three indices to be used for the false-color representation'''
 def raster_histogram(input_file, band_select = [0, 1, 2]):
     assert_exists(input_file)  # check file exists
     hdr = hdr_fn(input_file)  # locate header file
@@ -27,45 +28,70 @@ def raster_histogram(input_file, band_select = [0, 1, 2]):
     except:
         pass
 
-    # select bands for visualization # band_select = [3, 2, 1] if bands > 3 else [0, 1, 2]
-    '''band_select, ofn = [0, 1, 2], None
-    if len(args) < 5:
-        band_select = [i for i in range(bands)]
-    else:
-        try:  # see if we can set the (r,g,b) encoding (band selection) from command args
-            for i in range(0, 3):
-                bs = int(args[i + 2]) - 1
-                if bs < 0 or bs >= bands:
-                    err('band index out of range')
-                band_select[i] = bs
-        except:
-            pass
-    '''
-    N = len(band_select)
+    N = len(band_select)  
     rng = range(N)
     dat = [data[band_select[i],] for i in rng]
-    my_min = [np.nanmin(dat[i]) for i in rng]
-    my_max = [np.nanmax(dat[i]) for i in rng]
-    # print("min", my_min)
-    # print("max", my_max)
-    my_min = np.nanmin(my_min)
-    my_max = np.nanmax(my_max)
-    # print("min", my_min, "max", my_max)
+
+    my_min, my_max = [np.nanmin(dat[i]) for i in rng],\
+                     [np.nanmax(dat[i]) for i in rng]
+
+    my_min, my_max  = np.nanmin(my_min),\
+                      np.nanmax(my_max)
+    
     bs = (my_max - my_min) / n_bins
-    bins = [my_min + float(i + 1) *bs for i in range(n_bins)]
-    # print(bins)
-    # print("max/bs", math.floor((my_max - my_min) / bs))
-    # print("min/bs", math.floor(((bs * 1.) + my_min - my_min) / bs))
+    bins = [my_min + float(i + 1) * bs for i in range(n_bins)]
     M = len(dat[0])
     
     plt.figure()
     col = ['r', 'g', 'b']
     for i in range(N):
-        di = dat[i]
-        plt.hist(di, range=[my_min, my_max], bins=n_bins, histtype='step', label=bn[band_select[i]], color=col[i])
+        plt.hist(dat[i],
+                 range=[my_min, my_max],
+                 bins=n_bins,
+                 histtype='step',
+                 label=bn[band_select[i]],
+                 color=(col[i] if i < 3 else None))
     plt.tight_layout()
     plt.legend()
     plt.show()
+
+
+def raster_transect(input_file, band_select = [0, 1, 2], row_index= None):
+    assert_exists(input_file)  # check file exists
+    hdr = hdr_fn(input_file)  # locate header file
+    skip_plot = False
+    [samples, lines, bands] = [int(x) for x in read_hdr(hdr)]  # read header and print parameters
+    print(samples, lines, bands)
+
+    npx = lines * samples # number of pixels.. binary IEEE 32-bit float data
+    data = read_float(input_file).reshape((bands, npx))
+    # print("bytes read: " + str(data.size))
+
+    bn = None
+    try:
+        bn = band_names(hdr)  # try to read band names from hdr
+    except:
+        pass
+
+    N = len(band_select)
+    rng = range(N)
+    dat = [data[band_select[i],] for i in rng]
+
+    print(dat[0].shape)
+
+    plt.figure()
+    col = ['r', 'g', 'b']
+    for i in range(N):
+        plt.plot(range(samples),
+                 dat[i][samples * row_index: samples * (row_index + 1)],
+                 label = bn[band_select[i]],
+                 color = (col[i] if i < 3 else None))
+    plt.tight_layout()
+    plt.legend()
+    plt.legend()
+    plt.show()
+
+
 
 
 if __name__ == "__main__":
