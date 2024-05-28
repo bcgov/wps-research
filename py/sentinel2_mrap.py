a3cee1228ad646ceb0e476c70d8e36c16f698b65
ashlinrichardson <ashlin.richardson@gmail.com>
2024-05-27 23:21:41 -0700

    bugfix

--- py/sentinel2_mrap.py
+++ py/sentinel2_mrap.py
@@ -2,13 +2,15 @@
 
 Difference from sentinel2_extract_swir.py: 
 can't process in parallel (By tile, at least)
+
 NOTE: assumes sentinel2_extract_cloudfree.py has been run.'''
 from envi import envi_update_band_names
 from envi import envi_header_cleanup
+from misc import args, run, hdr_fn
 import multiprocessing as mp
 from osgeo import gdal
-from misc import args, run, hdr_fn
 import numpy as np
+import copy
 import sys
 import os
 
@@ -17,50 +19,41 @@ my_bands, my_proj, my_geo, my_xsize, my_ysize, nbands, file_name  = {}, None, No
 def extract(file_name):
     global my_proj, my_geo, my_bands, my_xsize, my_ysize, nbands
     print("+r", file_name)
-    d = gdal.Open(file_name)
+    d = gdal.Open(file_name)  # open the file brought in for this update step
     
     for i in range(1, d.RasterCount + 1):
         band = d.GetRasterBand(i)
-        array_data = band.ReadAsArray().astype(np.float32)
+        new_data = band.ReadAsArray().astype(np.float32)
         
-        if i not in my_bands:
-            my_bands[i] = array_data
+        if i not in my_bands:  #initialize the band / first time
+            my_bands[i] = new_data # new data for this band becomes the band
         else:
-            nans = np.isnan(array_data)
-            update = my_bands[i]
-            update[~nans] = array_data[~nans]
+            nans, update = np.isnan(new_data), copy.deepcopy(my_bands[i])  # forgot copy before?
+            update[~nans] = new_data[~nans]
             my_bands[i] = update
    
-    if my_proj is None:
-        my_proj = d.GetProjection()
-    if my_geo is None:
-        my_geo = d.GetGeoTransform()
+    my_proj = d.GetProjection() if my_proj == None else my_proj
+    my_geo = d.GetGeoTransform() if my_geo is None else my_geo
     if my_xsize is None:
         my_xsize, my_ysize, nbands = d.RasterXSize, d.RasterYSize, d.RasterCount 
 
+    d = None  # close input file brought in for this update step
+
     # write output file
-    out_file_name = file_name + '_MRAP.bin'
-    driver = gdal.GetDriverByName('ENVI')
-    print(out_file_name, 
-          my_xsize,
-          my_ysize,
-          nbands,
-          gdal.GDT_Float32)
-    stack_ds = driver.Create(out_file_name,
-                             my_xsize,
-                             my_ysize,
-                             nbands,
-                             gdal.GDT_Float32)
+    out_file_name, driver = file_name + '_MRAP.bin', gdal.GetDriverByName('ENVI')
+    print(out_file_name, my_xsize, my_ysize, nbands, gdal.GDT_Float32)
+    stack_ds = driver.Create(out_file_name, my_xsize, my_ysize, nbands, gdal.GDT_Float32)
     stack_ds.SetProjection(my_proj)
     stack_ds.SetGeoTransform(my_geo)
 
     for i in range(1, nbands + 1):
-        rb = stack_ds.GetRasterBand(i)
-        rb.WriteArray(my_bands[i])
+        stack_ds.GetRasterBand(i).WriteArray(my_bands[i])
     stack_ds = None
 
-    run('fh ' + out_file_name) # mrap_' + args[1] + '.hdr')
-    envi_update_band_names(['envi_update_band_names.py', hdr_fn(file_name), hdr_fn(out_file_name)])
+    run('fh ' + out_file_name)  # fix envi header, then reproduce the band names
+    envi_update_band_names(['envi_update_band_names.py', 
+                            hdr_fn(file_name),
+                            hdr_fn(out_file_name)])
 
 
 if __name__ == "__main__":
@@ -68,11 +61,11 @@ if __name__ == "__main__":
         err("python3 sentinel2_mrap.py [sentinel-2 gid] # [optional: yyyymmdd 'maxdate' parameter] ")
     else:
         gid = args[1]
-        lines = [x.strip() for x in os.popen("ls -1r S2*.bin")]  # sort dates in time
-        lines2 = [x.split('_') for x in lines]
-        lines3 = [[x[2], x] for x in lines2]
-        lines3.sort()
-        lines = ['_'.join(x[1]) for x in lines3]
+        lines = [x.strip() for x in os.popen("ls -1r S2*.bin").readlines()]  # sort dates in time
+        lines = [x.split('_') for x in lines]
+        lines = [[x[2], x] for x in lines]
+        lines.sort()
+        lines = ['_'.join(x[1]) for x in lines]
         
         for line in lines:
             extract(line)
