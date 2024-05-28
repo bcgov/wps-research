9779f23e385c0a206f43437ddf1b2ddf57417e99
ashlinrichardson <ashlin.richardson@gmail.com>
2024-05-27 17:45:30 -0700

    update

--- py/sentinel2_plot.py
+++ py/sentinel2_plot.py
@@ -1,6 +1,6 @@
 '''20240527 plot sentinel2 products (filenames beginning with frame ID) 
 '''
-from misc import err, run
+from misc import err, run, parfor
 import os
 
 lines = [x.strip() for x in os.popen('ls -1 S*.bin').readlines()]
@@ -10,8 +10,11 @@ lines = [[line.split('_')[2], line] for line in lines]
 lines.sort()
 lines = [line[1] for line in lines]
 
-for line in lines:
-    cmd = "raster_plot.py " + line + " 1 2 3 1 "
-    run(cmd)
 
+cmds = ["raster_plot.py " + line + " 1 2 3 1 " for line in lines]
+
+def r(x):
+    return os.system(x)
+
+parfor(r, cmds, 8) 
 
