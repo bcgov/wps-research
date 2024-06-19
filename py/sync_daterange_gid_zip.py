4ce2fee910df7455f7caeac62df0340e621d5af0
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-06-19 13:00:01 -0700

    update

--- py/sync_daterange_gid_zip.py
+++ py/sync_daterange_gid_zip.py
@@ -44,10 +44,11 @@ def download_by_gids(gids, yyyymmdd, yyyymmdd2):
     print(cmd)
     data = os.popen(cmd).read()
 
-    if not exists(my_path + 'listing'):  # json backup for analysis
-        os.mkdir(my_path + 'listing')
-    df = my_path + 'listing' + sep + ts + '_objects.txt'  # file to write
-    open(df, 'wb').write(data.encode())  # record json to file
+    if False:
+        if not exists(my_path + 'listing'):  # json backup for analysis
+            os.mkdir(my_path + 'listing')
+        df = my_path + 'listing' + sep + ts + '_objects.txt'  # file to write
+        open(df, 'wb').write(data.encode())  # record json to file
 
     jobs, d = [], None
     try:
