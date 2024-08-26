d397367660b93bf29e49752f6c1eb6596b1e06a1
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-08-26 10:14:20 -0700

    update

--- py/snap_build.py
+++ py/snap_build.py
@@ -1,31 +1,55 @@
 '''20240826 (last test date) building SNAP from source:
 
-How to build SNAP from sources:
-https://senbox.atlassian.net/wiki/spaces/SNAP/pages/10879039/How+to+build+SNAP+from+sources
+1) Note: assume SNAP repos will be cloned into the PRESENT working folder.
 
-STEP developers: 
-https://step.esa.int/main/community/developers/
+Background: 
+* How to build SNAP from sources: https://senbox.atlassian.net/wiki/spaces/SNAP/pages/10879039/How+to+build+SNAP+from+sources
+* STEP developers:  https://step.esa.int/main/community/developers/
 '''
 from misc import run, err
-sources = ['snap-engine', 'snap-desktop', 's1tbx', 's2tbx', 's3tbx']
-cmds = ['git clone https://github.com/senbox-org/' + x + '.git' for x in sources]
+import sys
+import os
 
-for c in cmds:
-    print(c)
+sources = ['snap-engine', 'snap-desktop', 's1tbx', 's2tbx', 's3tbx']
+cmds = [[x, 'git clone https://github.com/senbox-org/' + x + '.git'] for x in sources]
+
+for x, c in cmds:
+    if not os.path.exists(x + os.path.sep + '.git'):
+        run(c)
+    else:
+        print('+r ' + x + '.git')
 
 # SNAP needs java v. 8! 
-print('sudo apt install openjdk-8-jre openjdk-8-jdk')
-print('sudo update-java-alternatives --list')
-print('sudo update-alternatives --config java')
-print('java -version')
-print('sudo update-alternatives --config javac')
+lines = [x.strip().split('/')[0] for x in os.popen('apt list --installed | grep jdk').readlines()]
+to_install = ['openjdk-11-jre', 'openjdk-11-jdk']
+print(lines)
+for x in to_install:
+    if x not in lines:
+        run('sudo apt install ' + x)
 
-# https://computingforgeeks.com/how-to-set-default-java-version-on-ubuntu-debian/
-print("how to find out where your java is:")
-print("readlink -f /usr/bin/java")
+# not sure if we need these commands
+a = os.system('sudo update-java-alternatives --list')
+a = os.system('sudo update-alternatives --config java')
+a = os.system('java -version')
+a = os.system('sudo update-alternatives --config javac')
 
-print('add line to .bashrc:')
-print('export JAVA_HOME=/path/to/java/jre/') 
+
+# where is java ?
+java_path = os.popen('readlink -f /usr/bin/java').read().strip()  # where is your java?
+print(java_path)
+
+bashrc = '/home/' + os.popen('whoami').read().strip() + os.path.sep + '.bashrc'
+lines = [x.rstrip() for x in open(bashrc).readlines()]  # existing bashrc lines
+new_lines = []
+for line in lines:
+    if len(line.split('JAVA_HOME')) > 1:
+        pass
+    else:
+        new_lines += [line]
+new_lines += ['\nexport JAVA_HOME=' + java_path + ' # setting for ESA SNAP software']  # put this at end
+
+print('+w', bashrc)
+open(bashrc, 'wb').write(('\n'.join(new_lines)).encode())
 
 # assuming your java is set up:
 print("in each toolbox folder:")
