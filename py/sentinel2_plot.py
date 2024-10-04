d241d42c9f9c40ee16a2c17abc4317bf78be572a
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-10-04 15:41:28 -0700

    update

--- py/sentinel2_plot.py
+++ py/sentinel2_plot.py
@@ -23,9 +23,9 @@ parfor(r, cmds, 8)
 
 '''Now: prefix the S2.png files by date:
 '''
-lines = os.popen("ls -1 S2*.png").readlines()
+lines = os.popen("ls -1 S2*.png *mrap*png").readlines()
 lines = [x.strip() for x in lines]
 
 for line in lines:
     T = line.split('_')[2].split('T')[0]
-    run('mv -v ' + line + ' ' + T + '_' + line)
+    run('mv -v ' + line + ' ' + 'plot_' + T + '_' + line)
