72f221a841fa6ae14fa20c34c8fdc80a4c5b0f9d
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-10-15 12:10:41 -0700

    update

--- py/sentinel2_plot.py
+++ py/sentinel2_plot.py
@@ -2,25 +2,28 @@
 
 20241220: added support for *mrap.bin files
 '''
-from misc import err, run, parfor
+from misc import err, run, parfor, hdr_fn, read_hdr
 import os
 
-lines = [x.strip() for x in os.popen('ls -1 S*.bin').readlines()]
+lines = [x.strip() for x in os.popen('ls -1 S*.bin *mrap*.bin *BARC*.bin').readlines()]
 
 # process in order
 lines = [[line.split('_')[2], line] for line in lines]
 lines.sort()
 lines = [line[1] for line in lines]
 
-lines += [x.strip() for x in os.popen('ls -1 *mrap*.bin').readlines()]
-# lines += [x.strip() for x in os.popen('ls -1 *mrap*bin').readlines()]
-
 cmds = []
 
 for line in lines:
+    samples, lin, bands = read_hdr(hdr_fn(line))
+
     out_file = 'plot_1_' + line + '_1_2_3_rgb.png'
+
     if not os.path.exists(out_file):
-        cmds += ["raster_plot.py " + line + " 1 2 3 1 " for line in lines]
+        if int(bands) < 3:
+            cmds += ["raster_plot.py " + line + " 1 1 1 1 " for line in lines]
+        else:
+            cmds += ["raster_plot.py " + line + " 1 2 3 1 " for line in lines]
     # plot_1_20230902_mrap.bin_1_2_3_rgb.png
 
 def r(x):
@@ -30,7 +33,7 @@ parfor(r, cmds, 4)
 
 '''Now: prefix the S2.png files by date:
 '''
-lines = os.popen("ls -1 S2*.png *mrap*png").readlines()
+lines = os.popen("ls -1 S2*.png *mrap*png *BARC*png").readlines()
 lines = [x.strip() for x in lines]
 
 for line in lines:
