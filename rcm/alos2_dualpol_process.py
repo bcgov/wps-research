18ef4f833814bbb7bbc9dd8382c6c8cb262a4b3f
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-11-10 23:38:04 -0800

    update

--- rcm/alos2_dualpol_process.py
+++ rcm/alos2_dualpol_process.py
@@ -1,7 +1,7 @@
 '''20230123 process JAXA data retrieved from EODMS
 *** Assume each folder in present directory, is a dataset'''
 
-FILTER_SIZE = 3
+FILTER_SIZE = 5
 import os
 import sys
 sep = os.path.sep
@@ -233,6 +233,16 @@ Graph XML Format:
             '-t ' + p_7]) # output
     print(p_7)
     
+    p_8 = d + sep + '07_Box.data' + sep + ('C11.bin' if not QUAD_POL else 'T11.bin')
+
+    if not exist(p_8):
+        a = os.system("cd " + d + sep + '07_Box.data; snap2psp_inplace.py')
+
+    p_9 = d + sep + '07_Box.data' + sep +  d + '_rgb.bin'
+    if not exist(p_9):
+        if not(QUAD_POL):
+            a = os.system('cd ' + d + sep + '07_Box.data; convert_iq_to_cplx C12_real.bin C12_imag.bin C12.bin; abs C12.bin;  raster_stack.py C11.bin C22.bin C12.bin_abs.bin ' + d + '_rgb.bin; raster_zero_to_nan ' + d + '_rgb.bin') 
+    # C11, C22, C12_bin.abs
 
     # sys.exit(1)  # comment out to run on first set only
 
