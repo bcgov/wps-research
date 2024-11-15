c31283afed7d539e2c5f0a7f3fe23bac1657bd21
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-11-14 23:51:17 -0800

    update

--- rcm/alos2_warp.py
+++ rcm/alos2_warp.py
@@ -57,7 +57,7 @@ for i in range(1, len(by_date)):
 for i in range(0, len(by_date)):
     d = by_date[i][0]
     p_d = by_date[i][1] + '_rgb.bin'
-    if not exists(p_9):
+    if not exists(p_d):
         if is_quad_pol:
             a = os.system('cd ' + d + sep + '; raster_stack.py T22.bin T33.bin T11.bin ' + by_date[i][1] + '_rgb.bin; raster_zero_to_nan ' + d + '_rgb.bin') 
         elif not(quad_pol):
