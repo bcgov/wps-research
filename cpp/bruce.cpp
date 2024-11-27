afd121f5768d78825620af71485d38d8d650d146
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-11-26 21:44:05 -0800

    update

--- cpp/bruce.cpp
+++ cpp/bruce.cpp
@@ -6,7 +6,7 @@ int main(int argc, char ** argv){
   str fn(argv[1]); // input ENVI-format filename
   str hfn(hdr_fn(fn)); // auto-detect header file name
 
-  size_t nrow, ncol, nband, np, i // variables
+  size_t nrow, ncol, nband, np, i, k; // variables
   hread(hfn, nrow, ncol, nband); // read heade
 
   float * dat = bread(fn, nrow, ncol, nband); // load array of floats
@@ -14,10 +14,9 @@ int main(int argc, char ** argv){
   float total;
 
   for0(i, np){
-      total = 0.;
-      for0(k, nband) total += dat[k * np + i];
-      for0(k, nband) dat[k * np + i] /= total;
-    }
+    total = 0.;
+    for0(k, nband) total += dat[k * np + i];
+    for0(k, nband) dat[k * np + i] /= total;
   }
 
   str ofn(fn + str("_bruce.bin"));
