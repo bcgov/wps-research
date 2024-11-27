d90fce7491e78b42fd52d578be901c7b3dfb6779
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-11-26 21:50:33 -0800

    update

--- cpp/bruce.cpp
+++ cpp/bruce.cpp
@@ -4,26 +4,29 @@ int main(int argc, char ** argv){
 
   if(argc < 2) err("bruce [input binary file name]");
   str fn(argv[1]); // input ENVI-format filename
-  str hfn(hdr_fn(fn)); // auto-detect header file name
+  str hfn(hdr_fn(fn)); // detect header file name
 
   size_t nrow, ncol, nband, np, i, k; // variables
-  hread(hfn, nrow, ncol, nband); // read heade
+  hread(hfn, nrow, ncol, nband); // read header
 
-  float * dat = bread(fn, nrow, ncol, nband); // load array of floats
-  np = nrow * ncol;
-  float total;
+  float * dat = bread(fn, nrow, ncol, nband); // load float array
+  np = nrow * ncol; // number of pixels
+  float total; // L1 quantity
 
   for0(i, np){
     total = 0.;
-    for0(k, nband) total += dat[k * np + i];
-    for0(k, nband) dat[k * np + i] /= total;
+
+    for0(k, nband)
+      total += dat[k * np + i];
+
+    for0(k, nband)
+      dat[k * np + i] /= total;
   }
 
   str ofn(fn + str("_bruce.bin"));
   str ohfn(fn + str("_bruce.hdr"));
-  printf("nr %zu nc %zu nband %zu\n", nrow, ncol, nband);
-  bwrite(dat, ofn, nrow, ncol, nband);
-  hwrite(ohfn, nrow, ncol, nband); // write output header
+  hwrite(ohfn, nrow, ncol, nband); // write output hdr
+  bwrite(dat, ofn, nrow, ncol, nband); // write output data
   free(dat);
   return 0;
 }
