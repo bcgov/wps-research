0f8c598e9baeff6652694ca021967d7c671f7d10
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-11-26 21:58:40 -0800

    update

--- cpp/bruce.cpp
+++ cpp/bruce.cpp
@@ -1,17 +1,14 @@
 /* 20241122 use Dr. Bruce Chapman's (L1) normalization / color scheme for 3-band imagery */
 #include"misc.h"
 int main(int argc, char ** argv){
-
   if(argc < 2) err("bruce [input binary file name]");
   str fn(argv[1]); // input ENVI-format filename
   str hfn(hdr_fn(fn)); // detect header file name
-
   size_t nrow, ncol, nband, np, i, k; // variables
+  
   hread(hfn, nrow, ncol, nband); // read header
-
-  float * dat = bread(fn, nrow, ncol, nband); // load float array
+  float total, *dat = bread(fn, nrow, ncol, nband); // load float array
   np = nrow * ncol; // number of pixels
-  float total; // L1 quantity
 
   for0(i, np){
     total = 0.;
