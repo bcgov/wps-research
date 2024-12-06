aea796b10be643ce88ce5055085665651da8f85c
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-12-05 19:26:33 -0800

    update

---
+++ rcm/rcm_dualpol_grd.py
@@ -0,0 +1,36 @@
+'''20241205 simple processing of RCM dual-pol SAR data in GRD format
+'''
+FILTER_SIZE = 3
+import os
+import sys
+sep = os.path.sep
+my_path = sep.join(os.path.abspath(__file__).split(sep)[:-1]) + sep
+sys.path.append(my_path + ".." + sep + 'py')
+from misc import pd, sep, exist, args, cd, err, find_snap
+
+def run(x):
+    cmd = ' '.join(x)
+    print(cmd)
+    a = os.system(cmd)
+
+
+snap = find_snap()  # find snap gpt binary
+
+sets = [x.strip() for x in os.popen('ls -1d RCM*GRD').readlines()]
+
+for d in sets:
+    p0 = d + sep + 'manifest.safe'
+    p1 = d + sep + '01_calib.dim'
+    p2 = d + sep + '02_TC.dim'
+    if not exist(p1):
+        run([snap, 'Calibration',
+             '-Ssource=' + p0,
+             '-t ' + p1,
+             '-PoutputImageInComplex=true']) # '-PoutputBetaBand=true'])
+
+    if not exist(p2):
+        run([snap, 'Terrain-Correction',
+             '-PnodataValueAtSea=true',
+             '-Ssource=' + p1,
+             '-PdemName="Copernicus 30m Global DEM"',
+             '-t ' + p2])
