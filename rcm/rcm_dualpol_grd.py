d7cfa513f0ab1a8e18e5802572a86f6590808784
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-12-05 20:46:12 -0800

    update

--- rcm/rcm_dualpol_grd.py
+++ rcm/rcm_dualpol_grd.py
@@ -22,7 +22,7 @@ sets = [x.strip() for x in os.popen('ls -1d RCM*GRD').readlines()]
 
 for d in sets:
     p0 = d + sep + 'manifest.safe'
-    p1 = d + sep + '01_calib.dim'
+    p1 = d + sep + '01_Cal.dim'
     p2 = d + sep + '02_TC.dim'
     if not exist(p1):
         run([snap, 'Calibration',
@@ -36,3 +36,21 @@ for d in sets:
              '-Ssource=' + p1,
              '-PdemName="Copernicus 30m Global DEM"',
              '-t ' + p2])
+
+
+    p3 = d + sep + '02_TC.data' + sep + 'Sigma_HH.bin'
+    
+    if not exist(p3):
+        a = os.system('cd ' + d + sep + '02_TC.data; snap2psp_inplace.py')
+    
+    p4 = d + sep + '02_TC.data' + sep + d + '_rgb.bin'
+    if not exist(p4):
+        cmd = ('cd ' + d + sep + '02_TC.data; raster_div Sigma0_HH.bin Sigma0_HV.bin Sigma0_HH_HV.bin; cp Sigma0_HH.hdr Sigma0_HH_HV.hdr; raster_stack.py Sigma0_HH.bin Sigma0_HV.bin Sigma0_HH_HV.bin ' + d + '_rgb.bin; raster_zero_to_nan ' + d + '_rgb.bin')
+        print(cmd)
+        a = os.system(cmd)
+
+    hf = d + sep + '02_TC.data' + sep + d + '_rgb.hdr'
+    dd = open(hf).read().replace('Sigma0_HH}', 'Sigma0_HH/HV}')
+    open(hf, 'wb').write(dd.encode())
+
+    
