71e708c46036fdbdfe44744b167373928d925fcb
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-03 12:46:28 -0700

    update

--- py/sync_recent.py
+++ py/sync_recent.py
@@ -21,7 +21,7 @@ today = datetime.date.today()
 N = 1  # default
 
 # check if we're in an MRAP folder, only update the GID present in the filesystem structure:
-L2_folders = os.popen("ls -d1 L2_*/").read().strip()
+L2_folders = os.popen("ls -d1 L2_*").read().strip()
 if L2_folders != '':
     min_most_recent = None
     lines = [x.strip().strip(sep) for x in L2_folders.split('\n')]
