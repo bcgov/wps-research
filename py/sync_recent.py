f610798392629a63782eb6c65134f9330e2dbce6
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-03 11:59:21 -0700

    update

--- py/sync_recent.py
+++ py/sync_recent.py
@@ -19,7 +19,7 @@ gids = bc_gid # default to BC gids a
 # check if we're in an MRAP folder, only update the GID present in the filesystem structure:
 L2_folders = os.popen("ls -d1 L2_*/").read().strip()
 if L2_folders != '':
-    lines = [x.strip() for x in L2_folders.split('\n')]
+    lines = [x.strip().strip(sep) for x in L2_folders.split('\n')]
     print(lines)
 sys.exit(1)
 
