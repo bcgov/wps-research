1cbd45fdc26a0f705fb3473174579aa55101fdf5
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-03 11:44:40 -0700

    update

--- py/sync_recent.py
+++ py/sync_recent.py
@@ -4,7 +4,7 @@ need to add options for --L1, --L2 ( default ), --n ( number of days to go back,
 '''
 import os
 import sys
-import datetime
+import datetime, timedelta
 from gid import bc
 from misc import sep, assert_aws_cli_installed
 assert_aws_cli_installed()
@@ -12,28 +12,31 @@ bc_gid = bc()
 print("bc row-id under obs:", bc_gid)
 
 # today's date
-now = datetime.date.today()
-year, month, day = str(now.year).zfill(4), str(now.month).zfill(2), str(now.day).zfill(2)
+today = datetime.date.today()
+N = 1
+for i in range(0, N + 1):
+    now = today - timedelta(days=N)
+    year, month, day = str(now.year).zfill(4), str(now.month).zfill(2), str(now.day).zfill(2)
 
-print([year, month, day])
-ls = 'aws s3 ls --no-sign-request'
-path = 's3://sentinel-products-ca-mirror/Sentinel-2/'
-s2 = path
-a = " "
-c1, c2 = ' '.join([ls, path + 'S2MSI1C/']), ' '.join([ls, path + 'S2MSI2A/'])
-c1 = ls + a + s2 + 'S2MSI1C/' # + '/'.join([year, month, day]) + '/' 
-c2 = ls + a + s2 + 'S2MSI2A/' # + '/'.join([year, month, day]) + '/'
+    print([year, month, day])
+    ls = 'aws s3 ls --no-sign-request'
+    path = 's3://sentinel-products-ca-mirror/Sentinel-2/'
+    s2 = path
+    a = " "
+    c1, c2 = ' '.join([ls, path + 'S2MSI1C/']), ' '.join([ls, path + 'S2MSI2A/'])
+    c1 = ls + a + s2 + 'S2MSI1C/' # + '/'.join([year, month, day]) + '/' 
+    c2 = ls + a + s2 + 'S2MSI2A/' # + '/'.join([year, month, day]) + '/'
 
-def get(c):  # collect results from cli invocation
-    print(c)
-    t = [x.strip() for x in os.popen(c).read().strip().split('\n')]
-    return '\n'.join(t)
+    def get(c):  # collect results from cli invocation
+        print(c)
+        t = [x.strip() for x in os.popen(c).read().strip().split('\n')]
+        return '\n'.join(t)
 
-start_date = now
+    start_date = now
 
-cd = '/'.join([str(start_date.year).zfill(4),
-               str(start_date.month).zfill(2),
-               str(start_date.day).zfill(2)]) + '/'
+    cd = '/'.join([str(start_date.year).zfill(4),
+                   str(start_date.month).zfill(2),
+                   str(start_date.day).zfill(2)]) + '/'
 
-c1_d = get(c2 + cd)  # Level-2 data listings for today
+    c1_d = get(c2 + cd)  # Level-2 data listings for today
 
