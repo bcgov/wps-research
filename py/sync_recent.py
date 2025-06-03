51933ac3a626077d276d77d754da890c0f92ad15
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-03 12:52:46 -0700

    update

--- py/sync_recent.py
+++ py/sync_recent.py
@@ -108,6 +108,7 @@ for i in range(0, N + 1):
                              's3://sentinel-products-ca-mirror/Sentinel-2/S2MSI2A/' + cd + w[3].strip(),
                              'L2_' + tile_id + sep + w[3].strip()])
             print(cmd)
+            cmds += [cmd] 
 
 
 def runc(c):
