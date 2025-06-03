ee3adad6777d6b6224777a3f181cfa1fac73d892
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-03 13:08:57 -0700

    update

--- py/sentinel2_mrap.py
+++ py/sentinel2_mrap.py
@@ -6,6 +6,8 @@ NOTE: assumes sentinel2_extract_cloudfree.py has been run.
 20240618 NOTE: need to make this fully incremental. I.e., initialize buffer (per tile) with last avaiable MRAP date
 
 20250602: note: the program can't be run in parallel currently ( will try scoping the extract() function inside the run_mrap function)
+
+20250602: need to do the above before ____
 '''
 from envi import envi_update_band_names
 from envi import envi_header_cleanup
