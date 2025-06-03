17d14969c736994d3d5db8b56151c09cb53dd315
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-03 11:31:28 -0700

    update

--- py/misc.py
+++ py/misc.py
@@ -468,3 +468,10 @@ def find_snap():  # find location of ESA's SNAP tool, command line interface ( g
     if not exist(snap):
         err('snap binary (gpt) not found')
     return snap
+
+def asset_aws_cli_installed():
+    # check that aws cli installed
+    if len(os.popen("aws 2>&1").read().split("not found")) > 1:
+        print('Need to install aws cli: e.g.:')
+        print('  sudo apt install awscli')
+        sys.exit(1)
