23cda3c4d823426607f0c7667e0f125b22b3c2eb
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-03 20:30:22 -0700

    update

--- py/sentinel2_mrap.py
+++ py/sentinel2_mrap.py
@@ -19,74 +19,77 @@ import copy
 import sys
 import os
 
-my_bands, my_proj, my_geo, my_xsize, my_ysize, nbands, file_name  = {}, None, None, None, None, None, None
-
-def extract(file_name):
-    global my_proj, my_geo, my_bands, my_xsize, my_ysize, nbands
-    print("+r", file_name)
-    d = gdal.Open(file_name)  # open the file brought in for this update step
-    
-    for i in range(1, d.RasterCount + 1):
-        band = d.GetRasterBand(i)
-        new_data = band.ReadAsArray().astype(np.float32)
-        
-        if i not in my_bands:  #initialize the band / first time
-            my_bands[i] = new_data # new data for this band becomes the band
-        else:
-            nans, update = np.isnan(new_data), copy.deepcopy(my_bands[i])  # forgot copy before?
-            update[~nans] = new_data[~nans]
-            my_bands[i] = update
-            # my_bands[i][~nans] = new_data[~nans]
-
-    my_proj = d.GetProjection() if my_proj == None else my_proj
-    my_geo = d.GetGeoTransform() if my_geo is None else my_geo
-    if my_xsize is None:
-        my_xsize, my_ysize, nbands = d.RasterXSize, d.RasterYSize, d.RasterCount 
-
-    d = None  # close input file brought in for this update step
-
-    # write output file
-    out_file_name, driver = file_name + '_MRAP.bin', gdal.GetDriverByName('ENVI')
-    if not os.path.exists(out_file_name):
-        print(out_file_name, my_xsize, my_ysize, nbands, gdal.GDT_Float32)
-        stack_ds = driver.Create(out_file_name, my_xsize, my_ysize, nbands, gdal.GDT_Float32)
-        stack_ds.SetProjection(my_proj)
-        stack_ds.SetGeoTransform(my_geo)
-
-        for i in range(1, nbands + 1):
-            stack_ds.GetRasterBand(i).WriteArray(my_bands[i])
-        stack_ds = None
-
-        run('fh ' + out_file_name)  # fix envi header, then reproduce the band names
-        envi_update_band_names(['envi_update_band_names.py', 
-                                hdr_fn(file_name),
-                                hdr_fn(out_file_name)])
-    else:
-        print(out_file_name, 'exists [SKIP WRITE]')
 
 def run_mrap(gid):  # run MRAP on one tile
-    global my_proj, my_geo, my_bands, my_xsize, my_ysize, nbands
     my_bands, my_proj, my_geo, my_xsize, my_ysize, nbands, file_name  = {}, None, None, None, None, None, None
 
-    if True:
-        # look for all the dates in this tile's folder and sort them in aquisition time
-        
-        def get_filename_lines(search_cmd):
-            lines = [x.strip() for x in os.popen(search_cmd).readlines()]
-            lines = [x.split(os.path.sep)[-1].split('_') for x in lines]
-            lines = [[x[2], x] for x in lines]
-            lines.sort()
-            return ['_'.join(x[1]) for x in lines]
+    
+    def extract(file_name):
+        global my_proj, my_geo, my_bands, my_xsize, my_ysize, nbands
+        print("+r", file_name)
+        d = gdal.Open(file_name)  # open the file brought in for this update step
+    
+        for i in range(1, d.RasterCount + 1):
+            band = d.GetRasterBand(i)
+            new_data = band.ReadAsArray().astype(np.float32)
+    
+            if i not in my_bands:  #initialize the band / first time
+                my_bands[i] = new_data # new data for this band becomes the band
+            else:
+                nans, update = np.isnan(new_data), copy.deepcopy(my_bands[i])  # forgot copy before?
+                update[~nans] = new_data[~nans]
+                my_bands[i] = update
+                # my_bands[i][~nans] = new_data[~nans]
+    
+        my_proj = d.GetProjection() if my_proj == None else my_proj
+        my_geo = d.GetGeoTransform() if my_geo is None else my_geo
+        if my_xsize is None:
+            my_xsize, my_ysize, nbands = d.RasterXSize, d.RasterYSize, d.RasterCount
+    
+        d = None  # close input file brought in for this update step
+    
+        # write output file
+        out_file_name, driver = file_name + '_MRAP.bin', gdal.GetDriverByName('ENVI')
 
-        lines = get_filename_lines("ls -1r L2_" + gid + os.path.sep + "S2*_cloudfree.bin")
+        # skip files that already exist! 
+        if not os.path.exists(out_file_name):
+            print(out_file_name, my_xsize, my_ysize, nbands, gdal.GDT_Float32)
+            stack_ds = driver.Create(out_file_name, my_xsize, my_ysize, nbands, gdal.GDT_Float32)
+            stack_ds.SetProjection(my_proj)
+            stack_ds.SetGeoTransform(my_geo)
+    
+            for i in range(1, nbands + 1):
+                stack_ds.GetRasterBand(i).WriteArray(my_bands[i])
+            stack_ds = None
+    
+            run('fh ' + out_file_name)  # fix envi header, then reproduce the band names
+            envi_update_band_names(['envi_update_band_names.py',
+                                    hdr_fn(file_name),
+                                    hdr_fn(out_file_name)])
+        else:
+            print(out_file_name, 'exists [SKIP WRITE]')
 
-        for line in lines:
-            gid = line.split("_")[5]
-            extract("L2_" +  gid + os.path.sep + line)
+    # look for all the dates in this tile's folder and sort them in aquisition time
 
-        print("check sorting order")
-        for line in lines:
-            print("mrap " + line)
+    def get_filename_lines(search_cmd):
+        lines = [x.strip() for x in os.popen(search_cmd).readlines()]
+        lines = [x.split(os.path.sep)[-1].split('_') for x in lines]
+        lines = [[x[2], x] for x in lines]
+        lines.sort()
+        return ['_'.join(x[1]) for x in lines]
+    
+    lines = get_filename_lines("ls -1r L2_" + gid + os.path.sep + "S2*_cloudfree.bin")
+    
+    # before this step, check for latest completed MRAP file and "seed" with that..if possible!
+    
+    for line in lines:
+        gid = line.split("_")[5]
+        extract("L2_" +  gid + os.path.sep + line)
+    
+    print("check sorting order")
+    for line in lines:
+        print("mrap " + line)
+
 
 
 if __name__ == "__main__":
