812e113711e8cb8686591ff9633410a8d34846e7
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-06 09:32:47 -0700

    update

--- py/misc.py
+++ py/misc.py
@@ -251,11 +251,11 @@ def parfor(my_function,  # function to run in parallel
 '''
 
 def parfor(my_function, my_inputs, n_thread=int(mp.cpu_count())):
+    print("PARFOR",n_thread)
     if n_thread == 1 or single_thread:  # should default to old version if joblib not installed?
         return [my_function(my_inputs[i]) for i in range(len(my_inputs))]
     else:
         n_thread = mp.cpu_count() if n_thread is None else n_thread
-        print("PARFOR", n_thread)
         if my_inputs is None or type(my_inputs) == list and len(my_inputs) == 0:
             return []
 
