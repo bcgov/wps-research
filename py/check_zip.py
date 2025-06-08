e71d13a764ac13bdf028bd3db4a4fcabd0300d91
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-08 12:27:38 -0700

    update

--- py/check_zip.py
+++ py/check_zip.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
 
 
     if '--delete' in sys.argv:
-        for b in bad:
+        for b in bad_files:
             for f in [b, b[:-3] + 'hdr']:
                 if os.path.exists(f):
                     print('rm ' + f)
