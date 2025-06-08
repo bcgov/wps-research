a433de1b37596d11ffa3776d957d1573ca3af548
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-07 20:23:43 -0700

    update

---
+++ py/merge3.py
@@ -0,0 +1,72 @@
+'''20250607: mosaic all .bin files specified
+
+adapted from:
+20230516 mosaic all .bin files in present folder 
+Handles partial overlap. No-data value NAN assumed https://github.com/OSGeo/gdal/issues/3098
+
+Default EPSG for resampling: 3005 (BC Albers)
+
+*** add an extra arg to use 3347: Canada LCC'''
+EPSG = 3005  # BC Albers
+# EPSG = 3347 # Canada LCC
+import os
+import sys
+import multiprocessing as mp
+from misc import run, parfor, exists, sep, err
+'''assume we are in BC
+if len(sys.argv) > 1:
+    EPSG = 3347
+'''
+lines = sys.argv[1:] #  [x.strip() for x in os.popen('ls -1 *.bin').readlines()]
+
+if not exists('resample'):
+    os.mkdir('resample')
+else:
+    pass #     err('directory resample/ exists. Remove and run again')
+
+run('rm -f tmp_subset.*')
+run('rm -f merge.*') 
+
+cmds = []
+resampled = []
+for L in lines:
+    ofn = L + sep + '_resample.bin'
+    resampled += [ofn]
+    if not exists(ofn):
+        cmds += [' '.join(['gdalwarp',
+                           '-wo NUM_THREADS=16',
+                           '-multi',
+                           '-r bilinear',
+                           '-srcnodata nan',
+                           '-dstnodata nan',
+                           '-of ENVI',
+                           '-ot Float32',
+                           '-t_srs EPSG:' + str(EPSG),
+                           L,
+                           ofn])]
+    else:
+        print("Skipping:", ofn)
+parfor(run, cmds, int(mp.cpu_count()))
+
+run(' '.join(['gdalbuildvrt',
+              '-srcnodata nan',
+              '-vrtnodata nan',
+              '-resolution highest',
+              '-overwrite',
+              'merge.vrt'] + resampled))
+
+if not exists('merge.bin'):
+    run(' '.join(['gdalwarp',
+                  '-wo NUM_THREADS=16',
+                  '-multi',
+                  '-overwrite',
+                  '-r bilinear',
+                  '-of ENVI',
+                  '-ot Float32',
+                  '-srcnodata nan',
+                  '-dstnodata nan',
+                  'merge.vrt',
+                  'merge.bin']))  
+
+run('fh merge.hdr')
+run('envi_header_copy_bandnames.py ' + lines[0][:-4] + '.hdr merge.hdr')
