989a8d1a72dc84773e15dae86e993c408f5c06dd
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-18 21:57:08 -0700

    update

--- py/localtime.py
+++ py/localtime.py
@@ -10,3 +10,9 @@ b= run('vancouver_hour=$(TZ="America/Vancouver" date +"%H"); echo "$vancouver_ho
 a = run('vancouver_date=$(TZ="America/Vancouver" date +"%Y%m%d"); echo "$vancouver_date"')
 
 print(a, b)
+
+print("configured time: yyyymmdd hh")
+b = run('local_hour=$(date +"%H"); echo "$local_hour"')
+a = run('local_date=$(date +"%Y%m%d"); echo "$local_date"')
+print(a,b)
+
