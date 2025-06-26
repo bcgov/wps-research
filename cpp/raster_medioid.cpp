4e6099084a20f0dec4c3f80406f40e7c65114e12
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-26 09:19:11 -0700

    update

--- cpp/raster_medioid.cpp
+++ cpp/raster_medioid.cpp
@@ -3,11 +3,33 @@
 #include"misc.h"
 int main(int argc, char ** argv){
 
-  int i;
-  for0(i, argc){
-    printf("arg %d [%s]\n", i, argv[i]);
+  if(argc < 4){
+    err("raster_medioid [raster file 1] .. [raster file N] [output file]");
   }
 
+  int n_files = argc - 2;
+  FILE * outfile = wopen(argv[argc-1]);
+  
+  FILE ** infiles = malloc(sizeof(FILE *) * n_files);
+  if(!infiles) err("malloc failed");
+  memset(infiles, 0, sizeof(FILE *) * n_files);
+
+  for(int i = 0; i < n_files; i++){
+    infiles[i] = ropen(argv[i + 1]);
+    printf("+r %s\n", argv[i + 1]);
+    if(infiles[i]==NULL) err("failed to open input file");
+  }
+
+
+
+
+
+
+  fclose(outfile);
+  for(int i = 0; i < n_files; i++) {
+    if(infiles[i]) fclose(infiles[i]);
+  }
+  free(infiles);
   return 0;
 }
 
