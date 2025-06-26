8032175afdc166d5689ecc2095a302f64232983d
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-26 09:55:47 -0700

    update

--- cpp/raster_medioid.cpp
+++ cpp/raster_medioid.cpp
@@ -13,10 +13,10 @@ void medioid(size_t j){
   int i, k;
   for0(i, n_files){
     f = infiles[i];
-    data[i] = vector<float>(n_bands);
-    for0(k, n_bands){
+    data[i] = vector<float>(nband);
+    for0(k, nband){
       fseek(f, np * k + j, SEEK_SET);
-      fread(&d[k], sizeof(float), 1, f);
+      fread(&data[i][k], sizeof(float), 1, f);
     }
     cout << data[i] << endl;
   }
@@ -41,7 +41,7 @@ int main(int argc, char ** argv){
     printf("+r %s\n", argv[i + 1]);
     if(infiles[i]==NULL) err("failed to open input file");
 
-    str hfn(hdr_fn(str(argv[i + 1]));  // input header file name
+    str hfn(hdr_fn(str(argv[i + 1])));  // input header file name
     if(i==0){
       hread(hfn, nrow, ncol, nband);
       np = nrow * ncol; // number of pixels
@@ -49,7 +49,7 @@ int main(int argc, char ** argv){
     else{
       hread(hfn, nrow2, ncol2, nband2);
       if(nrow != nrow2 || ncol != ncol2 || nband != nband2){
-        err(str("file: ") + fn + str(" has different dimenions than ") + lines[0]);
+        err(str("file: ") + str(argv[i + 1]) + str(" has different dimenions than ") + str(argv[1]));
       }
     }
   }
