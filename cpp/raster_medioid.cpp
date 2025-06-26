1a1571b8bc112c78655b957cdc6656316344ef62
ashlin richardson <ashlin.richardson@gmail.com>
2025-06-26 09:22:51 -0700

    update

--- cpp/raster_medioid.cpp
+++ cpp/raster_medioid.cpp
@@ -1,7 +1,11 @@
 /* 20250626 compute medioid for a list of rasters. Use random file access, to avoid loading the whole files in memory */
 
 #include"misc.h"
+
+size_t nrow, ncol, nband;
+
 int main(int argc, char ** argv){
+  size_t i, j, np, k, nrow2, ncol2, nband2;
 
   if(argc < 4){
     err("raster_medioid [raster file 1] .. [raster file N] [output file]");
@@ -14,10 +18,21 @@ int main(int argc, char ** argv){
   if(!infiles) err("malloc failed");
   memset(infiles, 0, sizeof(FILE *) * n_files);
 
-  for(int i = 0; i < n_files; i++){
+  for(i = 0; i < n_files; i++){
     infiles[i] = ropen(argv[i + 1]);
     printf("+r %s\n", argv[i + 1]);
     if(infiles[i]==NULL) err("failed to open input file");
+
+    str hfn(hdr_fn(str(argv[i + 1]));  // input header file name
+    if(i==0){
+      hread(hfn, nrow, ncol, nband);
+    }
+    else{
+      hread(hfn, nrow2, ncol2, nband2);
+      if(nrow != nrow2 || ncol != ncol2 || nband != nband2){
+        err(str("file: ") + fn + str(" has different dimenions than ") + lines[0]);
+      }
+    }
   }
 
 
@@ -26,7 +41,7 @@ int main(int argc, char ** argv){
 
 
   fclose(outfile);
-  for(int i = 0; i < n_files; i++) {
+  for(i = 0; i < n_files; i++) {
     if(infiles[i]) fclose(infiles[i]);
   }
   free(infiles);
