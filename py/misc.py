b0fb23183e0d8164e122acb6adb5f3201abd4455
ashlin richardson <ashlin.richardson@gmail.com>
2025-09-08 15:09:07 -0700

    update

--- py/misc.py
+++ py/misc.py
@@ -480,3 +480,55 @@ def assert_aws_cli_installed():
         print('Need to install aws cli: e.g.:')
         print('  sudo apt install awscli')
         sys.exit(1)
+
+'''
+# Files to download, back up, and extract
+files_to_process = [{'filename': 'prot_current_fire_polys.zip',  # current fires polygon database
+                     'url_base': 'https://pub.data.gov.bc.ca/datasets/cdfc2d7b-c046-4bf0-90ac-4897232619e1/',},
+                    {'filename': 'prot_current_fire_points.zip',  # current fires points database
+                     'url_base': 'https://pub.data.gov.bc.ca/datasets/2790e3f7-6395-4230-8545-04efb5a18800/',}]
+
+files_to_process = [{'filename': 'PROT_FUEL_TYPE_SP.gdb.zip',  # current fuel type layer 
+                     'url_base': 'https://pub.data.gov.bc.ca/datasets/e18ef98c-e1bf-43ac-95e4-b473452f32ec/'}]
+'''
+
+def extract_zip(filename: str, extract_to: str = '.'):
+    import zipfile
+
+    """Extract a zip file to the given directory."""
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+    print(f"Extracted: {filename}")
+
+def download_file(url: str, filename: str, extract_zip=True):
+    import urllib.request
+    import datetime
+    import certifi
+    import shutil
+    import ssl   
+    import os
+
+    ssl_context = ssl.create_default_context(cafile=certifi.where()) # SSL context using certifi
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")  # timestamp for backups 
+
+    """Download a file and save a timestamped backup."""
+    with urllib.request.urlopen(url, context=context) as response, open(filename, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+    print(f"+w {filename}")
+
+    # Save a timestamped backup ( duplicate the file with a timestamped filename ) 
+    base, ext = os.path.splitext(filename)
+    backup_filename = f"{base}_{timestamp}{ext}"
+    shutil.copyfile(filename, backup_filename)
+    print(f"+w {backup_filename}")
+
+    # auto-extract zip if applicable !!
+    if ext.strip('.') == 'zip' and extract_zip:
+        extract_zip(filename)
+'''
+
+# Process each file
+for file in files_to_process:
+    full_url = file['url_base'] + file['filename']
+    download_file(full_url, file['filename'])
+'''
