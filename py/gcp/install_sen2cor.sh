9454daf26bb36573822877f9a45cca5a86d9c62e
ashlin richardson <ashlin.richardson@gmail.com>
2025-09-12 21:33:20 -0700

    update

--- py/gcp/install_sen2cor.sh
+++ py/gcp/install_sen2cor.sh
@@ -1,5 +1,9 @@
-mkdir ~/sen2cor/
-cd ~/sen2cor/
+USER_HOME=$(getent passwd "${SUDO_USER:-$USER}" | cut -d: -f6)
+
+# make a subdirectory inside it
+mkdir -p "$USER_HOME/sen2cor"
+cd "$USER_HOME/sen2cor"
+
 wget http://step.esa.int/thirdparties/sen2cor/2.5.5/Sen2Cor-02.05.05-Linux64.run
 
 # wget https://step.esa.int/thirdparties/sen2cor/2.12.0/Sen2Cor-02.12.03-Linux64.run
