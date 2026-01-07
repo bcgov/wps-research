ef42020d0b05394cf98db5dfbec804dd2cb1fee4
ashlin richardson <ashlin.richardson@gmail.com>
2026-01-07 13:10:07 -0800

    update

--- data/ubc/view.py
+++ data/ubc/view.py
@@ -1,49 +1,26 @@
-''' 20231230 view.py e.g. Usage:
+''' 
+20260107  usage:
+     python3 view.py G80223_20230513.bin # your image filename goes here.. 
+
+20231230 view.py e.g. Usage:
       python3 view.py G80223_20230513.tif
       python3 view.py G90292_20230514.tif
 '''
 import sys
 import math
 import numpy as np
-from osgeo import gdal
+from osgeo import gdal  # library for reading .bin file
 import matplotlib.pyplot as plt
 
 
-def scale(X):
-    # default: scale a band to [0, 1]  and then clip
-    mymin = np.nanmin(X) # np.nanmin(X))
-    mymax = np.nanmax(X) # np.nanmax(X))
-    X = (X-mymin) / (mymax - mymin)  # perform the linear transformation
+def scale(X, P_percent = 1.):  # 
+    x = X.ravel().tolist(); x.sort()
+    N, p = float(len(x)), P_percent / 100.
+    x0 = x[int(math.floor(p * N))]
+    x1 = x[int(math.floor((1 - p) * N))]
+    return  (X- x0) / ( x1 - x0 ) 
 
-    X[X < 0.] = 0.  # clip
-    X[X > 1.] = 1.
-
-    # use histogram trimming / turn it off to see what this step does!
-    if  True:
-        values = X.ravel().tolist()
-        values.sort()
-        n_pct = 1. # percent for stretch value
-        frac = n_pct / 100.
-        lower = int(math.floor(float(len(values))*frac))
-        upper = int(math.floor(float(len(values))*(1. - frac)))
-        mymin, mymax = values[lower], values[upper]
-        X = (X-mymin) / (mymax - mymin)  # perform the linear transformation
-
-    return X
-
-
-def plot(dataset):
-    # image dimensions
-    width = int(dataset.RasterXSize)
-    height = int(dataset.RasterYSize)
-
-    rgb = np.zeros((height, width, 3))
-
-    # Read the data from the raster bands (assuming RGB bands are 1, 2, and 3)
-    rgb[:, :, 0] = scale(dataset.GetRasterBand(1).ReadAsArray().reshape((height, width)))
-    rgb[:, :, 1] = scale(dataset.GetRasterBand(2).ReadAsArray().reshape((height, width)))
-    rgb[:, :, 2] = scale(dataset.GetRasterBand(3).ReadAsArray().reshape((height, width)))
-    ''' A data cube indexed by row, column and band index (band index is in 1,2,3 rather: 0,1,2 from 0) 
+''' A data cube indexed by row, column and band index (band index is in 1,2,3 rather: 0,1,2 from 0) 
 
     0,1,2 are not actually red, green blue. They are B12, B11, B9 from Sentinel-2:
         B12: 2190 nm
@@ -52,11 +29,18 @@ def plot(dataset):
     which are in the short-wave infrared (SWIR)
 
     We chose the false-color encoding (R,G,B) = (B12, B11, B9) because fire looks orange/red/brown ish
-    ''' 
-   
-    # Close the dataset
-    dataset = None
+'''
+def plot(dataset):
+    # image dimensions
+    width = int(dataset.RasterXSize)  # ncol
+    height = int(dataset.RasterYSize) # nrow
+    rgb = np.zeros((height, width, 3))
 
+    # Read the data from the raster bands (assuming RGB bands are 1, 2, and 3)
+    rgb[:, :, 0] = scale(dataset.GetRasterBand(1).ReadAsArray().reshape((height, width)))
+    rgb[:, :, 1] = scale(dataset.GetRasterBand(2).ReadAsArray().reshape((height, width)))
+    rgb[:, :, 2] = scale(dataset.GetRasterBand(3).ReadAsArray().reshape((height, width)))
+   
     # Plot the RGB image using Matplotlib
     plt.figure()
     plt.imshow(rgb)
@@ -75,4 +59,4 @@ if __name__ == '__main__':
         print(f"Failed to open the TIF file: {tif_file_path}")
     else:  
         plot(dataset)
-
+    dataset = None # can probably delete this
