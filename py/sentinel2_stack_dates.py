3f8ac2504ae56369a4665b05b2e4297d75c1a1c2
ashlin richardson <ashlin.richardson@gmail.com>
2026-01-22 11:25:29 -0800

    update

--- py/sentinel2_stack_dates.py
+++ py/sentinel2_stack_dates.py
@@ -1,7 +1,7 @@
 '''20240723 stack sentinel2 .bin files in temporal order
 
 one tile only supported'''
-from misc import run, hdr_fn, band_names
+from misc import run, hdr_fn, band_names, read_hdr
 import os
 
 lines = [x.strip() for x in os.popen('ls -1 S*.bin').readlines()]
@@ -11,8 +11,16 @@ lines = [[line.split('_')[2], line] for line in lines]
 lines.sort()
 lines = [line[1] for line in lines]
 
+new_bn = []
 for line in lines:
     ds = line.split('_')[2].split('T')[0]
     print(ds, line)
 
-run(' '.join(['raster_stack.py'] + lines + ['stack.bin']))
+    samples, lines, bands = read_hdr(hdr_fn(line))
+    bn = band_names(hdr_fn(line))
+
+    bn_new += [ds + ': ' + i for i in bn]
+
+
+print(bn_new)
+# run(' '.join(['raster_stack.py'] + lines + ['stack.bin']))
