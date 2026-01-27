e6ede8bebc8693ad84cb27173ec09ca302d3d0f0
ashlin richardson <ashlin.richardson@gmail.com>
2026-01-27 12:13:58 -0800

    update

--- py/aws_download.py
+++ py/aws_download.py
@@ -18,7 +18,7 @@ def aws_download(BUCKET, KEY, LOCAL_PATH):
     # LOCAL_PATH = Path("L2_T10VFL") / Path(KEY).name
     CHUNK_SIZE = 2 * 1024 * 1024  # 8 MB
     MAX_RETRIES = 500
-    NUM_THREADS = 10  # <== You can change this value
+    NUM_THREADS = 24  # <== You can change this value
 
     # === SETUP ===
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
