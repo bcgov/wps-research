d97d03d6f335422937f67266fecddadde2e54ed8
ashlin richardson <ashlin.richardson@gmail.com>
2026-01-28 11:49:42 -0800

    update

--- py/sentinel2_mrap.py
+++ py/sentinel2_mrap.py
@@ -1,14 +1,18 @@
 '''20230727 extract most recent available pixel (MRAP) from sentinel2 series.. assume tile based
-(*) Difference from sentinel2_extract_swir.py: 
+(*) Difference from sentinel2_extract_swir.py:
     can't process in parallel (By tile, at least)
 NOTE: assumes sentinel2_extract_cloudfree.py has been run.
 
 20240618 NOTE: need to make this fully incremental. I.e., initialize buffer (per tile) with last avaiable MRAP date
 
 20250602: note: the program can't be run in parallel currently ( will try scoping the extract() function inside the run_mrap function)
-20250602: need to do the above before ____ ( DONE ) 
+20250602: need to do the above before ____ ( DONE )
 
 20250602: NB need to run sentinel2_extract_cloudfree_swir_nir.py BEFORE running this script ( if we've updated with sync_recent.py )
+
+20250128: Added handling for duplicate acquisition timestamps - when multiple files have the same
+          acquisition timestamp (3rd field), only the one with the latest processing timestamp
+          (7th field) is used. This prevents NaN reversion errors in MRAP products.
 '''
 
 import warnings
@@ -29,7 +33,7 @@ import os
 def run_mrap(gid):  # run MRAP on one tile
     print(gid, '-----------------------------------------------------------------------')
     my_bands, my_proj, my_geo, my_xsize, my_ysize, nbands, file_name  = {}, None, None, None, None, None, None
-    
+
     def extract(file_name):
         nonlocal my_proj, my_geo, my_bands, my_xsize, my_ysize, nbands
         print("+r", file_name)
@@ -50,7 +54,7 @@ def run_mrap(gid):  # run MRAP on one tile
         if my_xsize is None:
             my_xsize, my_ysize, nbands = d.RasterXSize, d.RasterYSize, d.RasterCount
         d = None  # close input file brought in for this update step
-    
+
         # write output file
         out_file_name, driver = file_name + '_MRAP.bin', gdal.GetDriverByName('ENVI')
         print("+w", out_file_name)
@@ -61,7 +65,7 @@ def run_mrap(gid):  # run MRAP on one tile
             stack_ds = driver.Create(out_file_name, my_xsize, my_ysize, nbands, gdal.GDT_Float32)
             stack_ds.SetProjection(my_proj)
             stack_ds.SetGeoTransform(my_geo)
-    
+
             for i in range(1, nbands + 1):
                 stack_ds.GetRasterBand(i).WriteArray(my_bands[i])
             stack_ds = None
@@ -81,9 +85,60 @@ def run_mrap(gid):  # run MRAP on one tile
         lines = [[x[2], x] for x in lines]
         lines.sort()
         return [[x[0], '_'.join(x[1])] for x in lines]  # [date string, full S2 data filename]
-    
+
+    def deduplicate_by_processing_time(lines):
+        '''
+        When multiple files have the same acquisition timestamp (3rd field, index 2),
+        keep only the one with the latest processing timestamp (7th field, index 6).
+
+        Input: list of [acquisition_timestamp, full_filename] pairs
+        Output: deduplicated list with same format
+        '''
+        if not lines:
+            return lines
+
+        # Group files by acquisition timestamp
+        by_acq_time = {}
+        for [acq_ts, filename] in lines:
+            if acq_ts not in by_acq_time:
+                by_acq_time[acq_ts] = []
+            by_acq_time[acq_ts].append(filename)
+
+        # For each acquisition timestamp, select the file with latest processing timestamp
+        result = []
+        for acq_ts in sorted(by_acq_time.keys()):
+            files = by_acq_time[acq_ts]
+            if len(files) == 1:
+                result.append([acq_ts, files[0]])
+            else:
+                # Multiple files with same acquisition timestamp
+                # Sort by processing timestamp (7th field, index 6) and pick the latest
+                def get_processing_ts(filename):
+                    parts = filename.split('_')
+                    if len(parts) >= 7:
+                        return parts[6]  # 7th field (0-indexed: 6)
+                    return ''
+
+                files_sorted = sorted(files, key=get_processing_ts)
+                selected = files_sorted[-1]  # Latest processing timestamp
+
+                print(f"  Duplicate acquisition timestamp {acq_ts}:")
+                for f in files:
+                    marker = " [SELECTED]" if f == selected else " [SKIPPED]"
+                    print(f"    {f}{marker}")
+
+                result.append([acq_ts, selected])
+
+        return result
+
     data_lines = get_filename_lines("ls -1r L2_" + gid + os.path.sep + "S2*_cloudfree.bin")
     mrap_lines = get_filename_lines("ls -1r L2_" + gid + os.path.sep + "S2*_cloudfree.bin_MRAP.bin")
+
+    # Deduplicate data_lines - keep only files with latest processing timestamp for each acquisition timestamp
+    original_count = len(data_lines)
+    data_lines = deduplicate_by_processing_time(data_lines)
+    if len(data_lines) < original_count:
+        print(f"Deduplicated data files: {original_count} -> {len(data_lines)}")
 
     last_good_mrap_date = None   # last MRAP date that's still good.. i.e. before the first data file that doesn't have an MRAP file
     last_good_mrap_file = None
@@ -108,7 +163,7 @@ def run_mrap(gid):  # run MRAP on one tile
         gid = line.split("_")[5]
         extract_path = "L2_" +  gid + os.path.sep + line
         print('**' + mrap_date + " " + extract_path)
-    
+
     # find the last mrap date ( if applicable ) that's still good ( before first data file without MRAP file )
     for [line_date, line] in data_lines:
         if last_good_mrap_date is None or line_date in mrap_dates_set:
@@ -117,7 +172,7 @@ def run_mrap(gid):  # run MRAP on one tile
                     last_good_mrap_date = line_date
                 except:
                     pass # might not have any mrap files generated.
-                
+
         else:
             # print("line_date", line_date)
             # print("mrap_dates_set", mrap_dates_set)
@@ -140,13 +195,13 @@ def run_mrap(gid):  # run MRAP on one tile
         my_proj, my_geo = d.GetProjection(), d.GetGeoTransform()
         my_xsize, my_ysize, nbands = d.RasterXSize, d.RasterYSize, d.RasterCount
         # print(my_proj, my_geo, my_xsize, my_ysize, nbands)
-    
+
     print("run extract:")  # run extract() on data files later than the last MRAP date
     for [line_date, line] in data_lines:
         gid = line.split("_")[5]
         extract_path = "L2_" +  gid + os.path.sep + line
         if last_good_mrap_date is None or ( last_good_mrap_date is not None and line_date > last_good_mrap_date):
-            print('extract(' + extract_path + ')') 
+            print('extract(' + extract_path + ')')
             extract(extract_path)
 
     # THIS PART SHOULD MARK ( NEEDING REFRESHING ) MRAP COMPOSITES ( MERGED PRODUCTS ) THAT NEED REFRESHING / BY DELETING THEM !!!!
@@ -170,7 +225,7 @@ if __name__ == "__main__":
 
             gid = w[1]
             gids += [gid]
-            # should run on this frame here. 
+            # should run on this frame here.
             # but check for cloud-free data first
         # err("python3 sentinel2_mrap.py [sentinel-2 gid] # [optional: yyyymmdd 'maxdate' parameter] ")
         def f(fn):
@@ -178,3 +233,4 @@ if __name__ == "__main__":
         parfor(f, gids, 1) #  int(mp.cpu_count()))
     else:
         run_mrap(args[1])  # single tile mode: no mosaicing
+
