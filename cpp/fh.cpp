9145c09689d63035e290fc5f0dc9a431afa58e62
ashlin richardson <ashlin.richardson@gmail.com>
2026-01-28 18:52:44 -0800

    update

--- cpp/fh.cpp
+++ cpp/fh.cpp
@@ -1,5 +1,23 @@
-#include<stdlib.h>
-int main(int argc, char**argv){
-  system("fh.py");
-  return 0;
+/* 20260128 run python command to fix ENVI headers */
+#include <unistd.h>
+#include <vector>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    std::vector<char*> args;
+
+    // Program to execute
+    args.push_back(const_cast<char*>("fh.py"));
+
+    // Forward all arguments (if any)
+    for (int i = 1; i < argc; ++i) {
+        args.push_back(argv[i]);
+    }
+
+    args.push_back(nullptr); // execvp requires null-terminated argv
+
+    execvp("fh.py", args.data());
+
+    // Only reached if execvp fails
+    return 1;
 }
