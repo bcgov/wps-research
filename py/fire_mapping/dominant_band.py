18391c71dc9dc9f771cc33e6d3100dbfe5ae23b6
ashlin richardson <ashlin.richardson@gmail.com>
2026-02-18 10:29:24 -0800

    update

---
+++ py/fire_mapping/dominant_band.py
@@ -0,0 +1,146 @@
+'''
+Use this file to quickly see the idea of 'which colour wins'
+
+Syntax:
+  >> python3 dominant_band.py file.bin
+'''
+
+from raster import Raster
+
+import sys
+
+
+def dominant_band(
+        X, 
+        band_index: int
+    ):
+    '''
+    Description
+    -----------
+    This function imposes a threshold, which compares the selected channel with others.
+
+    
+    Parameters
+    ----------
+    X: a 3d matrix.
+
+    band_index: the channel (band) index which you want to compare against the others.
+
+    
+    Returns
+    -------
+    mask: 2D array of the dominant pixels only.
+    '''
+    import numpy as np
+
+    if ( band_index < 1 or band_index > X.shape[2] ):
+
+        raise IndexError("band_index out of range")
+    
+    idx = band_index - 1
+
+    target = X[:, :, idx]
+
+    others = np.delete(X, idx, axis=2)
+
+    mask = np.all(target[..., None] > others, axis=2)
+
+    return mask
+
+
+
+def plot_dominant_band(
+        X,
+        *,
+        title = 'RAW',
+        figsize = (15, 5)
+    ):
+
+    '''
+    Applies a logic masking, returns a boolean image where:
+
+    >> White (1): means the chosen channel has the high value at that pixel (dominates)
+
+    >> Black (0): otherwise
+
+    
+    Parameters
+    ---------
+    X: the dataset to be used
+
+    title: plot title
+
+    figsize: matplotlib size
+
+    
+    Returns
+    -------
+    A plot of 4 subfigures
+
+    The first one is the input data.
+
+    The other 3 are filtered by rgb channels.
+    '''
+
+    import matplotlib.pyplot as plt
+
+    r = dominant_band(X, 1)
+    g = dominant_band(X, 2)
+    b = dominant_band(X, 3)
+
+    fig, axes = plt.subplots(1, 4, figsize = figsize)
+
+    axes[0].imshow(X)
+    axes[0].axis("off")
+    axes[0].set_title(title)
+
+    axes[1].imshow(r, cmap='gray')
+    axes[1].axis("off")
+    axes[1].set_title("red wins")
+
+    axes[2].imshow(g, cmap='gray')
+    axes[2].axis("off")
+    axes[2].set_title("green wins")
+
+    axes[3].imshow(b, cmap='gray')
+    axes[3].axis("off")
+    axes[3].set_title("blue wins")
+
+    plt.tight_layout()
+
+    plt.show()
+    
+
+
+
+if __name__ == '__main__':
+
+    '''
+    Use Raster() to read raster and pass the rgb to plot
+    '''
+
+    #handling argv
+    if len(sys.argv) < 2:
+        print("Needs a file name")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    #Title
+    title = 'RAW'
+
+    if len(sys.argv) > 2: title = sys.argv[2]
+
+
+    #load raster and read
+    raster = Raster(file_name=filename)
+
+    X = raster.readBands_and_trim(
+        band_lst=[1,2,3]
+    )
+
+    plot_dominant_band(
+        X,
+        title=title
+    )
+
