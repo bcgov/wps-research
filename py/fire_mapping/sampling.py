90ea41d8184c97c1d757975783162f6b317df29c
ashlin richardson <ashlin.richardson@gmail.com>
2026-02-18 10:29:00 -0800

    update

---
+++ py/fire_mapping/sampling.py
@@ -0,0 +1,247 @@
+'''
+Sampling data.
+'''
+
+import numpy as np
+
+
+def indices_sampling(
+        start: int,
+        end: int,
+        size: int,
+        *,
+        nan_mask = None,
+        replacement = False,
+        seed = 42
+):
+    '''
+    Description
+    -----------
+    Sample n=size values between start and end - 1.
+
+    If there is nan in the data, using nan mask to see where the nans are
+
+    Sample until we get enough data size.
+    '''
+
+    rng = np.random.default_rng(seed)
+
+    idx = np.arange(start, end)
+
+    if nan_mask is not None:
+
+        idx = idx[np.asarray(nan_mask, bool)]
+
+    if not replacement and size > len(idx):
+
+        raise ValueError("(Sampling without replacement): Not enough valid indices to sample from.")
+
+    return rng.choice(idx, size=size, replace=replacement)
+
+
+
+def row_sampling(
+        X,
+        size,
+        *,
+        original_indices = None,
+        replacement = False,
+        filter_nan = False,
+        seed = 42
+):
+    '''
+    Description
+    -----------
+    Row sampling from a matrix. A 3D matrix is given, it will reshape to 2D (so each band is now 1 feature).
+
+    Sample from the rows. Designed specifically for pixel sampling.
+
+    
+    Parameters
+    ----------
+    X: a 2D or 3D matrix.
+    
+    size: how many rows to sample.
+
+    original_indices: sample from the given indices.
+
+    replacement: sampling with replacement or not.
+    
+    filter nan: To sample without nan values, set to True.
+
+    
+    Returns
+    -------
+    sampled_indices: original index of each sample
+
+    X_sample: sampled data
+    '''
+
+    from misc import ignore_nan_2D
+
+
+    X = X.reshape(-1, X.shape[-1])
+
+    #Contains rows with nan (if there is nan in the data)
+    nrow, _ = X.shape
+    
+    #nan mask is the list of original indices that contain NAN data.
+    nan_mask = None
+
+    if ( filter_nan ):
+        #Remove all nan related in rows before sampling
+
+        nan_mask, _ = ignore_nan_2D(
+            X = X, 
+
+            #axis = 1 is row filter, axis = 0 is column filter.
+            axis = 1
+        )
+
+
+    #If filter_nan is applied, we want to sample non-nan rows to get up to the size we need.
+    sampled_indices = indices_sampling(
+        start = 0,
+        end = nrow,
+        size  = size,
+        nan_mask = nan_mask,
+        replacement = replacement,
+        seed  = seed
+    )
+
+    sampled_original_indices = sampled_indices if ( original_indices is None ) else original_indices[sampled_indices]
+
+    X_sample = X[sampled_indices]
+
+    return sampled_original_indices, X_sample
+
+
+
+def regular_sampling(
+        raster_dat = None,
+        *,
+        raster_filename: str = None,
+        sample_size: int,
+        replacement: bool = False,
+        seed: int = 42
+):
+    '''
+    Description
+    -----------
+    Sampling data.
+
+    Notes
+    -----
+    NO RATIO of any kind is considered.
+    '''
+    from raster import Raster
+
+    if raster_dat is None and raster_filename is None:
+
+        raise ValueError('You need at least raster data or its filename to process.')
+    
+    #Assume raster data is always passed in, but raster file_name is always of higher priority.
+    if raster_filename is not None:
+        
+        raster_dat = Raster(raster_filename).read_bands('all')
+
+
+    #All data have been ready. Now sample.
+
+    org_idx, samples  = row_sampling(
+                            X=raster_dat,  
+                            size=sample_size,
+                            filter_nan=True,
+                            replacement=replacement,
+                            seed=seed
+                        )
+    
+
+    return org_idx, samples
+    
+
+
+def in_out_sampling(
+        raster_dat=None,
+        polygon_dat=None,
+        *,
+        raster_filename=None,
+        polygon_filename=None,
+        in_sample_size,
+        seed = 42
+):
+    '''
+    Description
+    -----------
+    Sampling data inside and outside of the polygon.
+
+
+    Returns
+    -------
+    original_indices: sampled indices from the original data.
+    
+    samples: The samples indexed by 'original indices' from the original data.
+    
+    out_in_ratio: ratio between original data from outside and inside of the polygon.
+
+    
+    Notes
+    -----
+    The sampling will guarantee the ratio of in-out.
+    '''
+
+    from polygon import split_in_out
+
+
+    #Extract inside and outside matrix (split)
+    inside_dat, in_indices, outside_dat, out_indices = split_in_out(
+        raster_dat=raster_dat,
+        polygon_dat=polygon_dat,
+        raster_filename = raster_filename,
+        polygon_filename= polygon_filename
+    )
+
+    #Ratio to maintain the true population proportion
+    out_in_ratio = len(inside_dat) / len(outside_dat)
+
+
+    #Sample size (reserves true proportion)
+    out_sample_size = int(in_sample_size * out_in_ratio)
+
+
+    #Sample now
+    org_in_idx, inside_samples  = row_sampling(X=inside_dat,  
+                                                 size=in_sample_size, 
+                                                 original_indices=in_indices,
+                                                 filter_nan=True,
+                                                 seed=seed)
+    
+    org_out_idx, outside_samples = row_sampling(X=outside_dat, 
+                                                 size=out_sample_size, 
+                                                 original_indices=out_indices,
+                                                 filter_nan=True,
+                                                 seed=seed)
+    
+
+    samples = np.vstack([
+        inside_samples,
+        outside_samples
+    ])
+
+
+    original_indices = np.concatenate([org_in_idx, 
+                                       org_out_idx])
+    
+
+    return original_indices, samples, out_in_ratio
+
+
+
+
+
+
+
+
+
+    
+
