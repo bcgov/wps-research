0e26f971bcffa6625e31e678fd3678067629d2f3
ashlin richardson <ashlin.richardson@gmail.com>
2026-03-02 09:23:07 -0800

    update

---
+++ py/force_restart_rdp.py
@@ -0,0 +1,16 @@
+'''20260302 force restart remote desktop
+'''
+import os
+
+def run(c):
+    print(c)
+    return os.system(c)
+
+cmds = ['sudo systemctl stop xrdp',
+        'sudo systemctl stop xrdp-sesman',
+        'sudo killall Xorg',
+        'sudo systemctl start xrdp',
+        'sudo systemctl start xrdp-sesman']
+
+for c in cmds:
+    run(c)
